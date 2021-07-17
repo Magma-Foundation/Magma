0da4efec758503bcc181427b6fcfdf7779b9f03e
SirMathhman <sirmathhman@gmail.com>
2021-07-16 23:02:08 -0400

    Add test for whitespace between names

--- src/test/java/com/meti/ApplicationTest.java
+++ src/test/java/com/meti/ApplicationTest.java
@@ -14,7 +14,8 @@ public class ApplicationTest {
         if (content.startsWith("def ")) {
             var from = "def ".length();
             var separator = content.indexOf('(');
-            var name = content.substring(from, separator);
+            var name = content.substring(from, separator).trim();
+            content.substring(content.indexOf(':') + 1, content.indexOf("=>"));
             return "int " + name + "(){return 0;}";
         } else {
             throw new ApplicationException();
@@ -24,5 +25,15 @@ public class ApplicationTest {
     @Test
     void test_name() throws ApplicationException {
         assertEquals("int test(){return 0;}", run("def test() : I16 => {return 0;}"));
+    }
+
+    @Test
+    void test_name_whitespace() throws ApplicationException {
+        assertEquals("int test(){return 0;}", run("def  test () : I16 => {return 0;}"));
+    }
+
+    @Test
+    void test_type() throws ApplicationException {
+        assertEquals("unsigned int main(){return 0;}", run("def main() : U16 => {return 0;}"));
     }
 }
