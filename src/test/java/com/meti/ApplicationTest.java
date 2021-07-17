3f2d10c8725bf9fe52ea985607e852974fd3bb81
SirMathhman <sirmathhman@gmail.com>
2021-07-16 22:59:17 -0400

    Add test for function name

--- src/test/java/com/meti/ApplicationTest.java
+++ src/test/java/com/meti/ApplicationTest.java
@@ -6,7 +6,21 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class ApplicationTest {
     @Test
-    void test() {
-        assertEquals("int main(){return 0;}", "int main(){return 0;}");
+    void test_main() throws ApplicationException {
+        assertEquals("int main(){return 0;}", run("def main() : I16 => {return 0;}"));
+    }
+
+    private String run(String content) throws ApplicationException {
+        if(content.startsWith("def ")) {
+            var separator = content.indexOf('(');
+            return "int " + content.substring("def ".length(), separator) + "(){return 0;}";
+        } else {
+            throw new ApplicationException();
+        }
+    }
+
+    @Test
+    void test_name() throws ApplicationException {
+        assertEquals("int test(){return 0;}", run("def test() : I16 => {return 0;}"));
     }
 }
