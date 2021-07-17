b3173ae511fb54d110454160b9b1b09f0d625c0e
SirMathhman <sirmathhman@gmail.com>
2021-07-16 23:08:23 -0400

    Extract subfunctions

--- src/test/java/com/meti/ApplicationTest.java
+++ src/test/java/com/meti/ApplicationTest.java
@@ -17,24 +17,33 @@ public class ApplicationTest {
 
     private String run(String content) throws ApplicationException {
         if (content.startsWith("def ")) {
-            var from = "def ".length();
-            var separator = content.indexOf('(');
-            var name = slice(content, from, separator);
-            var typeSeparator = content.indexOf(':');
-            var returnSeparator = content.indexOf("=>");
-            var typeString = slice(content, typeSeparator + 1, returnSeparator);
-            String typeToUse;
-            if (typeString.equals("I16")) {
-                typeToUse = "int";
-            } else if (typeString.equals("U16")) {
-                typeToUse = "unsigned int";
-            } else {
-                throw new ApplicationException("Unknown type: " + typeString);
-            }
-            return typeToUse + " " + name + "(){return 0;}";
+            return compileFunction(content);
         } else {
             throw new ApplicationException("Unknown content: " + content);
         }
+    }
+
+    private String compileFunction(String content) throws ApplicationException {
+        var from = "def ".length();
+        var separator = content.indexOf('(');
+        var name = slice(content, from, separator);
+        var typeSeparator = content.indexOf(':');
+        var returnSeparator = content.indexOf("=>");
+        var typeString = slice(content, typeSeparator + 1, returnSeparator);
+        var typeToUse = compileType(typeString);
+        return typeToUse + " " + name + "(){return 0;}";
+    }
+
+    private String compileType(String typeString) throws ApplicationException {
+        String typeToUse;
+        if (typeString.equals("I16")) {
+            typeToUse = "int";
+        } else if (typeString.equals("U16")) {
+            typeToUse = "unsigned int";
+        } else {
+            throw new ApplicationException("Unknown type: " + typeString);
+        }
+        return typeToUse;
     }
 
     private String slice(String content, int start, int end) {
