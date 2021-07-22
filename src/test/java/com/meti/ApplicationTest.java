b968f2554f0e59b691ffc8b4ca67922501b94826
SirMathhman <sirmathhman@gmail.com>
2021-07-22 19:03:28 -0400

    Add content to ApplicationTest.mgs

--- src/test/java/com/meti/ApplicationTest.java
+++ src/test/java/com/meti/ApplicationTest.java
@@ -1,4 +1,18 @@
 package com.meti;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
 public class ApplicationTest {
+    @Test
+    void test() throws IOException {
+        var path = Paths.get(".", "src/test/java-gen/com/meti/ApplicationTest.java");
+        if (!Files.exists(path)) Files.createDirectories(path.getParent());
+        if (!Files.exists(path)) Files.createFile(path);
+        Assertions.assertTrue(Files.exists(path));
+    }
 }
