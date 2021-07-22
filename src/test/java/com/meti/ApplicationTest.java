7740cd41854645c789166b0988461ad90c00d466
SirMathhman <sirmathhman@gmail.com>
2021-07-22 17:51:58 -0400

    Add ApplicationTest

---
+++ src/test/java/com/meti/ApplicationTest.java
@@ -0,0 +1,12 @@
+package com.meti;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class ApplicationTest {
+    @Test
+    void test() {
+        assertEquals("Successfully compiled 0 files in 0 ms.", "Successfully compiled 0 files in 0 ms.");
+    }
+}
