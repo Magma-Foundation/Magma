71dc930bf908c0e2f457d59649410f8aaaaa629c
SirMathhman <sirmathhman@gmail.com>
2021-08-08 17:16:55 -0400

    Fix tests by removing excess semicolon

--- src/main/java/com/meti/MagmaJavaCompiler.java
+++ src/main/java/com/meti/MagmaJavaCompiler.java
@@ -14,31 +14,26 @@ public class MagmaJavaCompiler {
     }
 
     String compile() {
-        String output;
         if (input.startsWith("import native ")) {
-            if (input.equals("import native {} from bar;")) {
-                output = "class __%s__{}".formatted(scriptName);
+            var separator = input.indexOf("from");
+            var basesName = input.substring("import native ".length(), separator).trim();
+            var bracketStart = basesName.indexOf('{');
+            var bracketEnd = basesName.indexOf('}');
+            var packageName = input.substring(separator + IMPORT_SEPARATOR + 1, input.length() - 1);
+            if (bracketStart == -1 || bracketEnd == -1) {
+                return "import %s.%s;class __%s__{}".formatted(packageName, basesName, scriptName);
             } else {
-                var separator = input.indexOf("from");
-                var basesName = input.substring("import native ".length(), separator).trim();
-                var bracketStart = basesName.indexOf('{');
-                var bracketEnd = basesName.indexOf('}');
-                var packageName = input.substring(separator + IMPORT_SEPARATOR + 1, input.length() - 1);
-                if (bracketStart == -1 || bracketEnd == -1) {
-                    output = "import %s.%s;class __%s__{}".formatted(packageName, basesName, scriptName);
-                } else {
-                    var childrenString = basesName.substring(bracketStart + 1, bracketEnd).trim();
-                    var children = childrenString.split(",");
-                    var newChildren = Arrays.stream(children)
-                            .map(String::trim)
-                            .map(child -> "import " + packageName + "." + child)
-                            .collect(Collectors.joining(";"));
-                    output = "%s;class __%s__{}".formatted(newChildren, scriptName);
-                }
+                var childrenString = basesName.substring(bracketStart + 1, bracketEnd).trim();
+                var children = childrenString.split(",");
+                var newChildren = Arrays.stream(children)
+                        .map(String::trim)
+                        .filter(value -> !value.isEmpty())
+                        .map(child -> "import " + packageName + "." + child + ";")
+                        .collect(Collectors.joining());
+                return "%sclass __%s__{}".formatted(newChildren, scriptName);
             }
         } else {
-            output = "class __%s__{}".formatted(scriptName);
+            return "class __%s__{}".formatted(scriptName);
         }
-        return output;
     }
 }
