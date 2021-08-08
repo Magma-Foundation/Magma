b0935d004440f366e347e9d15323716153435d7b
SirMathhman <sirmathhman@gmail.com>
2021-08-08 15:48:23 -0400

    Add non-existing source file test

--- src/test/java/com/meti/ApplicationTest.java
+++ src/test/java/com/meti/ApplicationTest.java
@@ -1,7 +1,6 @@
 package com.meti;
 
 import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
@@ -9,17 +8,35 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 public class ApplicationTest {
     public static final Path Target = Paths.get(".", "index.java");
+    public static final Path Source = Paths.get(".", "index.ms");
 
     @AfterEach
     void tearDown() throws IOException {
         Files.deleteIfExists(Target);
+        Files.deleteIfExists(Source);
     }
 
     @Test
     void test() throws IOException {
-        Files.writeString(Target, "class index{}");
-        Assertions.assertEquals("class index{}", Files.readString(Target));
+        Files.createFile(Source);
+        run();
+        assertTrue(Files.exists(Target));
+    }
+
+    private void run() throws IOException {
+        if (Files.exists(Source)) {
+            Files.createFile(Target);
+        }
+    }
+
+    @Test
+    void test1() throws IOException {
+        run();
+        assertFalse(Files.exists(Target));
     }
 }
