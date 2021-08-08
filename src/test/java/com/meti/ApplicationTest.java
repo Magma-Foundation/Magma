521cffd37e37d6027a897d0d5b8b47c7fef6ed0f
SirMathhman <sirmathhman@gmail.com>
2021-08-08 16:29:02 -0400

    Add test for file content

--- src/test/java/com/meti/ApplicationTest.java
+++ src/test/java/com/meti/ApplicationTest.java
@@ -8,12 +8,25 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class ApplicationTest {
     public static final Path Target = Paths.get(".", "index.java");
     public static final Path Source = Paths.get(".", "index.ms");
+
+    @Test
+    void content() throws IOException {
+        Files.createFile(Source);
+        run();
+
+        assertEquals("class __index__{}", Files.readString(Target));
+    }
+
+    private void run() throws IOException {
+        if (Files.exists(Source)) {
+            Files.writeString(Target, "class __index__{}");
+        }
+    }
 
     @AfterEach
     void tearDown() throws IOException {
@@ -28,20 +41,9 @@ public class ApplicationTest {
         assertTrue(Files.exists(Target));
     }
 
-    private void run() throws IOException {
-        if (Files.exists(Source)) {
-            Files.createFile(Target);
-        }
-    }
-
     @Test
     void without_source() throws IOException {
         run();
         assertFalse(Files.exists(Target));
-    }
-
-    @Test
-    void test(){
-
     }
 }
