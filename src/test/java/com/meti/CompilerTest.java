8a68ab883cb78c8a5955de0bbe3b13fad545ad43
SirMathhman <sirmathhman@gmail.com>
2021-09-17 18:29:12 -0400

    Add test for value changing

--- src/test/java/com/meti/CompilerTest.java
+++ src/test/java/com/meti/CompilerTest.java
@@ -10,4 +10,14 @@ class CompilerTest {
         var output = new Compiler("index", "").compile();
         assertEquals("struct ___index___{};struct ___index___ __index__(){struct ___index___ this={};return this;}", output);
     }
+
+    @Test
+    void value(){
+        var output = new Compiler("index", "const x : I16 = 20;").compile();
+        assertEquals("struct ___index___{};" +
+                     "struct ___index___ __index__(){" +
+                     "struct ___index___ this={};" +
+                     "int x=20;" +
+                     "return this;}", output);
+    }
 }
