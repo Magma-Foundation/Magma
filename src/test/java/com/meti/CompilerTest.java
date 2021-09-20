2d33956b5771644c9df374c904fc2614836f53eb
SirMathhman <SirMathhman@gmail.com>
2021-09-20 19:03:41 -0400

    Remove duplicates

--- src/test/java/com/meti/CompilerTest.java
+++ src/test/java/com/meti/CompilerTest.java
@@ -8,25 +8,26 @@ class CompilerTest {
 
     @Test
     void test_function() throws ApplicationException {
-        var compiler = new Compiler();
-        var output = compiler.compile("index", "def empty() : Void => {}");
-        var source = output.getSourceContent();
-        assertEquals("void empty(void* __self__){" +
+        assertCompiles("def empty() : Void => {}", "void empty(void* __self__){" +
                 "struct _index_* this=(struct _index_*) self;}" +
                 "struct _index_ __index__(){" +
                 "struct _index_ this={};" +
-                "return this;}", source);
+                "return this;}");
+    }
+
+    private void assertCompiles(String input, String output) throws ApplicationException {
+        var compiler = new Compiler();
+        var compiled = compiler.compile("index", input);
+        var source = compiled.getSourceContent();
+        assertEquals(output, source);
     }
 
     @Test
     void test_another_function() throws ApplicationException {
-        var compiler = new Compiler();
-        var output = compiler.compile("index", "def test() : Void => {}");
-        var source = output.getSourceContent();
-        assertEquals("void test(void* __self__){" +
+        assertCompiles("def test() : Void => {}", "void test(void* __self__){" +
                 "struct _index_* this=(struct _index_*) self;}" +
                 "struct _index_ __index__(){" +
                 "struct _index_ this={};" +
-                "return this;}", source);
+                "return this;}");
     }
 }
