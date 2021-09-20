8f76dd8693626d6403c5644770884054c3e6d8ec
SirMathhman <SirMathhman@gmail.com>
2021-09-20 14:16:30 -0400

    Extract main object and remove duplicates

--- src/test/java/com/meti/Application.java
+++ src/test/java/com/meti/Application.java
@@ -2,12 +2,32 @@ package com.meti;
 
 import java.io.IOException;
 import java.nio.file.Files;
+import java.nio.file.Path;
 
 public class Application {
+    private final Path source;
+
+    public Application(Path source) {
+        this.source = source;
+    }
+
     void run() throws IOException {
-        if (Files.exists(ApplicationTest.Source)) {
-            Files.createFile(ApplicationTest.TargetHeader);
-            Files.createFile(ApplicationTest.TargetSource);
+        if (Files.exists(source)) {
+            var nativeName = source.getName(source.getNameCount() - 1)
+                    .toString();
+            var separator = nativeName.indexOf('.');
+            String name;
+            if (separator == -1) {
+                name = nativeName;
+            } else {
+                name = nativeName.substring(0, separator);
+            }
+
+            var targetHeader = source.resolveSibling(name + ".h");
+            var targetSource = source.resolveSibling(name + ".c");
+
+            Files.createFile(targetHeader);
+            Files.createFile(targetSource);
         }
     }
 }
--- src/test/java/com/meti/ApplicationTest.java
+++ src/test/java/com/meti/ApplicationTest.java
@@ -19,26 +19,26 @@ public class ApplicationTest {
     @Test
     void target_header_present() throws IOException {
         Files.createFile(Source);
-        new Application().run();
+        new Application(Source).run();
         assertTrue(Files.exists(TargetHeader));
     }
 
     @Test
     void target_source_present() throws IOException {
         Files.createFile(Source);
-        new Application().run();
+        new Application(Source).run();
         assertTrue(Files.exists(TargetSource));
     }
 
     @Test
     void target_header_missing() throws IOException {
-        new Application().run();
+        new Application(Source).run();
         assertFalse(Files.exists(TargetHeader));
     }
 
     @Test
     void target_source_missing() throws IOException {
-        new Application().run();
+        new Application(Source).run();
         assertFalse(Files.exists(TargetSource));
     }
 
