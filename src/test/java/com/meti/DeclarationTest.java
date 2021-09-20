93c5a59879ef246a0c8a5451db0ee665a7b70912
SirMathhman <SirMathhman@gmail.com>
2021-09-20 19:23:14 -0400

    Fix test for declarations

--- src/test/java/com/meti/DeclarationTest.java
+++ src/test/java/com/meti/DeclarationTest.java
@@ -5,6 +5,10 @@ import org.junit.jupiter.api.Test;
 public class DeclarationTest extends FeatureTest {
     @Test
     void test() {
-        assertCompiles("const x : I16 = 0", "");
+        assertCompiles("const x : I16 = 0",
+                "struct _index_ __index__(){" +
+                        "struct _index_ this={};" +
+                        "int x=0;" +
+                        "return this;}");
     }
 }
