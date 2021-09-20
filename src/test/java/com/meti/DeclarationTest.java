47ad00acfb88a5fabe417a1be687ff359440131e
SirMathhman <SirMathhman@gmail.com>
2021-09-20 19:23:31 -0400

    Fix indentation

--- src/test/java/com/meti/DeclarationTest.java
+++ src/test/java/com/meti/DeclarationTest.java
@@ -5,10 +5,9 @@ import org.junit.jupiter.api.Test;
 public class DeclarationTest extends FeatureTest {
     @Test
     void test() {
-        assertCompiles("const x : I16 = 0",
-                "struct _index_ __index__(){" +
-                        "struct _index_ this={};" +
-                        "int x=0;" +
-                        "return this;}");
+        assertCompiles("const x : I16 = 0", "struct _index_ __index__(){" +
+                "struct _index_ this={};" +
+                "int x=0;" +
+                "return this;}");
     }
 }
