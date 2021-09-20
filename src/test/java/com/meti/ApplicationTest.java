b00b0c4504e878e7ee4fa073c13bc5952628bded
SirMathhman <SirMathhman@gmail.com>
2021-09-20 14:07:19 -0400

    Add basic tests for repeat sources

--- src/test/java/com/meti/ApplicationTest.java
+++ src/test/java/com/meti/ApplicationTest.java
@@ -9,18 +9,50 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class ApplicationTest {
-
-    public static final Path Target = Paths.get(".", "index.c");
+    public static final Path Source = Paths.get(".", "index.mgs");
+    public static final Path TargetHeader = Paths.get(".", "index.h");
+    public static final Path TargetSource = Paths.get(".", "index.c");
 
     @Test
-    void no_target() {
-        assertFalse(Files.exists(Target));
+    void target_header_present() throws IOException {
+        Files.createFile(Source);
+        run();
+        assertTrue(Files.exists(TargetHeader));
+    }
+
+    @Test
+    void target_source_present() throws IOException {
+        Files.createFile(Source);
+        run();
+        assertTrue(Files.exists(TargetSource));
+    }
+
+    private void run() throws IOException {
+        if (Files.exists(Source)) {
+            Files.createFile(TargetHeader);
+            Files.createFile(TargetSource);
+        }
+    }
+
+    @Test
+    void target_header_missing() throws IOException {
+        run();
+        assertFalse(Files.exists(TargetHeader));
+    }
+
+    @Test
+    void target_source_missing() throws IOException {
+        run();
+        assertFalse(Files.exists(TargetSource));
     }
 
     @AfterEach
     void tearDown() throws IOException {
-        Files.deleteIfExists(Target);
+        Files.deleteIfExists(Source);
+        Files.deleteIfExists(TargetHeader);
+        Files.deleteIfExists(TargetSource);
     }
 }
