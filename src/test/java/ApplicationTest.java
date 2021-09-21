7b566c8b32135fa277c84311510159ea942697bf
SirMathhman <SirMathhman@gmail.com>
2021-09-21 15:46:20 -0400

    Add test for targets

--- src/test/java/ApplicationTest.java
+++ src/test/java/ApplicationTest.java
@@ -7,17 +7,34 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class ApplicationTest {
     public static final Path Target = Paths.get(".", "index.c");
+    public static final Path Source = Paths.get(".", "index.mgs");
 
     @Test
-    void test() {
+    void target() throws IOException {
+        Files.createFile(Source);
+        run();
+        assertTrue(Files.exists(Target));
+    }
+
+    private void run() throws IOException {
+        if (Files.exists(Source)) {
+            Files.createFile(Target);
+        }
+    }
+
+    @Test
+    void no_target() throws IOException {
+        run();
         assertFalse(Files.exists(Target));
     }
 
     @AfterEach
     void tearDown() throws IOException {
         Files.deleteIfExists(Target);
+        Files.deleteIfExists(Source);
     }
 }
