d7e3692d73c9bf703eb69b0deac4b614bd807d59
SirMathhman <SirMathhman@gmail.com>
2021-09-21 15:41:31 -0400

    Add ApplicationTest

---
+++ src/test/java/ApplicationTest.java
@@ -0,0 +1,23 @@
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
+public class ApplicationTest {
+    public static final Path Target = Paths.get(".", "index.c");
+
+    @Test
+    void test() {
+        assertFalse(Files.exists(Target));
+    }
+
+    @AfterEach
+    void tearDown() throws IOException {
+        Files.deleteIfExists(Target);
+    }
+}
