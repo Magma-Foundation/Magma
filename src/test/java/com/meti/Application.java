bc4279994ee4c2a3f72bdd58929a630d8f8d1a52
SirMathhman <sirmathhman@gmail.com>
2021-09-22 18:57:10 -0400

    Extract Application

---
+++ src/test/java/com/meti/Application.java
@@ -0,0 +1,22 @@
+package com.meti;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+public class Application {
+    private final Path source;
+
+    public Application(Path source) {
+        this.source = source;
+    }
+
+    boolean run() throws IOException {
+        if (Files.exists(source)) {
+            Files.createFile(source.resolveSibling("index.c"));
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
--- src/test/java/com/meti/ApplicationTest.java
+++ src/test/java/com/meti/ApplicationTest.java
@@ -2,11 +2,41 @@ package com.meti;
 
 import org.junit.jupiter.api.Test;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class ApplicationTest {
+    private static final Path ROOT = Paths.get(".");
+    private static final Path SOURCE = ROOT.resolve("index" + ".mgs");
+
     @Test
-    void not_generated() {
-        assertFalse(false);
+    void generated() throws IOException {
+        boolean condition = runWithSource();
+        assertTrue(condition);
+    }
+
+    private boolean runWithSource() throws IOException {
+        Files.createFile(SOURCE);
+        return runApplication();
+    }
+
+    private boolean runApplication() throws IOException {
+        return new Application(ApplicationTest.SOURCE).run();
+    }
+
+    @Test
+    void generated_content() throws IOException {
+        runWithSource();
+        assertTrue(Files.exists(SOURCE.resolveSibling("index.c")));
+    }
+
+    @Test
+    void not_generated() throws IOException {
+        assertFalse(runApplication());
     }
 }
