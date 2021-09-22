fd2193e548b9fa7ba8065be6330d2b7b7d648c1d
SirMathhman <sirmathhman@gmail.com>
2021-09-22 18:49:58 -0400

    Add ApplicationTest

---
+++ src/test/java/com/meti/ApplicationTest.java
@@ -0,0 +1,12 @@
+package com.meti;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
+public class ApplicationTest {
+    @Test
+    void not_generated() {
+        assertFalse(false);
+    }
+}
