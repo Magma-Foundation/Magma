63fc6e8515f24f7af3772fc39ccb74d4b151f5c1
SirMathhman <sirmathhman@gmail.com>
2021-09-27 18:27:42 -0400

    Extract subroutines

--- src/main/java/com/meti/Compiler.java
+++ src/main/java/com/meti/Compiler.java
@@ -9,28 +9,38 @@ public record Compiler(String input) {
             var paramStart = input.indexOf('(');
             var paramEnd = input.indexOf(')');
             var paramString = slice(input, paramStart + 1, paramEnd);
-            String parameters;
-            if (paramString.endsWith(" : I16")) {
-                var separator = paramString.indexOf(':');
-                var name = slice(paramString, 0, separator);
-                parameters = "int " + name;
-            } else {
-                parameters = "";
-            }
+            var parameters = parseParameters(paramString);
 
             var name = slice(input, "def ".length(), paramStart);
             var returnTypeString = slice(input, input.lastIndexOf(':') + 1, input.indexOf("=>"));
-            String returnType;
-            if (returnTypeString.equals("Void")) {
-                returnType = "void";
-            } else {
-                returnType = "int";
-            }
+            var returnType = resolveTypeName(returnTypeString);
             var bodyStart = input.indexOf('{');
             var body = slice(input, bodyStart, input.length());
             output = returnType + " " + name + "(" + parameters + ")" + body;
         }
         return output;
+    }
+
+    private String parseParameters(String paramString) {
+        String parameters;
+        if (paramString.endsWith(" : I16")) {
+            var separator = paramString.indexOf(':');
+            var name = slice(paramString, 0, separator);
+            parameters = "int " + name;
+        } else {
+            parameters = "";
+        }
+        return parameters;
+    }
+
+    private String resolveTypeName(String returnTypeString) {
+        String returnType;
+        if (returnTypeString.equals("Void")) {
+            returnType = "void";
+        } else {
+            returnType = "int";
+        }
+        return returnType;
     }
 
     private String slice(String value, int start, int end) {
