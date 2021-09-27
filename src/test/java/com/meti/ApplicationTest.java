dbf243e464f721a941714be829417db578db3117
SirMathhman <SirMathhman@gmail.com>
2021-09-27 14:17:27 -0400

    Add test for output

--- src/test/java/com/meti/ApplicationTest.java
+++ src/test/java/com/meti/ApplicationTest.java
@@ -8,8 +8,7 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class ApplicationTest {
     private static final Path Root = Paths.get(".");
@@ -17,19 +16,30 @@ public class ApplicationTest {
     private static final Path Target = Root.resolve(Application.formatTargetName("index"));
 
     @Test
-    void target() throws IOException {
+    void main() throws IOException {
+        runWithSource();
+        var output = Files.readString(Target);
+        assertEquals("void main(){}", output);
+    }
+
+    private void runWithSource() throws IOException {
         Files.createFile(Source);
-        runImpl();
+        run();
+    }
+
+    @Test
+    void target() throws IOException {
+        runWithSource();
         assertTrue(Files.exists(Target));
     }
 
-    private void runImpl() throws IOException {
+    private void run() throws IOException {
         new Application(Source).run();
     }
 
     @Test
     void no_target() throws IOException {
-        runImpl();
+        run();
         assertFalse(Files.exists(Target));
     }
 
