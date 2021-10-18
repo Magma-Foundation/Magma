e5e630661018967e21807595c481bc79f1b58102
SirMathhman <SirMathhman@gmail.com>
2021-10-18 10:45:27 -0400

    Introduce new ApplicationTest

---
+++ src/test/java/com/meti/ApplicationTest.java
@@ -0,0 +1,36 @@
+package com.meti;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class ApplicationTest {
+    public static final Path Source = Paths.get(".", "index.mgs");
+
+    @Test
+    void exists() throws IOException {
+        Files.createFile(Source);
+        runAndAssertSourceExists();
+    }
+
+    @Test
+    void no_exists() throws IOException {
+        runAndAssertSourceExists();
+    }
+
+    private void runAndAssertSourceExists() throws IOException {
+        if (!Files.exists(Source)) Files.createFile(Source);
+        assertTrue(Files.exists(Source));
+    }
+
+    @AfterEach
+    void tearDown() throws IOException {
+        Files.deleteIfExists(Source);
+    }
+}
