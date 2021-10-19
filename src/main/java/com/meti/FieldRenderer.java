5d11d5a3e40a998a1772a9f74d344fdd6db93dba
SirMathhman <SirMathhman@gmail.com>
2021-10-19 16:07:01 -0400

    Remove duplicates

--- src/main/java/com/meti/FieldRenderer.java
+++ src/main/java/com/meti/FieldRenderer.java
@@ -1,7 +1,7 @@
 package com.meti;
 
 public class FieldRenderer {
-    static String renderField(String name, String type) {
+    static String render(String name, String type) {
         return type + " " + name;
     }
 }
