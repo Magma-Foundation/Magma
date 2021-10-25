9126db4bb6a767882a735d42b1b4bba62cc3b668
SirMathhman <SirMathhman@gmail.com>
2021-10-25 13:46:39 -0400

    Add CompilerTest

---
+++ src/test/java/com/meti/CompilerTest.java
@@ -0,0 +1,14 @@
+package com.meti;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class CompilerTest {
+    @Test
+    void integer() {
+        var compile = new Compiler("420");
+        var output = compile.compile();
+        assertEquals("420", output);
+    }
+}
