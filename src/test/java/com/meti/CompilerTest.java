d76cb320eab86912af3beeb38414e7fdd115bbf2
SirMathhman <SirMathhman@gmail.com>
2021-10-25 13:55:45 -0400

    Replace static import

--- src/test/java/com/meti/CompilerTest.java
+++ src/test/java/com/meti/CompilerTest.java
@@ -2,7 +2,9 @@ package com.meti;
 
 import org.junit.jupiter.api.Test;
 
-class CompilerTest extends CompiledTests {
+import static com.meti.CompiledTests.assertCompile;
+
+class CompilerTest {
     private static void assertInteger(int value) {
         var rendered = Compiler.renderInteger(value);
         assertCompile(rendered, rendered);
