62d5295417115147dcb77c5489708dbebb72e3da
SirMathhman <SirMathhman@gmail.com>
2021-10-26 14:43:48 -0400

    Fixed bug where sliced lines did not work correctly

--- src/main/java/com/meti/compile/Compiler.java
+++ src/main/java/com/meti/compile/Compiler.java
@@ -7,7 +7,7 @@ import com.meti.compile.node.attribute.Attribute;
 import com.meti.compile.node.output.Output;
 import com.meti.magma.MagmaLexingStage;
 
-import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.stream.Collectors;
 
 public record Compiler(String input) {
@@ -19,8 +19,23 @@ public record Compiler(String input) {
 
     public String compile() {
         if (input.isBlank()) return "";
-        var lines = input.split(";");
-        return Arrays.stream(lines)
+        var lines = new ArrayList<String>();
+        var builder = new StringBuilder();
+        var depth = 0;
+        for (int i = 0; i < input.length(); i++) {
+            var c = input.charAt(i);
+            if (c == ';' && depth == 0) {
+                lines.add(builder.toString());
+                builder = new StringBuilder();
+            } else {
+                if (c == '{') depth++;
+                if (c == '}') depth--;
+                builder.append(c);
+            }
+        }
+        lines.add(builder.toString());
+        lines.removeIf(String::isBlank);
+        return lines.stream()
                 .map(this::compileLine)
                 .collect(Collectors.joining());
     }
