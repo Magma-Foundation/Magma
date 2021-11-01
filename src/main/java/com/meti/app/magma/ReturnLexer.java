3f89fb8059e68472813734a995786f537976fe74
SirMathhman <SirMathhman@gmail.com>
2021-11-01 13:05:52 -0400

    Add test for flatMap

--- src/main/java/com/meti/app/magma/ReturnLexer.java
+++ src/main/java/com/meti/app/magma/ReturnLexer.java
@@ -4,8 +4,8 @@ import com.meti.api.option.None;
 import com.meti.api.option.Option;
 import com.meti.api.option.Some;
 import com.meti.app.clang.Processor;
-import com.meti.app.compile.feature.IntegerNode;
 import com.meti.app.compile.feature.Return;
+import com.meti.app.compile.node.Content;
 import com.meti.app.compile.node.Input;
 import com.meti.app.compile.node.Node;
 
@@ -14,7 +14,7 @@ public record ReturnLexer(Input root) implements Processor<Node> {
     public Option<Node> process() {
         if (root.startsWithString("return ")) {
             var value = root.truncate("return ");
-            return new Some<>(new Return(new IntegerNode(Integer.parseInt(value))));
+            return new Some<>(new Return(new Content(new Input(value))));
         }
         return new None<>();
     }
---
+++ src/test/java/com/meti/app/compile/AbstractStageTest.java
@@ -0,0 +1,19 @@
+package com.meti.app.compile;
+
+import com.meti.app.compile.feature.Return;
+import com.meti.app.compile.node.Content;
+import com.meti.app.compile.node.Input;
+import com.meti.app.magma.MagmaLexingStage;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class AbstractStageTest {
+
+    @Test
+    void process() throws CompileException {
+        var expected = new Return(new Content(new Input("420")));
+        var actual = new MagmaLexingStage(new Input("return 420")).process();
+        assertEquals(expected, actual);
+    }
+}
