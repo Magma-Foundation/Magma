3807fb31692c130ae0d9b29879239d13224e096c
SirMathhman <SirMathhman@gmail.com>
2021-11-01 14:08:16 -0400

    Remove bridge code

--- src/main/java/com/meti/app/compile/MagmaLexingStage.java
+++ src/main/java/com/meti/app/compile/MagmaLexingStage.java
@@ -1,8 +1,5 @@
 package com.meti.app.compile;
 
-import com.meti.api.option.None;
-import com.meti.api.option.Option;
-import com.meti.api.option.Some;
 import com.meti.app.compile.node.Input;
 import com.meti.app.compile.node.Node;
 import com.meti.app.compile.node.attribute.Attribute;
@@ -15,16 +12,7 @@ public record MagmaLexingStage(String line) {
     public static Node lexTree(Node node) throws AttributeException {
         var children = node.stream(Attribute.Group.Node).collect(Collectors.toList());
         for (Attribute.Type child : children) {
-            Option<Attribute> result;
-            try {
-                result = new Some<>(node.apply(child));
-            } catch (AttributeException e) {
-                result = new None<>();
-            }
-            node = result
-                    .map(Attribute::asNode)
-                    .map(MagmaLexingStage::lexTree)
-                    .orElse(node);
+            node = lexTree(node.apply(child).asNode());
         }
         return node;
     }
