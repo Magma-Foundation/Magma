e3441294b1ec092c80ce9310691a8b35ae4197d9
SirMathhman <SirMathhman@gmail.com>
2021-11-01 18:12:58 -0400

    Replace with stream injection

--- src/main/java/com/meti/app/compile/split/Splitter.java
+++ src/main/java/com/meti/app/compile/split/Splitter.java
@@ -1,10 +1,14 @@
 package com.meti.app.compile.split;
 
+import com.meti.api.ListStream;
+import com.meti.api.StreamException;
+import com.meti.app.compile.CompileException;
+
 import java.util.ArrayList;
 import java.util.List;
 
 public record Splitter(String input) {
-    public List<String> split() {
+    public List<String> split() throws CompileException {
         var lines = new ArrayList<String>();
         var builder = new StringBuilder();
         var depth = 0;
@@ -21,6 +25,16 @@ public record Splitter(String input) {
         }
         lines.add(builder.toString());
         lines.removeIf(String::isBlank);
-        return lines;
+        try {
+            return new ListStream<>(lines)
+                    .map(String::trim)
+                    .filter(value -> !value.isBlank())
+                    .foldRight(new ArrayList<>(), (sum, next) -> {
+                        sum.add(next);
+                        return sum;
+                    });
+        } catch (StreamException e) {
+            throw new CompileException(e);
+        }
     }
 }
