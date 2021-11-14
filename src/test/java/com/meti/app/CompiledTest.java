2b6b6a41b94cad5fabebe5e27a408d39f57886f6
SirMathhman <sirmathhman@gmail.com>
2021-11-13 21:42:51 -0500

    Add test for native imports

---
+++ src/test/java/com/meti/app/CompiledTest.java
@@ -0,0 +1,15 @@
+package com.meti.app;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
+
+public class CompiledTest {
+    static void assertCompile(String input, String output) {
+        try {
+            var actual = new Compiler(input).compile();
+            assertEquals(output, actual);
+        } catch (CompileException e) {
+            fail(e);
+        }
+    }
+}
--- src/test/java/com/meti/app/CompilerTest.java
+++ src/test/java/com/meti/app/CompilerTest.java
@@ -9,15 +9,9 @@ import org.junit.jupiter.api.Test;
 import java.util.Collections;
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.fail;
 
 public class CompilerTest {
-    @Test
-    void empty() {
-        assertCompile("", "");
-    }
-
     private void assertFunction(String name, String type) {
         try {
             var input = new MagmaRenderer(name, type).render();
@@ -30,24 +24,20 @@ public class CompilerTest {
                     .process()
                     .apply(Attribute.Type.Value)
                     .asString();
-            assertCompile(input, output);
+            CompiledTest.assertCompile(input, output);
         } catch (CompileException e) {
             fail(e);
         }
     }
 
     @Test
-    void name() {
-        assertFunction("empty", "I16");
+    void empty() {
+        CompiledTest.assertCompile("", "");
     }
 
-    private static void assertCompile(String input, String output) {
-        try {
-            var actual = new Compiler(input).compile();
-            assertEquals(output, actual);
-        } catch (CompileException e) {
-            fail(e);
-        }
+    @Test
+    void name() {
+        assertFunction("empty", "I16");
     }
 
     @Test
