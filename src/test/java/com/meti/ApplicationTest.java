4f1b9754578cf500ef9f81b69424fd815283ca0e
SirMathhman <sirmathhman@gmail.com>
2021-11-28 18:16:17 -0500

    Add cleanup

--- src/test/java/com/meti/ApplicationTest.java
+++ src/test/java/com/meti/ApplicationTest.java
@@ -1,10 +1,28 @@
 package com.meti;
 
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 
-public class ApplicationTest {
-    @Test
-    void test() {
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class ApplicationTest {
+    private static final Path SourceDirectoryPath = Paths.get(".", "source");
+
+    @AfterEach
+    void tearDown() throws IOException {
+        Files.delete(SourceDirectoryPath);
+    }
+
+    @Test
+    void test() throws IOException {
+        if (!Files.exists(SourceDirectoryPath)) {
+            Files.createDirectory(SourceDirectoryPath);
+        }
+        assertTrue(Files.exists(SourceDirectoryPath));
     }
 }
