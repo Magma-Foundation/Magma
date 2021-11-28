c2e62de3be96ca2fbe5cbeaf1edd19fd98b66f2e
SirMathhman <sirmathhman@gmail.com>
2021-11-28 18:20:55 -0500

    Add tests for directory creation

--- src/test/java/com/meti/ApplicationTest.java
+++ src/test/java/com/meti/ApplicationTest.java
@@ -4,25 +4,71 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
+import java.nio.file.*;
+import java.nio.file.attribute.BasicFileAttributes;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class ApplicationTest {
     private static final Path SourceDirectoryPath = Paths.get(".", "source");
+    private static final Path TestDirectoryPath = SourceDirectoryPath.resolve("test").resolve("magma");
+    private static final Path MainDirectoryPath = SourceDirectoryPath.resolve("main").resolve("magma");
 
-    @AfterEach
-    void tearDown() throws IOException {
-        Files.delete(SourceDirectoryPath);
+    @Test
+    void creates_main() {
+        assertTrue(Files.exists(MainDirectoryPath));
     }
 
     @Test
-    void test() throws IOException {
-        if (!Files.exists(SourceDirectoryPath)) {
-            Files.createDirectory(SourceDirectoryPath);
-        }
+    void creates_source() throws IOException {
+        run();
         assertTrue(Files.exists(SourceDirectoryPath));
+    }
+
+    private void run() throws IOException {
+        ensure(SourceDirectoryPath);
+        ensure(MainDirectoryPath);
+        ensure(TestDirectoryPath);
+    }
+
+    private void ensure(Path path) throws IOException {
+        if (!Files.exists(path)) {
+            Files.createDirectories(path);
+        }
+    }
+
+    @Test
+    void creates_test() {
+        assertTrue(Files.exists(TestDirectoryPath));
+    }
+
+    @AfterEach
+    void tearDown() throws IOException {
+        Files.walkFileTree(SourceDirectoryPath, new DeletingVisitor());
+        Files.delete(SourceDirectoryPath);
+    }
+
+    private static class DeletingVisitor implements FileVisitor<Path> {
+        @Override
+        public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attrs) {
+            return FileVisitResult.CONTINUE;
+        }
+
+        @Override
+        public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+            Files.delete(file);
+            return FileVisitResult.CONTINUE;
+        }
+
+        @Override
+        public FileVisitResult visitFileFailed(Path file, IOException exc) {
+            return FileVisitResult.CONTINUE;
+        }
+
+        @Override
+        public FileVisitResult postVisitDirectory(Path dir, IOException exc) throws IOException {
+            Files.delete(dir);
+            return FileVisitResult.CONTINUE;
+        }
     }
 }
