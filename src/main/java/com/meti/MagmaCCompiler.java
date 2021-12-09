f80574bfe2226344b61d539c021c266228b3c4fb
SirMathhman <sirmathhman@gmail.com>
2021-12-08 21:54:02 -0500

    Add return types to functions

--- src/main/java/com/meti/MagmaCCompiler.java
+++ src/main/java/com/meti/MagmaCCompiler.java
@@ -11,11 +11,26 @@ public class MagmaCCompiler {
         String output;
         if (input.startsWith("def ")) {
             var parameterStart = input.indexOf('(');
-            var name = input.substring("def ".length(), parameterStart);
-            output = "int " + name + "(){return 0;}";
+            var name = slice(parameterStart, "def ".length());
+            var typeSeparator = input.indexOf(':');
+            var returnSeparator = input.indexOf("=>");
+            var typeString = slice(returnSeparator, typeSeparator + 1);
+
+            String type;
+            if (typeString.equals("I16")) {
+                type = "int";
+            } else {
+                type = "unsigned int";
+            }
+
+            output = type + " " + name + "()" + "{return 0;}";
         } else {
             output = "";
         }
         return output;
+    }
+
+    private String slice(int parameterStart, int length) {
+        return input.substring(length, parameterStart).trim();
     }
 }
--- src/test/java/com/meti/MagmaCCompilerTest.java
+++ src/test/java/com/meti/MagmaCCompilerTest.java
@@ -7,8 +7,17 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 class MagmaCCompilerTest {
     @Test
     void name() {
-        var output = new MagmaCCompiler("def test() : I16 => {return 0;}")
+        assertCompile("def test() : I16 => {return 0;}", "int test(){return 0;}");
+    }
+
+    private void assertCompile(String input, String output) {
+        var actual = new MagmaCCompiler(input)
                 .compile();
-        assertEquals("int test(){return 0;}", output);
+        assertEquals(output, actual);
+    }
+
+    @Test
+    void type() {
+        assertCompile("def main() : U16 => {return 0;}", "unsigned int main(){return 0;}");
     }
 }
