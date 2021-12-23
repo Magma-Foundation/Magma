8323dbe75fdf838b8ec724fc8a291dc8aabdaada
SirMathhman <sirmathhman@gmail.com>
2021-12-23 12:13:10 -0500

    Extract Input

--- src/main/java/com/meti/MCCompiler.java
+++ src/main/java/com/meti/MCCompiler.java
@@ -1,31 +1,35 @@
 package com.meti;
 
 public final class MCCompiler {
-    private final String input;
+    private final Input input;
 
     public MCCompiler(String input) {
-        this.input = input;
+        this.input = new Input(input);
     }
 
     String compile() throws CompileException {
-        var typeSeparator = input.indexOf(':');
-        var valueSeparator = input.indexOf('=');
+        var typeSeparator = input.firstChar(':').orElse(-1);
+        var valueSeparator = input.firstChar('=').orElse(-1);
 
-        var keys = slice(typeSeparator, 0);
-        var nameSeparator = keys.lastIndexOf(' ');
-        var name = keys.substring(nameSeparator + 1).trim();
+        var slice = input.slice(0, typeSeparator);
+        var nameSeparator = slice.lastChar();
+        var name = slice.sliceToEnd(nameSeparator + 1).compute();
 
-        var typeString = slice(valueSeparator, typeSeparator + 1);
-        var type = switch (typeString) {
-            case "I16" -> "int";
-            case "U16" -> "unsigned int";
-            default -> throw new CompileException("Unknown type: " + typeString);
-        };
+        var typeString = input.slice(typeSeparator + 1, valueSeparator);
+        var type = lexType(typeString);
 
         return type + " " + name + "=420;";
     }
 
-    private String slice(int typeSeparator, int i) {
-        return input.substring(i, typeSeparator).trim();
+    private String lexType(Input typeString) throws CompileException {
+        String type;
+        if (typeString.contains("I16")) {
+            type = "int";
+        } else if (typeString.contains("U16")) {
+            type = "unsigned int";
+        } else {
+            throw new CompileException("Unknown type:" + typeString);
+        }
+        return type;
     }
 }
