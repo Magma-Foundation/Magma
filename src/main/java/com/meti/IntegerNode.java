0c11d53a471152d1615bf8883a8a1f1c44880587
SirMathhman <sirmathhman@gmail.com>
2021-12-23 14:11:00 -0500

    Split lexing and rendering

---
+++ src/main/java/com/meti/IntegerNode.java
@@ -0,0 +1,13 @@
+package com.meti;
+
+public class IntegerNode {
+    private final String value;
+
+    public IntegerNode(String value) {
+        this.value = value;
+    }
+
+    public String getValue() {
+        return value;
+    }
+}
--- src/main/java/com/meti/MCCompiler.java
+++ src/main/java/com/meti/MCCompiler.java
@@ -7,16 +7,21 @@ import com.meti.option.Some;
 public record MCCompiler(Input input) {
     String compile() throws CompileException {
         return lexInteger()
+                .map(this::render)
                 .orElseThrow(() -> new CompileException("Invalid input."));
     }
 
-    private Option<String> lexInteger() {
+    private Option<IntegerNode> lexInteger() {
         try {
             var value = input.compute();
             Integer.parseInt(value);
-            return new Some<>(value);
+            return new Some<>(new IntegerNode(value));
         } catch (NumberFormatException e) {
-            return new None<String>();
+            return new None<>();
         }
+    }
+
+    private String render(IntegerNode integerNode) {
+        return integerNode.getValue();
     }
 }
