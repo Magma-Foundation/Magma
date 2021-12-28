ac5a3c6bf9b41e160b1cd63446f607b14c74ae8a
SirMathhman <sirmathhman@gmail.com>
2021-12-28 18:02:22 -0500

    ???

--- src/test/java/com/meti/ApplicationE2ETest.java
+++ src/test/java/com/meti/ApplicationE2ETest.java
@@ -2,9 +2,11 @@ package com.meti;
 
 import org.junit.jupiter.api.Test;
 
+import java.nio.file.Paths;
+
 public class ApplicationE2ETest {
     @Test
-    void test() {
-
+    void no_writes_target() {
+        Paths.get(".", "index.c");
     }
 }
