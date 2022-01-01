ac282d15dae1fc2aff3643924d74879123c57079
SirMathhman <sirmathhman@gmail.com>
2022-01-01 16:42:57 -0500

    Add multiple functions

--- src/test/java/com/meti/Compiler.java
+++ src/test/java/com/meti/Compiler.java
@@ -1,7 +1,34 @@
 package com.meti;
 
+import java.util.ArrayList;
+
 public record Compiler(String input) {
     String compile() {
+        var lines = new ArrayList<String>();
+        var buffer = new StringBuilder();
+        var depth = 0;
+        for (int i = 0; i < input.length(); i++) {
+            var c = input.charAt(i);
+            if (depth == 0 && c == ';') {
+                lines.add(buffer.toString());
+                buffer = new StringBuilder();
+            } else {
+                if (c == '{') depth++;
+                if (c == '}') depth--;
+                buffer.append(c);
+            }
+        }
+        lines.add(buffer.toString());
+        lines.removeIf(String::isBlank);
+
+        var output = new StringBuilder();
+        for (String line : lines) {
+            output.append(compileNode(line));
+        }
+        return output.toString();
+    }
+
+    private String compileNode(String input) {
         if (input.startsWith("def ")) {
             var paramStart = input.indexOf('(');
             var name = input.substring("def ".length(), paramStart).trim();
--- src/test/java/com/meti/CompilerTest.java
+++ src/test/java/com/meti/CompilerTest.java
@@ -6,6 +6,12 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class CompilerTest {
     @Test
+    void multiple() {
+        assertCompile("def first() : I16 => {return 0;};def second() : I16 => {return 0;}",
+                "int first(){return 0;}int second(){return 0;}");
+    }
+
+    @Test
     void name() {
         assertCompile("def supplier() : I16 => {return 0;}", "int supplier(){return 0;}");
     }
