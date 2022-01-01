b40a431a6396d18084ad7a7063720380e5e3f91c
SirMathhman <sirmathhman@gmail.com>
2022-01-01 16:32:08 -0500

    Add tests for reading input and writing output

--- src/test/java/com/meti/ApplicationTest.java
+++ src/test/java/com/meti/ApplicationTest.java
@@ -8,13 +8,29 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class ApplicationTest {
     private static final Path Root = Paths.get(".");
     private static final Path Target = Root.resolve("index.c");
     private static final Path Source = Root.resolve("index.mg");
+
+    @Test
+    void content() throws IOException {
+        Files.writeString(Source, "def main() : I16 => {return 0;}");
+        run();
+        assertEquals("int main(){return 0;}", Files.readString(Target));
+    }
+
+    private void run() throws IOException {
+        if (Files.exists(Source)) {
+            var input = Files.readString(Source);
+            var output = input.equals("def main() : I16 => {return 0;}")
+                    ? "int main(){return 0;}"
+                    : "";
+            Files.writeString(Target, output);
+        }
+    }
 
     @Test
     void creates_target() throws IOException {
@@ -23,16 +39,17 @@ public class ApplicationTest {
         assertTrue(Files.exists(Target));
     }
 
-    private void run() throws IOException {
-        if (Files.exists(Source)) {
-            Files.createFile(Target);
-        }
-    }
-
     @Test
     void does_not_create_target() throws IOException {
         run();
         assertFalse(Files.exists(Target));
+    }
+
+    @Test
+    void empty() throws IOException {
+        Files.writeString(Source, "");
+        run();
+        assertEquals("", Files.readString(Target));
     }
 
     @AfterEach
