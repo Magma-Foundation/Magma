3d2979a4ca22bbf745aeb4a8ad27e23c9a2a8209
SirMathhman <sirmathhman@gmail.com>
2022-01-08 16:12:14 -0500

    Add function return types

--- src/main/java/com/meti/Compiler.java
+++ src/main/java/com/meti/Compiler.java
@@ -2,10 +2,19 @@ package com.meti;
 
 public record Compiler(String input) {
     String compile() {
-        if(input.startsWith("def ")) {
+        if (input.startsWith("def ")) {
             var paramStart = input.indexOf('(');
             var name = input.substring("def ".length(), paramStart);
-            return "int " + name + "(){return 0;}";
+            var typeSeparator = input.indexOf(':');
+            var valueSeparator = input.indexOf("=>");
+            var typeString = input.substring(typeSeparator + 1, valueSeparator).trim();
+            String type;
+            if (typeString.equals("I16")) {
+                type = "int";
+            } else {
+                type = "unsigned int";
+            }
+            return type + " " + name + "(){return 0;}";
         }
         return "";
     }
--- src/test/java/com/meti/CompilerTest.java
+++ src/test/java/com/meti/CompilerTest.java
@@ -5,11 +5,19 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class CompilerTest {
-
     @Test
     void compile() {
-        var output = new Compiler("def test() : I16 => {return 0;}")
-                .compile();
-        assertEquals("int test(){return 0;}", output);
+        assertCompile("def test() : I16 => {return 0;}", "int test(){return 0;}");
+    }
+
+    private void assertCompile(String input, String output) {
+        var compiler = new Compiler(input);
+        var actual = compiler.compile();
+        assertEquals(output, actual);
+    }
+
+    @Test
+    void return_type(){
+        assertCompile("def test() : U16 => {return 0;}", "unsigned int test(){return 0;}");
     }
 }
