837f8ee76acbabcc8c246811d78528425eb82e32
SirMathhman <sirmathhman@gmail.com>
2022-01-08 17:00:47 -0500

    Extract lexBlock

--- src/main/java/com/meti/option/None.java
+++ src/main/java/com/meti/option/None.java
@@ -4,12 +4,22 @@ import com.meti.core.F1;
 
 public class None<T> implements Option<T> {
     @Override
-    public <R, E extends Exception> Option<R> map(F1<T, R, E> mapper) throws E {
+    public boolean isPresent() {
+        return false;
+    }
+
+    @Override
+    public <R, E extends Exception> Option<R> map(F1<T, R, E> mapper) {
         return new None<>();
     }
 
     @Override
     public T orElse(T other) {
         return other;
+    }
+
+    @Override
+    public <E extends Exception> T orElseGet(Supplier<T, E> getter) throws E {
+        return getter.get();
     }
 }
--- src/main/java/com/meti/option/Some.java
+++ src/main/java/com/meti/option/Some.java
@@ -4,12 +4,22 @@ import com.meti.core.F1;
 
 public record Some<T>(T value) implements Option<T> {
     @Override
+    public boolean isPresent() {
+        return true;
+    }
+
+    @Override
     public <R, E extends Exception> Option<R> map(F1<T, R, E> mapper) throws E {
         return new Some<>(mapper.apply(value));
     }
 
     @Override
     public T orElse(T other) {
+        return value;
+    }
+
+    @Override
+    public <E extends Exception> T orElseGet(Supplier<T, E> getter) {
         return value;
     }
 }
