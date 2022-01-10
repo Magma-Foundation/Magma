028471c795f7fbc6dfb19b63e2a1bf22730a3ba8
SirMathhman <sirmathhman@gmail.com>
2022-01-10 17:09:58 -0500

    Fix bug where return values weren't being parsed

--- src/main/java/com/meti/compile/common/returns/Return.java
+++ src/main/java/com/meti/compile/common/returns/Return.java
@@ -5,20 +5,29 @@ import com.meti.compile.attribute.AttributeException;
 import com.meti.compile.attribute.NodeAttribute;
 import com.meti.compile.node.Node;
 
+import java.util.stream.Stream;
+
 record Return(Node value) implements Node {
     @Override
-    public Attribute apply(Attribute.Type type) throws AttributeException {
-        if(type == Attribute.Type.Value) return new NodeAttribute(value);
-        throw new AttributeException(type);
+    public Stream<Attribute.Type> apply(Attribute.Group group) throws AttributeException {
+        return group == Attribute.Group.Node
+                ? Stream.of(Attribute.Type.Value)
+                : Stream.empty();
     }
 
     @Override
-    public Node with(Attribute.Type type, Attribute attribute) throws AttributeException {
-        return new Return(attribute.asNode());
+    public Attribute apply(Attribute.Type type) throws AttributeException {
+        if (type == Attribute.Type.Value) return new NodeAttribute(value);
+        throw new AttributeException(type);
     }
 
     @Override
     public boolean is(Type type) {
         return type == Node.Type.Return;
+    }
+
+    @Override
+    public Node with(Attribute.Type type, Attribute attribute) throws AttributeException {
+        return new Return(attribute.asNode());
     }
 }
