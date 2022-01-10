765ca010d38526d3ca3c17a1163dd3ed82aa8d12
SirMathhman <sirmathhman@gmail.com>
2022-01-10 13:19:33 -0500

    Prepare for field parsing

--- src/main/java/com/meti/compile/clang/StructureRenderer.java
+++ src/main/java/com/meti/compile/clang/StructureRenderer.java
@@ -9,12 +9,23 @@ import com.meti.option.None;
 import com.meti.option.Option;
 import com.meti.option.Some;
 
+import java.util.stream.Collectors;
+
 record StructureRenderer(Node node) implements Renderer {
     @Override
     public Option<Text> render() throws AttributeException {
         if (node.is(Node.Type.Structure)) {
             var name = node.apply(Attribute.Type.Name).asText();
-            return new Some<>(name.prepend("struct ").append("{};"));
+            var fields = node.apply(Attribute.Type.Fields).asStreamOfNodes().collect(Collectors.toList());
+            var builder = new StringBuilder();
+            for (Node field : fields) {
+                builder.append(field.apply(Attribute.Type.Value).asText().compute()).append(";");
+            }
+            return new Some<>(name.prepend("struct ")
+                    .append("{")
+                    .append(builder.toString())
+                    .append("}")
+                    .append(";"));
         }
         return new None<>();
     }
