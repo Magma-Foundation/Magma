9ec64b4281299db8cb344b19598c61f9684e963a
SirMathhman <sirmathhman@gmail.com>
2022-01-10 16:52:41 -0500

    Add test for two parameters

--- src/test/java/com/meti/compile/FunctionTest.java
+++ src/test/java/com/meti/compile/FunctionTest.java
@@ -16,6 +16,11 @@ public class FunctionTest {
     }
 
     @Test
+    void two_parameters(){
+        assertSourceCompile("def Point(x : I16, y : I16) : Void => {}", "void Point(int x,int y){}");
+    }
+
+    @Test
     void return_type() {
         assertSourceCompile("def test() : U16 => {return 0;}", "unsigned int test(){return 0;}");
     }
