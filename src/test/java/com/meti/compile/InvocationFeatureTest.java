b48187b959dcb038f2980b0c8cfb134e9dd00ed8
SirMathhman <sirmathhman@gmail.com>
2022-01-12 00:29:34 -0500

    Add test for trailing semicolon

--- src/test/java/com/meti/compile/InvocationFeatureTest.java
+++ src/test/java/com/meti/compile/InvocationFeatureTest.java
@@ -2,19 +2,26 @@ package com.meti.compile;
 
 import org.junit.jupiter.api.Test;
 
+import static com.meti.compile.CompiledTest.assertSourceCompile;
+
 public class InvocationFeatureTest {
     @Test
+    void block() {
+        assertSourceCompile("{test(1);}", "{test(1);}");
+    }
+
+    @Test
     void one_parameter() {
-        CompiledTest.assertSourceCompile("test(1)", "test(1)");
+        assertSourceCompile("test(1)", "test(1)");
     }
 
     @Test
     void test() {
-        CompiledTest.assertSourceCompile("test()", "test()");
+        assertSourceCompile("test()", "test()");
     }
 
     @Test
     void two_parameters() {
-        CompiledTest.assertSourceCompile("test(1, 2)", "test(1,2)");
+        assertSourceCompile("test(1, 2)", "test(1,2)");
     }
 }
