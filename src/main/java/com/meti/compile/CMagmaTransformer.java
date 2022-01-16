bd820617b064a4433385954af453388b32ec6953
SirMathhman <sirmathhman@gmail.com>
2022-01-16 15:52:35 -0500

    Make boolean transformation optionanl

--- src/main/java/com/meti/compile/CMagmaTransformer.java
+++ src/main/java/com/meti/compile/CMagmaTransformer.java
@@ -4,18 +4,25 @@ import com.meti.compile.attribute.Attribute;
 import com.meti.compile.attribute.AttributeException;
 import com.meti.compile.common.integer.IntegerNode;
 import com.meti.compile.node.Node;
+import com.meti.option.None;
+import com.meti.option.Option;
+import com.meti.option.Some;
 
 public record CMagmaTransformer() {
     Node transform(Node node) throws TransformationException {
+        return transformBoolean(node).orElse(node);
+    }
+
+    private Option<Node> transformBoolean(Node node) throws TransformationException {
         if (node.is(Node.Type.Boolean)) {
             try {
                 var value = node.apply(Attribute.Type.Value);
                 var state = value.asBoolean();
-                return new IntegerNode(state ? 1 : 0);
+                return new Some<>(new IntegerNode(state ? 1 : 0));
             } catch (AttributeException e) {
                 throw new TransformationException(e);
             }
         }
-        return node;
+        return new None<>();
     }
 }
