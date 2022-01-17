edb67110d65bf950a207e5c8594e98fd5abfd02d
SirMathhman <sirmathhman@gmail.com>
2022-01-17 17:44:39 -0500

    Migrate Invocation

--- src/main/java/com/meti/compile/common/invoke/Invocation.java
+++ src/main/java/com/meti/compile/common/invoke/Invocation.java
@@ -1,32 +1,46 @@
 package com.meti.compile.common.invoke;
 
 import com.meti.collect.JavaList;
+import com.meti.collect.StreamException;
 import com.meti.compile.attribute.Attribute;
 import com.meti.compile.attribute.AttributeException;
 import com.meti.compile.attribute.NodeAttribute;
-import com.meti.compile.attribute.NodesAttribute;
+import com.meti.compile.attribute.NodesAttribute1;
 import com.meti.compile.node.Node;
 
-import java.util.List;
+import java.util.Objects;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
-public record Invocation(Node caller, List<Node> arguments) implements Node {
-    @Override
-    public Stream<Attribute.Type> apply(Attribute.Group group) throws AttributeException {
-        return switch (group) {
-            case Node -> Stream.of(Attribute.Type.Caller);
-            case Nodes -> Stream.of(Attribute.Type.Arguments);
-            default -> Stream.empty();
-        };
+public final class Invocation implements Node {
+    private final Node caller;
+    private final JavaList<Node> arguments;
+
+    public Invocation(Node caller, JavaList<Node> arguments) {
+        this.caller = caller;
+        this.arguments = arguments;
+    }
+
+    public Invocation(Node caller, Node... arguments) {
+        this.caller = caller;
+        this.arguments = JavaList.apply(arguments);
     }
 
     @Override
     public Attribute apply(Attribute.Type type) throws AttributeException {
         return switch (type) {
             case Caller -> new NodeAttribute(caller);
-            case Arguments -> new NodesAttribute(arguments);
+            case Arguments -> new NodesAttribute1(arguments);
             default -> throw new AttributeException(type);
+        };
+    }
+
+    @Override
+    public Stream<Attribute.Type> apply(Attribute.Group group) throws AttributeException {
+        return switch (group) {
+            case Node -> Stream.of(Attribute.Type.Caller);
+            case Nodes -> Stream.of(Attribute.Type.Arguments);
+            default -> Stream.empty();
         };
     }
 
@@ -42,10 +56,36 @@ public record Invocation(Node caller, List<Node> arguments) implements Node {
 
     @Override
     public Node with(Attribute.Type type, Attribute attribute) throws AttributeException {
-        return switch (type) {
-            case Caller -> new Invocation(attribute.asNode(), arguments);
-            case Arguments -> new Invocation(caller, attribute.asStreamOfNodes().collect(Collectors.toList()));
-            default -> this;
-        };
+        try {
+            return switch (type) {
+                case Caller -> new Invocation(attribute.asNode(), arguments);
+                case Arguments -> new Invocation(caller, attribute
+                        .asStreamOfNodes1()
+                        .foldRight(new JavaList<>(), JavaList::add));
+                default -> this;
+            };
+        } catch (StreamException e) {
+            throw new AttributeException(e);
+        }
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(caller, arguments);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Invocation that)) return false;
+        return Objects.equals(caller, that.caller) && Objects.equals(arguments, that.arguments);
+    }
+
+    @Override
+    public String toString() {
+        return "{" +
+               "\n\t\"caller\":" + caller +
+               ",\n\t\"arguments\":" + arguments +
+               '}';
     }
 }
--- src/main/java/com/meti/compile/common/invoke/InvocationLexer.java
+++ src/main/java/com/meti/compile/common/invoke/InvocationLexer.java
@@ -1,33 +1,38 @@
 package com.meti.compile.common.invoke;
 
+import com.meti.collect.JavaList;
+import com.meti.collect.StreamException;
+import com.meti.collect.Streams;
+import com.meti.compile.CompileException;
 import com.meti.compile.lex.Lexer;
 import com.meti.compile.node.Content;
 import com.meti.compile.node.Node;
 import com.meti.compile.node.Text;
 import com.meti.option.Option;
 
-import java.util.Arrays;
-import java.util.stream.Collectors;
-
 public record InvocationLexer(Text text) implements Lexer {
     @Override
-    public Option<Node> lex() {
+    public Option<Node> lex() throws CompileException {
         return text.firstIndexOfChar('(')
-                .flatMap(start -> text.firstIndexOfChar(')')
-                .filter(end -> end >= start)
-                .map(end -> extract(start, end)));
+                .flatMap(start -> text.lastIndexOfChar(')')
+                        .filter(end -> end >= start)
+                        .map(end -> extract(start, end)));
     }
 
-    private Invocation extract(Integer start, Integer end) {
-        var callerText = text.slice(0, start);
-        var arguments = Arrays.stream(text.slice(start + 1, end)
-                .computeTrimmed()
-                .split(","))
-                .filter(s -> !s.isBlank())
-                .map(Text::new)
-                .map(Content::new)
-                .collect(Collectors.<Node>toList());
-        var caller = new Content(callerText);
-        return new Invocation(caller, arguments);
+    private Invocation extract(Integer start, Integer end) throws CompileException {
+        try {
+            var callerText = text.slice(0, start);
+            var arguments = Streams.apply(text.slice(start + 1, end)
+                    .computeTrimmed()
+                    .split(","))
+                    .filter(s -> !s.isBlank())
+                    .map(Text::new)
+                    .map(Content::new)
+                    .foldRight(new JavaList<Node>(), JavaList::add);
+            var caller = new Content(callerText);
+            return new Invocation(caller, arguments);
+        } catch (StreamException e) {
+            throw new CompileException(e);
+        }
     }
 }
