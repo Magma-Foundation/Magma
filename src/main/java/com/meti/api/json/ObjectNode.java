5cfda7a83919438e8e3fff1c8d3544b46d1f16b0
SirMathhman <sirmathhman@gmail.com>
2022-01-24 16:17:35 -0500

    Add basic formatting

--- src/main/java/com/meti/api/json/ObjectNode.java
+++ src/main/java/com/meti/api/json/ObjectNode.java
@@ -21,7 +21,21 @@ public class ObjectNode implements JSONNode {
 
     @Override
     public String format() {
-        return toString();
+        var value = toString();
+        var buffer = new StringBuilder();
+        for (int i = 0; i < value.length(); i++) {
+            var c = value.charAt(i);
+            if (c == '{') {
+                buffer.append(c).append("\n\t");
+            } else if (c == ':') {
+                buffer.append(" : ");
+            } else if (c == '}') {
+                buffer.append("\n").append(c);
+            } else {
+                buffer.append(c);
+            }
+        }
+        return buffer.toString();
     }
 
     @Override
--- src/test/java/com/meti/api/json/ObjectNodeTest.java
+++ src/test/java/com/meti/api/json/ObjectNodeTest.java
@@ -6,6 +6,15 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class ObjectNodeTest {
     @Test
+    void formattedInner() {
+        var expected = "{\n\t\"test\" : 5\n}";
+        var actual = new ObjectNode()
+                .append("test", 5)
+                .format();
+        assertEquals(expected, actual);
+    }
+
+    @Test
     void unformatted() {
         var expected = "{\"test\":5}";
         var actual = new ObjectNode()
