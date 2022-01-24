27df1ab4d663e985512ca79c044d615051516890
SirMathhman <SirMathhman@gmail.com>
2022-01-24 14:23:21 -0500

    Add more descriptive error message

--- src/main/java/com/meti/app/compile/node/attribute/AttributeException.java
+++ src/main/java/com/meti/app/compile/node/attribute/AttributeException.java
@@ -1,8 +1,13 @@
 package com.meti.app.compile.node.attribute;
 
+import com.meti.app.compile.node.Node;
 import com.meti.app.compile.stage.CompileException;
 
 public class AttributeException extends CompileException {
+    public AttributeException(Node node, Attribute.Type type) {
+        this("Type '" + type + "' was not present in node: '" + node + "'.");
+    }
+
     public AttributeException(Attribute.Type type) {
         this("Unknown attribute: " + type);
     }
