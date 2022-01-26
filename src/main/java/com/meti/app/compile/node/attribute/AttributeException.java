b2f78bdce2aeb60d8ad622670559cb11f61d6706
SirMathhman <sirmathhman@gmail.com>
2022-01-26 16:20:07 -0500

    Format exceptions better

--- src/main/java/com/meti/app/compile/node/attribute/AttributeException.java
+++ src/main/java/com/meti/app/compile/node/attribute/AttributeException.java
@@ -5,7 +5,7 @@ import com.meti.app.compile.stage.CompileException;
 
 public class AttributeException extends CompileException {
     public AttributeException(Node node, Attribute.Type type) {
-        this("Type '%s' was not present in node: '%s'.".formatted(type, node));
+        this("Type '%s' was not present in node:\n-----\n%s\n-----\n".formatted(type, node));
     }
 
     public AttributeException(String message) {
