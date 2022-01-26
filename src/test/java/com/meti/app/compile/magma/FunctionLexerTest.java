db2a0ad551dad3f1f545574bdc4d56b9e608df1b
SirMathhman <sirmathhman@gmail.com>
2022-01-26 18:07:46 -0500

    Add test for body lexing

--- src/test/java/com/meti/app/compile/magma/FunctionLexerTest.java
+++ src/test/java/com/meti/app/compile/magma/FunctionLexerTest.java
@@ -5,17 +5,29 @@ import com.meti.app.compile.common.EmptyField;
 import com.meti.app.compile.common.Field;
 import com.meti.app.compile.common.Fields;
 import com.meti.app.compile.common.Implementation;
-import com.meti.app.compile.node.EmptyNode;
 import com.meti.app.compile.node.InputNode;
+import com.meti.app.compile.node.attribute.Attribute;
 import com.meti.app.compile.stage.CompileException;
 import com.meti.app.compile.text.RootText;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import static com.meti.app.compile.node.EmptyNode.EmptyNode_;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 
 class FunctionLexerTest {
+    @Test
+    void body() throws CompileException {
+        var input = new RootText("def main() : I16 => {return 0;}");
+        var output = new FunctionLexer(input)
+                .process()
+                .orElse(EmptyNode_);
+        var expected = new InputNode(new RootText("{return 0;}"));
+        var actual = output.apply(Attribute.Type.Value).asNode();
+        assertEquals(expected, actual);
+    }
+
     @Test
     void invalid() throws CompileException {
         assertFalse(new FunctionLexer(new RootText("test(() => {})"))
@@ -29,7 +41,7 @@ class FunctionLexerTest {
                 .process();
         var identity = new EmptyField(new RootText(""), ImplicitType.ImplicitType_, List.createList());
         var expected = new Implementation(identity, new InputNode(new RootText("{}")), List.createList());
-        var actual = node.orElse(EmptyNode.EmptyNode_);
+        var actual = node.orElse(EmptyNode_);
         assertEquals(expected, actual);
     }
 
