54f6ee2beb491658977525c4b73ccaf0eff8c03f
SirMathhman <SirMathhman@gmail.com>
2022-01-26 14:02:41 -0500

    Remove deprecated method implementations of EmptyNodes

--- src/main/java/com/meti/app/compile/node/EmptyNode.java
+++ src/main/java/com/meti/app/compile/node/EmptyNode.java
@@ -1,13 +1,7 @@
 package com.meti.app.compile.node;
 
-import com.meti.api.collect.java.List;
 import com.meti.api.json.JSONNode;
 import com.meti.api.json.ObjectNode;
-import com.meti.app.compile.node.attribute.Attribute;
-import com.meti.app.compile.node.attribute.AttributeException;
-
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 public class EmptyNode implements Node {
     public static final EmptyNode EmptyNode_ = new EmptyNode();
@@ -18,17 +12,6 @@ public class EmptyNode implements Node {
     @Override
     public JSONNode toJSON() {
         return new ObjectNode().add("empty", true);
-    }
-
-    @Override
-    @Deprecated
-    public Stream<Attribute.Type> apply(Attribute.Group group) throws AttributeException {
-        return Stream.empty();
-    }
-
-    @Override
-    public com.meti.api.collect.stream.Stream<Attribute.Type> apply1(Attribute.Group group) throws AttributeException {
-        return List.createList(apply(group).collect(Collectors.toList())).stream();
     }
 
     @Override
