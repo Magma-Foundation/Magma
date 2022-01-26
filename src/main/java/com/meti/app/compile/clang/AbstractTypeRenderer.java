9b4c4b1e9857503e0d41b58437f5d9519a4402f5
SirMathhman <sirmathhman@gmail.com>
2022-01-26 18:21:48 -0500

    Make AbstractTypeRenderer process to Node

--- src/main/java/com/meti/app/compile/clang/AbstractTypeRenderer.java
+++ src/main/java/com/meti/app/compile/clang/AbstractTypeRenderer.java
@@ -4,13 +4,14 @@ import com.meti.api.option.None;
 import com.meti.api.option.Option;
 import com.meti.api.option.Some;
 import com.meti.app.compile.node.Node;
+import com.meti.app.compile.node.OutputNode;
 import com.meti.app.compile.node.attribute.AttributeException;
 import com.meti.app.compile.process.Processor;
 import com.meti.app.compile.stage.CompileException;
 import com.meti.app.compile.text.Input;
 import com.meti.app.compile.text.Output;
 
-public abstract class AbstractTypeRenderer implements Processor<Output> {
+public abstract class AbstractTypeRenderer implements Processor<Node> {
     protected final Input name;
     protected final Node type;
     protected final Node.Type nodeType;
@@ -22,8 +23,8 @@ public abstract class AbstractTypeRenderer implements Processor<Output> {
     }
 
     @Override
-    public Option<Output> process() throws CompileException {
-        return type.is(nodeType) ? new Some<>(processValid()) : new None<Output>();
+    public Option<Node> process() throws CompileException {
+        return type.is(nodeType) ? new Some<>(new OutputNode((processValid()))) : new None<>();
     }
 
     protected abstract Output processValid() throws AttributeException;
