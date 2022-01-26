92b1fad9f9642b9a570202ca3f9fd4f7f35f737e
SirMathhman <sirmathhman@gmail.com>
2022-01-26 16:47:12 -0500

    Remove implementations of deprecated methods

--- src/main/java/com/meti/app/compile/node/Primitive.java
+++ src/main/java/com/meti/app/compile/node/Primitive.java
@@ -1,6 +1,5 @@
 package com.meti.app.compile.node;
 
-import com.meti.api.collect.java.List;
 import com.meti.api.json.JSONNode;
 import com.meti.api.json.ObjectNode;
 import com.meti.app.compile.node.attribute.Attribute;
@@ -8,27 +7,19 @@ import com.meti.app.compile.node.attribute.AttributeException;
 import com.meti.app.compile.node.attribute.InputAttribute;
 import com.meti.app.compile.text.RootText;
 
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
-
 public enum Primitive implements Node {
     Bool,
     Void;
 
     @Override
-    public Attribute apply(Attribute.Type type) throws AttributeException {
-        if (type == Attribute.Type.Name) return new InputAttribute(new RootText(name()));
-        throw new AttributeException(type);
-    }
-
-    @Deprecated
-    private Stream<Attribute.Type> apply2(Attribute.Group group) {
-        return Stream.empty();
+    public boolean is(Type type) {
+        return type == Type.Primitive;
     }
 
     @Override
-    public String toString() {
-        return "\"" + name() + "\"";
+    public Attribute apply(Attribute.Type type) throws AttributeException {
+        if (type == Attribute.Type.Name) return new InputAttribute(new RootText(name()));
+        throw new AttributeException(type);
     }
 
     @Override
@@ -37,12 +28,7 @@ public enum Primitive implements Node {
     }
 
     @Override
-    public com.meti.api.collect.stream.Stream<Attribute.Type> apply(Attribute.Group group) throws AttributeException {
-        return List.createList(apply2(group).collect(Collectors.toList())).stream();
-    }
-
-    @Override
-    public boolean is(Type type) {
-        return type == Type.Primitive;
+    public String toString() {
+        return "\"" + name() + "\"";
     }
 }
