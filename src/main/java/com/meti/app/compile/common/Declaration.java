e28c9ca6eb919c19e69861e62eef7cbb28d00201
SirMathhman <sirmathhman@gmail.com>
2022-01-28 22:50:11 -0500

    Nuke parser

--- src/main/java/com/meti/app/compile/common/Declaration.java
+++ src/main/java/com/meti/app/compile/common/Declaration.java
@@ -5,12 +5,19 @@ import com.meti.api.collect.stream.Streams;
 import com.meti.api.json.JSONException;
 import com.meti.api.json.JSONNode;
 import com.meti.api.json.ObjectNode;
+import com.meti.app.compile.node.AbstractNode;
 import com.meti.app.compile.node.Node;
 import com.meti.app.compile.node.attribute.Attribute;
 import com.meti.app.compile.node.attribute.AttributeException;
 import com.meti.app.compile.node.attribute.NodeAttribute;
 
-public record Declaration(Node identity) implements Node {
+public final class Declaration extends AbstractNode {
+    private final Node identity;
+
+    public Declaration(Node identity) {
+        this.identity = identity;
+    }
+
     @Override
     public Stream<Attribute.Type> apply(Attribute.Group group) throws AttributeException {
         return group == Attribute.Group.Definition ? Streams.apply(Attribute.Type.Identity) : Streams.empty();
