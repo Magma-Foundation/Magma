947bd4a8ca5cc7f0bedcde14cd5b2a67391bd62d
SirMathhman <sirmathhman@gmail.com>
2022-01-31 18:20:35 -0500

    Rename tests for accuracy

--- src/test/java/com/meti/app/compile/scope/DeclarationTest.java
+++ src/test/java/com/meti/app/compile/scope/DeclarationTest.java
@@ -7,32 +7,32 @@ import static com.meti.app.compile.CompiledTest.assertSourceCompile;
 
 public class DeclarationTest {
     @Test
-    void conflicting_flags() {
+    void flag_conflict() {
         assertDoesNotCompile("const let x = 420");
     }
 
     @Test
-    void duplicate_flags() {
+    void flag_duplicate() {
         assertDoesNotCompile("const const x = 420");
     }
 
     @Test
-    void empty() {
-        assertSourceCompile("let x : I16", "int x;");
-    }
-
-    @Test
-    void implicit() {
-        assertSourceCompile("const x = 420", "int x=420;");
-    }
-
-    @Test
-    void repeated() {
+    void name_duplicate() {
         assertDoesNotCompile("const x : I16=420;const x : U16=420;");
     }
 
     @Test
-    void simple() {
+    void nominal() {
         assertSourceCompile("const x : I16 = 420", "int x=420;");
+    }
+
+    @Test
+    void type_none() {
+        assertSourceCompile("const x = 420", "int x=420;");
+    }
+
+    @Test
+    void value_none() {
+        assertSourceCompile("let x : I16", "int x;");
     }
 }
