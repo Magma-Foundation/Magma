afceedf59e94e33e357cfc3199e4bdff634403e7
SirMathhman <sirmathhman@gmail.com>
2022-02-01 23:32:57 -0500

    Rename for accuracy

--- src/test/java/com/meti/app/compile/function/FunctionTest.java
+++ src/test/java/com/meti/app/compile/function/FunctionTest.java
@@ -7,28 +7,18 @@ import static com.meti.app.compile.CompiledTest.assertSourceCompile;
 
 public class FunctionTest {
     @Test
-    void empty() {
+    void nominal() {
         assertSourceCompile("def empty() : Void => {}", "void empty(){}");
     }
 
     @Test
-    void implicit() {
-        assertSourceCompile("def empty() => {}", "void empty(){}");
+    void parameter_multiple() {
+        assertSourceCompile("def Point(x : I16, y : I16) : Void => {}", "void Point(int x,int y){}");
     }
 
     @Test
-    void implicit_return() {
-        assertSourceCompile("def value() => {return 420;}", "int value(){return 420;}");
-    }
-
-    @Test
-    void one_parameter() {
+    void parameter_single() {
         assertSourceCompile("def one_parameter(value : I16) : Void => {}", "void one_parameter(int value){}");
-    }
-
-    @Test
-    void return_type() {
-        assertSourceCompile("def test() : U16 => {return 0;}", "unsigned int test(){return 0;}");
     }
 
     @Test
@@ -37,7 +27,17 @@ public class FunctionTest {
     }
 
     @Test
-    void two_parameters() {
-        assertSourceCompile("def Point(x : I16, y : I16) : Void => {}", "void Point(int x,int y){}");
+    void type() {
+        assertSourceCompile("def test() : U16 => {return 0;}", "unsigned int test(){return 0;}");
+    }
+
+    @Test
+    void type_implicit_content() {
+        assertSourceCompile("def value() => {return 420;}", "int value(){return 420;}");
+    }
+
+    @Test
+    void type_implicit_no_content() {
+        assertSourceCompile("def empty() => {}", "void empty(){}");
     }
 }
