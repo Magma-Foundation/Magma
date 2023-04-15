1a6391b2f09cb1f945794b9d56a4f8f8897d4d18
SirMathhman <sirmathhman@gmail.com>
2023-04-15 17:25:35 -0400

    Add import compilation

--- src/main/java/com/meti/Main.java
+++ src/main/java/com/meti/Main.java
@@ -4,6 +4,9 @@ import java.io.IOException;
 import java.nio.file.*;
 import java.nio.file.attribute.BasicFileAttributes;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) throws IOException {
@@ -17,17 +20,43 @@ public class Main {
             }
         });
 
+        var target = Paths.get(".", "target");
         for (Path file : files) {
-            var relative = sourceDirectory.relativize(file);
-            var leaf = Paths.get(".", "target")
-                    .resolve(relative);
+            var relativeOriginal = sourceDirectory.relativize(file);
+            var fileName = relativeOriginal.getFileName().toString();
+            var separator= fileName.indexOf(".");
+            var fileNameWithoutExtension = fileName.substring(0, separator);
 
-            var parent = leaf.getParent();
-            if(!Files.exists(parent)) {
-                Files.createDirectories(parent);
+            Path leafDirectory;
+            var parent = relativeOriginal.getParent();
+            if (parent == null) {
+                leafDirectory = target;
+            } else {
+                leafDirectory = target.resolve(parent);
             }
 
-            Files.createFile(leaf);
+            var leaf = leafDirectory
+                    .resolve(fileNameWithoutExtension + ".mgs");
+
+            var input = Files.readString(file);
+            var lines = Arrays.stream(input.split(";"))
+                    .map(String::strip)
+                    .filter(value -> !value.isEmpty())
+                    .toList();
+
+            var output = new StringBuilder();
+            for (String line : lines) {
+                if(line.startsWith("import ")) {
+                    output.append(line).append(";\n");
+                }
+            }
+
+            var actualParent = leaf.getParent();
+            if(!Files.exists(actualParent)) {
+                Files.createDirectories(actualParent);
+            }
+
+            Files.writeString(leaf, output.toString());
         }
     }
 }
