f26a262b5713fc60cfee74eec7385cb988621564
SirMathhman <sirmathhman@gmail.com>
2023-04-16 20:39:22 -0400

    Add java.lang

---
+++ src/main/java/com/meti/RuleLexer.java
@@ -0,0 +1,12 @@
+package com.meti;
+
+import java.util.Optional;
+
+public class RuleLexer implements Lexer {
+
+
+    @Override
+    public Optional<Node> lex() {
+        return Optional.empty();
+    }
+}
