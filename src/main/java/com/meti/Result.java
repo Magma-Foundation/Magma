6e857d56d19699e97f7364c0c76e528eba62cf81
SirMathhman <sirmathhman@gmail.com>
2023-05-22 12:24:57 -0400

    Kaboom

--- src/main/java/com/meti/Result.java
+++
@@ -1,8 +0,0 @@
-package com.meti;
-
-import java.io.IOException;
-import java.util.function.Consumer;
-
-public interface Result<C> {
-    void match(C acceptValue, Consumer<IOException> acceptError);
-}
