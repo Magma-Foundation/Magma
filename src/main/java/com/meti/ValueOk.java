1d92d9c77a07e691dde58a1e4c0a5ce74f5f04e9
SirMathhman <sirmathhman@gmail.com>
2023-05-22 12:36:58 -0400

    Add missing implementation

--- src/main/java/com/meti/ValueOk.java
+++ src/main/java/com/meti/ValueOk.java
@@ -4,11 +4,14 @@ import java.io.IOException;
 import java.util.function.Consumer;
 
 public class ValueOk implements Result<Consumer<String>> {
+    private final String input;
+
     public ValueOk(String input) {
+        this.input = input;
     }
 
     @Override
     public void match(Consumer<String> onOk, Consumer<IOException> onError) {
-
+        onOk.accept(input);
     }
 }
