f98f51c127e1eb5e49190b5250b2d45a7a206b63
SirMathhman <sirmathhman@gmail.com>
2023-05-22 20:42:13 -0400

    Extract run

--- src/test/java/com/meti/ApplicationTest.java
+++ src/test/java/com/meti/ApplicationTest.java
@@ -8,7 +8,9 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.Optional;
+import java.util.Set;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
@@ -18,23 +20,38 @@ public class ApplicationTest {
     private static Path source;
     private static Path target;
 
-    private static Path resolveFile(String extension) {
-        return Paths.get(".", "Main." + extension);
+    private static Path resolveFile(String name, String extension) {
+        return Paths.get(".", name + "." + extension);
     }
 
     private static Optional<Path> run(Path source) throws IOException {
-        if (Files.exists(source)) {
-            Files.createFile(target);
-            return Optional.of(target);
-        } else {
-            return Optional.empty();
+        var sources = Set.of(source);
+        var list = run(sources);
+
+        return list.stream().findAny();
+    }
+
+    private static ArrayList<Path> run(Set<Path> sources) throws IOException {
+        var list = new ArrayList<Path>();
+        for (var path : sources) {
+            if (Files.exists(path)) {
+                var fileName = path.getFileName().toString();
+                var separator = fileName.indexOf(".");
+                var fileNameWithoutExtension = fileName.substring(0, separator);
+
+                var target = resolveFile(fileNameWithoutExtension, "mgs");
+                Files.createFile(target);
+                list.add(target);
+            }
         }
+
+        return list;
     }
 
     @BeforeEach
     void setUp() {
-        source = resolveFile("java");
-        target = resolveFile("mgs");
+        source = resolveFile("Main", "java");
+        target = resolveFile("Main", "mgs");
     }
 
     @AfterEach
