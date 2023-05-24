0d0d081ffd4130865faabed668ad14954d35cd2b
SirMathhman <sirmathhman@gmail.com>
2023-05-24 17:26:33 -0400

    Add basic target generation

--- src/test/java/com/meti/com/meti/ApplicationTest.java
+++ src/test/java/com/meti/com/meti/ApplicationTest.java
@@ -1,15 +1,49 @@
 package com.meti.com.meti;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.io.IOException;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
 
-import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class ApplicationTest {
+    private static Path source;
+    private static Path target;
+
+    private static void run() {
+        try {
+            runExceptionally();
+        } catch (IOException e) {
+            fail(e);
+        }
+    }
+
+    private static void runExceptionally() throws IOException {
+        if (Files.exists(source)) {
+            Files.createFile(target);
+        }
+    }
+
+    @BeforeEach
+    void setUp() {
+        source = Paths.get(".", "Main.java");
+        target = Paths.get(".", "Main.mgs");
+    }
+
     @Test
-    void test() {
-        assertFalse(Files.exists(Paths.get(".", "Main.mgs")));
+    void generates_target() throws IOException {
+        Files.createFile(source);
+        run();
+        assertTrue(Files.exists(target));
+    }
+
+    @Test
+    void generates_nothing() {
+        run();
+        assertFalse(Files.exists(target));
     }
 }
