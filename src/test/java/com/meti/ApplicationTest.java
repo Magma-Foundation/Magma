ee0b15f5b72b8da76fba7e25a9d41531e9ce779c
SirMathhman <sirmathhman@gmail.com>
2023-05-28 19:44:04 -0400

    Remove duplicate information

--- src/test/java/com/meti/ApplicationTest.java
+++ src/test/java/com/meti/ApplicationTest.java
@@ -45,6 +45,10 @@ public class ApplicationTest {
 
     private Optional<Path> runExceptionally() throws IOException {
         if (Files.exists(source)) {
+            var fileName = source.getFileName().toString();
+            var separator = fileName.indexOf('.');
+            var fileNameWithoutExtension = fileName.substring(0, separator);
+            var target = source.resolveSibling(fileNameWithoutExtension + ".mgs");
             Files.createFile(target);
             return Optional.of(target);
         }
