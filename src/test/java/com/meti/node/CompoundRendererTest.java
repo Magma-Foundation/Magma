ab36bdfd7d3b97833a2ab554aace6648ded94e0c
SirMathhman <sirmathhman@gmail.com>
2023-05-28 15:44:42 -0400

    Test empty RenderStages

---
+++ src/test/java/com/meti/node/CompoundRendererTest.java
@@ -0,0 +1,13 @@
+package com.meti.node;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CompoundRendererTest {
+
+    @Test
+    void render() {
+
+    }
+}
