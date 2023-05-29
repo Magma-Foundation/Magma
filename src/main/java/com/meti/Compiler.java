97bfc30c6ee0b9e23efd14f566ce008f1113609a
SirMathhman <sirmathhman@gmail.com>
2023-05-28 20:32:01 -0400

    Remove duplicates

--- src/main/java/com/meti/Compiler.java
+++ src/main/java/com/meti/Compiler.java
@@ -1,10 +1,18 @@
 package com.meti;
 
 public record Compiler(String input) {
+    static String renderMagmaImport() {
+        return "import { IOException } from java.io;";
+    }
+
+    static String renderJavaImport() {
+        return "import java.io.IOException;";
+    }
+
     String compile() {
         String output;
-        if(input.equals("import java.io.IOException;")) {
-            output = "import { IOException } from java.io;";
+        if(input.equals(renderJavaImport())) {
+            output = renderMagmaImport();
         } else {
             output = "";
         }
--- src/test/java/com/meti/ApplicationTest.java
+++ src/test/java/com/meti/ApplicationTest.java
@@ -46,7 +46,7 @@ public class ApplicationTest {
 
     @Test
     void generatesWithImport() {
-        assertCompilesIntegrably("import java.io.IOException;", "import { IOException } from java.io;");
+        assertCompilesIntegrably(Compiler.renderJavaImport(), Compiler.renderMagmaImport());
     }
 
     @Test
