3a3cae48252e9cd33ede5a697933ab401f4a3c78
SirMathhman <sirmathhman@gmail.com>
2023-05-28 21:05:16 -0400

    Merge children with same parent

--- src/main/java/com/meti/Compiler.java
+++ src/main/java/com/meti/Compiler.java
@@ -1,6 +1,10 @@
 package com.meti;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 public record Compiler(String input) {
 
@@ -16,13 +20,24 @@ public record Compiler(String input) {
 
     String compile() {
         var lines = input.split(";");
-        var builder = new StringBuilder();
-        for (int i = 0; i < lines.length; i++) {
-            var line = lines[i];
-            var compiled = compileLine(line);
-            builder.append(compiled);
+        var cache = new HashMap<String, List<String>>();
 
-            if (lines.length > 1 && i == lines.length - 2) {
+        var builder = new StringBuilder();
+        for (String line : lines) {
+            compileLine(line, cache);
+        }
+
+        var namespaces = cache.keySet()
+                .stream()
+                .sorted()
+                .toList();
+
+        for (int i = 0; i < namespaces.size(); i++) {
+            var namespace = namespaces.get(i);
+            var joinedNames = String.join(", ", cache.get(namespace));
+            builder.append(renderMagmaImport1(joinedNames, List.of(namespace.split("\\."))));
+
+            if (namespaces.size() > 1 && i == namespaces.size() - 2) {
                 builder.append("\n");
             }
         }
@@ -30,17 +45,26 @@ public record Compiler(String input) {
         return builder.toString();
     }
 
-    private String compileLine(String line) {
+    private void compileLine(String line, Map<String, List<String>> cache) {
         if (line.startsWith(PREFIX)) {
             var args = line.substring(PREFIX.length());
             var argsList = List.of(args.split("\\."));
 
             var name = argsList.get(argsList.size() - 1);
             var namespace = argsList.subList(0, argsList.size() - 1);
-            var renderedNamespace = String.join(".", namespace);
-            return renderMagmaImport(renderedNamespace, name);
+
+            var namespaceString = String.join(".", namespace);
+            if (!cache.containsKey(namespaceString)) {
+                cache.put(namespaceString, new ArrayList<>());
+            }
+            cache.get(namespaceString).add(name);
         } else {
             throw new IllegalArgumentException(line);
         }
+    }
+
+    private static String renderMagmaImport1(String name, List<String> namespace) {
+        var renderedNamespace = String.join(".", namespace);
+        return renderMagmaImport(renderedNamespace, name);
     }
 }
