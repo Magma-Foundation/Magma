7b3b8ce313651b7c0c8bc1a0c6695d127e3b1d0b
SirMathhman <sirmathhman@gmail.com>
2023-05-30 20:05:19 -0400

    Add basics to Main

--- src/main/java/com/meti/Main.java
+++ src/main/java/com/meti/Main.java
@@ -1,4 +1,15 @@
 package com.meti;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
 public class Main {
+    public static void main(String[] args) {
+        try {
+            Files.writeString(Paths.get(".", "Main.mgs"), "");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
