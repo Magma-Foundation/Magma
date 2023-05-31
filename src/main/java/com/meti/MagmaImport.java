379c921f2dd05566cd686d9856fa15e688005079
SirMathhman <sirmathhman@gmail.com>
2023-05-30 20:03:28 -0400

    Kaboom

--- src/main/java/com/meti/MagmaImport.java
+++
@@ -1,9 +0,0 @@
-package com.meti;
-
-public record MagmaImport(Node segment) implements Node {
-    @Override
-    public String render() {
-        var s = segment().render();
-        return "import " + s + ";";
-    }
-}
--- src/main/java/com/meti/Package.java
+++
@@ -1,9 +0,0 @@
-package com.meti;
-
-import java.util.List;
-
-public interface Package {
-    List<String> computeNamespace();
-
-    String computeName();
-}
