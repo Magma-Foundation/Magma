29499eb57accfbcf8502cf0d34a1647c004ebafb
SirMathhman <sirmathhman@gmail.com>
2023-06-01 14:08:51 -0400

    Generate target

--- src/test/java/com/meti/ApplicationTest.java
+++ src/test/java/com/meti/ApplicationTest.java
@@ -1,15 +1,45 @@
 package com.meti;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.io.IOException;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class ApplicationTest {
+
+    private Path target;
+    private Path source;
+
+    @BeforeEach
+    void setUp() {
+        target = Paths.get(".", "Main.mgs");
+        source = Paths.get(".", "Main.java");
+    }
+
+    @Test
+    void generatesTarget() throws IOException {
+        Files.createFile(source);
+        if (Files.exists(source)) {
+            Files.createFile(target);
+        }
+        assertTrue(Files.exists(target));
+    }
+
+    @AfterEach
+    void tearDown() throws IOException {
+        Files.deleteIfExists(target);
+        Files.deleteIfExists(source);
+    }
+
     @Test
     void generatesNothing() {
-        assertFalse(Files.exists(Paths.get(".", "Main.java")));
+        assertFalse(Files.exists(target));
     }
 }
