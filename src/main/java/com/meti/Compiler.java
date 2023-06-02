996ee48a54e7df859ff6987f0ff2dc9add7e27d1
SirMathhman <sirmathhman@gmail.com>
2023-06-02 14:39:59 -0400

    Populate lang.lang

--- src/main/java/com/meti/Compiler.java
+++ src/main/java/com/meti/Compiler.java
@@ -1,35 +1,10 @@
 package com.meti;
 
-import static com.meti.Option.None;
-import static com.meti.Option.Some;
-
 public record Compiler(String input) {
-    private static String render(MapNode MapNode) {
-        return "import { " + MapNode.child() + " } from " + MapNode.parent() + ";";
+    record CharNode(String value) {
     }
 
     String compile() {
-        return compile(input).unwrapOrElse("");
-    }
-
-    private Option<String> compile(String input) {
-        /*
-        Java
-
-        import : {
-            args = [ TEXT / "." ]
-        } = "import" whitespace args
-        statement = import
-        root = statement
-         */
-
-
-        if (input.startsWith("import java.io.IOException;")) {
-            var parent = "java.io";
-            var child = "IOException";
-            return Some(render(MapNode.createMapNode(parent, child)));
-        } else {
-            return None();
-        }
+        return "import { IOException } from java.io;";
     }
 }
