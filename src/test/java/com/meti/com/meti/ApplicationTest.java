76f38fe35118ab53740eb33d3efb715b669b2413
SirMathhman <sirmathhman@gmail.com>
2023-06-14 22:32:44 -0400

    Add integers

---
+++ src/test/java/com/meti/com/meti/ApplicationTest.java
@@ -0,0 +1,12 @@
+package com.meti.com.meti;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class ApplicationTest {
+    @Test
+    void integer() {
+        assertEquals("5", "5");
+    }
+}
