77aaa8099f4ca41c8063c262813ef074a3f0d21e
SirMathhman <sirmathhman@gmail.com>
2023-06-19 17:28:17 -0400

    Add integers

--- src/test/java/com/meti/ApplicationTest.java
+++ src/test/java/com/meti/ApplicationTest.java
@@ -8,17 +8,32 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class ApplicationTest {
     private static String compile(String input) {
-        return "";
+        try {
+            Integer.parseInt(input);
+            return input;
+        } catch (NumberFormatException e) {
+            return "";
+        }
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"0", "1", "2"})
+    void integer(String input) {
+        assertInterpret(input, input);
     }
 
     @ParameterizedTest
     @ValueSource(strings = {" ", "\t", "\n"})
     void whitespace(String input) {
-        assertEquals("", compile(input));
+        assertInterpret(input, "");
+    }
+
+    private static void assertInterpret(String input, String output) {
+        assertEquals(output, compile(input));
     }
 
     @Test
     void empty() {
-        assertEquals("", compile(""));
+        assertInterpret("", "");
     }
 }
