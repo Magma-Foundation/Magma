0c0cea9f1210975a0f90f48a48730dba833ef182
SirMathhman <sirmathhman@gmail.com>
2023-06-19 17:26:26 -0400

    Add whitespace

--- src/test/java/com/meti/ApplicationTest.java
+++ src/test/java/com/meti/ApplicationTest.java
@@ -1,13 +1,24 @@
 package com.meti;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class ApplicationTest {
+    private static String compile(String input) {
+        return "";
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {" ", "\t", "\n"})
+    void whitespace(String input) {
+        assertEquals("", compile(input));
+    }
+
     @Test
     void empty() {
-        assertEquals("", "");
+        assertEquals("", compile(""));
     }
 }
