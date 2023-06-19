2523f49b02c2782e4a2f65f893aa92c1eb67a8d1
SirMathhman <sirmathhman@gmail.com>
2023-06-19 17:24:58 -0400

    Add empty assertion

---
+++ src/test/java/com/meti/ApplicationTest.java
@@ -0,0 +1,13 @@
+package com.meti;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class ApplicationTest {
+    @Test
+    void empty() {
+        assertEquals("", "");
+    }
+}
