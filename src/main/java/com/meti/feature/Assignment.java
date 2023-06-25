7fd216b64c6b70e21955441e38824f834deb328d
SirMathhman <sirmathhman@gmail.com>
2023-06-25 16:35:43 -0400

    Extract Parser

---
+++ src/main/java/com/meti/feature/Assignment.java
@@ -0,0 +1,6 @@
+package com.meti.feature;
+
+import com.meti.safe.NativeString;
+
+public record Assignment(NativeString name, NativeString value) {
+}
--- src/main/java/com/meti/feature/AssignmentActor.java
+++ src/main/java/com/meti/feature/AssignmentActor.java
@@ -1,11 +1,8 @@
 package com.meti.feature;
 
 import com.meti.InterpretationError;
-import com.meti.Resolver;
 import com.meti.safe.NativeString;
 import com.meti.safe.option.Option;
-import com.meti.safe.result.Err;
-import com.meti.safe.result.Ok;
 import com.meti.safe.result.Result;
 import com.meti.state.State;
 
@@ -16,16 +13,7 @@ public record AssignmentActor(State state, NativeString input) implements Actor 
             var split = input.splitExcludingAt(equals);
             var name = split.left();
             var value = split.right();
-            return state.mapStack(stack -> stack.mapDefinition(name, definition -> {
-                var actualType = new Resolver(value).resolve();
-                if (definition.type().equalsTo(actualType)) {
-                    return Ok.apply(definition.withValue(value));
-                } else {
-                    var format = "Type mismatch of '%s'. Expected '%s' but was actually '%s'.";
-                    var message = format.formatted(name, definition.type().internalValue(), actualType.internalValue());
-                    return new Err<>(new InterpretationError(message));
-                }
-            }));
+            return new Parser(state, new Assignment(name, value)).parse();
         });
     }
 }
