4a57e8bd990b2f2e14496fc5f12d57382f094afb
SirMathhman <sirmathhman@gmail.com>
2023-06-26 16:03:12 -0400

    Extract DefinitionLexer

--- src/main/java/com/meti/feature/definition/DefinitionActor.java
+++ src/main/java/com/meti/feature/definition/DefinitionActor.java
@@ -1,7 +1,6 @@
 package com.meti.feature.definition;
 
 import com.meti.InterpretationError;
-import com.meti.Resolver;
 import com.meti.feature.Actor;
 import com.meti.safe.NativeString;
 import com.meti.safe.option.Option;
@@ -9,30 +8,14 @@ import com.meti.safe.result.Result;
 import com.meti.state.State;
 
 public record DefinitionActor(State state, NativeString input) implements Actor {
-    private static Option<ImplicitDefinition> lex(NativeString range) {
-        return range.firstIndexOfChar('=').map(valueSeparator -> {
-            var split = range.splitExcludingAt(valueSeparator);
-            var definition = split.left().strip();
-            var value = split.right().strip();
-            var expectedType = new Resolver(value).resolve();
-
-            return definition.firstIndexOfChar(':').map(definition::splitExcludingAt).<ImplicitDefinition>map(tuple -> {
-                var name = tuple.left().strip();
-                var actualType = tuple.right().strip();
-
-                return new ExplicitDefinition(name, actualType, value);
-            }).unwrapOrElseGet(() -> new ImplicitDefinition(definition, value));
-        });
-    }
 
     @Override
     public Option<Result<State, InterpretationError>> act() {
         // let x : u64 = 10
         // x : u64 = 10
-        return input.firstIndexAfterSlice(NativeString.from("let"))
-                .map(input::sliceFrom)
-                .flatMap(DefinitionActor::lex)
+        return new DefinitionLexer(input).lex()
                 .map(result -> new DefinitionParser(state, result).parse())
                 .flatMap(value -> value);
     }
+
 }
---
+++ src/main/java/com/meti/feature/definition/DefinitionLexer.java
@@ -0,0 +1,27 @@
+package com.meti.feature.definition;
+
+import com.meti.feature.Node;
+import com.meti.safe.NativeString;
+import com.meti.safe.option.Option;
+
+public record DefinitionLexer(NativeString input) {
+    static Option<Node> lexHelper(NativeString range) {
+        return range.firstIndexOfChar('=').map(valueSeparator -> {
+            var split = range.splitExcludingAt(valueSeparator);
+            var definition = split.left().strip();
+            var value = split.right().strip();
+            return definition.firstIndexOfChar(':').map(definition::splitExcludingAt).<ImplicitDefinition>map(tuple -> {
+                var name = tuple.left().strip();
+                var actualType = tuple.right().strip();
+
+                return new ExplicitDefinition(name, actualType, value);
+            }).unwrapOrElseGet(() -> new ImplicitDefinition(definition, value));
+        });
+    }
+
+    Option<Node> lex() {
+        return input().firstIndexAfterSlice(NativeString.from("let"))
+                .map(input()::sliceFrom)
+                .flatMap(DefinitionLexer::lexHelper);
+    }
+}
