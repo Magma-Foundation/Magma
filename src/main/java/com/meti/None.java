6a00386c5b2edcad32ea34c1ce77cdffc02144c0
SirMathhman <sirmathhman@gmail.com>
2023-07-08 16:45:08 -0400

    Add missing implementations

--- src/main/java/com/meti/None.java
+++ src/main/java/com/meti/None.java
@@ -1,8 +1,25 @@
 package com.meti;
 
+import java.util.function.Function;
+
 class None<T> implements Option<T> {
     @Override
     public T unwrapOrPanic() {
         throw new RuntimeException("No value present.");
+    }
+
+    @Override
+    public boolean isPresent() {
+        return false;
+    }
+
+    @Override
+    public <R> Option<R> map(Function<T, R> mapper) {
+        return new None<>();
+    }
+
+    @Override
+    public T unwrapOrElse(T other) {
+        return other;
     }
 }
--- src/main/java/com/meti/Some.java
+++ src/main/java/com/meti/Some.java
@@ -1,8 +1,25 @@
 package com.meti;
 
+import java.util.function.Function;
+
 record Some<T>(T value) implements Option<T> {
     @Override
     public T unwrapOrPanic() {
+        return value;
+    }
+
+    @Override
+    public boolean isPresent() {
+        return true;
+    }
+
+    @Override
+    public <R> Option<R> map(Function<T, R> mapper) {
+        return new Some<>(mapper.apply(value));
+    }
+
+    @Override
+    public T unwrapOrElse(T other) {
         return value;
     }
 }
