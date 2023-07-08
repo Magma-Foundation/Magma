ea68fb70d5c243622bf93d42dfbc31bedba342aa
SirMathhman <sirmathhman@gmail.com>
2023-07-08 16:21:20 -0400

    Add tearDown

--- src/test/java/com/meti/ApplicationTest.java
+++ src/test/java/com/meti/ApplicationTest.java
@@ -1,15 +1,43 @@
 package com.meti;
 
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 
+import java.io.IOException;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class ApplicationTest {
+
+    private final Path source = Paths.get(".", "Main.java");
+    private final Path target = Paths.get(".", "Main.mgs");
+
+    @AfterEach
+    void tearDown() throws IOException {
+        Files.deleteIfExists(source);
+        Files.deleteIfExists(target);
+    }
+
     @Test
-    void generatesNothing() {
-        assertFalse(Files.exists(Paths.get(".", "Main.mgs")));
+    void generatesTarget() throws IOException {
+        Files.createFile(source);
+        run();
+        assertTrue(Files.exists(target));
+    }
+
+    private void run() throws IOException {
+        if (Files.exists(source)) {
+            Files.createFile(target);
+        }
+    }
+
+    @Test
+    void generatesNothing() throws IOException {
+        run();
+        assertFalse(Files.exists(target));
     }
 }
