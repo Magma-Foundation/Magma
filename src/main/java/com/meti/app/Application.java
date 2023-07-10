1e23b77348fc3bcec3b05fcb48a683380e4689c9
SirMathhman <sirmathhman@gmail.com>
2023-07-10 13:48:13 -0400

    Extract Compiler

--- src/main/java/com/meti/app/Application.java
+++ src/main/java/com/meti/app/Application.java
@@ -1,11 +1,10 @@
 package com.meti.app;
 
-import com.meti.core.*;
-import com.meti.iterate.Iterators;
+import com.meti.core.Result;
+import com.meti.core.Results;
+import com.meti.core.ThrowableResult;
 import com.meti.iterate.ResultIterator;
-import com.meti.java.*;
-
-import static com.meti.core.Results.$Result;
+import com.meti.java.JavaSet;
 
 public final class Application {
     private final Sources sources;
@@ -14,32 +13,6 @@ public final class Application {
     public Application(Sources sources, Targets targets) {
         this.sources = sources;
         this.targets = targets;
-    }
-
-    private static Result<Option<JavaString>, CompileException> compile(JavaString line) {
-        if (line.startsWith("package ")) return Ok.apply(new None<>());
-        return line.firstIndexOfSlice("import ").map(index -> {
-            var collect = index.nextExclusive("import ".length()).map(s -> line.sliceFrom(s)
-                    .split("\\.")
-                    .collect(JavaList.asList())).unwrapOrElse(JavaList.empty());
-
-            return collect.into(NonEmptyJavaList::from).map(names -> $Result(() -> {
-                var last = names.lastIndex();
-                var before = names.sliceTo(last);
-                var after = names.sliceFrom(last)
-                        .into(NonEmptyJavaList::from)
-                        .map(NonEmptyList::first)
-                        .map(Ok::<JavaString, CompileException>apply)
-                        .unwrapOrElse(new Err<>(new CompileException("No name present in import.")))
-                        .into(ThrowableResult::new)
-                        .$();
-
-                return before.iter().collect(JavaString.joining(new JavaString("."))).map(parent -> parent
-                        .prepend(" } from ")
-                        .prependOwned(after)
-                        .prepend("import { "));
-            })).unwrapOrElse(new Err<>(new CompileException("Insufficient values.")));
-        }).unwrapOrElse(Ok.apply(new Some<>(line)));
     }
 
     Result<JavaSet<NIOTarget>, CompileException> compileAll() {
@@ -60,13 +33,10 @@ public final class Application {
                     .mapErr(CompileException::new)
                     .into(ThrowableResult::new).$();
 
-            var output = input.split(";")
-                    .map(Application::compile)
-                    .into(ResultIterator::new)
-                    .flatMapInner(Iterators::fromOption)
-                    .collectToResult(JavaString.joining(new JavaString(";\n")))
-                    .into(ThrowableResult::new).$()
-                    .unwrapOrElse(JavaString.empty());
+            var output = new Compiler(input)
+                    .compile()
+                    .into(ThrowableResult::new)
+                    .$();
 
             var target = targets.resolve(package_, other)
                     .mapErr(CompileException::new)
