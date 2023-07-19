beb3942c00064800694c9b4b4a1d03a35e6141e3
SirMathhman <sirmathhman@gmail.com>
2023-07-19 17:56:15 -0400

    ???

--- src/main/java/com/meti/app/Import.java
+++ src/main/java/com/meti/app/Import.java
@@ -5,7 +5,7 @@ import com.meti.java.List;
 import com.meti.java.NonEmptyJavaList;
 import com.meti.java.String_;
 
-record Import(String_ value, List<Import> children) {
+public record Import(String_ value, List<Import> children) {
     public Import(String_ value) {
         this(value, JavaList.empty());
     }
@@ -15,22 +15,18 @@ record Import(String_ value, List<Import> children) {
     }
 
     public Import addPath(List<String_> path) {
-        return path.into(NonEmptyJavaList::from)
-                .map(list -> {
-                    var currentValue = list.first();
-                    var existingChild = children.iter()
-                            .filter(node -> node.value().equals(currentValue))
-                            .head();
+        return path.into(NonEmptyJavaList::from).map(list -> {
+            var currentValue = list.first();
+            var existingChild = children.iter()
+                    .filter(node -> node.value().equals(currentValue))
+                    .head();
 
-                    var newNode = existingChild
-                            .unwrapOrElseGet(() -> new Import(currentValue))
-                            .addPath(list.sliceWithoutFirst());
+            var newNode = existingChild
+                    .unwrapOrElseGet(() -> new Import(currentValue))
+                    .addPath(list.sliceWithoutFirst());
 
-                    return addChild(newNode);
-                })
-                .unwrapOrElseGet(() -> {
-                    return this;
-                });
+            return addChild(newNode);
+        }).unwrapOrElseGet(() -> this);
     }
 }
 
