613f661d7c8e6f150f6d9a92ae8eb02fa54c4880
SirMathhman <sirmathhman@gmail.com>
2023-07-21 12:44:51 -0400

    Remove packages

--- src/test/java/com/meti/app/CompilerTest.java
+++ src/test/java/com/meti/app/CompilerTest.java
@@ -1,15 +1,31 @@
 package com.meti.app;
 
+import com.meti.java.String_;
 import org.junit.jupiter.api.Test;
 
 import static com.meti.core.Results.unwrap;
 import static com.meti.java.JavaString.Empty;
+import static com.meti.java.JavaString.fromSlice;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
 
 class CompilerTest {
+    private static void assertCompile(String_ input) {
+        try {
+            var actual = unwrap(new Compiler(input).compile());
+            assertEquals("", actual.unwrap());
+        } catch (CompileException e) {
+            fail(e);
+        }
+    }
+
     @Test
-    void empty() throws CompileException {
-        var actual = unwrap(new Compiler(Empty).compile());
-        assertEquals("", actual.unwrap());
+    void package_() {
+        assertCompile(fromSlice("package test"));
+    }
+
+    @Test
+    void empty() {
+        assertCompile(Empty);
     }
 }
