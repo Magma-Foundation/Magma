024f4ec8d5d24a786093b5f663efdbe49fe8ba52
SirMathhman <sirmathhman@gmail.com>
2023-07-23 16:26:08 -0400

    Simplify

--- src/main/java/com/meti/app/compile/function/MethodLexer.java
+++ src/main/java/com/meti/app/compile/function/MethodLexer.java
@@ -11,7 +11,9 @@ import static com.meti.core.Options.$Option;
 import static com.meti.java.JavaString.fromSlice;
 
 public record MethodLexer(String_ line) implements Lexer {
-    private Option<Node> lex1() {
+
+    @Override
+    public Option<Result<Node, CompileException>> lex() {
         return $Option(() -> {
             var paramStart = line.firstIndexOfChar('(').$();
             var paramEnd = line.firstIndexOfChar(')').$();
@@ -30,28 +32,23 @@ public record MethodLexer(String_ line) implements Lexer {
             var type = keys.last();
             var name = args.last();
 
-            var paramString = line().sliceBetween(paramStart.nextExclusive().$().to(paramEnd).$());
+            var paramString = this.line().sliceBetween(paramStart.nextExclusive().$().to(paramEnd).$());
             var parameters = paramString.split(",")
                     .map(String_::strip)
                     .filter(value -> !value.isEmpty())
                     .map(Content::new)
                     .collect(JavaSet.asSet());
 
-            var bodyStart = line().firstIndexOfChar('{').$();
-            var body = line().sliceFrom(bodyStart);
+            var bodyStart = this.line().firstIndexOfChar('{').$();
+            var body = this.line().sliceFrom(bodyStart);
             var node = new Content(body);
 
-            return new MapNode(JavaMap.<String_, Attribute>empty()
+            return Ok.apply(new MapNode(JavaMap.<String_, Attribute>empty()
                     .insert(fromSlice("keywords"), new StringSetAttribute(keywords))
                     .insert(fromSlice("name"), new StringAttribute(name))
                     .insert(fromSlice("parameters"), new NodeSetAttribute(parameters))
                     .insert(fromSlice("body"), new NodeAttribute(node))
-                    .insert(fromSlice("returns"), new NodeAttribute(new Content(type))));
+                    .insert(fromSlice("returns"), new NodeAttribute(new Content(type)))));
         });
-    }
-
-    @Override
-    public Option<Result<Node, CompileException>> lex() {
-        return lex1().map(Ok::apply);
     }
 }
