927825865f69be3ade98f1cf6aa5291df4a53e04
SirMathhman <sirmathhman@gmail.com>
2023-07-23 15:20:18 -0400

    Add NodeSetAttribute

--- src/main/java/com/meti/app/Attribute.java
+++ src/main/java/com/meti/app/Attribute.java
@@ -4,9 +4,14 @@ import com.meti.app.compile.Node;
 import com.meti.core.None;
 import com.meti.core.Option;
 import com.meti.java.List;
+import com.meti.java.Set;
 import com.meti.java.String_;
 
 public interface Attribute {
+    default Option<Set<? extends Node>> asSetOfNodes() {
+        return None.apply();
+    }
+
     default Option<List<? extends Node>> asListOfNodes() {
         return None.apply();
     }
---
+++ src/main/java/com/meti/app/compile/NodeSetAttribute.java
@@ -0,0 +1,13 @@
+package com.meti.app.compile;
+
+import com.meti.app.Attribute;
+import com.meti.core.Option;
+import com.meti.core.Some;
+import com.meti.java.Set;
+
+public record NodeSetAttribute(Set<? extends Node> values) implements Attribute {
+    @Override
+    public Option<Set<? extends Node>> asSetOfNodes() {
+        return Some.apply(values);
+    }
+}
