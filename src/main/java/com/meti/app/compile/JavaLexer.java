bae8f6a285899ef85ce951e1d0ef0fb49bdfc2d3
SirMathhman <sirmathhman@gmail.com>
2023-08-04 13:12:22 -0400

    Extract ListLexers

--- src/main/java/com/meti/app/compile/JavaLexer.java
+++ src/main/java/com/meti/app/compile/JavaLexer.java
@@ -10,21 +10,26 @@ import com.meti.core.Result;
 import com.meti.iterate.Iterators;
 import com.meti.iterate.ResultIterator;
 import com.meti.java.JavaList;
+import com.meti.java.List;
 import com.meti.java.String_;
 
 public record JavaLexer(String_ line) implements Lexer {
     @Override
     public Option<Result<Node, CompileException>> lex() {
-        return JavaList.<Lexer>of(
-                        new ClassLexer(line()),
-                        new BlockLexer(line()),
-                        new MethodLexer(line()),
-                        new DeclarationLexer(line()),
-                        new ImportLexer(line()))
+        return listLexers()
                 .iter()
                 .map(Lexer::lex)
                 .flatMap(Iterators::fromOption)
                 .into(ResultIterator::new)
                 .head();
+    }
+
+    private List<Lexer> listLexers() {
+        return JavaList.of(
+                new ClassLexer(line),
+                new BlockLexer(line),
+                new MethodLexer(line),
+                new DeclarationLexer(line),
+                new ImportLexer(line));
     }
 }
