03ecc54f6f876e1121da609d13700f619ddbdceb
SirMathhman <sirmathhman@gmail.com>
2023-08-05 21:22:49 -0400

    Add public keyword

--- src/test/java/com/meti/app/feature/InterfaceTest.java
+++ src/test/java/com/meti/app/feature/InterfaceTest.java
@@ -4,6 +4,11 @@ import org.junit.jupiter.api.Test;
 
 public class InterfaceTest extends CompiledTest {
     @Test
+    void public_() {
+        assertCompile("public interface Test {}", "trait Test {}");
+    }
+
+    @Test
     void generic() {
         assertCompile("interface Test<T> {}", "trait Test<T>{}");
     }
