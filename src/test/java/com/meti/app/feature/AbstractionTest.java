c1fd519eddf3607fca8923cfa4aad53dcd59a3cf
SirMathhman <sirmathhman@gmail.com>
2023-08-05 21:28:00 -0400

    Validate abstraction parameters

--- src/test/java/com/meti/app/feature/AbstractionTest.java
+++ src/test/java/com/meti/app/feature/AbstractionTest.java
@@ -4,6 +4,11 @@ import org.junit.jupiter.api.Test;
 
 public class AbstractionTest extends CompiledTest {
     @Test
+    void parameter() {
+        assertCompile("void test(int value);", "def test(value : I16) : Void");
+    }
+
+    @Test
     void test() {
         assertCompile("void test();", "def test() : Void");
     }
