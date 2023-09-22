19f6aa3cc56fa6a56d9e819e6ec5b5271c68fc38
SirMathhman <sirmathhman@gmail.com>
2023-09-22 16:07:53 -0400

    Copy source files to Dist

--- magmac/src/Main.java
+++ magmac/src/Main.java
@@ -1,14 +1,34 @@
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) {
-        try (var list = Files.list(Paths.get(".", "magmac", "src"))) {
-            System.out.println(list.collect(Collectors.toSet()));
+        var root = Paths.get(".", "magmac");
+        var source = root.resolve("src");
+        var dist = root.resolve("dist");
+
+        if (!Files.exists(dist)) {
+            try {
+                Files.createDirectories(dist);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        try (var list = Files.list(source)) {
+            list.forEach(file -> {
+                try {
+                    var content = Files.readString(file);
+                    var relative = source.relativize(file);
+                    var outputFile = dist.resolve(relative);
+                    Files.writeString(outputFile, content);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            });
         } catch (IOException e) {
-            throw new RuntimeException(e);
+            e.printStackTrace();
         }
     }
 }
