9e47ca8568340609dae487f1d7a24b6e78aaa6c2
SirMathhman <sirmathhman@gmail.com>
2023-09-22 16:14:21 -0400

    Cleanup imports

--- magmac/src/Main.java
+++ magmac/src/Main.java
@@ -1,6 +1,8 @@
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Main {
     public static void main(String[] args) {
@@ -19,10 +21,25 @@ public class Main {
         try (var list = Files.list(source)) {
             list.forEach(file -> {
                 try {
-                    var content = Files.readString(file);
+                    var input = Files.readString(file);
+                    var output = Stream.of(input.split(";"))
+                            .map(line -> {
+                                var stripped = line.strip();
+                                if (stripped.startsWith("import ")) {
+                                    var name = stripped.substring("import ".length());
+                                    var separator = name.lastIndexOf('.');
+                                    var parent = name.substring(0, separator);
+                                    var child = name.substring(separator + 1);
+                                    return "import { " + child + " } from " + parent + ";\n";
+                                } else {
+                                    return line + ";";
+                                }
+                            })
+                            .collect(Collectors.joining());
+
                     var relative = source.relativize(file);
                     var outputFile = dist.resolve(relative);
-                    Files.writeString(outputFile, content);
+                    Files.writeString(outputFile, output);
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
