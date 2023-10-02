0f4f0792a15632842fbad02d75281cfa3f40face
SirMathhman <SirMathhman@gmail.com>
2023-10-02 15:55:44 -0400

    Prepare for writing interfaces

--- magmac/src/com/meti/compile/JavaLexer.java
+++ magmac/src/com/meti/compile/JavaLexer.java
@@ -1,9 +1,11 @@
 package com.meti.compile;
 
+import com.meti.api.collect.Index;
 import com.meti.api.collect.Iterator;
 import com.meti.api.collect.JavaString;
 import com.meti.api.iterate.Iterators;
 import com.meti.api.option.Option;
+import com.meti.api.option.Options;
 import com.meti.compile.block.BlockLexer;
 import com.meti.compile.clazz.ClassLexer;
 import com.meti.compile.function.RecordLexer;
@@ -13,6 +15,18 @@ import com.meti.compile.package_.PackageLexer;
 public record JavaLexer(JavaString stripped) implements Lexer {
     static Iterator<Lexer> enumerateLexers(JavaString stripped) {
         return Iterators.from(
+                new Lexer() {
+                    @Override
+                    public Option<Node> lex() {
+                        return Options.$Option(() -> {
+                            var nameStart = stripped.firstIndexOfChar('<').$();
+                            var keywordEnd = stripped.firstIndexOfSlice("interface ").$()
+                                    .nextBy("interface ".length()).$();
+                            var name = stripped.sliceBetween(keywordEnd.to(nameStart).$()).strip();
+                            return new MapNode(new JavaString("interface"));
+                        });
+                    }
+                },
                 new PackageLexer(stripped),
                 new ImportLexer(stripped),
                 new BlockLexer(stripped),
--- magmac/src/com/meti/compile/MapNode.java
+++ magmac/src/com/meti/compile/MapNode.java
@@ -2,7 +2,7 @@ package com.meti.compile;
 
 import com.meti.api.collect.JavaString;
 
-public record MapNode(JavaString name) implements Node{
+public record MapNode(JavaString name) implements Node {
     @Override
     public boolean is(String name) {
         return this.name.equalsToSlice(name);
