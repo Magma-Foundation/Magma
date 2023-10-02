d02be2321c55effbbee091f3077b5cdd4f959f29
SirMathhman <sirmathhman@gmail.com>
2023-10-02 18:31:23 -0400

    Implement AbstractIterator.foldRight

--- magmac/src/com/meti/api/collect/AbstractIterator.java
+++ magmac/src/com/meti/api/collect/AbstractIterator.java
@@ -5,13 +5,32 @@ import com.meti.api.option.None;
 import com.meti.api.option.Option;
 import com.meti.api.option.Some;
 
+import java.util.function.BiFunction;
 import java.util.function.Function;
 import java.util.function.Predicate;
 
 public abstract class AbstractIterator<T> implements Iterator<T> {
     @Override
+    public <R> R foldRight(R initial, BiFunction<R, T, R> mapper) {
+        var current = initial;
+
+        while (true) {
+            R finalCurrent = current;
+            var tuple = head()
+                    .map(head -> mapper.apply(finalCurrent, head))
+                    .unwrapToTuple(current);
+
+            if (tuple.a()) {
+                current = tuple.b();
+            } else {
+                return current;
+            }
+        }
+    }
+
+    @Override
     public <R> Iterator<R> map(Function<T, R> mapper) {
-        return new AbstractIterator<R>() {
+        return new AbstractIterator<>() {
             @Override
             public Option<R> head() {
                 return AbstractIterator.this.head().map(mapper);
@@ -29,47 +48,32 @@ public abstract class AbstractIterator<T> implements Iterator<T> {
 
     @Override
     public <R> Iterator<R> flatMap(Function<T, Iterator<R>> mapper) {
-        return head().map(mapper).<Iterator<R>>map(initial -> {
-            return new AbstractIterator<R>() {
-                private Iterator<R> current = initial;
+        return head().map(mapper).<Iterator<R>>map(initial -> new AbstractIterator<>() {
+            private Iterator<R> current = initial;
 
-                @Override
-                public Option<R> head() {
-                    while (true) {
-                        var head = current.head();
-                        if (head.isPresent()) {
-                            return head;
+            @Override
+            public Option<R> head() {
+                while (true) {
+                    var head = current.head();
+                    if (head.isPresent()) {
+                        return head;
+                    } else {
+                        var tuple = AbstractIterator.this.head()
+                                .map(mapper)
+                                .unwrapToTuple(Iterators.empty());
+                        if (tuple.a()) {
+                            this.current = tuple.b();
                         } else {
-                            var tuple = AbstractIterator.this.head()
-                                    .map(mapper)
-                                    .unwrapToTuple(Iterators.empty());
-                            if (tuple.a()) {
-                                this.current = tuple.b();
-                            } else {
-                                return None.apply();
-                            }
+                            return None.apply();
                         }
                     }
                 }
-            };
+            }
         }).unwrapOrElse(Iterators.empty());
     }
 
     @Override
     public <R> R collect(Collector<T, R> collector) {
-        var current = collector.initial();
-
-        while (true) {
-            R finalCurrent = current;
-            var tuple = head()
-                    .map(head -> collector.fold(finalCurrent, head))
-                    .unwrapToTuple(current);
-
-            if (tuple.a()) {
-                current = tuple.b();
-            } else {
-                return current;
-            }
-        }
+        return foldRight(collector.initial(), collector::fold);
     }
 }
