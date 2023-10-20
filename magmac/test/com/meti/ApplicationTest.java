c8f7e7d543c68cebd740ecd907443fc1d73c4351
SirMathhman <sirmathhman@gmail.com>
2023-10-20 18:00:00 -0400

    Generate no target

--- magmac/test/com/meti/ApplicationTest.java
+++ magmac/test/com/meti/ApplicationTest.java
@@ -1,10 +1,25 @@
 package com.meti;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-public class ApplicationTest {
-    @Test
-    void test() {
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
+public class ApplicationTest {
+
+    private Path target;
+
+    @BeforeEach
+    void setUp() {
+        target = Paths.get(".", "Test.mgs");
+    }
+
+    @Test
+    void generatesNoTarget() {
+        assertFalse(Files.exists(target));
     }
 }
