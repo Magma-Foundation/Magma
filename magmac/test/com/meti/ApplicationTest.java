4300ae658a6933ce90c67ad2bbfb16071ddc8b69
SirMathhman <sirmathhman@gmail.com>
2023-10-20 18:01:16 -0400

    Add target generation test

--- magmac/test/com/meti/ApplicationTest.java
+++ magmac/test/com/meti/ApplicationTest.java
@@ -1,8 +1,10 @@
 package com.meti;
 
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -12,10 +14,27 @@ import static org.junit.jupiter.api.Assertions.assertFalse;
 public class ApplicationTest {
 
     private Path target;
+    private Path source;
 
     @BeforeEach
     void setUp() {
         target = Paths.get(".", "Test.mgs");
+        source = Paths.get(".", "Test.java");
+    }
+
+    @Test
+    void generatesTarget() throws IOException {
+        Files.createFile(source);
+        if (Files.exists(source)) {
+            Files.createFile(target);
+        }
+        Files.delete(target);
+    }
+
+    @AfterEach
+    void tearDown() throws IOException {
+        Files.deleteIfExists(source);
+        Files.deleteIfExists(target);
     }
 
     @Test
