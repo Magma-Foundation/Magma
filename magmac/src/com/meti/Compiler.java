1b38ef47c4c269e75d81016e7451b214b087e96e
SirMathhman <sirmathhman@gmail.com>
2023-10-20 18:18:00 -0400

    Extract compileLine

--- magmac/src/com/meti/Compiler.java
+++ magmac/src/com/meti/Compiler.java
@@ -4,19 +4,21 @@ import java.util.Arrays;
 import java.util.stream.Collectors;
 
 public record Compiler(String input) {
+    private static String compileLine(String lines) {
+        String output;
+        if (lines.startsWith("class ")) {
+            var bodyStart = lines.indexOf('{');
+            var className = lines.substring("class ".length(), bodyStart).strip();
+            output = "class def " + className + "() => {}";
+        } else {
+            output = "";
+        }
+        return output;
+    }
+
     String compile() {
         return Arrays.stream(input.split(";"))
-                .map(lines -> {
-                    String output;
-                    if (lines.startsWith("class ")) {
-                        var bodyStart = lines.indexOf('{');
-                        var className = lines.substring("class ".length(), bodyStart).strip();
-                        output = "class def " + className + "() => {}";
-                    } else {
-                        output = "";
-                    }
-                    return output;
-                })
+                .map(Compiler::compileLine)
                 .collect(Collectors.joining());
     }
 }
