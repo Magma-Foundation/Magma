7efba6f473072d76c3461e8abd3fc35900a4f35b
SirMathhman <sirmathhman@gmail.com>
2023-10-26 13:08:47 -0400

    Initial acceptance tests

--- magmac/test/com/meti/ApplicationTest.java
+++ magmac/test/com/meti/ApplicationTest.java
@@ -1,15 +1,50 @@
 package com.meti;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.io.IOException;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class ApplicationTest {
+
+    private Path target;
+    private Path source;
+
+    @BeforeEach
+    void setUp() {
+        target = Paths.get(".", "Index.mgs");
+        source = Paths.get(".", "Index.java");
+    }
+
+    @AfterEach
+    void tearDown() throws IOException {
+        Files.deleteIfExists(target);
+        Files.deleteIfExists(source);
+    }
+
     @Test
-    void test() {
-        assertFalse(Files.exists(Paths.get(".", "Target.mgs")));
+    void generatesTarget() throws IOException {
+        Files.createFile(source);
+        run();
+        assertTrue(Files.exists(target));
+    }
+
+    private void run() throws IOException {
+        if (Files.exists(source)) {
+            Files.createFile(target);
+        }
+    }
+
+    @Test
+    void generatesNoTarget() throws IOException {
+        run();
+        assertFalse(Files.exists(target));
     }
 }
