e5b534f5ae48b3fd8a25948edaadbb2a8395924b
SirMathhman <SirMathhman@gmail.com>
2023-10-26 15:29:16 -0400

    Clean Application

--- magmac/src/com/meti/Application.java
+++ magmac/src/com/meti/Application.java
@@ -1,11 +1,45 @@
 package com.meti;
 
+import org.jetbrains.annotations.NotNull;
+
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.Optional;
 
 public record Application(Path source) {
+    private static String compileImport(String input) throws CompileException {
+        String output;
+        var separator = input.indexOf('.');
+        if (separator == -1) {
+            throw new CompileException("Invalid import syntax.");
+        } else {
+            var parent = input.substring("import ".length(), separator).strip();
+            var child = input.substring(separator + 1).strip();
+            output = "import { %s } from %s;".formatted(child, parent);
+        }
+        return output;
+    }
+
+    private static String compile(String input) throws CompileException {
+        String output;
+        if (input.startsWith("import ")) {
+            output = compileImport(input);
+        } else {
+            output = "";
+        }
+        return output;
+    }
+
+    private static Optional<Path> writeTarget(String output, Path target) throws CompileException {
+        try {
+            Files.writeString(target, output);
+            return Optional.of(target);
+        } catch (IOException e) {
+            throw new CompileException("Failed to write target.", e);
+        }
+    }
+
     Optional<Path> run() throws CompileException {
         if (Files.exists(source())) {
             return compileSource();
@@ -14,36 +48,27 @@ public record Application(Path source) {
     }
 
     private Optional<Path> compileSource() throws CompileException {
+        var input = readSource();
+        var output = compile(input);
+        var target = resolveTarget();
+        return writeTarget(output, target);
+    }
+
+    @NotNull
+    private Path resolveTarget() {
+        var fileName = source().getFileName().toString();
+        var separator = fileName.indexOf('.');
+        var fileNameWithoutExtension = separator == -1 ? fileName : fileName.substring(0, separator);
+        return this.source().resolveSibling(fileNameWithoutExtension + ".mgs");
+    }
+
+    private String readSource() throws CompileException {
         String input;
         try {
             input = Files.readString(source);
         } catch (IOException e) {
             throw new CompileException("Failed to read source.", e);
         }
-
-        String output;
-        if (input.startsWith("import ")) {
-            var separator = input.indexOf('.');
-            if (separator == -1) {
-                throw new CompileException("Invalid import syntax.");
-            } else {
-                var parent = input.substring("import ".length(), separator).strip();
-                var child = input.substring(separator + 1).strip();
-                output = "import { %s } from %s;".formatted(child, parent);
-            }
-        } else {
-            output = "";
-        }
-
-        var fileName = source().getFileName().toString();
-        var separator = fileName.indexOf('.');
-        var fileNameWithoutExtension = separator == -1 ? fileName : fileName.substring(0, separator);
-        var target = source().resolveSibling(fileNameWithoutExtension + ".mgs");
-        try {
-            Files.writeString(target, output);
-            return Optional.of(target);
-        } catch (IOException e) {
-            throw new CompileException("Failed to write target.", e);
-        }
+        return input;
     }
 }
