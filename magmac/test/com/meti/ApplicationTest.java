07a0f2e0d1b77cb42be5bdf447b825f23ff91947
SirMathhman <sirmathhman@gmail.com>
2023-11-13 22:25:12 -0500

    Remove duplicate information

--- magmac/test/com/meti/ApplicationTest.java
+++ magmac/test/com/meti/ApplicationTest.java
@@ -46,6 +46,10 @@ public class ApplicationTest {
 
     private Optional<Path> run() throws IOException {
         if (Files.exists(source)) {
+            var fileName = source.getFileName().toString();
+            var separator = fileName.indexOf('.');
+            var name = fileName.substring(0, separator);
+            var target = source.resolveSibling(name + ".mgs");
             Files.createFile(target);
             return Optional.of(target);
         } else {
