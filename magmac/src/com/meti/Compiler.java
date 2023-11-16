df3303b20bf71314bcb003ab354b1e07e6e323e6
SirMathhman <SirMathhman@gmail.com>
2023-11-16 15:37:57 -0500

    Test abstract methods

--- magmac/src/com/meti/Compiler.java
+++ magmac/src/com/meti/Compiler.java
@@ -14,12 +14,17 @@ public record Compiler(String input) {
         String output;
         if (input.isEmpty()) {
             output = "";
-        } else {
+        } else if(input.startsWith("import ")){
             var slice = input.substring("import ".length());
             var importSeparator = slice.indexOf('.');
             var parent = slice.substring(0, importSeparator);
             var child = slice.substring(importSeparator + 1);
             output = "import { " + child + " } from " + parent + ";";
+        } else {
+            var start = input.indexOf(' ');
+            var end = input.indexOf('(');
+            var name = input.substring(start + 1, end).strip();
+            output = "def " + name + "() : Void";
         }
         return output;
     }
--- magmac/test/com/meti/CompilerTest.java
+++ magmac/test/com/meti/CompilerTest.java
@@ -1,13 +1,25 @@
 package com.meti;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class CompilerTest {
+    private static void assertCompile(String input, String output) {
+        var actual = new Compiler(input).compile();
+        assertEquals(output, actual);
+    }
+
     @Test
-    void multiple(){
-        var output = new Compiler("import a.B;import c.D").compile();
-        assertEquals("import { B } from a;import { D } from c;", output);
+    void multiple() {
+        assertCompile("import a.B;import c.D", "import { B } from a;import { D } from c;");
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"first", "second"})
+    void abstractMethod(String name) {
+        assertCompile("void " + name + "()", "def " + name + "() : Void");
     }
 }
