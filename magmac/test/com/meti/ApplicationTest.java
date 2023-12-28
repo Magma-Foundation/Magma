9f62a4b170a77ab975d31b073932354eb6031f3f
SirMathhman <sirmathhman@gmail.com>
2023-12-28 10:55:25 -0500

    Fix bug where indexOf separator was -1

--- magmac/test/com/meti/ApplicationTest.java
+++ magmac/test/com/meti/ApplicationTest.java
@@ -45,7 +45,7 @@ public class ApplicationTest {
         if (Files.exists(Source)) {
             var fileNameAsString = Source.getFileName().toString();
             var separator = fileNameAsString.indexOf('.');
-            var fileName = fileNameAsString.substring(0, separator);
+            var fileName = separator == -1 ? fileNameAsString : fileNameAsString.substring(0, separator);
             var target = Source.resolveSibling(fileName + ".mgs");
             Files.createFile(target);
             return Optional.of(target);
