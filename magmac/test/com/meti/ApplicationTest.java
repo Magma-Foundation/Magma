e7bc2fb3b7af01242f1dcdebb7ba31bf1f72e296
SirMathhman <sirmathhman@gmail.com>
2023-12-28 10:49:11 -0500

    Add basic tests

--- magmac/test/com/meti/ApplicationTest.java
+++ magmac/test/com/meti/ApplicationTest.java
@@ -1,15 +1,39 @@
 package com.meti;
 
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 
+import java.io.IOException;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class ApplicationTest {
+
+    public static final Path Target = Paths.get(".", "Index.mgs");
+    public static final Path Source = Paths.get(".", "Index.java");
+
+    @AfterEach
+    void tearDown() throws IOException {
+        Files.deleteIfExists(Source);
+        Files.deleteIfExists(Target);
+    }
+
     @Test
-    void test() {
-        assertFalse(Files.exists(Paths.get(".", "Index.mgs")));
+    void generateTarget() throws IOException {
+        Files.createFile(Source);
+        if (Files.exists(Source)) {
+            Files.createFile(Target);
+        }
+
+        assertTrue(Files.exists(Target));
+    }
+
+    @Test
+    void generatesNoTarget() {
+        assertFalse(Files.exists(Target));
     }
 }
