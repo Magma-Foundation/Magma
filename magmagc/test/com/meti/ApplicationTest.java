c3edb7e6448a218bec9ae1a7e6c4feb580e757b6
SirMathhman <SirMathhman@gmail.com>
2024-03-13 11:05:59 -0400

    Generate a file

--- magmagc/test/com/meti/ApplicationTest.java
+++ magmagc/test/com/meti/ApplicationTest.java
@@ -1,17 +1,43 @@
 package com.meti;
 
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 
+import java.io.IOException;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class ApplicationTest {
-    @Test
-    void generatesNothing() {
-        assertFalse(Files.exists(Paths.get(".", "Index.mgs")));
+
+    public static final Path TARGET = Paths.get(".", "Index.mgs");
+    public static final Path SOURCE = Paths.get(".", "Index.java");
+
+    private static void run() throws IOException {
+        if (Files.exists(SOURCE)) {
+            Files.createFile(SOURCE);
+        }
     }
 
+    @Test
+    void generatesNothing() throws IOException {
+        run();
+        assertFalse(Files.exists(TARGET));
+    }
 
+    @AfterEach
+    void tearDown() throws IOException {
+        Files.deleteIfExists(SOURCE);
+        Files.deleteIfExists(TARGET);
+    }
+
+    @Test
+    void generatesSomething() throws IOException {
+        Files.createFile(SOURCE);
+        run();
+        assertTrue(Files.exists(TARGET));
+    }
 }
