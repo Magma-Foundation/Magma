c0a293cf0bd5e7381248c000d15438db6ba11e74
SirMathhman <sirmathhman@gmail.com>
2024-03-13 21:32:15 -0400

    Copy public classes

--- magmac/src/com/meti/Application.java
+++ magmac/src/com/meti/Application.java
@@ -3,8 +3,9 @@ package com.meti;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import static com.meti.None.None;
 import static com.meti.Some.Some;
@@ -12,7 +13,15 @@ import static com.meti.Some.Some;
 public record Application(Path source) {
     private static String compileLine(String line) {
         var stripped = line.strip();
-        if (stripped.startsWith("import ")) {
+        if (stripped.startsWith("public class ")) {
+            var start = stripped.indexOf("{");
+            var end = stripped.lastIndexOf("}");
+
+            var name = stripped.substring("public class ".length(), start).strip();
+            var content = stripped.substring(start);
+
+            return "export class def " + name + "() => " + content;
+        } else if (stripped.startsWith("import ")) {
             var isStatic = stripped.startsWith("import static ");
             var content = stripped.substring(isStatic
                     ? "import static ".length()
@@ -38,11 +47,30 @@ public record Application(Path source) {
         }
     }
 
+    private static Stream<String> split(String input) {
+        var lines = new ArrayList<String>();
+        var builder = new StringBuilder();
+        var depth = 0;
+        for (int i = 0; i < input.length(); i++) {
+            var c = input.charAt(i);
+            if (c == ';' && depth == 0) {
+                lines.add(builder.toString());
+                builder = new StringBuilder();
+            } else {
+                if(c == '{') depth++;
+                if(c == '}') depth--;
+                builder.append(c);
+            }
+        }
+        lines.add(builder.toString());
+        return lines.stream();
+    }
+
     Option<Path> run() throws IOException {
         if (!Files.exists(source)) return None();
 
         var input = Files.readString(source);
-        var root = Arrays.stream(input.split(";"))
+        var root = split(input)
                 .map(Application::compileLine)
                 .collect(Collectors.joining());
 
