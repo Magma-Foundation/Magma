c73f5a861080795dd0ed602f845dfcb5a742d638
SirMathhman <sirmathhman@gmail.com>
2024-03-13 21:27:21 -0400

    Cleanup Application

--- magmac/src/com/meti/Application.java
+++ magmac/src/com/meti/Application.java
@@ -4,47 +4,53 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.Arrays;
+import java.util.stream.Collectors;
+
+import static com.meti.None.None;
+import static com.meti.Some.Some;
 
 public record Application(Path source) {
-    Option<Path> run() throws IOException {
-        if (Files.exists(source())) {
-            var input = Files.readString(source);
-            var lines = Arrays.stream(input.split(";")).toList();
-            var output = new StringBuilder();
-            for (String line : lines) {
-                var stripped = line.strip();
-                if (stripped.startsWith("import ")) {
-                    var isStatic = stripped.startsWith("import static ");
-                    var content = stripped.substring(isStatic
-                            ? "import static ".length()
-                            : "import ".length());
+    private static String compileLine(String line) {
+        var stripped = line.strip();
+        if (stripped.startsWith("import ")) {
+            var isStatic = stripped.startsWith("import static ");
+            var content = stripped.substring(isStatic
+                    ? "import static ".length()
+                    : "import ".length());
 
-                    var last = content.lastIndexOf('.');
-                    var child = content.substring(last + 1).strip();
-                    var parent = content.substring(0, last).strip();
+            var last = content.lastIndexOf('.');
+            var child = content.substring(last + 1).strip();
+            var parent = content.substring(0, last).strip();
 
-                    if (child.equals("*")) {
-                        output.append("import ")
-                                .append(parent)
-                                .append(";\n");
-                    } else {
-                        output.append("import { ")
-                                .append(child)
-                                .append(" } from ")
-                                .append(parent)
-                                .append(";\n");
-                    }
-                }
+            if (child.equals("*")) {
+                return "import " +
+                       parent +
+                       ";\n";
+            } else {
+                return "import { " +
+                       child +
+                       " } from " +
+                       parent +
+                       ";\n";
             }
-
-            var fileName = source().getFileName().toString();
-            var index = fileName.indexOf(".");
-            var name = fileName.substring(0, index);
-            var target = source().resolveSibling(name + ".mgs");
-            Files.writeString(target, output.toString());
-            return Some.Some(target);
         } else {
-            return None.None();
+            return "";
         }
+    }
+
+    Option<Path> run() throws IOException {
+        if (!Files.exists(source)) return None();
+
+        var input = Files.readString(source);
+        var root = Arrays.stream(input.split(";"))
+                .map(Application::compileLine)
+                .collect(Collectors.joining());
+
+        var fileName = source().getFileName().toString();
+        var index = fileName.indexOf(".");
+        var name = fileName.substring(0, index);
+        var target = source().resolveSibling(name + ".mgs");
+        Files.writeString(target, root);
+        return Some(target);
     }
 }
