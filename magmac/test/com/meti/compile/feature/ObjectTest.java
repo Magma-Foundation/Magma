3c5f7d480e5ea09c897014ee67b2e7d927dbd2e6
SirMathhman <sirmathhman@gmail.com>
2024-03-17 19:15:02 -0400

    Prepare to test extends

--- magmac/test/com/meti/compile/feature/ObjectTest.java
+++ magmac/test/com/meti/compile/feature/ObjectTest.java
@@ -5,6 +5,12 @@ import org.junit.jupiter.api.Test;
 
 public class ObjectTest extends FeatureTest {
     @Test
+    void extends_() {
+        assertCompile("class IntentionalException extends Exception {}",
+                "object IntentionalException = {implements Exception;}");
+    }
+
+    @Test
     void test() {
         assertCompile("class Test {}", "\n" +
                                        "object Test = {\n" +
