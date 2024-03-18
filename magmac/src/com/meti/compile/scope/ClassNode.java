f3612228c491fe78a30cfdf87673db92b48e882f
SirMathhman <sirmathhman@gmail.com>
2024-03-17 20:03:56 -0400

    OK???

--- magmac/src/com/meti/compile/scope/ClassNode.java
+++ magmac/src/com/meti/compile/scope/ClassNode.java
@@ -1,6 +1,7 @@
 package com.meti.compile.scope;
 
 import com.meti.collect.JavaList;
+import com.meti.collect.option.None;
 import com.meti.collect.option.Option;
 import com.meti.compile.node.*;
 import com.meti.java.JavaString;
@@ -12,6 +13,8 @@ import static com.meti.collect.option.Some.Some;
 
 public record ClassNode(JavaList<JavaString> flags, JavaString name,
                         Node value) implements Node {
+
+
     private Option<Node> findValueAsNode() {
         return Some(value);
     }
@@ -30,57 +33,29 @@ public record ClassNode(JavaList<JavaString> flags, JavaString name,
         return switch (name) {
             case "children" -> attribute.asListOfNodes()
                     .map(JavaList::unwrap)
-                    .flatMap(this::withChildren);
+                    .flatMap(children -> {
+                        return None();
+                    });
             case "value" -> attribute.asNode().flatMap(this::withValue);
             default -> None();
         };
     }
 
-    private Option<Node> withChildren(List<? extends Node> children) {
-        return None();
-    }
-
     @Override
     public Option<Attribute> apply(String name) {
-        switch (name) {
-            case "children":
-                return findChildren()
-                        .map(JavaList::new)
-                        .map(NodeListAttribute::new);
-            case "value":
-                return findValueAsNode()
-                        .<Attribute>map(NodeAttribute::new)
-                        .orLazy(() -> findValueAsString()
-                                .map(JavaString::new)
-                                .map(StringAttribute::new));
-            case "flags":
-                return findFlags().map(StringListAttribute::new);
-            case "name":
-                return findName().map(StringAttribute::new);
-            case "indent":
-                return findIndent().map(IntegerAttribute::new);
-            default:
-                return None();
-        }
-    }
-
-    private Option<List<? extends Node>> findChildren() {
-        return None();
-    }
-
-    private Option<String> findValueAsString() {
-        return None();
-    }
-
-    private Option<Integer> findIndent() {
-        return None();
-    }
-
-    private Option<JavaList<JavaString>> findFlags() {
-        return None();
-    }
-
-    private Option<JavaString> findName() {
-        return None();
+        return switch (name) {
+            case "children" -> None.<List<? extends Node>>None()
+                    .map(JavaList::new)
+                    .map(NodeListAttribute::new);
+            case "value" -> findValueAsNode()
+                    .<Attribute>map(NodeAttribute::new)
+                    .orLazy(() -> None.<String>None()
+                            .map(JavaString::new)
+                            .map(StringAttribute::new));
+            case "flags" -> Some(flags).map(StringListAttribute::new);
+            case "name" -> Some(this.name).map(StringAttribute::new);
+            case "indent" -> None.<Integer>None().map(IntegerAttribute::new);
+            default -> None();
+        };
     }
 }
