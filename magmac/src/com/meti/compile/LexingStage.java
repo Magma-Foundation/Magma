56991d1c676a2183234073a7565f2863cdfcdf1b
SirMathhman <sirmathhman@gmail.com>
2024-03-20 21:37:47 -0400

    Cleanup lexing stage

--- magmac/src/com/meti/compile/LexingStage.java
+++ magmac/src/com/meti/compile/LexingStage.java
@@ -7,10 +7,7 @@ import com.meti.collect.result.Results;
 import com.meti.collect.result.ThrowableOption;
 import com.meti.collect.stream.Collectors;
 import com.meti.compile.java.JavaLexer;
-import com.meti.compile.node.Attribute;
-import com.meti.compile.node.Content;
-import com.meti.compile.node.Node;
-import com.meti.compile.node.NodeAttribute;
+import com.meti.compile.node.*;
 import com.meti.java.JavaString;
 import org.jetbrains.annotations.NotNull;
 
@@ -19,7 +16,56 @@ import static com.meti.collect.result.Ok.Ok;
 import static com.meti.collect.result.Results.$Result;
 
 public class LexingStage {
+    private static JavaList<Node> applyChoicesToNode(Node root, JavaList<JavaList<Node>> initialChoices) {
+        return crossCombinations(initialChoices)
+                .stream()
+                .map(combination -> root.with("children", new NodeListAttribute(combination)).orElse(root))
+                .collect(Collectors.toList());
+    }
+
+    private static JavaList<JavaList<Node>> crossCombinations(JavaList<JavaList<Node>> list) {
+        return list.stream()
+                .foldRight(LexingStage::wrapEachChildAsInitialCombinations, LexingStage::crossChildrenCombinations)
+                .orElse(JavaList.empty());
+    }
+
+    private static JavaList<JavaList<Node>> wrapEachChildAsInitialCombinations(JavaList<Node> first) {
+        return first.stream().map(JavaList::from).collect(Collectors.toList());
+    }
+
+    private static JavaList<JavaList<Node>> crossChildrenCombinations(JavaList<JavaList<Node>> previousChildrenCombinations, JavaList<Node> childOptions) {
+        return previousChildrenCombinations.stream().cross(childOptions::stream)
+                .map(pair -> pair.left().add(pair.right()))
+                .collect(Collectors.toList());
+    }
+
     public Result<JavaList<Node>, CompileException> lexTree(Node node) {
+        return withNode(node).flatMapValue(this::withChildrenForNodes);
+    }
+
+    private Result<JavaList<Node>, CompileException> withChildrenForNodes(JavaList<Node> withNodes) {
+        return withNodes.stream().map(this::withChildrenForNode).into(ExceptionalStream::new)
+                .mapInner(JavaList::stream)
+                .flatMap(Results::invertStream)
+                .collect(Collectors.exceptionally(Collectors.toList()));
+    }
+
+    private Result<JavaList<Node>, CompileException> withChildrenForNode(Node withNode) {
+        return withNode.apply("children")
+                .flatMap(Attribute::asListOfNodes)
+                .map(this::lexListOfContent)
+                .map(result -> result.mapValue(list -> applyChoicesToNode(withNode, list)))
+                .orElse(Ok(JavaList.empty()));
+    }
+
+    private Result<JavaList<JavaList<Node>>, CompileException> lexListOfContent(JavaList<? extends Node> children) {
+        return children.stream()
+                .map(this::lexContent)
+                .into(ExceptionalStream::new)
+                .collectExceptionally(Collectors.toList());
+    }
+
+    private Result<JavaList<Node>, CompileException> withNode(Node node) {
         return node.apply("value")
                 .flatMap(Attribute::asNode)
                 .map(this::lexContent)
