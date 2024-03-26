77a773097477aaa3a317f005c4fec63c65a59144
SirMathhman <SirMathhman@gmail.com>
2024-03-25 23:51:51 -0400

    Replace string with a rule

--- magmac/src/com/meti/rule/Rules.java
+++ magmac/src/com/meti/rule/Rules.java
@@ -1,17 +1,14 @@
 package com.meti.rule;
 
-import com.meti.node.Attribute;
-
 import java.util.Collections;
-import java.util.Map;
+import java.util.HashMap;
 import java.util.Optional;
 
 public class Rules {
-    public static Rule Empty = new Rule() {
-        @Override
-        public Optional<Map<String, Attribute>> apply(String input) {
-            if(input.isEmpty()) return Optional.of(Collections.emptyMap());
-            else return Optional.empty();
-        }
+    public static Rule Empty = input -> {
+        if (input.isEmpty()) return Optional.of(Collections.emptyMap());
+        else return Optional.empty();
     };
+
+    public static Rule Any = input -> Optional.of(new HashMap<>());
 }
