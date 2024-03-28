5fae51fc52c7cf4c21c0e59975fdc4146cb2512a
SirMathhman <SirMathhman@gmail.com>
2024-03-27 20:46:49 -0400

    Revert "???"
    This reverts commit c974f92b610a16ed65efd4499d109eb12889f262.

--- magmac/test/com/meti/rule/ListDelimitingRuleTest.java
+++ magmac/test/com/meti/rule/ListDelimitingRuleTest.java
@@ -3,8 +3,11 @@ package com.meti.rule;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.util.Stack;
+
 class ListDelimitingRuleTest {
 
+    private static final Stack<String> stack = new Stack<>();
 
     @Test
     void empty() {
