cfe0472fa53e2d6013f9fde1ce732700e02cd8d6
SirMathhman <SirMathhman@gmail.com>
2024-03-27 23:34:44 -0400

    Revert "Wrap in timeout"
    This reverts commit 937672effc815bb995c403a42124c6b39b7dc99d.

--- magmac/test/com/meti/bug/Bug0.java
+++ magmac/test/com/meti/bug/Bug0.java
@@ -5,22 +5,16 @@ import com.meti.rule.NodeElementRule;
 import com.meti.rule.RequireRule;
 import org.junit.jupiter.api.Test;
 
-import java.time.Duration;
 import java.util.Stack;
 
 import static com.meti.stage.JavaLexingStage.CLASS_MEMBER;
-import static org.junit.jupiter.api.Assertions.assertTimeoutPreemptively;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class Bug0 {
     @Test
     void multiple() {
-        assertTimeoutPreemptively(Duration.ofSeconds(2), () -> {
-            var present = new ListDelimitingRule(new RequireRule(";"), new NodeElementRule("body", CLASS_MEMBER))
-                    .lex("int first = 1; int second = 2;", new Stack<>())
-                    .isPresent();
-
-            assertTrue(present);
-        });
+        assertTrue(new ListDelimitingRule(new RequireRule(";"), new NodeElementRule("body", CLASS_MEMBER))
+                .lex("int first = 1; int second = 2;", new Stack<>())
+                .isPresent());
     }
 }
