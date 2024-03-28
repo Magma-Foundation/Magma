937672effc815bb995c403a42124c6b39b7dc99d
SirMathhman <SirMathhman@gmail.com>
2024-03-27 22:22:27 -0400

    Wrap in timeout

--- magmac/test/com/meti/bug/Bug0.java
+++ magmac/test/com/meti/bug/Bug0.java
@@ -5,16 +5,22 @@ import com.meti.rule.NodeElementRule;
 import com.meti.rule.RequireRule;
 import org.junit.jupiter.api.Test;
 
+import java.time.Duration;
 import java.util.Stack;
 
 import static com.meti.stage.JavaLexingStage.CLASS_MEMBER;
+import static org.junit.jupiter.api.Assertions.assertTimeoutPreemptively;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class Bug0 {
     @Test
     void multiple() {
-        assertTrue(new ListDelimitingRule(new RequireRule(";"), new NodeElementRule("body", CLASS_MEMBER))
-                .lex("int first = 1; int second = 2;", new Stack<>())
-                .isPresent());
+        assertTimeoutPreemptively(Duration.ofSeconds(2), () -> {
+            var present = new ListDelimitingRule(new RequireRule(";"), new NodeElementRule("body", CLASS_MEMBER))
+                    .lex("int first = 1; int second = 2;", new Stack<>())
+                    .isPresent();
+
+            assertTrue(present);
+        });
     }
 }
