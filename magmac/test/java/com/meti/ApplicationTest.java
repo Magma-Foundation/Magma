dad530bc58ebb0d7bdb8dad9f3386e39302c6dd5
SirMathhman <SirMathhman@gmail.com>
2024-03-28 17:03:02 -0400

    Generate nothing

---
+++ magmac/test/java/com/meti/ApplicationTest.java
@@ -0,0 +1,15 @@
+package com.meti;
+
+import org.junit.jupiter.api.Test;
+
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
+public class ApplicationTest {
+    @Test
+    void generatesNothing() {
+        assertFalse(Files.exists(Paths.get(".", "ApplicationTest.java")));
+    }
+}
