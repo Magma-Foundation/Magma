5d27bcb7469d3c69acb0a17e17bd99120f7a8960
SirMathhman <SirMathhman@gmail.com>
2024-03-27 20:46:57 -0400

    Revert "Apparently noEnd10 needs these three items for it to function"
    This reverts commit 192144b40fa3150a9dd91a3ca43d5a8e96f9e2c9.

--- magmac/test/com/meti/bug/Bug0.java
+++ magmac/test/com/meti/bug/Bug0.java
@@ -1,126 +1,60 @@
 package com.meti.bug;
 
-import com.meti.rule.*;
+import com.meti.rule.ListDelimitingRule;
+import com.meti.rule.NodeElementRule;
+import com.meti.rule.RequireRule;
+import com.meti.rule.WhitespaceRule;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import java.time.Duration;
-
-import static com.meti.rule.AndRule.And;
-import static com.meti.rule.WhitespaceRule.PADDING;
-import static com.meti.rule.WhitespaceRule.WHITESPACE;
-import static com.meti.stage.JavaLexingStage.*;
+import static com.meti.stage.JavaLexingStage.CLASS_MEMBER;
 
 public class Bug0 {
 
-    private static void assertValid(String input, Rule rule) {
-        Assertions.assertTimeoutPreemptively(Duration.ofSeconds(1), () -> {
-            var present = rule
-                    .lexImpl(input)
-                    .isPresent();
-
-            Assertions.assertTrue(present);
-        });
-    }
-
     @Test
-    void empty() {
-        assertValid("", new ListDelimitingRule(new RequireRule(";"), new NodeElementRule("body", CLASS_MEMBER)));
-    }
+    void empty(){
+        var present = new ListDelimitingRule(new RequireRule(";"), new NodeElementRule("body", CLASS_MEMBER))
+                .lexImpl("")
+                .isPresent();
 
-    @Test
-    void delimiter() {
-        assertValid(";", new ListDelimitingRule(new RequireRule(";"), WhitespaceRule.PADDING));
-    }
-
-    @Test
-    void line() {
-        assertValid("int first = 1;", new ListDelimitingRule(new RequireRule(";"), new NodeElementRule("body", CLASS_MEMBER)));
-    }
-
-    @Test
-    void test() {
-        assertValid("int first = 1; int second = 2;", new ListDelimitingRule(new RequireRule(";"), new NodeElementRule("body", CLASS_MEMBER)));
-    }
-
-    @Test
-    void noEnd() {
-        assertValid("int first = 1; int second = 2",
-                new ListDelimitingRule(new RequireRule(";"), new NodeElementRule("body", CLASS_MEMBER)));
-    }
-
-    @Test
-    void noEnd1() {
-        assertValid("int first = 1; int second = 1",
-                new ListDelimitingRule(new RequireRule(";"), new NodeElementRule("body", CLASS_MEMBER)));
-    }
-
-    @Test
-    void noEnd2() {
-        assertValid("int first = 1; int second = 1",
-                new ListDelimitingRule(new RequireRule(";"), new NodeElementRule("body", new NamedRule("definition", DEFINITION_RULE))));
-    }
-
-    @Test
-    void noEnd10() {
-        assertValid("first 1;second 1",
-                new ListDelimitingRule(new RequireRule(";"), new NodeElementRule("body", new NamedRule("definition", And(
-                        Rules.ExtractSymbol("name"),
-                        PADDING,
-                        new NodeRule("value", VOLATILE_VALUE_RULE)
-                )))));
-    }
-
-    @Test
-    void noEnd9() {
-        assertValid(" 1; 1",
-                new ListDelimitingRule(new RequireRule(";"), new NodeElementRule("body", new NamedRule("definition", And(
-                        PADDING,
-                        new NodeRule("value", VOLATILE_VALUE_RULE)
-                )))));
-    }
-
-    @Test
-    void noEnd8() {
-        assertValid("int first =; int second =",
-                new ListDelimitingRule(new RequireRule(";"), new NodeElementRule("body", new NamedRule("definition", And(
-                        new ListDelimitingRule(WHITESPACE, new StringListRule("flags", Rules.Enum("public", "final"))),
-                        PADDING,
-                        Rules.ExtractSymbol("type"),
-                        WHITESPACE,
-                        Rules.ExtractSymbol("name"),
-                        PADDING,
-                        new RequireRule("=")
-                )))));
-    }
-
-    @Test
-    void noEnd7() {
-        assertValid("1;1",
-                new ListDelimitingRule(new RequireRule(";"), new NodeElementRule("value", VOLATILE_VALUE_RULE)));
-    }
-
-    @Test
-    void noEnd6() {
-        assertValid("1;1",
-                new ListDelimitingRule(new RequireRule(";"), new NodeRule("value", VOLATILE_VALUE_RULE)));
+        Assertions.assertTrue(present);
     }
 
 
     @Test
-    void noEnd4() {
-        assertValid("test();test()", new ListDelimitingRule(new RequireRule(";"), INVOKE));
+    void delimiter(){
+        var present = new ListDelimitingRule(new RequireRule(";"), WhitespaceRule.PADDING)
+                .lexImpl(";")
+                .isPresent();
+
+        Assertions.assertTrue(present);
+    }
+
+
+    @Test
+    void line(){
+        var present = new ListDelimitingRule(new RequireRule(";"), new NodeElementRule("body", CLASS_MEMBER))
+                .lexImpl("int first = 1;")
+                .isPresent();
+
+        Assertions.assertTrue(present);
     }
 
     @Test
-    void noEnd3() {
-        assertValid("1;1",
-                new ListDelimitingRule(new RequireRule(";"), INT));
+    void test(){
+        var present = new ListDelimitingRule(new RequireRule(";"), new NodeElementRule("body", CLASS_MEMBER))
+                .lexImpl("int first = 1; int second = 2;")
+                .isPresent();
+
+        Assertions.assertTrue(present);
     }
 
     @Test
-    void noEnd5() {
-        assertValid("1;1", new ListDelimitingRule(new RequireRule(";"),
-                new NodeElementRule("int", new NamedRule("int", INT))));
+    void noEnd(){
+        var present = new ListDelimitingRule(new RequireRule(";"), new NodeElementRule("body", CLASS_MEMBER))
+                .lexImpl("int first = 1; int second = 2")
+                .isPresent();
+
+        Assertions.assertTrue(present);
     }
 }
