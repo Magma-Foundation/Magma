a0268e04fbb2c742700c59225cdf24e9ec2ad87f
SirMathhman <SirMathhman@gmail.com>
2024-03-28 22:09:36 -0400

    Change splitting algorithm

--- magmac/src/java/com/meti/Compiler.java
+++ magmac/src/java/com/meti/Compiler.java
@@ -45,12 +45,65 @@ public record Compiler(String input, String sourceExtension, String targetExtens
     }
 
     List<Application.State> compile() {
+        var lines = split(input());
+
+        if (sourceExtension().equals(".java")) {
+            lines.removeIf(line -> line.startsWith("package"));
+        }
+
+        return lines.stream()
+                .map(String::strip)
+                .filter(value -> !value.isEmpty())
+                .map(this::compileLine)
+                .collect(Collectors.toList());
+    }
+
+    private Application.State compileLine(String line) {
+        var compiled = compileImport(line, sourceExtension(), targetExtension());
+        if (compiled.isPresent()) return new Application.State("", Optional.empty(), compiled);
+
+        if (line.startsWith("export class def ")) {
+            var paramStart = line.indexOf('(');
+            var name = line.substring("export class def ".length(), paramStart).strip();
+            var body = line.substring(line.indexOf('{') + 1, line.lastIndexOf('}'));
+
+            if (targetExtension().equals(".js")) {
+                return new Application.State("\nfunction " + name + "(){" + body + "\treturn {};\n}", Optional.of(name));
+            }
+
+            if (targetExtension().equals(".h")) {
+                return new Application.State("\nvoid " + name + "();", Optional.empty(),
+                        Optional.of("\nstruct " + name + " {}"));
+            } else if (targetExtension().equals(".c")) {
+                return new Application.State("\nstruct " + name + " " + name + "(){" + body +
+                                             "\n\tstruct " + name + " " + "this" + " = {};" +
+                                             "\n\treturn this;" +
+                                             "\n}", Optional.empty());
+            }
+        }
+
+        if (line.startsWith("public class ")) {
+            var name = line.substring("public class ".length(), line.indexOf('{')).strip();
+            var body = line.substring(line.indexOf('{'), line.lastIndexOf('}') + 1).strip();
+
+            return new Application.State("\nexport class def " + name + "() => " + body, Optional.empty());
+        }
+
+        return new Application.State(line, Optional.empty());
+    }
+
+    private ArrayList<String> split(String input) {
         var lines = new ArrayList<String>();
         var builder = new StringBuilder();
         var depth = 0;
-        for (int i = 0; i < input().length(); i++) {
-            var c = input().charAt(i);
+        for (int i = 0; i < input.length(); i++) {
+            var c = input.charAt(i);
             if (c == ';' && depth == 0) {
+                lines.add(builder.toString());
+                builder = new StringBuilder();
+            } else if (c == '}' && depth == 1) {
+                builder.append(c);
+                depth--;
                 lines.add(builder.toString());
                 builder = new StringBuilder();
             } else {
@@ -62,47 +115,6 @@ public record Compiler(String input, String sourceExtension, String targetExtens
 
         lines.add(builder.toString());
         lines.removeIf(String::isBlank);
-
-        if (sourceExtension().equals(".java")) {
-            lines.removeIf(line -> line.startsWith("package"));
-        }
-
-        return lines.stream()
-                .map(String::strip)
-                .filter(value -> !value.isEmpty())
-                .map(line -> {
-                    var compiled = compileImport(line, sourceExtension(), targetExtension());
-                    if (compiled.isPresent()) return new Application.State("", Optional.empty(), compiled);
-
-                    if (line.startsWith("export class def ")) {
-                        var paramStart = line.indexOf('(');
-                        var name = line.substring("export class def ".length(), paramStart).strip();
-                        var body = line.substring(line.indexOf('{') + 1, line.lastIndexOf('}'));
-
-                        if (targetExtension().equals(".js")) {
-                            return new Application.State("\nfunction " + name + "(){" + body + "\treturn {};\n}", Optional.of(name));
-                        }
-
-                        if (targetExtension().equals(".h")) {
-                            return new Application.State("\nvoid " + name + "();", Optional.empty(),
-                                    Optional.of("\nstruct " + name + " {}"));
-                        } else if (targetExtension().equals(".c")) {
-                            return new Application.State("\nstruct " + name + " " + name + "(){" + body +
-                                                         "\n\tstruct " + name + " " + "this" + " = {};" +
-                                                         "\n\treturn this;" +
-                                                         "\n}", Optional.empty());
-                        }
-                    }
-
-                    if (line.startsWith("public class ")) {
-                        var name = line.substring("public class ".length(), line.indexOf('{')).strip();
-                        var body = line.substring(line.indexOf('{'), line.lastIndexOf('}') + 1).strip();
-
-                        return new Application.State("\nexport class def " + name + "() => " + body, Optional.empty());
-                    }
-
-                    return new Application.State(line, Optional.empty());
-                })
-                .collect(Collectors.toList());
+        return lines;
     }
 }
