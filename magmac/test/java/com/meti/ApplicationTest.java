acbbbf598a315e534bb5b188f52d981142cfa1fb
SirMathhman <sirmathhman@gmail.com>
2024-03-30 18:32:54 -0400

    Generate targets

--- magmac/test/java/com/meti/ApplicationTest.java
+++ magmac/test/java/com/meti/ApplicationTest.java
@@ -1,15 +1,44 @@
 package com.meti;
 
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 
+import java.io.IOException;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class ApplicationTest {
+
+    public static final Path ROOT = Paths.get(".");
+    public static final Path TARGET = ROOT.resolve("ApplicationTest.mgs");
+    public static final Path SOURCE = ROOT.resolve("ApplicationTest.java");
+
+    private static void run() throws IOException {
+        if (Files.exists(SOURCE)) {
+            Files.createFile(TARGET);
+        }
+    }
+
+    @AfterEach
+    void tearDown() throws IOException {
+        Files.deleteIfExists(TARGET);
+        Files.deleteIfExists(SOURCE);
+    }
+
     @Test
-    void test() {
-        assertFalse(Files.exists(Paths.get(".", "ApplicationTest.mgs")));
+    void generatesTarget() throws IOException {
+        Files.createFile(SOURCE);
+        run();
+        assertTrue(Files.exists(TARGET));
+    }
+
+    @Test
+    void test() throws IOException {
+        run();
+        assertFalse(Files.exists(TARGET));
     }
 }
