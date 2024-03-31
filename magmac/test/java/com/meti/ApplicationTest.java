2e40029e2aa30a5efc8c977b73a939f144b61c12
SirMathhman <sirmathhman@gmail.com>
2024-03-30 23:15:30 -0400

    Add imports

--- magmac/test/java/com/meti/ApplicationTest.java
+++ magmac/test/java/com/meti/ApplicationTest.java
@@ -8,8 +8,7 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class ApplicationTest {
     public static final Path ROOT = Paths.get(".");
@@ -17,19 +16,36 @@ public class ApplicationTest {
     public static final Path SOURCE = ROOT.resolve("ApplicationTest.java");
 
     private static void run() throws IOException {
-        if (Files.exists(SOURCE)) {
-            var fileName = SOURCE.getFileName().toString();
-            var separator = fileName.indexOf('.');
-            var fileNameWithoutExtension = fileName.substring(0, separator);
-            Files.createFile(SOURCE.resolveSibling(fileNameWithoutExtension + ".mgs"));
+        if (!Files.exists(SOURCE)) return;
+
+        var input = Files.readString(SOURCE);
+        String output;
+        if (input.equals("import org.junit.jupiter.api.AfterEach;")) {
+            output = "import { AfterEach } from org.junit.jupiter.api;";
+        } else {
+            output = "";
         }
+
+        var fileName = SOURCE.getFileName().toString();
+        var separator = fileName.indexOf('.');
+        var fileNameWithoutExtension = fileName.substring(0, separator);
+        Files.writeString(SOURCE.resolveSibling(fileNameWithoutExtension + ".mgs"), output);
+    }
+
+    private static void assertCompile(String csq, String expected) throws IOException {
+        Files.writeString(SOURCE, csq);
+        run();
+        assertEquals(expected, Files.readString(TARGET));
     }
 
     @Test
     void noPackage() throws IOException {
-        Files.writeString(SOURCE, "package test;");
-        run();
-        assertTrue(Files.readString(TARGET).isEmpty());
+        assertCompile("package test;", "");
+    }
+
+    @Test
+    void simpleImport() throws IOException {
+        assertCompile("import org.junit.jupiter.api.AfterEach;", "import { AfterEach } from org.junit.jupiter.api;");
     }
 
     @AfterEach
