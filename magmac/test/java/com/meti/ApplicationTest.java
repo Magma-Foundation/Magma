fd1062cf2b479dbcf2632a8671feb157b1cf41a3
SirMathhman <sirmathhman@gmail.com>
2024-04-02 20:19:33 -0400

    Invalid multiple package statements

--- magmac/test/java/com/meti/ApplicationTest.java
+++ magmac/test/java/com/meti/ApplicationTest.java
@@ -2,20 +2,39 @@ package com.meti;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class ApplicationTest {
-    @Test
-    void empty() {
-        assertEquals("", run(""));
+    private static String run(String s) throws CompileException {
+        var first = s.indexOf("package");
+        if (first == -1) return "";
+
+        var second = s.indexOf("package", first + "package ".length());
+        if (second == -1) return "";
+
+        throw new CompileException("Duplicate package statement.");
     }
 
     @Test
-    void package_string() {
-        assertEquals("", run("package com.meti;"));
+    void testEmpty() {
+        assertEquals("", runImpl(""));
     }
 
-    private static String run(String s) {
-        return "";
+    private static String runImpl(String s) {
+        try {
+            return run(s);
+        } catch (CompileException e) {
+            return fail(e);
+        }
+    }
+
+    @Test
+    void testPackages() {
+        assertEquals("", runImpl("package com.meti;"));
+    }
+
+    @Test
+    void testMultiplePackages() {
+        assertThrows(CompileException.class, () -> run("package first;package second"));
     }
 }
