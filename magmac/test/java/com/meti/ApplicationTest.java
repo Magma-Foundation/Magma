5d0e84576cc271c3319cba5e63c615b4e77307fd
SirMathhman <sirmathhman@gmail.com>
2024-04-05 14:54:15 -0400

    Invalidate multiple classes

--- magmac/test/java/com/meti/ApplicationTest.java
+++ magmac/test/java/com/meti/ApplicationTest.java
@@ -10,9 +10,16 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 public class ApplicationTest {
     public static final String CLASS_KEYWORD = "class ";
     public static final String EMPTY_CONTENT = "{}";
+    public static final String TEST_NAME = "First";
 
     private static String run(String input) throws CompileException {
-        if (input.startsWith(CLASS_KEYWORD)) {
+        var first = input.indexOf(CLASS_KEYWORD);
+        if (first != -1) {
+            var after = input.indexOf(CLASS_KEYWORD, first + CLASS_KEYWORD.length());
+            if (after != -1) {
+                throw new CompileException("Multiple classes are not permitted.");
+            }
+
             var name = input.substring(CLASS_KEYWORD.length(), input.indexOf(EMPTY_CONTENT)).strip();
             return renderMagmaClass(name);
         }
@@ -23,14 +30,23 @@ public class ApplicationTest {
         return CLASS_KEYWORD + "def " + name + "() => " + EMPTY_CONTENT;
     }
 
+    private static String renderJavaClass(String name) {
+        return CLASS_KEYWORD + name + " " + EMPTY_CONTENT;
+    }
+
     @Test
     void empty() {
         assertThrows(CompileException.class, () -> run(""));
     }
 
     @ParameterizedTest
-    @ValueSource(strings = {"First", "Second"})
+    @ValueSource(strings = {TEST_NAME, "Second"})
     void simpleClass(String name) throws CompileException {
-        assertEquals(renderMagmaClass(name), run(CLASS_KEYWORD + name + " " + EMPTY_CONTENT));
+        assertEquals(renderMagmaClass(name), run(renderJavaClass(name)));
+    }
+
+    @Test
+    void multipleClass() {
+        assertThrows(CompileException.class, () -> run(renderJavaClass(TEST_NAME) + renderJavaClass(TEST_NAME)));
     }
 }
