ef0f04779123710d0669ebba5a0939a52de7f5c7
SirMathhman <sirmathhman@gmail.com>
2024-04-05 17:45:39 -0400

    Parameterize test

--- magmac/test/java/com/meti/ApplicationTest.java
+++ magmac/test/java/com/meti/ApplicationTest.java
@@ -1,28 +1,30 @@
 package com.meti;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 public class ApplicationTest {
-    @Test
-    void test() {
-        assertEquals(createError(), run());
+    @ParameterizedTest
+    @ValueSource(strings = {"a", "b"})
+    void test(String c) {
+        assertEquals(createError(c), run(c));
     }
 
-    private static String run() {
-        return createError();
+    private static String run(String c) {
+        return createError(c);
     }
 
-    private static String createError() {
-        return """
-                **Location**: com.meti.Test
-                **Message**: Unknown Token*
-
-                **Details**:
-                ```
-                1) a
-                   ^
-                ```""";
+    private static String createError(String c) {
+        return "**Location**: com.meti.Test\n" +
+               "**Message**: Unknown Token*\n" +
+               "\n" +
+               "**Details**:\n" +
+               "```\n" +
+               "1) " + c + "\n" +
+               "   ^\n" +
+               "```";
     }
 }
