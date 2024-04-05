3921e890e164d5a0901b35c1792643ae5cf7d4b9
SirMathhman <sirmathhman@gmail.com>
2024-04-05 17:38:52 -0400

    Baseline test

--- magmac/test/java/com/meti/ApplicationTest.java
+++ magmac/test/java/com/meti/ApplicationTest.java
@@ -1,4 +1,26 @@
 package com.meti;
 
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class ApplicationTest {
+
+    private static String getString(String c) {
+        return "**Location**: com.meti.Test\n" +
+               "**Message**: Unknown Token*\n" +
+               "\n" +
+               "**Details**:\n" +
+               "```\n" +
+               "1) " + c + "\n" +
+               "   ^\n" +
+               "```";
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"a", "b"})
+    void testOneChar(String c) {
+        assertEquals(getString(c), getString(c));
+    }
 }
