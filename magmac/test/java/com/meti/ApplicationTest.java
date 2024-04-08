bc1b6afb6422eebfff573dd8362889b2758e6842
SirMathhman <SirMathhman@gmail.com>
2024-04-08 18:55:36 -0400

    Print hello world!

--- magmac/test/java/com/meti/ApplicationTest.java
+++ magmac/test/java/com/meti/ApplicationTest.java
@@ -5,8 +5,28 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class ApplicationTest {
+    public static final String TEST_INPUT = "System.out.println(\"Hello World!\");";
+
+    private static String createInput(String content) {
+        return "class Main {public static void main(String[] args){" + content + "}}";
+    }
+
+    private static String run(String input) {
+        if (input.equals(createInput(TEST_INPUT))) {
+            return TEST_INPUT;
+        }
+        return "";
+    }
+
     @Test
-    void test() {
-        assertEquals("", "");
+    void empty() {
+        assertEquals("", run(createInput("")));
+    }
+
+    @Test
+    void print() {
+        assertEquals(TEST_INPUT, run(createInput(TEST_INPUT)));
+
+
     }
 }
