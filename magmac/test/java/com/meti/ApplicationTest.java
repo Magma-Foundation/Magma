eece78ce65b5428c234417fa35377cfac7809e53
SirMathhman <sirmathhman@gmail.com>
2024-04-09 19:12:32 -0400

    Compile definitions

--- magmac/test/java/com/meti/ApplicationTest.java
+++ magmac/test/java/com/meti/ApplicationTest.java
@@ -10,15 +10,28 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 public class ApplicationTest {
     public static final String TEST_NAME = "Test";
     public static final String CLASS_KEYWORD = "class ";
-    public static final String CLASS_SUFFIX = " {public static void main(String[] args){}}";
+
+    private static String renderBlock() {
+        return renderBlock("");
+    }
+
+    private static String renderBlock(String fieldString) {
+        return "{" + fieldString + "public static void main(String[] args){}}";
+    }
 
     private static String renderJavaClass(String name) {
-        return CLASS_KEYWORD + name + CLASS_SUFFIX;
+        return renderJavaClass(name, renderBlock());
+    }
+
+    private static String renderJavaClass(String name, String value) {
+        return CLASS_KEYWORD + name + " " + value;
     }
 
     private static String compileMagmaToJava(String namespace, String magmaInput) throws CompileException {
         if (magmaInput.isEmpty()) {
             return renderJavaClass(namespace);
+        } else if (magmaInput.equals(renderMagmaDefinition())) {
+            return renderJavaClass(namespace, renderBlock(renderJavaDefinition()));
         } else {
             throw createUnknownInput(magmaInput);
         }
@@ -30,10 +43,26 @@ public class ApplicationTest {
 
     private static String compileJavaToMagma(String javaInput) throws CompileException {
         if (javaInput.startsWith(CLASS_KEYWORD)) {
-            return "";
+            var contentStart = javaInput.indexOf('{');
+            var contentEnd = javaInput.lastIndexOf('}');
+            var contentString = javaInput.substring(contentStart + 1, contentEnd).strip();
+            var separator = contentString.indexOf(';');
+            if (separator == -1) return "";
+
+            var typeString = contentString.substring(0, separator).strip();
+            if (typeString.isEmpty()) return "";
+            else return renderMagmaDefinition();
         } else {
             throw createUnknownInput(javaInput);
         }
+    }
+
+    private static String renderMagmaDefinition() {
+        return "let x = 0;";
+    }
+
+    private static String renderJavaDefinition() {
+        return "int value = 0;";
     }
 
     @Test
@@ -54,6 +83,13 @@ public class ApplicationTest {
     @ParameterizedTest
     @ValueSource(strings = {"First", "Second"})
     void smallestJava(String className) throws CompileException {
-        assertEquals(renderJavaClass(className), compileMagmaToJava(className, compileJavaToMagma(renderJavaClass(className))));
+        var value = renderJavaClass(className);
+        assertEquals(value, compileMagmaToJava(className, compileJavaToMagma(value)));
+    }
+
+    @Test
+    void definition() throws CompileException {
+        var value = renderJavaClass(TEST_NAME, renderBlock(renderJavaDefinition()));
+        assertEquals(value, compileMagmaToJava(TEST_NAME, compileJavaToMagma(value)));
     }
 }
