44ee18fdd83915ce4464e6d5dd8cac766bea535a
SirMathhman <SirMathhman@gmail.com>
2024-04-09 13:49:51 -0400

    Remove bad code

--- magmac/test/java/com/meti/ApplicationTest.java
+++ magmac/test/java/com/meti/ApplicationTest.java
@@ -1,35 +1,5 @@
 package com.meti;
 
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.ValueSource;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
 public class ApplicationTest {
 
-    private static String createInput(String content) {
-        return "class Main {public static void main(String[] args){" + content + "}}";
-    }
-
-    private static String run(String input) {
-        var prefix = "class Main {public static void main(String[] args){";
-        var suffix = "}}";
-
-        if (!input.startsWith(prefix)) return "";
-        if (!input.endsWith(suffix)) return "";
-
-        return input.substring(prefix.length(), input.length() - suffix.length());
-    }
-
-    @Test
-    void empty() {
-        assertEquals("", run(createInput("")));
-    }
-
-    @ParameterizedTest
-    @ValueSource(strings = {"System.out.println(\"Hello World!\");", "System.out.println(\"Test!\");"})
-    void print(String input) {
-        assertEquals(input, run(createInput(input)));
-    }
 }
