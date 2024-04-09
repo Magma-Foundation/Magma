966e16f025d2282a7df6861b31bc04c34f975aae
SirMathhman <sirmathhman@gmail.com>
2024-04-09 18:38:15 -0400

    Add reverse methods

--- magmac/test/java/com/meti/ApplicationTest.java
+++ magmac/test/java/com/meti/ApplicationTest.java
@@ -7,13 +7,21 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class ApplicationTest {
     public static final String SIMPLEST_JAVA = "class Test {public static void main(String[] args){}}";
 
+    private static String compileJavaFromMagma(String magmaInput) {
+        return magmaInput;
+    }
+
+    private static String compileMagmaFromJava(String javaInput) {
+        return javaInput;
+    }
+
     @Test
     void smallestMagma() {
-        assertEquals("", "");
+        assertEquals("", compileMagmaFromJava(compileJavaFromMagma("")));
     }
 
     @Test
     void smallestJava() {
-        assertEquals(SIMPLEST_JAVA, SIMPLEST_JAVA);
+        assertEquals(SIMPLEST_JAVA, compileJavaFromMagma(compileMagmaFromJava(SIMPLEST_JAVA)));
     }
 }
