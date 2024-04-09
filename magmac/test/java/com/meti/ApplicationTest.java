9d0b7798c01e4e94c290ba5c876d43629a7fd703
SirMathhman <SirMathhman@gmail.com>
2024-04-09 13:54:01 -0400

    Easy pipes

--- magmac/test/java/com/meti/ApplicationTest.java
+++ magmac/test/java/com/meti/ApplicationTest.java
@@ -5,8 +5,23 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class ApplicationTest {
+    public static final String JAVA_CLASS = "class Test {public static void main(String args[]){}}";
+
+    private static String compileMagma(String input) {
+        return input;
+    }
+
+    private static String compileJava(String input) {
+        return input;
+    }
+
     @Test
-    void test() {
-        assertEquals("", "");
+    void emptyMagma() {
+        assertEquals("", compileJava(compileMagma("")));
+    }
+
+    @Test
+    void emptyJava() {
+        assertEquals(JAVA_CLASS, compileMagma(compileJava(JAVA_CLASS)));
     }
 }
