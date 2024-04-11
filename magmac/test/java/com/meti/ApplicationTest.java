963ad6f5e28d29eda9f2d32edcca2fdbb1815383
SirMathhman <sirmathhman@gmail.com>
2024-04-11 14:59:05 -0400

    Add simple expressions

--- magmac/test/java/com/meti/ApplicationTest.java
+++ magmac/test/java/com/meti/ApplicationTest.java
@@ -7,9 +7,23 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class ApplicationTest {
 
     public static final String JAVA_CLASS = "class Main {}";
+    public static final String MAGMA_CLASS = "class def Main() => {}";
+
+    private static String compileJavaToMagma(String javaClass) {
+        return javaClass;
+    }
+
+    private static String compileMagmaToJava(String input) {
+        return input;
+    }
 
     @Test
-    void test() {
-        assertEquals(JAVA_CLASS, JAVA_CLASS);
+    void magmaSimple() {
+        assertEquals(MAGMA_CLASS, compileJavaToMagma(compileMagmaToJava(MAGMA_CLASS)));
+    }
+
+    @Test
+    void javaSimple() {
+        assertEquals(JAVA_CLASS, compileMagmaToJava(compileJavaToMagma(JAVA_CLASS)));
     }
 }
