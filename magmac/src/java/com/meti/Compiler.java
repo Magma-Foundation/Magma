e21ac7287b18a8015a4f28f7c616194525efb44b
SirMathhman <sirmathhman@gmail.com>
2024-04-11 14:55:32 -0400

    Kaboom

--- magmac/src/java/com/meti/Compiler.java
+++
@@ -1,7 +0,0 @@
-package com.meti;
-
-public class Compiler {
-    static CompileException createUnknownInput(String magmaInput) {
-        return new CompileException("Unknown input: " + magmaInput);
-    }
-}
--- magmac/test/java/com/meti/FeatureTest.java
+++
@@ -1,5 +0,0 @@
-package com.meti;
-
-public class FeatureTest {
-    public static final String TEST_UPPER_SYMBOL = "Test";
-}
