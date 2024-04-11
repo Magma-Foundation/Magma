3eee513cfed802e81b79de798c5ee228ca8a02a6
SirMathhman <sirmathhman@gmail.com>
2024-04-11 15:01:27 -0400

    Invalidation

--- magmac/test/java/com/meti/ApplicationTest.java
+++ magmac/test/java/com/meti/ApplicationTest.java
@@ -3,27 +3,46 @@ package com.meti;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class ApplicationTest {
 
     public static final String JAVA_CLASS = "class Main {}";
     public static final String MAGMA_CLASS = "class def Main() => {}";
 
-    private static String compileJavaToMagma(String javaClass) {
-        return javaClass;
+    private static String compileJavaToMagma(String input) throws CompileException {
+        if (input.equals(JAVA_CLASS)) {
+            return MAGMA_CLASS;
+        } else {
+            throw new CompileException("Unknown input: " + input);
+        }
     }
 
-    private static String compileMagmaToJava(String input) {
-        return input;
+    private static String compileMagmaToJava(String input) throws CompileException {
+        if (input.equals(MAGMA_CLASS)) {
+            return JAVA_CLASS;
+        } else {
+            throw new CompileException("Unknown input: " + input);
+        }
     }
 
     @Test
-    void magmaSimple() {
+    void javaInvalid() {
+        assertThrows(CompileException.class, () -> compileJavaToMagma(""));
+    }
+
+    @Test
+    void magmaInvalid() {
+        assertThrows(CompileException.class, () -> compileMagmaToJava("test"));
+    }
+
+    @Test
+    void magmaSimple() throws CompileException {
         assertEquals(MAGMA_CLASS, compileJavaToMagma(compileMagmaToJava(MAGMA_CLASS)));
     }
 
     @Test
-    void javaSimple() {
+    void javaSimple() throws CompileException {
         assertEquals(JAVA_CLASS, compileMagmaToJava(compileJavaToMagma(JAVA_CLASS)));
     }
 }
---
+++ magmac/test/java/com/meti/CompileException.java
@@ -0,0 +1,8 @@
+package com.meti;
+
+public class CompileException extends Exception {
+
+    public CompileException(String message) {
+        super(message);
+    }
+}
