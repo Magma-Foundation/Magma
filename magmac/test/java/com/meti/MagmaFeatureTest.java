b8681f2c5d9b886d7f17a4e0211b5ea6e9dbce75
SirMathhman <sirmathhman@gmail.com>
2024-04-11 15:47:15 -0400

    Separate tests

---
+++ magmac/test/java/com/meti/MagmaFeatureTest.java
@@ -0,0 +1,34 @@
+package com.meti;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static com.meti.Application.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class MagmaFeatureTest {
+    private static void assertMagma(String input) {
+        try {
+            assertEquals(input, compileJavaToMagma(compileMagmaToJava(input)));
+        } catch (CompileException e) {
+            fail(e);
+        }
+    }
+
+    @Test
+    void magmaInvalid() {
+        assertThrows(CompileException.class, () -> compileMagmaToJava(CompiledTest.TEST_UPPER_SYMBOL));
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"First", "Second"})
+    void magmaSimple(String name) {
+        assertMagma(renderMagmaFunction(name));
+    }
+
+    @Test
+    void magmaPublic() {
+        assertMagma(EXPORT_KEYWORD + renderMagmaFunction(CompiledTest.TEST_UPPER_SYMBOL));
+    }
+}
