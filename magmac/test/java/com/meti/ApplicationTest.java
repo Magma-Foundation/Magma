19576908975763f45d9553bf7030fe49d3f3060d
SirMathhman <sirmathhman@gmail.com>
2024-04-20 18:23:37 -0400

    Compile simple class names

--- magmac/test/java/com/meti/ApplicationTest.java
+++ magmac/test/java/com/meti/ApplicationTest.java
@@ -1,14 +1,33 @@
 package com.meti;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class ApplicationTest {
-    public static final String CLASS_STRING = "class def ApplicationTest() => {}";
 
-    @Test
-    void test() {
-        assertEquals(CLASS_STRING, CLASS_STRING);
+    public static final String CLASS_KEYWORD_WITH_SPACE = "class ";
+    public static final String CLASS_CONTENT = " {}";
+
+    private static String renderMagmaFunction(String name) {
+        return CLASS_KEYWORD_WITH_SPACE + "def " + name + "() =>" + CLASS_CONTENT;
+    }
+
+    private static String renderJavaClass(String name) {
+        return CLASS_KEYWORD_WITH_SPACE + name + CLASS_CONTENT;
+    }
+
+    private static String compile(String input) {
+        var name = input.substring(CLASS_KEYWORD_WITH_SPACE.length(), input.indexOf(CLASS_CONTENT));
+        return renderMagmaFunction(name);
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"First", "Second"})
+    void simple(String name) {
+        var input = renderJavaClass(name);
+        var output = renderMagmaFunction(name);
+        assertEquals(output, compile(input));
     }
 }
