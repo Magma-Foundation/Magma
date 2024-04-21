0b77fba861e16aaabb1183f1acc12d1d870f4999
SirMathhman <sirmathhman@gmail.com>
2024-04-21 19:45:56 -0400

    Move stuff to Compiler

--- magmac/test/java/com/meti/ApplicationTest.java
+++ magmac/test/java/com/meti/ApplicationTest.java
@@ -4,7 +4,6 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
-import java.util.*;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
@@ -12,104 +11,19 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class ApplicationTest {
 
-    public static final String CLASS_KEYWORD_WITH_SPACE = "class ";
-    public static final String CLASS_CONTENT = " {}";
     public static final String TEST_UPPER_SYMBOL = "Test";
-    public static final String PUBLIC_KEYWORD_WITH_SPACE = "public ";
-    public static final String EXPORT_KEYWORD_WITH_SPACE = "export ";
     public static final String TEST_LOWER_SYMBOL = "test";
-
-    private static String renderMagmaFunction(String name) {
-        return renderMagmaFunction("", name);
-    }
-
-    private static String renderMagmaFunction(String modifierString, String name) {
-        return modifierString + CLASS_KEYWORD_WITH_SPACE + "def " + name + "() =>" + CLASS_CONTENT;
-    }
 
     private static String renderJavaClass(String name) {
         return renderJavaClass("", name);
     }
 
     private static String renderJavaClass(String modifierString, String name) {
-        return modifierString + CLASS_KEYWORD_WITH_SPACE + name + CLASS_CONTENT;
-    }
-
-    private static String compile(String input) {
-        var lines = input.split(";");
-
-        var rootImportsWithChildren = new HashMap<String, List<String>>();
-        var rootImports = new HashSet<String>();
-
-        for (int i = 0; i < lines.length - 1; i++) {
-            String line = lines[i];
-            var option = compileStatement(line);
-            if (option.isPresent()) {
-                var value = option.get();
-                if (value instanceof MagmaImport node) {
-                    var parent = node.parent();
-                    var child = node.child();
-
-                    if (!rootImportsWithChildren.containsKey(parent)) {
-                        rootImportsWithChildren.put(parent, new ArrayList<>());
-                    }
-
-                    rootImportsWithChildren.get(parent).addAll(child);
-                } else if (value instanceof Import node) {
-                    rootImports.add(node.name());
-                } else {
-                    throw new UnsupportedOperationException("Unknown node type: " + value.getClass());
-                }
-            }
-        }
-
-        var parentSet = new HashSet<>(rootImportsWithChildren.keySet());
-        parentSet.addAll(rootImports);
-
-        var combinedLines = parentSet.stream()
-                .sorted()
-                .map(parent -> {
-                    var outputImports = new ArrayList<Node>();
-                    if (rootImports.contains(parent)) outputImports.add(new Import(parent));
-                    if(rootImportsWithChildren.containsKey(parent)) {
-                        outputImports.add(new MagmaImport(parent, rootImportsWithChildren.get(parent)));
-                    }
-
-                    return outputImports;
-                })
-                .flatMap(Collection::stream)
-                .map(Node::render)
-                .collect(Collectors.joining());
-
-        return combinedLines + compileClass(lines[lines.length - 1]);
-    }
-
-    private static Optional<Node> compileStatement(String before) {
-        if (!before.startsWith(Compiler.IMPORT_KEYWORD_WITH_SPACE)) return Optional.empty();
-
-        var total = before.substring(Compiler.IMPORT_KEYWORD_WITH_SPACE.length());
-        var parentSeparator = total.lastIndexOf('.');
-        if (parentSeparator == -1) {
-            return Optional.of(new Import(total));
-        } else {
-            var parent = total.substring(0, parentSeparator);
-            var child = total.substring(parentSeparator + 1);
-            return Optional.of(new MagmaImport(parent, child));
-        }
-    }
-
-    private static String compileClass(String input) {
-        var nameStart = input.indexOf(CLASS_KEYWORD_WITH_SPACE) + CLASS_KEYWORD_WITH_SPACE.length();
-        var nameEnd = input.indexOf(CLASS_CONTENT);
-        var name = input.substring(nameStart, nameEnd);
-
-        var modifierString = input.startsWith(PUBLIC_KEYWORD_WITH_SPACE) ? EXPORT_KEYWORD_WITH_SPACE : "";
-
-        return renderMagmaFunction(modifierString, name);
+        return modifierString + Compiler.CLASS_KEYWORD_WITH_SPACE + name + Compiler.CLASS_CONTENT;
     }
 
     private static void assertCompile(String input, String output) {
-        assertEquals(output, compile(input));
+        assertEquals(output, Compiler.compile(input));
     }
 
     private static String renderPackageStatement(String name) {
@@ -117,7 +31,7 @@ public class ApplicationTest {
     }
 
     private static void assertCompileWithClass(String input, String output) {
-        assertCompile(input + renderJavaClass(TEST_UPPER_SYMBOL), output + renderMagmaFunction(TEST_UPPER_SYMBOL));
+        assertCompile(input + renderJavaClass(TEST_UPPER_SYMBOL), output + Compiler.renderMagmaFunction(TEST_UPPER_SYMBOL));
     }
 
     private static String renderJavaImport(String parent, String child) {
@@ -167,12 +81,12 @@ public class ApplicationTest {
 
     @Test
     void publicClass() {
-        assertCompile(renderJavaClass(PUBLIC_KEYWORD_WITH_SPACE, TEST_UPPER_SYMBOL), renderMagmaFunction(EXPORT_KEYWORD_WITH_SPACE, TEST_UPPER_SYMBOL));
+        assertCompile(renderJavaClass(Compiler.PUBLIC_KEYWORD_WITH_SPACE, TEST_UPPER_SYMBOL), Compiler.renderMagmaFunction(Compiler.EXPORT_KEYWORD_WITH_SPACE, TEST_UPPER_SYMBOL));
     }
 
     @ParameterizedTest
     @ValueSource(strings = {"First", "Second"})
     void simpleClass(String name) {
-        assertCompile(renderJavaClass(name), renderMagmaFunction(name));
+        assertCompile(renderJavaClass(name), Compiler.renderMagmaFunction(name));
     }
 }
--- magmac/test/java/com/meti/Compiler.java
+++ magmac/test/java/com/meti/Compiler.java
@@ -1,10 +1,102 @@
 package com.meti;
 
+import java.util.*;
+import java.util.stream.Collectors;
+
 public class Compiler {
     public static final String IMPORT_KEYWORD_WITH_SPACE = "import ";
     public static final String STATEMENT_END = ";";
+    public static final String CLASS_KEYWORD_WITH_SPACE = "class ";
+    public static final String CLASS_CONTENT = " {}";
+    public static final String PUBLIC_KEYWORD_WITH_SPACE = "public ";
+    public static final String EXPORT_KEYWORD_WITH_SPACE = "export ";
 
     static String renderImport(String importString) {
         return IMPORT_KEYWORD_WITH_SPACE + importString + STATEMENT_END;
+    }
+
+    static String renderMagmaFunction(String modifierString, String name) {
+        return modifierString + CLASS_KEYWORD_WITH_SPACE + "def " + name + "() =>" + CLASS_CONTENT;
+    }
+
+    static String compile(String input) {
+        var lines = input.split(";");
+
+        var rootImportsWithChildren = new HashMap<String, List<String>>();
+        var rootImports = new HashSet<String>();
+
+        for (int i = 0; i < lines.length - 1; i++) {
+            String line = lines[i];
+            var option = compileStatement(line);
+            if (option.isEmpty()) continue;
+
+            var value = option.get();
+            if (value instanceof MagmaImport node) {
+                var parent = node.parent();
+                var child = node.child();
+
+                ensureKey(rootImportsWithChildren, parent);
+
+                rootImportsWithChildren.get(parent).addAll(child);
+            } else if (value instanceof Import node) {
+                rootImports.add(node.name());
+            } else {
+                throw new UnsupportedOperationException("Unknown node type: " + value.getClass());
+            }
+        }
+
+        var parentSet = new HashSet<>(rootImportsWithChildren.keySet());
+        parentSet.addAll(rootImports);
+
+        var combinedLines = parentSet.stream()
+                .sorted()
+                .map(parent -> createImports(parent, rootImports, rootImportsWithChildren))
+                .flatMap(Collection::stream)
+                .map(Node::render)
+                .collect(Collectors.joining());
+
+        return combinedLines + compileClass(lines[lines.length - 1]);
+    }
+
+    private static void ensureKey(Map<String, List<String>> map, String key) {
+        if (!map.containsKey(key)) {
+            map.put(key, new ArrayList<>());
+        }
+    }
+
+    private static List<Node> createImports(String parent, HashSet<String> rootImports, HashMap<String, List<String>> rootImportsWithChildren) {
+        var outputImports = new ArrayList<Node>();
+        if (rootImports.contains(parent)) outputImports.add(new Import(parent));
+        if (rootImportsWithChildren.containsKey(parent))
+            outputImports.add(new MagmaImport(parent, rootImportsWithChildren.get(parent)));
+
+        return outputImports;
+    }
+
+    private static Optional<Node> compileStatement(String before) {
+        if (!before.startsWith(IMPORT_KEYWORD_WITH_SPACE)) return Optional.empty();
+
+        var total = before.substring(IMPORT_KEYWORD_WITH_SPACE.length());
+        var parentSeparator = total.lastIndexOf('.');
+        if (parentSeparator == -1) {
+            return Optional.of(new Import(total));
+        } else {
+            var parent = total.substring(0, parentSeparator);
+            var child = total.substring(parentSeparator + 1);
+            return Optional.of(new MagmaImport(parent, child));
+        }
+    }
+
+    private static String compileClass(String input) {
+        var nameStart = input.indexOf(CLASS_KEYWORD_WITH_SPACE) + CLASS_KEYWORD_WITH_SPACE.length();
+        var nameEnd = input.indexOf(CLASS_CONTENT);
+        var name = input.substring(nameStart, nameEnd);
+
+        var modifierString = input.startsWith(PUBLIC_KEYWORD_WITH_SPACE) ? EXPORT_KEYWORD_WITH_SPACE : "";
+        return renderMagmaFunction(modifierString, name);
+    }
+
+    public static String renderMagmaFunction(String name) {
+        return renderMagmaFunction("", name);
     }
 }
