651c48885815bbf93aff0a0a7564fedc4045e57d
SirMathhman <sirmathhman@gmail.com>
2024-04-23 18:14:08 -0400

    Magma definition types

--- magmac/test/java/com/meti/ApplicationTest.java
+++ magmac/test/java/com/meti/ApplicationTest.java
@@ -10,34 +10,51 @@ public class ApplicationTest {
     public static final String USE_STRICT = "\"use strict\"\n";
     public static final String PREFIX = "class Main(){public static void main(String[] args){";
     public static final String SUFFIX = "}}";
-    public static final String INT_KEYWORD = "int ";
+    public static final String INT_TYPE = "int";
+    public static final String INT_KEYWORD_WITH_SPACE = INT_TYPE + " ";
     public static final String DEFINITION_SUFFIX = " = 0;";
     public static final String LET_KEYWORD_WITH_SPACE = "let ";
+    public static final String TEST_LOWER_SYMBOL = "test";
+    public static final String I16_TYPE = "I16";
+    public static final String NUMBER_TYPE = "number";
+    public static final String TYPE_SEPARATOR = " : ";
+    public static final String LONG_TYPE = "long";
+    public static final String I32_TYPE = "I32";
 
     private static String renderCProgram() {
         return renderCProgram("");
     }
 
     private static String renderCProgram(String input) {
-        return "int main(){" + input + "return 0;}";
+        return INT_KEYWORD_WITH_SPACE + " main(){" + input + "return 0;}";
     }
 
     private static String compileMagmaFromJava(String javaSource) {
         var content = javaSource.substring(PREFIX.length(), javaSource.indexOf(SUFFIX));
-        if (content.isEmpty()) {
-            return "";
+        if (content.isEmpty()) return "";
+
+        var before = content.substring(0, content.indexOf(DEFINITION_SUFFIX));
+        var separator = before.indexOf(' ');
+        var name = before.substring(separator + 1);
+        var inputType = before.substring(0, separator);
+        String outputType;
+        if(inputType.equals(INT_TYPE)) {
+            outputType = I16_TYPE;
+        } else if(inputType.equals(LONG_TYPE)) {
+            outputType = I32_TYPE;
         } else {
-            var name = content.substring(INT_KEYWORD.length(), content.indexOf(DEFINITION_SUFFIX));
-            return renderTSDefinition(name);
+            throw new UnsupportedOperationException("Unknown type: " + inputType);
         }
+
+        return renderTSDefinition(name, outputType);
     }
 
     private static String wrapInMain(String content) {
         return PREFIX + content + SUFFIX;
     }
 
-    private static String renderTSDefinition(String name) {
-        return LET_KEYWORD_WITH_SPACE + name + SUFFIX;
+    private static String renderTSDefinition(String name, String type) {
+        return LET_KEYWORD_WITH_SPACE + name + TYPE_SEPARATOR + type + DEFINITION_SUFFIX;
     }
 
     private static String compileTSFromMagma(String input) {
@@ -50,8 +67,8 @@ public class ApplicationTest {
         return USE_STRICT + output;
     }
 
-    private static String renderJavaDefinition(String name) {
-        return INT_KEYWORD + name + DEFINITION_SUFFIX;
+    private static String renderJavaDefinition(String type, String name) {
+        return type + " " + name + DEFINITION_SUFFIX;
     }
 
     private static String compileCFromMagma(String input) {
@@ -59,8 +76,8 @@ public class ApplicationTest {
         if (input.isEmpty()) {
             output = "";
         } else {
-            var name = input.substring(LET_KEYWORD_WITH_SPACE.length(), input.indexOf(SUFFIX));
-            output = renderJavaDefinition(name);
+            var name = input.substring(LET_KEYWORD_WITH_SPACE.length(), input.indexOf(TYPE_SEPARATOR));
+            output = renderJavaDefinition(INT_TYPE, name);
         }
         return renderCProgram(output);
     }
@@ -73,13 +90,18 @@ public class ApplicationTest {
     @ParameterizedTest
     @ValueSource(strings = {"first", "second"})
     void javaDefinition(String name) {
-        assertEquals(renderTSDefinition(name), compileMagmaFromJava(wrapInMain(renderJavaDefinition(name))));
+        assertEquals(renderTSDefinition(name, I16_TYPE), compileMagmaFromJava(wrapInMain(renderJavaDefinition(INT_TYPE, name))));
+    }
+
+    @Test
+    void javaDefinitionType() {
+        assertEquals(renderTSDefinition(TEST_LOWER_SYMBOL, I32_TYPE), compileMagmaFromJava(wrapInMain(renderJavaDefinition(LONG_TYPE, TEST_LOWER_SYMBOL))));
     }
 
     @ParameterizedTest
     @ValueSource(strings = {"first", "second"})
     void tsDefinition(String name) {
-        assertEquals(USE_STRICT + renderTSDefinition(name), compileTSFromMagma(renderTSDefinition(name)));
+        assertEquals(USE_STRICT + renderTSDefinition(name, I16_TYPE), compileTSFromMagma(renderTSDefinition(name, I16_TYPE)));
     }
 
     @Test
@@ -95,6 +117,6 @@ public class ApplicationTest {
     @ParameterizedTest
     @ValueSource(strings = {"first", "second"})
     void cDefinition(String name) {
-        assertEquals(renderCProgram(renderJavaDefinition(name)), compileCFromMagma(renderTSDefinition(name)));
+        assertEquals(renderCProgram(renderJavaDefinition(INT_TYPE, name)), compileCFromMagma(renderTSDefinition(name, I16_TYPE)));
     }
 }
