1222c2e56b7cf9196da7e1197d6834efaa2b6988
SirMathhman <sirmathhman@gmail.com>
2024-04-23 16:07:44 -0400

    Compile C

--- magmac/test/java/com/meti/ApplicationTest.java
+++ magmac/test/java/com/meti/ApplicationTest.java
@@ -7,12 +7,20 @@ import org.junit.jupiter.params.provider.ValueSource;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class ApplicationTest {
-    public static final String USE_STRICT = "\"use strict\"";
-    public static final String C_PROGRAM = "int main(){return 0;}";
+    public static final String USE_STRICT = "\"use strict\"\n";
     public static final String PREFIX = "class Main(){public static void main(String[] args){";
     public static final String SUFFIX = "}}";
     public static final String INT_KEYWORD = "int ";
     public static final String DEFINITION_SUFFIX = " = 0;";
+    public static final String LET_KEYWORD_WITH_SPACE = "let ";
+
+    private static String renderCProgram() {
+        return renderCProgram("");
+    }
+
+    private static String renderCProgram(String input) {
+        return "int main(){" + input + "return 0;}";
+    }
 
     private static String compileMagmaFromJava(String javaSource) {
         var content = javaSource.substring(PREFIX.length(), javaSource.indexOf(SUFFIX));
@@ -20,7 +28,7 @@ public class ApplicationTest {
             return "";
         } else {
             var name = content.substring(INT_KEYWORD.length(), content.indexOf(DEFINITION_SUFFIX));
-            return renderMagmaDefinition(name);
+            return renderTSDefinition(name);
         }
     }
 
@@ -28,8 +36,33 @@ public class ApplicationTest {
         return PREFIX + content + SUFFIX;
     }
 
-    private static String renderMagmaDefinition(String name) {
-        return "let " + name + SUFFIX;
+    private static String renderTSDefinition(String name) {
+        return LET_KEYWORD_WITH_SPACE + name + SUFFIX;
+    }
+
+    private static String compileTSFromMagma(String input) {
+        String output;
+        if (input.isEmpty()) {
+            output = "";
+        } else {
+            output = input;
+        }
+        return USE_STRICT + output;
+    }
+
+    private static String renderJavaDefinition(String name) {
+        return INT_KEYWORD + name + DEFINITION_SUFFIX;
+    }
+
+    private static String compileCFromMagma(String input) {
+        String output;
+        if (input.isEmpty()) {
+            output = "";
+        } else {
+            var name = input.substring(LET_KEYWORD_WITH_SPACE.length(), input.indexOf(SUFFIX));
+            output = renderJavaDefinition(name);
+        }
+        return renderCProgram(output);
     }
 
     @Test
@@ -40,16 +73,28 @@ public class ApplicationTest {
     @ParameterizedTest
     @ValueSource(strings = {"first", "second"})
     void javaDefinition(String name) {
-        assertEquals(renderMagmaDefinition(name), compileMagmaFromJava(wrapInMain(INT_KEYWORD + name + DEFINITION_SUFFIX)));
+        assertEquals(renderTSDefinition(name), compileMagmaFromJava(wrapInMain(renderJavaDefinition(name))));
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"first", "second"})
+    void tsDefinition(String name) {
+        assertEquals(USE_STRICT + renderTSDefinition(name), compileTSFromMagma(renderTSDefinition(name)));
     }
 
     @Test
-    void compileJS() {
-        assertEquals(USE_STRICT, USE_STRICT);
+    void ts() {
+        assertEquals(USE_STRICT, compileTSFromMagma(""));
     }
 
     @Test
     void compileC() {
-        assertEquals(C_PROGRAM, C_PROGRAM);
+        assertEquals(renderCProgram(), compileCFromMagma(""));
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"first", "second"})
+    void cDefinition(String name) {
+        assertEquals(renderCProgram(renderJavaDefinition(name)), compileCFromMagma(renderTSDefinition(name)));
     }
 }
