b9011d6a0c6918dcc35ab08ad81b74946191760c
SirMathhman <sirmathhman@gmail.com>
2024-04-23 15:18:42 -0400

    Remove bad code

--- magmac/src/java/com/meti/Import.java
+++
@@ -1,8 +0,0 @@
-package com.meti;
-
-public record Import(String name) implements Node {
-    @Override
-    public String render() {
-        return Compiler.renderImport(name());
-    }
-}
--- magmac/src/java/com/meti/Node.java
+++
@@ -1,5 +0,0 @@
-package com.meti;
-
-public interface Node {
-    String render();
-}
