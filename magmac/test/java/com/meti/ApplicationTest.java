47a329a5ba9edc0dc35983cc4f93bace1d77a0fb
SirMathhman <sirmathhman@gmail.com>
2024-04-24 15:55:32 -0400

    Subfunctions OK

--- magmac/test/java/com/meti/ApplicationTest.java
+++ magmac/test/java/com/meti/ApplicationTest.java
@@ -1,9 +1,16 @@
 package com.meti;
 
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class ApplicationTest {
     public static final String CLASS_KEYWORD_WITH_SPACE = "class ";
@@ -11,25 +18,88 @@ public class ApplicationTest {
     public static final String DEF_KEYWORD_WITH_SPACE = "def ";
     public static final String MAGMA_FUNCTION_PREFIX = CLASS_KEYWORD_WITH_SPACE + DEF_KEYWORD_WITH_SPACE;
     public static final String FUNCTION_SEPARATOR = "() =>";
+    public static final String TEST_LOWER_SYMBOL = "test";
 
     private static String renderMagmaFunction(String name) {
         return MAGMA_FUNCTION_PREFIX + name + FUNCTION_SEPARATOR + CLASS_CONTENT;
     }
 
     private static String run(String input, boolean isJava) throws CompileException {
+        return getString(input, isJava)
+                .entrySet()
+                .stream()
+                .findFirst()
+                .orElseThrow()
+                .getValue();
+    }
+
+    private static Map<String, String> getString(String input, boolean isJava) throws CompileException {
         if (isJava) {
-            if (input.startsWith(CLASS_KEYWORD_WITH_SPACE)) {
-                var name = input.substring(CLASS_KEYWORD_WITH_SPACE.length(), input.indexOf(CLASS_CONTENT));
-                return renderMagmaFunction(name);
-            }
+            return compileMagmaFromJava(input);
         } else {
+            return compileJavaFromMagma(input);
+        }
+    }
+
+    private static Map<String, String> compileMagmaFromJava(String input) throws CompileException {
+        var classIndex = input.indexOf(CLASS_KEYWORD_WITH_SPACE);
+        if (classIndex == -1) {
+            throw new CompileException("No class present.");
+        } else {
+            var next = input.indexOf(CLASS_KEYWORD_WITH_SPACE, classIndex + CLASS_KEYWORD_WITH_SPACE.length());
+            if (next == -1) {
+                var name = input.substring(CLASS_KEYWORD_WITH_SPACE.length(), input.indexOf(CLASS_CONTENT));
+                return Map.of(name, renderMagmaFunction(name));
+            } else {
+                throw new CompileException("Multiple classes are not allowed.");
+            }
+        }
+    }
+
+    private static HashMap<String, String> compileJavaFromMagma(String input) throws CompileException {
+        var lines = split(input);
+        var set = new HashSet<String>();
+        var map = new HashMap<String, String>();
+
+        for (String sequence : lines) {
             if (input.startsWith(MAGMA_FUNCTION_PREFIX)) {
                 var name = input.substring(MAGMA_FUNCTION_PREFIX.length(), input.indexOf(FUNCTION_SEPARATOR));
-                return renderJavaClass(name);
+                if (set.contains(name)) {
+                    throw new CompileException("Two functions cannot have the same name of: " + name);
+                } else {
+                    set.add(name);
+                    map.put(name, renderJavaClass(name));
+                }
+            } else {
+                throw new CompileException("Invalid token sequence: " + sequence);
             }
         }
 
-        throw new CompileException("No class present.");
+        return map;
+    }
+
+    private static ArrayList<String> split(String input) {
+        var lines = new ArrayList<String>();
+        var builder = new StringBuilder();
+        var depth = 0;
+
+        for (int i = 0; i < input.length(); i++) {
+            var c = input.charAt(i);
+            if (c == '}' && depth == 1) {
+                builder.append(c);
+                depth = 0;
+                lines.add(builder.toString());
+                builder = new StringBuilder();
+            } else {
+                if (c == '{') depth++;
+                if (c == '}') depth--;
+                builder.append(c);
+            }
+        }
+
+        lines.add(builder.toString());
+        lines.removeIf(String::isBlank);
+        return lines;
     }
 
     private static String renderJavaClass(String name) {
@@ -40,6 +110,16 @@ public class ApplicationTest {
     @ValueSource(strings = {"First", "Second"})
     void compileMagmaToJava(String name) throws CompileException {
         assertEquals(renderJavaClass(name), run(renderMagmaFunction(name), false));
+    }
+
+    @Test
+    void invalidJava() {
+        assertThrows(CompileException.class, () -> run(renderJavaClass(TEST_LOWER_SYMBOL).repeat(2), true));
+    }
+
+    @Test
+    void invalidMagma() {
+        assertThrows(CompileException.class, () -> run(renderMagmaFunction(TEST_LOWER_SYMBOL).repeat(2), false));
     }
 
     @ParameterizedTest
