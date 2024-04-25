173be728c0d54c83344c2a1d64413ed03eb5ec7e
SirMathhman <sirmathhman@gmail.com>
2024-04-25 12:32:30 -0400

    Implementation

--- magmac/test/java/com/meti/ApplicationTest.java
+++ magmac/test/java/com/meti/ApplicationTest.java
@@ -1,19 +1,31 @@
 package com.meti;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class ApplicationTest {
 
-    public static final String TEST_FUNCTION = "class def ApplicationTest() => {}";
+    public static final String CLASS_KEYWORD_WITH_SPACE = "class ";
+    public static final String CLASS_END = " {}";
 
-    @Test
-    void test() {
-        assertEquals(TEST_FUNCTION, run());
+    private static String renderMagmaFunction(String name) {
+        return CLASS_KEYWORD_WITH_SPACE + "def " + name + "() =>" + CLASS_END;
     }
 
-    private static String run() {
-        return TEST_FUNCTION;
+    private static String run(String input) {
+        if (input.startsWith(CLASS_KEYWORD_WITH_SPACE)) {
+            var name = input.substring(CLASS_KEYWORD_WITH_SPACE.length(), input.indexOf(CLASS_END));
+            return renderMagmaFunction(name);
+        } else {
+            throw new UnsupportedOperationException("No class present.");
+        }
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"first", "second"})
+    void className(String name) {
+        assertEquals(renderMagmaFunction(name), run(CLASS_KEYWORD_WITH_SPACE + name + CLASS_END));
     }
 }
