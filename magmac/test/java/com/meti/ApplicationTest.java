6653a856ae04344c05c708cf20d49734ca232895
SirMathhman <sirmathhman@gmail.com>
2024-04-25 12:40:06 -0400

    Add export keyword

--- magmac/test/java/com/meti/ApplicationTest.java
+++ magmac/test/java/com/meti/ApplicationTest.java
@@ -1,5 +1,6 @@
 package com.meti;
 
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
@@ -9,23 +10,44 @@ public class ApplicationTest {
 
     public static final String CLASS_KEYWORD_WITH_SPACE = "class ";
     public static final String CLASS_END = " {}";
+    public static final String TEST_UPPER_SYMBOL = "Test";
+    public static final String EXPORT_KEYWORD_WITH_SPACE = "export ";
+    public static final String PUBLIC_KEYWORD_WITH_SPACE = "public ";
 
     private static String renderMagmaFunction(String name) {
-        return CLASS_KEYWORD_WITH_SPACE + "def " + name + "() =>" + CLASS_END;
+        return renderMagmaFunction("", name);
+    }
+
+    private static String renderMagmaFunction(String modifiersString, String name) {
+        return modifiersString + CLASS_KEYWORD_WITH_SPACE + "def " + name + "() =>" + CLASS_END;
     }
 
     private static String run(String input) {
-        if (input.startsWith(CLASS_KEYWORD_WITH_SPACE)) {
-            var name = input.substring(CLASS_KEYWORD_WITH_SPACE.length(), input.indexOf(CLASS_END));
-            return renderMagmaFunction(name);
-        } else {
-            throw new UnsupportedOperationException("No class present.");
-        }
+        var classIndex = input.indexOf(CLASS_KEYWORD_WITH_SPACE);
+        if (classIndex == -1) throw new UnsupportedOperationException("No class present.");
+
+        var name = input.substring(classIndex + CLASS_KEYWORD_WITH_SPACE.length(), input.indexOf(CLASS_END));
+        var modifierString = input.startsWith(PUBLIC_KEYWORD_WITH_SPACE) ? EXPORT_KEYWORD_WITH_SPACE : "";
+
+        return renderMagmaFunction(modifierString, name);
+    }
+
+    private static String renderJavaClass(String name) {
+        return renderJavaClass("", name);
+    }
+
+    private static String renderJavaClass(String modifiersString, String name) {
+        return modifiersString + CLASS_KEYWORD_WITH_SPACE + name + CLASS_END;
+    }
+
+    @Test
+    void classPublic() {
+        assertEquals(renderMagmaFunction(EXPORT_KEYWORD_WITH_SPACE, TEST_UPPER_SYMBOL), run(renderJavaClass(PUBLIC_KEYWORD_WITH_SPACE, TEST_UPPER_SYMBOL)));
     }
 
     @ParameterizedTest
     @ValueSource(strings = {"first", "second"})
     void className(String name) {
-        assertEquals(renderMagmaFunction(name), run(CLASS_KEYWORD_WITH_SPACE + name + CLASS_END));
+        assertEquals(renderMagmaFunction(name), run(renderJavaClass(name)));
     }
 }
