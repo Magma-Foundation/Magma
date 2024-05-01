4846032099ae46dee1be954cb8d2359cde1f563b
SirMathhman <sirmathhman@gmail.com>
2024-05-01 15:16:59 -0400

    Extract Compiler

--- magmac/test/java/com/meti/ApplicationTest.java
+++ magmac/test/java/com/meti/ApplicationTest.java
@@ -4,34 +4,35 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
+import static com.meti.Compiler.*;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class ApplicationTest {
-
     private static void assertRun(String input, String output) {
-        assertEquals(output, Compiler.run(input));
+        assertEquals(output, run(input));
     }
 
-    @Test
-    void importChild() {
-        assertRun(Compiler.JAVA_IMPORT + Compiler.renderJavaClass(Compiler.TEST_UPPER_SYMBOL),
-                Compiler.MAGMA_IMPORT + Compiler.renderMagmaFunction(Compiler.TEST_UPPER_SYMBOL));
+    @ParameterizedTest
+    @ValueSource(strings = {"First", "Second"})
+    void importChild(String name) {
+        assertRun(renderJavaImport(name) + renderJavaClass(TEST_UPPER_SYMBOL),
+                renderMagmaImport(name) + renderMagmaFunction(TEST_UPPER_SYMBOL));
     }
 
     @ParameterizedTest
     @ValueSource(strings = {"First", "Second"})
     void className(String name) {
-        assertRun(Compiler.renderJavaClass(name), Compiler.renderMagmaFunction(name));
+        assertRun(renderJavaClass(name), renderMagmaFunction(name));
     }
 
     @ParameterizedTest
     @ValueSource(strings = {"first", "second"})
     void packageStatement(String name) {
-        assertRun("package " + name + ";" + Compiler.renderJavaClass(Compiler.TEST_UPPER_SYMBOL), Compiler.renderMagmaFunction(Compiler.TEST_UPPER_SYMBOL));
+        assertRun("package " + name + ";" + renderJavaClass(TEST_UPPER_SYMBOL), renderMagmaFunction(TEST_UPPER_SYMBOL));
     }
 
     @Test
     void classPublic() {
-        assertRun(Compiler.renderJavaClass(Compiler.TEST_UPPER_SYMBOL, Compiler.PUBLIC_KEYWORD_WITH_SPACE), Compiler.renderMagmaFunction(Compiler.TEST_UPPER_SYMBOL, Compiler.EXPORT_KEYWORD_WITH_SPACE));
+        assertRun(renderJavaClass(TEST_UPPER_SYMBOL, PUBLIC_KEYWORD_WITH_SPACE), renderMagmaFunction(TEST_UPPER_SYMBOL, EXPORT_KEYWORD_WITH_SPACE));
     }
 }
--- magmac/test/java/com/meti/Compiler.java
+++ magmac/test/java/com/meti/Compiler.java
@@ -6,8 +6,17 @@ public class Compiler {
     public static final String TEST_UPPER_SYMBOL = "Test";
     public static final String PUBLIC_KEYWORD_WITH_SPACE = "public ";
     public static final String EXPORT_KEYWORD_WITH_SPACE = "export ";
-    public static final String JAVA_IMPORT = "import parent.Child;";
-    public static final String MAGMA_IMPORT = "import { Child } from parent;";
+    public static final String IMPORT_KEYWORD = "import ";
+    public static final String IMPORT_BEFORE = IMPORT_KEYWORD + "parent.";
+    public static final String STATEMENT_END = ";";
+
+    public static String renderJavaImport(String name) {
+        return IMPORT_KEYWORD + "parent." + name + STATEMENT_END;
+    }
+
+    public static String renderMagmaImport(String name) {
+        return IMPORT_KEYWORD + "{ " + name + " } from parent" + STATEMENT_END;
+    }
 
     static String renderMagmaFunction(String name) {
         return renderMagmaFunction(name, "");
@@ -26,26 +35,23 @@ public class Compiler {
     }
 
     static String run(String input) {
-        var separator = input.indexOf(';');
+        var separator = input.indexOf(STATEMENT_END);
         if (separator == -1) {
             return compileClass(input);
-        } else {
-            var before = input.substring(0, separator + 1);
-            var after = input.substring(separator + 1);
-
-            var newBefore = computeNewBefore(before);
-            return newBefore + compileClass(after);
         }
+
+        var before = input.substring(0, separator + 1);
+        var after = input.substring(separator + 1);
+
+        var newBefore = compileStatement(before);
+        return newBefore + compileClass(after);
     }
 
-    private static String computeNewBefore(String before) {
-        String newBefore;
-        if (before.equals(JAVA_IMPORT)) {
-            newBefore = MAGMA_IMPORT;
-        } else {
-            newBefore = "";
-        }
-        return newBefore;
+    private static String compileStatement(String before) {
+        if (!before.startsWith(IMPORT_BEFORE)) return "";
+
+        var name = before.substring(IMPORT_BEFORE.length(), before.indexOf(STATEMENT_END));
+        return renderMagmaImport(name);
     }
 
     private static String compileClass(String input) {
