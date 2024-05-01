86552ed7b2d9d05cbc4dc1c15d86d2dd3a249a5c
SirMathhman <sirmathhman@gmail.com>
2024-05-01 14:43:13 -0400

    Add class names

--- magmac/test/java/com/meti/ApplicationTest.java
+++ magmac/test/java/com/meti/ApplicationTest.java
@@ -1,14 +1,30 @@
 package com.meti;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class ApplicationTest {
-    public static final String MAGMA_FUNCTION = "class def ApplicationTest() => {}";
+    public static final String CLASS_KEYWORD_WITH_SPACE = "class ";
+    public static final String CONTENT = " {}";
 
-    @Test
-    void test() {
-        assertEquals(MAGMA_FUNCTION, MAGMA_FUNCTION);
+    private static String renderMagmaFunction(String name) {
+        return CLASS_KEYWORD_WITH_SPACE + "def " + name + "() =>" + CONTENT;
+    }
+
+    private static String renderJavaClass(String name) {
+        return CLASS_KEYWORD_WITH_SPACE + name + CONTENT;
+    }
+
+    private static String run(String input) {
+        var name = input.substring(CLASS_KEYWORD_WITH_SPACE.length(), input.indexOf(CONTENT));
+        return renderMagmaFunction(name);
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"First", "Second"})
+    void className(String name) {
+        assertEquals(renderMagmaFunction(name), run(renderJavaClass(name)));
     }
 }
