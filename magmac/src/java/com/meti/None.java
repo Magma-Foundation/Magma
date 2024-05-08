f8384c9577f5b53b9e655631664cb8b11fb44454
SirMathhman <sirmathhman@gmail.com>
2024-05-07 21:10:11 -0400

    ???

--- magmac/src/java/com/meti/None.java
+++ magmac/src/java/com/meti/None.java
@@ -1,6 +1,7 @@
 package com.meti;
 
 import java.util.function.Function;
+import java.util.function.Supplier;
 
 public class None<T> implements Option<T> {
     @Override
@@ -11,5 +12,30 @@ public class None<T> implements Option<T> {
     @Override
     public <R> Option<R> map(Function<T, R> mapper) {
         return new None<>();
+    }
+
+    @Override
+    public Option<T> orLazy(Supplier<Option<T>> other) {
+        return other.get();
+    }
+
+    @Override
+    public <R> Option<R> flatMap(Function<T, Option<R>> mapper) {
+        return new None<>();
+    }
+
+    @Override
+    public T orElse(T other) {
+        return other;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return true;
+    }
+
+    @Override
+    public T orElseNull() {
+        return null;
     }
 }
