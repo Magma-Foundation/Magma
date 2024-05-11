fabaea88f33419815d1d81dde00b2eeaa2d313ae
SirMathhman <sirmathhman@gmail.com>
2024-05-11 17:27:43 -0400

    Just fail on compile

--- magmac/src/java/com/meti/Main.java
+++ magmac/src/java/com/meti/Main.java
@@ -10,9 +10,19 @@ public class Main {
         try {
             var input = Files.readString(source);
             var target = source.resolveSibling("Main.mgs");
-            Files.writeString(target, input);
-        } catch (IOException e) {
+            Files.writeString(target, compile(input));
+        } catch (IOException | CompileException e){
             throw new RuntimeException(source.toAbsolutePath().toString(), e);
+        }
+    }
+
+    private static String compile(String input) throws CompileException {
+        throw new CompileException("Unknown input: " + input);
+    }
+
+    static class CompileException extends Exception {
+        public CompileException(String message) {
+            super(message);
         }
     }
 }
