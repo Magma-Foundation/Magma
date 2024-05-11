6a46edd068f6897e921c7c2da5b0963ac0c51736
SirMathhman <sirmathhman@gmail.com>
2024-05-11 17:29:24 -0400

    Compile statements

--- magmac/src/java/com/meti/Main.java
+++ magmac/src/java/com/meti/Main.java
@@ -3,6 +3,7 @@ package com.meti;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
@@ -11,12 +12,37 @@ public class Main {
             var input = Files.readString(source);
             var target = source.resolveSibling("Main.mgs");
             Files.writeString(target, compile(input));
-        } catch (IOException | CompileException e){
+        } catch (IOException | CompileException e) {
             throw new RuntimeException(source.toAbsolutePath().toString(), e);
         }
     }
 
     private static String compile(String input) throws CompileException {
+        var lines = new ArrayList<String>();
+        var buffer = new StringBuilder();
+        var depth = 0;
+        for (int i = 0; i < input.length(); i++) {
+            var c = input.charAt(i);
+            if (c == ';' && depth == 0) {
+                lines.add(buffer.toString());
+                buffer = new StringBuilder();
+            } else {
+                if(c == '{') depth++;
+                if(c == '}') depth--;
+                buffer.append(c);
+            }
+        }
+        lines.add(buffer.toString());
+
+        var output = new StringBuilder();
+        for (String line : lines) {
+            output.append(compileClassMember(line));
+        }
+
+        return output.toString();
+    }
+
+    private static String compileClassMember(String input) throws CompileException {
         throw new CompileException("Unknown input: " + input);
     }
 
