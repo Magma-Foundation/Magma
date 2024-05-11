2706347c0b8aef7af2f8fc8036680bb490258d46
SirMathhman <sirmathhman@gmail.com>
2024-05-11 17:25:50 -0400

    OK

--- magmac/src/java/com/meti/Main.java
+++ magmac/src/java/com/meti/Main.java
@@ -2,17 +2,16 @@ package com.meti;
 
 import java.io.IOException;
 import java.nio.file.Files;
-import java.nio.file.Path;
 import java.nio.file.Paths;
 
 public class Main {
     public static void main(String[] args) {
+        var source = Paths.get(".", "magmac", "src", "java", "com", "meti", "Main.java");
         try {
-            var source = Paths.get(".", "src", "java", "com", "meti", "Main.java");
             var input = Files.readString(source);
             Files.writeString(source.resolve("Main.mgs"), input);
         } catch (IOException e) {
-            throw new RuntimeException(e);
+            throw new RuntimeException(source.toAbsolutePath().toString(), e);
         }
     }
 }
