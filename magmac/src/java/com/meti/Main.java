fa62b79eae9fff863a7bebae544495c0305127e9
SirMathhman <sirmathhman@gmail.com>
2024-05-11 23:47:23 -0400

    Add exceptions

--- magmac/src/java/com/meti/Main.java
+++ magmac/src/java/com/meti/Main.java
@@ -12,14 +12,18 @@ public class Main {
             extracted("mgs", "d.ts");
             extracted("mgs", "c");
             extracted("mgs", "h");
-        } catch (IOException e) {
+        } catch (IOException | CompileException e) {
             e.printStackTrace();
         }
     }
 
-    private static void extracted(String sourceExtension, String targetExtension) throws IOException {
+    private static void extracted(String sourceExtension, String targetExtension) throws IOException, CompileException {
         var source = Paths.get(".", "magmac", "src", "java", "com", "meti", "Main." + sourceExtension);
         var input = Files.readString(source);
-        Files.writeString(source.resolveSibling("Main." + targetExtension), input);
+        Files.writeString(source.resolveSibling("Main." + targetExtension), compile(input));
+    }
+
+    private static String compile(String input) throws CompileException {
+        throw new CompileException(input);
     }
 }
