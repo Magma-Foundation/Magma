86d18b1974f29dcd5132ec20ac4058571f37b3ec
SirMathhman <sirmathhman@gmail.com>
2024-06-11 12:08:44 -0400

    Just copy over content

--- magmac/src/magma/Main.java
+++ magmac/src/magma/Main.java
@@ -1,4 +1,19 @@
 package magma;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
 public class Main {
+    public static void main(String[] args) {
+        try {
+            var source = Paths.get(".", "magmac", "src", "magma", "Main.java");
+            var input = Files.readString(source);
+            var target = source.resolveSibling("Main.mgs");
+            Files.writeString(target, input);
+        } catch (IOException e) {
+            //noinspection CallToPrintStackTrace
+            e.printStackTrace();
+        }
+    }
 }
