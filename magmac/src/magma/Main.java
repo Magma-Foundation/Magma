dcb6f610f7aaa55d4b3902091a113ecd9a349e24
SirMathhman <sirmathhman@gmail.com>
2024-06-11 12:18:46 -0400

    Strip content

--- magmac/src/magma/Main.java
+++ magmac/src/magma/Main.java
@@ -28,26 +28,46 @@ public class Main {
         }
 
         var inputTokens = current.advance().tokens;
-        return String.join("", inputTokens);
+        var outputTokens = new ArrayList<String>();
+        for (String inputToken : inputTokens) {
+            outputTokens.add(inputToken.strip());
+        }
+
+        return String.join("", outputTokens);
     }
 
     private static State processChar(State state, char c) {
-        return c == ';' ? state.advance() : state;
+        if (c == ';' && state.isLevel()) return state.advance();
+        if (c == '{') return state.enter();
+        if (c == '}') return state.exit();
+        return state;
     }
 
-    record State(List<String> tokens, StringBuilder buffer) {
+    record State(List<String> tokens, StringBuilder buffer, int depth) {
         public State() {
-            this(Collections.emptyList(), new StringBuilder());
+            this(Collections.emptyList(), new StringBuilder(), 0);
         }
 
         private State advance() {
             var copy = new ArrayList<>(this.tokens);
             copy.add(this.buffer.toString());
-            return new State(copy, new StringBuilder());
+            return new State(copy, new StringBuilder(), depth);
         }
 
         private State append(char c) {
-            return new State(tokens, this.buffer.append(c));
+            return new State(tokens, this.buffer.append(c), depth);
+        }
+
+        public boolean isLevel() {
+            return this.depth == 0;
+        }
+
+        public State enter() {
+            return new State(tokens, buffer, depth + 1);
+        }
+
+        public State exit() {
+            return new State(tokens, buffer, depth - 1);
         }
     }
 }
