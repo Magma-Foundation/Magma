452bda481c762d43fe17edc61bcf3b73e7a9cc3b
SirMathhman <sirmathhman@gmail.com>
2024-06-12 11:05:54 -0400

    Copy over class content

--- magmac/src/magma/compile/lang/JavaLang.java
+++ magmac/src/magma/compile/lang/JavaLang.java
@@ -4,14 +4,14 @@ import magma.Main;
 import magma.compile.rule.EmptyRule;
 import magma.compile.rule.MembersRule;
 import magma.compile.rule.OrRule;
-import magma.compile.rule.text.extract.ExtractStringListRule;
-import magma.compile.rule.text.extract.ExtractStringRule;
+import magma.compile.rule.Rule;
+import magma.compile.rule.TypeRule;
+import magma.compile.rule.result.SplitAtSliceRule;
 import magma.compile.rule.text.LeftRule;
 import magma.compile.rule.text.RightRule;
-import magma.compile.rule.Rule;
-import magma.compile.rule.result.SplitAtSliceRule;
 import magma.compile.rule.text.StripRule;
-import magma.compile.rule.TypeRule;
+import magma.compile.rule.text.extract.ExtractStringListRule;
+import magma.compile.rule.text.extract.ExtractStringRule;
 
 import java.util.List;
 
@@ -19,29 +19,23 @@ public class JavaLang {
     public static Rule createClassRule() {
         var modifiers = new ExtractStringListRule("modifiers", " ");
         var name = new StripRule(new ExtractStringRule("name"));
-        var content = new ExtractStringRule("content");
+        var content = new MembersRule("content", createClassMemberRule());
 
         var splitAtSliceRule = new SplitAtSliceRule(new StripRule(modifiers), Main.CLASS_KEYWORD_WITH_SPACE, new StripRule(new SplitAtSliceRule(name, "{", new StripRule(new RightRule(content, "}")))));
         return new TypeRule("class", splitAtSliceRule);
     }
 
-    public static Rule createPackageRule() {
-        return new TypeRule("package", new LeftRule("package ", new ExtractStringListRule("namespace", ".")));
-    }
-
-    public static Rule createWhitespaceRule() {
-        return new TypeRule("whitespace", new EmptyRule());
-    }
-
-    public static Rule createImportRule() {
-        return new TypeRule("import", new LeftRule("import ", new ExtractStringRule("value")));
+    private static Rule createClassMemberRule() {
+        return new OrRule(List.of(
+                new TypeRule("any", new ExtractStringRule("content"))
+        ));
     }
 
     public static TypeRule createRootRule() {
         var childRule = new OrRule(List.of(
-                createWhitespaceRule(),
-                createPackageRule(),
-                createImportRule(),
+                new TypeRule("whitespace", new EmptyRule()),
+                new TypeRule("package", new LeftRule("package ", new ExtractStringListRule("namespace", "."))),
+                new TypeRule("import", new LeftRule("import ", new ExtractStringRule("value"))),
                 createClassRule()));
 
         return new TypeRule("root", new MembersRule("children", new StripRule(childRule)));
--- magmac/src/magma/compile/lang/MagmaLang.java
+++ magmac/src/magma/compile/lang/MagmaLang.java
@@ -2,7 +2,9 @@ package magma.compile.lang;
 
 import magma.compile.rule.MembersRule;
 import magma.compile.rule.OrRule;
+import magma.compile.rule.Rule;
 import magma.compile.rule.TypeRule;
+import magma.compile.rule.text.LeftRule;
 import magma.compile.rule.text.extract.ExtractStringListRule;
 import magma.compile.rule.text.extract.ExtractStringRule;
 import magma.compile.rule.text.RightRule;
@@ -14,14 +16,20 @@ public class MagmaLang {
     public static SplitAtSliceRule createFunctionRule() {
         var modifiers = new ExtractStringListRule("modifiers", " ");
         var name = new ExtractStringRule("name");
-        var content = new ExtractStringRule("content");
+        var content = new MembersRule("content", createStatementRule());
 
         return new SplitAtSliceRule(modifiers, " ", new SplitAtSliceRule(name, "() => {\n\t", new RightRule(content, "}")));
     }
 
+    private static Rule createStatementRule() {
+        return new OrRule(List.of(
+                new TypeRule("any", new ExtractStringRule("content"))
+        ));
+    }
+
     public static TypeRule createRootRule() {
         return new TypeRule("root", new MembersRule("children", new OrRule(List.of(
-                new RightRule(JavaLang.createImportRule(), "\n"),
+                new RightRule(new TypeRule("import", new LeftRule("import ", new ExtractStringRule("value"))), "\n"),
                 new TypeRule("function", createFunctionRule())
         ))));
     }
