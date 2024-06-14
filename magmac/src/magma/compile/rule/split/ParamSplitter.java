1d6be128546f7cb0ae81e02a1fb58d8308ce353c
SirMathhman <sirmathhman@gmail.com>
2024-06-14 19:19:02 -0400

    Make sure params don't split in ()

--- magmac/src/magma/compile/rule/split/ParamSplitter.java
+++ magmac/src/magma/compile/rule/split/ParamSplitter.java
@@ -1,12 +1,32 @@
 package magma.compile.rule.split;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 public class ParamSplitter implements Splitter {
     @Override
     public List<String> split(String input) {
-        return Arrays.stream(input.split(","))
+        var segments = new ArrayList<String>();
+        var buffer = new StringBuilder();
+        var depth = 0;
+
+        for (int i = 0; i < input.length(); i++) {
+            var c = input.charAt(i);
+
+            if (c == ',' && depth == 0) {
+                segments.add(buffer.toString());
+                buffer = new StringBuilder();
+            } else {
+                if (c == '(') depth++;
+                if (c == ')') depth--;
+                buffer.append(c);
+            }
+        }
+
+        segments.add(buffer.toString());
+
+        return segments.stream()
                 .filter(value -> !value.isBlank())
                 .toList();
     }
