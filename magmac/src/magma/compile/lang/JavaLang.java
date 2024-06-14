57ec8a2fedc090eec1f0c80c95b3e25be3795700
SirMathhman <sirmathhman@gmail.com>
2024-06-14 18:30:14 -0400

    Split children

--- magmac/src/magma/compile/lang/JavaLang.java
+++ magmac/src/magma/compile/lang/JavaLang.java
@@ -2,10 +2,13 @@ package magma.compile.lang;
 
 import magma.compile.rule.Rule;
 import magma.compile.rule.TypeRule;
+import magma.compile.rule.split.MembersSplitter;
+import magma.compile.rule.split.SplitMultipleRule;
 import magma.compile.rule.text.extract.ExtractStringRule;
 
 public class JavaLang {
     public static Rule createRootRule() {
-        return new TypeRule("block", new ExtractStringRule("value"));
+        var any = new TypeRule("any", new ExtractStringRule("value"));
+        return new TypeRule("block", new SplitMultipleRule(new MembersSplitter(), "", "children", any));
     }
 }
