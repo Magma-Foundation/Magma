c61837524eaeb81ac1562ca491ac377b9802eb57
SirMathhman <sirmathhman@gmail.com>
2024-06-14 19:13:43 -0400

    Filter out empty args

--- magmac/src/magma/compile/rule/split/ParamSplitter.java
+++ magmac/src/magma/compile/rule/split/ParamSplitter.java
@@ -1,10 +1,13 @@
 package magma.compile.rule.split;
 
+import java.util.Arrays;
 import java.util.List;
 
 public class ParamSplitter implements Splitter {
     @Override
     public List<String> split(String input) {
-        return List.of(input.split(","));
+        return Arrays.stream(input.split(","))
+                .filter(value -> !value.isBlank())
+                .toList();
     }
 }
