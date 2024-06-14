170db8d1608d720251e7ca79971b7f2415afa096
SirMathhman <sirmathhman@gmail.com>
2024-06-14 19:17:13 -0400

    Fix nested params ()

--- magmac/src/magma/compile/lang/JavaLang.java
+++ magmac/src/magma/compile/lang/JavaLang.java
@@ -95,9 +95,13 @@ public class JavaLang {
 
         @Override
         protected Optional<Integer> computeIndex(String input) {
+            var depth = 0;
+
             for (int i = input.length() - 1; i >= 0; i--) {
                 var c = input.charAt(i);
-                if (c == '(') return Optional.of(i);
+                if (c == '(' && depth == 0) return Optional.of(i);
+                else if (c == ')') depth++;
+                else if (c == '(') depth--;
             }
 
             return Optional.empty();
