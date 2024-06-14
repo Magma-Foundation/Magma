c3609e5e90efc38d92669ef18d4975c3c3b8c3c9
SirMathhman <sirmathhman@gmail.com>
2024-06-14 15:07:48 -0400

    Remove unused class syntax

--- magmac/src/magma/compile/lang/ClassSplitter.java
+++ magmac/src/magma/compile/lang/ClassSplitter.java
@@ -14,6 +14,22 @@ import java.util.List;
 import java.util.stream.Stream;
 
 public class ClassSplitter extends Modifier {
+    private static Node createObject(Node modified, Node staticBlock) {
+        return modified.retype("object").mapAttributes(attributes -> attributes.with("content", new NodeAttribute(staticBlock)));
+    }
+
+    private static Node createFunction(Node modified, Node instanceBlock) {
+        return modified.retype("function").mapAttributes(attributes -> attributes
+                .mapValue("modifiers", StringListAttribute.Factory, list -> {
+                    var copy = new ArrayList<>(list);
+                    copy.add("class");
+                    copy.add("def");
+                    return copy;
+                })
+                .with("params", new StringAttribute(""))
+                .with("content", new NodeAttribute(instanceBlock)));
+    }
+
     @Override
     protected Node preVisit(Node node) {
         if (node.is("block")) {
@@ -62,18 +78,17 @@ public class ClassSplitter extends Modifier {
                 });
             });
 
-            var asFunction = modified.retype("function").mapAttributes(attributes -> attributes
-                    .mapValue("modifiers", StringListAttribute.Factory, list -> {
-                        var copy = new ArrayList<>(list);
-                        copy.add("class");
-                        copy.add("def");
-                        return copy;
-                    })
-                    .with("params", new StringAttribute(""))
-                    .with("content", new NodeAttribute(instanceBlock)));
-            var asObject = modified.retype("object").mapAttributes(attributes -> attributes.with("content", new NodeAttribute(staticBlock)));
-
-            return Stream.of(asFunction, asObject);
+            if (instances.isEmpty()) {
+                var asObject = createObject(modified, staticBlock);
+                return Stream.of(asObject);
+            } else if (statics.isEmpty()) {
+                var asFunction = createFunction(modified, instanceBlock);
+                return Stream.of(asFunction);
+            } else {
+                var asFunction = createFunction(modified, instanceBlock);
+                var asObject = createObject(modified, staticBlock);
+                return Stream.of(asFunction, asObject);
+            }
         } else {
             return Stream.of(child);
         }
