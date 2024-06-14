619ec6e63817d333e83efdfd143434f567084e15
SirMathhman <sirmathhman@gmail.com>
2024-06-14 18:30:50 -0400

    Strip

--- magmac/src/magma/compile/lang/JavaLang.java
+++ magmac/src/magma/compile/lang/JavaLang.java
@@ -4,11 +4,12 @@ import magma.compile.rule.Rule;
 import magma.compile.rule.TypeRule;
 import magma.compile.rule.split.MembersSplitter;
 import magma.compile.rule.split.SplitMultipleRule;
+import magma.compile.rule.text.StripRule;
 import magma.compile.rule.text.extract.ExtractStringRule;
 
 public class JavaLang {
     public static Rule createRootRule() {
         var any = new TypeRule("any", new ExtractStringRule("value"));
-        return new TypeRule("block", new SplitMultipleRule(new MembersSplitter(), "", "children", any));
+        return new TypeRule("block", new SplitMultipleRule(new MembersSplitter(), "", "children", new StripRule(any)));
     }
 }
