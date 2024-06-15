ef67fab308586796af04fd746055e7f542d2541d
SirMathhman <sirmathhman@gmail.com>
2024-06-15 11:19:28 -0400

    Functions

--- magmac/src/magma/compile/lang/MagmaLang.java
+++ magmac/src/magma/compile/lang/MagmaLang.java
@@ -3,6 +3,8 @@ package magma.compile.lang;
 import magma.compile.rule.LazyRule;
 import magma.compile.rule.OrRule;
 import magma.compile.rule.Rule;
+import magma.compile.rule.TypeRule;
+import magma.compile.rule.text.extract.ExtractNodeRule;
 
 import java.util.List;
 
@@ -10,8 +12,8 @@ public class MagmaLang {
     public static Rule createRootRule() {
         var statement = new LazyRule();
         statement.setRule(new OrRule(List.of(
+                new TypeRule("function", new ExtractNodeRule("child", Lang.createBlock(statement)))
         )));
-
 
         return Lang.createBlock(new OrRule(List.of(
                 Lang.createImportRule(Lang.createNamespaceRule()),
