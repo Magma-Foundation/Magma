e3b66250887e9309deac97e0709f1c6f2d228aa7
SirMathhman <sirmathhman@gmail.com>
2024-06-14 20:41:25 -0400

    Imports?

--- magmac/src/magma/compile/lang/MagmaLang.java
+++ magmac/src/magma/compile/lang/MagmaLang.java
@@ -1,11 +1,18 @@
 package magma.compile.lang;
 
 import magma.compile.rule.EmptyRule;
+import magma.compile.rule.OrRule;
 import magma.compile.rule.Rule;
 import magma.compile.rule.TypeRule;
+import magma.compile.rule.text.LeftRule;
+import magma.compile.rule.text.RightRule;
+
+import java.util.List;
 
 public class MagmaLang {
     public static Rule createRootRule() {
-        return Lang.createBlock(new TypeRule("empty", new EmptyRule()));
+        return Lang.createBlock(new OrRule(List.of(
+                new TypeRule("import", new LeftRule("import ", new RightRule(new EmptyRule(), ";")))
+        )));
     }
 }
