b3bee947108e2a798feb70c38196e77cd0444cc5
SirMathhman <sirmathhman@gmail.com>
2024-06-15 11:45:37 -0400

    Cut out formatting

--- magmac/src/magma/compile/lang/MagmaFormatter.java
+++ magmac/src/magma/compile/lang/MagmaFormatter.java
@@ -1,11 +1,7 @@
 package magma.compile.lang;
 
 import magma.api.Tuple;
-import magma.compile.attribute.NodeListAttribute;
 import magma.compile.rule.Node;
-
-import java.util.ArrayList;
-import java.util.List;
 
 public class MagmaFormatter extends Generator {
     @Override
@@ -16,33 +12,6 @@ public class MagmaFormatter extends Generator {
 
     @Override
     protected Tuple<Node, Integer> postVisit(Node node, int depth) {
-        if (node.is("block")) {
-            var children = node.mapAttributes(attributes -> attributes.mapValue("children", NodeListAttribute.Factory, list -> {
-                List<Node> result = new ArrayList<>();
-
-                for (int i = 0; i < list.size(); i++) {
-                    Node child = list.get(i);
-                    Node withString;
-                    if (i == 0 && depth == 0) {
-                        withString = child;
-                    } else {
-                        var withString1 = child.withString("left-indent", "\n" + "\t".repeat(depth));
-                        if (i == list.size() - 1 && depth != 0) {
-                            withString = withString1.withString("right-indent", "\n" + "\t".repeat(depth - 1));
-                        } else {
-                            withString = withString1;
-                        }
-                    }
-
-                    result.add(withString);
-                }
-
-                return result;
-            }));
-
-            return new Tuple<>(children, depth - 1);
-        }
-
         return new Tuple<>(node, depth);
     }
 }
