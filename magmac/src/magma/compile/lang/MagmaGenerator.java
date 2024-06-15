cf3689ca0ff4c9f33fb8c03e55c2c3a4229209eb
SirMathhman <sirmathhman@gmail.com>
2024-06-15 12:22:18 -0400

    Functions OK

--- magmac/src/magma/compile/lang/MagmaGenerator.java
+++ magmac/src/magma/compile/lang/MagmaGenerator.java
@@ -1,6 +1,7 @@
 package magma.compile.lang;
 
 import magma.api.Tuple;
+import magma.compile.attribute.NodeAttribute;
 import magma.compile.attribute.StringListAttribute;
 import magma.compile.rule.Node;
 
@@ -11,14 +12,23 @@ public class MagmaGenerator extends Generator {
     protected Tuple<Node, Integer> postVisit(Node node, int depth) {
         if (!node.is("function")) return new Tuple<>(node, depth);
 
-        return new Tuple<>(node.mapAttributes(attributes -> attributes.mapValue("modifiers", StringListAttribute.Factory, list -> {
-            var copy = new ArrayList<String>();
-            if (list.contains("public")) {
-                copy.add("export");
-            }
+        return new Tuple<>(node.mapAttributes(attributes -> {
+            return attributes.mapValue("definition", NodeAttribute.Factory, definition -> {
+                return definition.mapAttributes(attributes1 -> {
+                    return attributes1.mapValue("modifiers", StringListAttribute.Factory, list -> {
+                        if (list.contains("class")) return list;
 
-            copy.add("def");
-            return copy;
-        })), depth);
+                        var copy = new ArrayList<String>();
+                        if (list.contains("public")) {
+                            copy.add("public");
+                        }
+
+                        copy.add("def");
+                        return copy;
+                    });
+                });
+            });
+
+        }), depth);
     }
 }
