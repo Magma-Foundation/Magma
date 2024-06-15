69d6f223d28bfbdedb93ecc14dc29368dc865cb6
SirMathhman <sirmathhman@gmail.com>
2024-06-15 12:33:07 -0400

    Values OK

--- magmac/src/magma/compile/lang/MagmaGenerator.java
+++ magmac/src/magma/compile/lang/MagmaGenerator.java
@@ -1,6 +1,7 @@
 package magma.compile.lang;
 
 import magma.api.Tuple;
+import magma.compile.attribute.Attribute;
 import magma.compile.attribute.NodeAttribute;
 import magma.compile.attribute.StringListAttribute;
 import magma.compile.rule.Node;
@@ -8,27 +9,81 @@ import magma.compile.rule.Node;
 import java.util.ArrayList;
 
 public class MagmaGenerator extends Generator {
+    private static Node attachModifiers(Node definition) {
+        var oldModifiers = definition.attributes()
+                .apply("modifiers")
+                .flatMap(Attribute::asStringList)
+                .orElse(new ArrayList<>());
+
+        var copy = new ArrayList<String>();
+        if (oldModifiers.contains("final")) {
+            copy.add("const");
+        } else {
+            copy.add("let");
+        }
+
+        var withModifiers = definition.withStringList("modifiers", copy);
+        return withModifiers;
+    }
+
+    private static Node removeImplicitType(Node definition) {
+        Node maybeWithType;
+        var type = definition.attributes()
+                .apply("type")
+                .flatMap(Attribute::asNode)
+                .orElseThrow();
+
+        if (type.is("symbol")) {
+            var value = type.attributes()
+                    .apply("value")
+                    .flatMap(Attribute::asString)
+                    .orElseThrow();
+
+            if (value.equals("var")) {
+                maybeWithType = definition.remove("type");
+            } else {
+                maybeWithType = definition;
+            }
+        } else {
+            maybeWithType = definition;
+        }
+        return maybeWithType;
+    }
+
     @Override
     protected Tuple<Node, Integer> postVisit(Node node, int depth) {
-        if (!node.is("function")) return new Tuple<>(node, depth);
+        if (node.is("function")) {
+            return new Tuple<>(node.mapAttributes(attributes -> {
+                return attributes.mapValue("definition", NodeAttribute.Factory, definition -> {
+                    return definition.mapAttributes(attributes1 -> {
+                        return attributes1.mapValue("modifiers", StringListAttribute.Factory, list -> {
+                            if (list.contains("class")) return list;
 
-        return new Tuple<>(node.mapAttributes(attributes -> {
-            return attributes.mapValue("definition", NodeAttribute.Factory, definition -> {
-                return definition.mapAttributes(attributes1 -> {
-                    return attributes1.mapValue("modifiers", StringListAttribute.Factory, list -> {
-                        if (list.contains("class")) return list;
+                            var copy = new ArrayList<String>();
+                            if (list.contains("public")) {
+                                copy.add("public");
+                            }
 
-                        var copy = new ArrayList<String>();
-                        if (list.contains("public")) {
-                            copy.add("public");
-                        }
-
-                        copy.add("def");
-                        return copy;
+                            copy.add("def");
+                            return copy;
+                        });
                     });
+                });
+
+            }), depth);
+        }
+
+        if (node.is("declaration")) {
+            var node1 = node.mapAttributes(attributes -> {
+                return attributes.mapValue("definition", NodeAttribute.Factory, definition -> {
+                    var withModifiers = attachModifiers(definition);
+                    return removeImplicitType(withModifiers);
                 });
             });
 
-        }), depth);
+            return new Tuple<>(node1, depth);
+        }
+
+        return new Tuple<>(node, depth);
     }
 }
