1193e80d87327b1d9729f6f519a352794ba5330b
SirMathhman <sirmathhman@gmail.com>
2024-06-15 20:16:27 -0400

    Pass through collectors

--- magmac/src/java/magma/compile/rule/SymbolRule.java
+++ magmac/src/java/magma/compile/rule/SymbolRule.java
@@ -9,9 +9,10 @@ public final class SymbolRule extends FilterRule {
     protected boolean filter(String input) {
         for (int i = 0; i < input.length(); i++) {
             var c = input.charAt(i);
-            if (!Character.isLetter(c) && c != '_') {
-                return false;
+            if (Character.isLetter(c) || c == '_' || (i != 0 && Character.isDigit(c))) {
+                continue;
             }
+            return false;
         }
 
         return true;
