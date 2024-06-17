20b7d7f2289858d46a7f9d8d495231b3541e8e6c
SirMathhman <sirmathhman@gmail.com>
2024-06-16 21:45:22 -0400

    Add type params

--- magmac/src/java/magma/compile/lang/JavaDefinitionHeaderFactory.java
+++ magmac/src/java/magma/compile/lang/JavaDefinitionHeaderFactory.java
@@ -1,24 +1,51 @@
 package magma.compile.lang;
 
+import magma.compile.CompileError;
+import magma.compile.Error_;
 import magma.compile.rule.OrRule;
 import magma.compile.rule.Rule;
 import magma.compile.rule.TypeRule;
+import magma.compile.rule.split.BackwardsRule;
 import magma.compile.rule.split.LastRule;
+import magma.compile.rule.text.LeftRule;
+import magma.compile.rule.text.RightRule;
 import magma.compile.rule.text.extract.ExtractNodeRule;
+import magma.compile.rule.text.extract.ExtractStringListRule;
 import magma.compile.rule.text.extract.ExtractStringRule;
-import magma.compile.rule.text.extract.SimpleExtractStringListRule;
 
 import java.util.List;
+import java.util.Optional;
 
 public class JavaDefinitionHeaderFactory {
     static Rule createDefinitionHeaderRule() {
         var type = new ExtractNodeRule("type", Lang.createTypeRule());
         var name = new ExtractStringRule("name");
 
-        var modifiers = new SimpleExtractStringListRule("modifiers", " ");
-        var withModifiers = new LastRule(modifiers, " " , type);
-        var maybeModifiers = new OrRule(List.of(withModifiers, type));
+        var generics = new LeftRule("<", new RightRule(new ExtractStringRule("type-params"), ">"));
+        var withGenerics = new LastRule(generics, " ", type);
+        var maybeGenerics = new OrRule(List.of(withGenerics, type));
+
+        var modifiers = new ModifiersRule();
+        var withModifiers = new BackwardsRule(modifiers, " ", maybeGenerics);
+        var maybeModifiers = new OrRule(List.of(withModifiers, maybeGenerics));
 
         return new TypeRule("definition", new LastRule(maybeModifiers, " ", name));
+    }
+
+    private static class ModifiersRule extends ExtractStringListRule {
+        public static final List<String> MODIFIERS = List.of("public", "static");
+
+        public ModifiersRule() {
+            super("modifiers", " ");
+        }
+
+        @Override
+        protected Optional<Error_> qualify(String child) {
+            if (MODIFIERS.contains(child)) {
+                return Optional.empty();
+            } else {
+                return Optional.of(new CompileError("Invalid modifier.", child));
+            }
+        }
     }
 }
--- magmac/test/java/magma/compile/lang/JavaDefinitionHeaderFactoryTest.java
+++ magmac/test/java/magma/compile/lang/JavaDefinitionHeaderFactoryTest.java
@@ -1,32 +1,69 @@
 package magma.compile.lang;
 
+import magma.compile.Error_;
 import magma.compile.attribute.Attribute;
 import magma.compile.attribute.NodeAttribute;
 import magma.compile.attribute.StringAttribute;
 import magma.compile.attribute.StringListAttribute;
+import magma.compile.rule.Node;
 import org.junit.jupiter.api.Test;
 
+import java.util.Collections;
 import java.util.List;
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.fail;
 
 class JavaDefinitionHeaderFactoryTest {
     private static void assertParse(String input, String propertyKey, Attribute test) {
-        var rule = JavaDefinitionHeaderFactory.createDefinitionHeaderRule();
-        var result = rule.toNode(input);
-        if (result.findError().isPresent())
-            throw new RuntimeException(result.findError().get().findMessage().orElseThrow());
-
-        assertEquals(test, result.create().orElseThrow()
+        var optional = parseImpl(input);
+        assertEquals(test, optional.orElseThrow()
                 .attributes()
                 .apply(propertyKey)
                 .orElseThrow());
     }
 
+    private static Optional<Node> parseImpl(String input) {
+        var rule = JavaDefinitionHeaderFactory.createDefinitionHeaderRule();
+        var result = rule.toNode(input);
+        if (result.findError().isPresent()) {
+            var error = result.findError().orElseThrow();
+            fail(toException(error));
+        }
 
+        return result.create();
+    }
+
+    private static RuntimeException toException(Error_ error) {
+        var message = error.findMessage().orElse("");
+        var context = error.findContext().orElse("");
+
+        var causes = error.findCauses().orElse(Collections.emptyList());
+        if(causes.isEmpty()) {
+            return new RuntimeException(message + context);
+        } else {
+            return new RuntimeException(message, toException(causes.get(0)));
+        }
+    }
 
     private static void assertParseModifiers(String input, List<String> list) {
         assertParse(input, "modifiers", new StringListAttribute(list));
+    }
+
+    private static void assertParseToString(String input, String propertyKey, String propertyValue) {
+        assertParse(input, propertyKey, new StringAttribute(propertyValue));
+    }
+
+    @Test
+    void modifiersAndGenerics() {
+        assertTrue(parseImpl("public <T> var test").isPresent());
+    }
+
+    @Test
+    void generics() {
+        assertParseToString("<T> var test", "type-params", "T");
     }
 
     @Test
@@ -41,7 +78,7 @@ class JavaDefinitionHeaderFactoryTest {
 
     @Test
     void name() {
-        assertParse("var test", "name", new StringAttribute("test"));
+        assertParseToString("var test", "name", "test");
     }
 
     @Test
