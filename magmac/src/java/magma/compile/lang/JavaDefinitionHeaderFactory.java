f60c2f42a7d0ce86a09b00d485ab1a4cfbbe4558
SirMathhman <sirmathhman@gmail.com>
2024-06-16 21:24:08 -0400

    Type

--- magmac/src/java/magma/compile/lang/JavaDefinitionHeaderFactory.java
+++ magmac/src/java/magma/compile/lang/JavaDefinitionHeaderFactory.java
@@ -1,13 +1,16 @@
 package magma.compile.lang;
 
-import magma.compile.rule.EmptyRule;
 import magma.compile.rule.Rule;
 import magma.compile.rule.TypeRule;
-import magma.compile.rule.text.LeftRule;
+import magma.compile.rule.split.LastRule;
+import magma.compile.rule.text.extract.ExtractNodeRule;
 import magma.compile.rule.text.extract.ExtractStringRule;
 
 public class JavaDefinitionHeaderFactory {
     static Rule createDefinitionHeaderRule() {
-        return new TypeRule("definition", new LeftRule("var ", new ExtractStringRule("name")));
+        var type = new ExtractNodeRule("type", Lang.createTypeRule());
+        var name = new ExtractStringRule("name");
+
+        return new TypeRule("definition", new LastRule(type, " ", name));
     }
 }
--- magmac/test/java/magma/compile/lang/JavaDefinitionHeaderFactoryTest.java
+++ magmac/test/java/magma/compile/lang/JavaDefinitionHeaderFactoryTest.java
@@ -1,22 +1,32 @@
 package magma.compile.lang;
 
 import magma.compile.attribute.Attribute;
+import magma.compile.attribute.NodeAttribute;
+import magma.compile.attribute.StringAttribute;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class JavaDefinitionHeaderFactoryTest {
-    @Test
-    void test() {
+    private static void assertParse(String input, String propertyKey, Attribute test) {
         var rule = JavaDefinitionHeaderFactory.createDefinitionHeaderRule();
-        var result = rule.toNode("var test");
+        var result = rule.toNode(input);
         if (result.findError().isPresent())
             throw new RuntimeException(result.findError().get().findMessage().orElseThrow());
 
-        assertEquals("test", result.create().orElseThrow()
+        assertEquals(test, result.create().orElseThrow()
                 .attributes()
-                .apply("name")
-                .flatMap(Attribute::asString)
+                .apply(propertyKey)
                 .orElseThrow());
+    }
+
+    @Test
+    void name() {
+        assertParse("var test", "name", new StringAttribute("test"));
+    }
+
+    @Test
+    void type() {
+        assertParse("var test", "type", new NodeAttribute(Lang.createTypeRule().toNode("var").create().orElseThrow()));
     }
 }
