085ce875b97ae30adccbc96c2a07ea00571865fe
SirMathhman <sirmathhman@gmail.com>
2024-06-17 22:36:54 -0400

    Remove packages

--- magmac/src/java/magma/compile/lang/JavaToMagmaGenerator.java
+++ magmac/src/java/magma/compile/lang/JavaToMagmaGenerator.java
@@ -1,4 +1,20 @@
 package magma.compile.lang;
 
+import magma.api.Tuple;
+import magma.compile.rule.Node;
+
 public class JavaToMagmaGenerator extends Generator {
+    private static Node removePackagesFromBlock(Node node) {
+        if (!node.is("block")) return node;
+
+        return node.mapNodes("children", children -> children.stream()
+                .filter(child -> !child.is("package"))
+                .toList());
+    }
+
+    @Override
+    protected Tuple<Node, Integer> preVisit(Node node, int depth) {
+        var newNode = removePackagesFromBlock(node);
+        return new Tuple<>(newNode, depth);
+    }
 }
