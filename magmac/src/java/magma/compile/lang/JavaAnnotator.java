b0044718a1b8f07e5c91d87bf26b6635480e4248
SirMathhman <sirmathhman@gmail.com>
2024-06-20 15:23:58 -0400

    Define method params

--- magmac/src/java/magma/compile/lang/JavaAnnotator.java
+++ magmac/src/java/magma/compile/lang/JavaAnnotator.java
@@ -8,12 +8,39 @@ import magma.compile.CompileError;
 import magma.compile.Error_;
 import magma.compile.rule.Node;
 
+import java.util.Collections;
+
 public class JavaAnnotator extends Generator {
     @Override
+    protected Result<Tuple<Node, State>, Error_> preVisit(Node node, State state) {
+        if (node.is("block")) {
+            return new Ok<>(new Tuple<>(node, state.enter()));
+        }
+
+        if (node.is("method")) {
+            var params = node.findNodeList("params").orElse(Collections.emptyList());
+            Result<State, Error_> defined = new Ok<>(state);
+            for (Node param : params) {
+                var name = param.findString("name").orElseThrow();
+                defined = defined.flatMapValue(inner -> inner.define(name));
+            }
+
+            return defined.mapValue(newState -> new Tuple<>(node, newState));
+        }
+
+        return new Ok<>(new Tuple<>(node, state));
+    }
+
+    @Override
     protected Result<Tuple<Node, State>, Error_> postVisit(Node node, State state) {
-        if(node.is("symbol")) {
+        if (node.is("block")) {
+            return new Ok<>(new Tuple<>(node, state.exit()));
+        }
+
+
+        if (node.is("symbol")) {
             var value = node.findString("value").orElseThrow();
-            if(!state.isDefined(value)) {
+            if (!state.isDefined(value)) {
                 return new Err<>(new CompileError("Symbol not defined.", value));
             }
         }
