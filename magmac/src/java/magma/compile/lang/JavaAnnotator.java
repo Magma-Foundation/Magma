b80c822fff6a63e18309c3d18ac35b2c8b7a27e3
SirMathhman <sirmathhman@gmail.com>
2024-06-20 16:49:53 -0400

    Fix annotator

--- magmac/src/java/magma/compile/lang/JavaAnnotator.java
+++ magmac/src/java/magma/compile/lang/JavaAnnotator.java
@@ -8,14 +8,21 @@ import magma.compile.CompileError;
 import magma.compile.Error_;
 import magma.compile.rule.Node;
 
-import java.util.Collections;
 import java.util.List;
-import java.util.Optional;
 
 public class JavaAnnotator extends Generator {
+    private static Result<State, Error_> defineParams(State state, List<Node> params) {
+        Result<State, Error_> defined = new Ok<>(state);
+        for (Node param : params) {
+            var name = param.findString("name").orElseThrow();
+            defined = defined.flatMapValue(inner -> inner.define(name));
+        }
+        return defined;
+    }
+
     @Override
     protected Result<Tuple<Node, State>, Error_> preVisit(Node node, State state) {
-        if(node.is("lambda")) {
+        if (node.is("lambda")) {
             var nameOptional = node.findString("param-name");
             if (nameOptional.isPresent()) {
                 return state.define(nameOptional.get()).mapValue(inner -> new Tuple<>(node, inner));
@@ -27,27 +34,41 @@ public class JavaAnnotator extends Generator {
         }
 
         if (node.is("method") || node.is("record")) {
-            var defined = defineParams(state, node.findNodeList("params")
-                    .orElse(Collections.emptyList()));
+            var params = node.findNodeList("params").orElseThrow();
+            return defineParams(state, params).mapValue(inner -> new Tuple<>(node, inner));
+        }
 
-            return defined.mapValue(newState -> new Tuple<>(node, newState));
+        if (node.is("class")) {
+            var children = node.findNode("child")
+                    .orElseThrow()
+                    .findNodeList("children")
+                    .orElseThrow();
+
+            Result<State, Error_> defined = new Ok<>(state.enter());
+            for (Node child : children) {
+                if (child.is("method")) {
+                    var name = child.findNode("definition")
+                            .orElseThrow()
+                            .findString("name")
+                            .orElseThrow();
+
+                    defined = defined.flatMapValue(inner -> inner.define(name));
+                }
+            }
+
+            return defined.mapValue(inner -> new Tuple<>(node, inner));
         }
 
         return new Ok<>(new Tuple<>(node, state));
     }
 
-    private static Result<State, Error_> defineParams(State state, List<Node> params) {
-        Result<State, Error_> defined = new Ok<>(state);
-        for (Node param : params) {
-            var name = param.findString("name").orElseThrow();
-            defined = defined.flatMapValue(inner -> inner.define(name));
-        }
-        return defined;
-    }
-
     @Override
     protected Result<Tuple<Node, State>, Error_> postVisit(Node node, State state) {
         if (node.is("block")) {
+            return new Ok<>(new Tuple<>(node, state.exit()));
+        }
+
+        if (node.is("class")) {
             return new Ok<>(new Tuple<>(node, state.exit()));
         }
 
