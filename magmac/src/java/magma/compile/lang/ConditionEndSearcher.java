f3531565a2a1a07cc0854895add6cfb81cdf2569
SirMathhman <sirmathhman@gmail.com>
2024-06-21 20:17:12 -0400

    Check nested values

--- magmac/src/java/magma/compile/lang/ConditionEndSearcher.java
+++ magmac/src/java/magma/compile/lang/ConditionEndSearcher.java
@@ -7,6 +7,17 @@ import java.util.Optional;
 public class ConditionEndSearcher implements Searcher {
     @Override
     public Optional<Integer> search(String input) {
+        var depth = 0;
+        for (int i = 0; i < input.length(); i++) {
+            var c = input.charAt(i);
+            if (c == ')' && depth == 1) {
+                return Optional.of(i);
+            } else {
+                if (c == '(') depth++;
+                if (c == ')') depth--;
+            }
+        }
+
         return Optional.empty();
     }
 }
--- magmac/test/java/magma/compile/lang/ConditionEndSearcherTest.java
+++ magmac/test/java/magma/compile/lang/ConditionEndSearcherTest.java
@@ -1,7 +1,31 @@
 package magma.compile.lang;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class ConditionEndSearcherTest {
+    private static void assertSearch(String input, int index) {
+        assertEquals(index, new ConditionEndSearcher()
+                .search(input)
+                .orElseThrow());
+    }
 
+    @Test
+    void empty() {
+        assertTrue(new ConditionEndSearcher()
+                .search("")
+                .isEmpty());
+    }
+
+    @Test
+    void simple() {
+        assertSearch("()", 1);
+    }
+
+    @Test
+    void nested() {
+        assertSearch("(())", 3);
+    }
 }
