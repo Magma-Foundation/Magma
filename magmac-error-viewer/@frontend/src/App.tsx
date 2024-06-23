1ad68c85a1beba7a9a52869707fee5a710eaa211
SirMathhman <sirmathhman@gmail.com>
2024-06-23 15:09:10 -0400

    Line numbers?

--- magmac-error-viewer/@frontend/src/App.tsx
+++ magmac-error-viewer/@frontend/src/App.tsx
@@ -1,26 +1,32 @@
-import './App.css'
 import axios from "axios";
 import {useEffect, useState} from "react";
 
 function App() {
-    const [state, setState] = useState("test");
+    const [state, setState] = useState<string[]>([]);
 
     useEffect(() => {
         axios({
             method: "get",
             url: "http://localhost:3000"
         }).then(response => {
-            const data = response.data;
-            console.log(data);
-            setState(data);
+            const data1 = response.data as string;
+            const lines = data1
+                .split("\\r\\n")
+                .map((line, index) => index + " " + line);
+            setState(lines);
+        }).catch(error => {
+            console.error("There was an error fetching the data!", error);
         });
-    });
+    }, []);
 
     return (
         <>
-            <span>
-                {state}
-            </span>
+            <pre>
+                {state.map((line, index) => (
+                    <div key={index}>{line}
+                    </div>
+                ))}
+            </pre>
         </>
     )
 }
