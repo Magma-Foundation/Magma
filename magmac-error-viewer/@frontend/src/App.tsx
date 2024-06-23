0e203fb263996d304ff20a504b9dcf28088f0489
SirMathhman <sirmathhman@gmail.com>
2024-06-23 15:22:51 -0400

    OK

--- magmac-error-viewer/@frontend/src/App.tsx
+++ magmac-error-viewer/@frontend/src/App.tsx
@@ -1,5 +1,5 @@
 import axios from "axios";
-import {useEffect, useState} from "react";
+import React, {useEffect, useState} from "react";
 
 function countDigits(index: number) {
     return Math.floor(index === 0 ? 0 : Math.log10(index)) + 1;
@@ -11,22 +11,22 @@ function format(index: number, length: number) {
     return " ".repeat(totalLength - number) + index;
 }
 
-function Card({title, lines} : {title : string, lines : string[]}) {
+type Element = React.JSX.Element;
+
+function Card({label, children}: { label: string, children: Element }) {
     return (
-        <div>
+        <div style={{
+            width: "100%",
+            height: "100%"
+        }}>
             <div style={{padding: "1rem"}}>
-                {title}
+                {label}
             </div>
             <hr/>
             <div style={{
                 padding: "1rem"
             }}>
-                <pre>
-                    {lines.map((line, index) => (
-                        <div key={index}>{line}
-                        </div>
-                    ))}
-                </pre>
+                {children}
             </div>
         </div>
     );
@@ -51,7 +51,29 @@ function App() {
 
     return (
         <>
-            <Card title="Viewer" lines={state}></Card>
+            <div style={{
+                display: "flex",
+                "flexDirection": "row",
+                width: "100%",
+                height: "100%"
+            }}>
+                <div style={{
+                    width: "30%"
+                }}>
+                    <Card label="Navigator">
+                        <div>
+                        </div>
+                    </Card>
+                </div>
+                <Card label="Viewer">
+                <pre>
+                    {state.map((line, index) => (
+                        <div key={index}>{line}
+                        </div>
+                    ))}
+                </pre>
+                </Card>
+            </div>
         </>
     )
 }
