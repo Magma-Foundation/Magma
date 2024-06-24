cc4cc45669f9d2f58a00e3d239cd21f0d113668b
SirMathhman <sirmathhman@gmail.com>
2024-06-24 13:25:53 -0400

    OK

--- magmac-error-viewer/@frontend/src/App.tsx
+++ magmac-error-viewer/@frontend/src/App.tsx
@@ -1,6 +1,39 @@
 function App() {
     return (
-        <div>
+        <div style={{
+            display: "flex",
+            "flex-direction": "row",
+            "justify-content": "center",
+            "align-items": "center",
+            width: "100%",
+            height: "100%"
+        }}>
+            <div style={{
+                width: "80vw",
+                height: "80vh"
+            }}>
+                <div style={{
+                    display: "flex",
+                    "flex-direction": "row",
+                    width: "100%",
+                    height: "100%"
+                }}>
+                    <div style={{
+                        width: "100%",
+                        height: "100%"
+                    }}>
+                        Navigator
+                    </div>
+                    <div style={{
+                        width: "100%",
+                        height: "100%"
+                    }}>
+                <span>
+                    Content
+                </span>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
