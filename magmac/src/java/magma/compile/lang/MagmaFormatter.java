0953161d238639d978c52afe3e38892ff12d5fdc
SirMathhman <sirmathhman@gmail.com>
2024-06-24 15:22:40 -0400

    New line

--- magmac/src/java/magma/compile/lang/MagmaFormatter.java
+++ magmac/src/java/magma/compile/lang/MagmaFormatter.java
@@ -1,18 +1,35 @@
 package magma.compile.lang;
 
 import magma.api.Tuple;
-import magma.api.result.Err;
 import magma.api.result.Ok;
 import magma.api.result.Result;
-import magma.compile.CompileError;
 import magma.compile.Error_;
 import magma.compile.annotate.State;
 import magma.compile.rule.Node;
 import magma.compile.rule.text.StripRule;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Optional;
-
 public class MagmaFormatter extends TreeGenerator {
+    @Override
+    protected Result<Tuple<Node, State>, Error_> preVisit(Node node, State state) {
+        if (node.is("block")) {
+            return new Ok<>(new Tuple<>(node, state.enter()));
+        }
+
+        return new Ok<>(new Tuple<>(node, state));
+    }
+
+    @Override
+    protected Result<Tuple<Node, State>, Error_> postVisit(Node node, State state) {
+        if (node.is("block")) {
+            var node1 = node.mapNodes("children", children -> {
+                return children.stream()
+                        .map(child -> child.withString(StripRule.DEFAULT_LEFT, "\n"))
+                        .toList();
+            });
+
+            return new Ok<>(new Tuple<>(node1, state.exit()));
+        }
+
+        return new Ok<>(new Tuple<>(node, state));
+    }
 }
