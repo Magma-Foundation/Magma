ae2a81eb87e699c89dfb4acd369a89bd17fb1f6a
SirMathhman <sirmathhman@gmail.com>
2024-06-24 20:50:19 -0400

    Just replace with while for now

--- magmac/src/java/magma/api/json/JSON.java
+++ magmac/src/java/magma/api/json/JSON.java
@@ -11,7 +11,8 @@ public class JSON {
         var buffer = new StringBuilder();
         var depth = 0;
 
-        for (int i = 0; i < input.length(); i++) {
+        int i = 0;
+        while (i < input.length()) {
             var c = input.charAt(i);
             if (c == ',' && depth == 0) {
                 list.add(buffer.toString());
@@ -21,6 +22,7 @@ public class JSON {
                 if (c == '}' || c == ']') depth--;
                 buffer.append(c);
             }
+            i++;
         }
 
         list.add(buffer.toString());
