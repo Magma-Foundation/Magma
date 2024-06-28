6ad271e858d7bcc953dd809c5a8ec5d534a75745
SirMathhman <sirmathhman@gmail.com>
2024-06-28 15:35:26 -0400

    Remove constructor

--- magmac/src/java/magma/api/collect/stream/EmptyHead.java
+++ magmac/src/java/magma/api/collect/stream/EmptyHead.java
@@ -6,9 +6,6 @@ import magma.lang.Instance;
 import static magma.api.option.None.None;
 
 public class EmptyHead<T> implements Head<T> {
-    private EmptyHead() {
-    }
-
     @Instance
     public static <T> Head<T> EmptyHead() {
         return new EmptyHead<>();
