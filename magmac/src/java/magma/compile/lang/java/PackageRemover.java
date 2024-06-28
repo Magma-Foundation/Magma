695c7959140521855442ff3e057e59ab3feb96fd
SirMathhman <sirmathhman@gmail.com>
2024-06-28 13:59:31 -0400

    Cleanup PackageRemover

--- magmac/src/java/magma/compile/lang/java/PackageRemover.java
+++ magmac/src/java/magma/compile/lang/java/PackageRemover.java
@@ -1,6 +1,7 @@
 package magma.compile.lang.java;
 
 import magma.api.Tuple;
+import magma.api.collect.List;
 import magma.api.result.Ok;
 import magma.api.result.Result;
 import magma.compile.Error_;
@@ -9,15 +10,15 @@ import magma.compile.lang.Visitor;
 import magma.compile.rule.Node;
 import magma.java.JavaList;
 
-import java.util.List;
-import java.util.function.Function;
-
 public class PackageRemover implements Visitor {
+    private static List<Node> removeFromChildren(List<Node> children) {
+        return children.stream()
+                .filter(child -> !child.is("package"))
+                .collect(JavaList.collecting());
+    }
+
     @Override
     public Result<Tuple<Node, State>, Error_> preVisit(Node node, State state) {
-        return new Ok<>(new Tuple<>(node.mapNodes("children", nodeList -> JavaList.fromNative(((Function<List<Node>, List<Node>>) children -> children.stream()
-                .filter(child -> !child.is("package"))
-                .toList()).apply(JavaList.toNative(nodeList)))), state));
-
+        return new Ok<>(new Tuple<>(node.mapNodes("children", PackageRemover::removeFromChildren), state));
     }
 }
