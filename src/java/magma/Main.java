38beeb82b4e5895f90c8699cb34f51bd2f74506a
SirMathhman <sirmathhman@gmail.com>
2025-03-22 17:54:00 -0400

    Main

--- src/java/magma/Main.java
+++ src/java/magma/Main.java
@@ -1,7 +1,17 @@
 package magma;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
 public class Main {
     public static void main(String[] args) {
-
+        try {
+            final var input = Files.readString(Paths.get(".", "src", "java", "magma", "Main.java"));
+            System.err.println("Invalid root: " + input);
+        } catch (IOException e) {
+            //noinspection CallToPrintStackTrace
+            e.printStackTrace();
+        }
     }
 }
