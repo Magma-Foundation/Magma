65c9250a318161976cbfc0058f7c79b14d89ac21
SirMathhman <sirmathhman@gmail.com>
2025-03-23 15:39:30 -0400

    Actual compilation

--- src/windows/magma/error/Error.c
+++ src/windows/magma/error/Error.c
@@ -1,2 +1,24 @@
-struct Temp {
+struct Error_VTable {
+    void (*display)(String);
+    void (*drop)(void* self);
+}
+
+struct Error {
+    void* self;
+    Error_VTable vtable;
+    void (*drop)(void* self);
 };
+
+void drop_Error(void* self) {
+    struct Error this = (struct Error*) self;
+
+    this.drop(this.self);
+    free(this.self);
+}
+
+struct Error Error(void* self, Error_VTable vtable) {
+    struct Error this;
+    this.self = self;
+    this.vtable = vtable;
+    return this;
+}
