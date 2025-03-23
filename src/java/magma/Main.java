21f2ae5ed5c76fbde79c6deccb85f8e3a7bac1d8
SirMathhman <sirmathhman@gmail.com>
2025-03-23 13:45:54 -0400

    Initial build

--- src/java/magma/Main.java
+++ src/java/magma/Main.java
@@ -4,24 +4,72 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.Set;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class Main {
+
+    public static final Path SOURCE_DIRECTORY = Paths.get(".", "src", "java");
+
     public static void main(String[] args) {
-        try (Stream<Path> paths = Files.walk(Paths.get(".", "src", "java"))) {
+        try (Stream<Path> paths = Files.walk(SOURCE_DIRECTORY)) {
             Set<Path> sources = paths.filter(Files::isRegularFile)
                     .filter(path -> path.toString().endsWith(".java"))
                     .collect(Collectors.toSet());
 
             for (Path source : sources) {
                 String input = Files.readString(source);
-                throw new CompileException("Invalid root", input);
+                String output = compile(input);
+
+                Path relative = SOURCE_DIRECTORY.relativize(source);
+                Path parent = relative.getParent();
+
+                String nameWithExt = relative.getFileName().toString();
+                String name = nameWithExt.substring(0, nameWithExt.lastIndexOf('.'));
+
+                Path targetParent = Paths.get(".", "src", "windows").resolve(parent);
+                if (!Files.exists(targetParent)) Files.createDirectories(targetParent);
+
+                Path target = targetParent.resolve(name + ".c");
+                Files.writeString(target, output);
             }
         } catch (CompileException | IOException e) {
             //noinspection CallToPrintStackTrace
             e.printStackTrace();
         }
+    }
+
+    private static String compile(String input) throws CompileException {
+        ArrayList<String> segments = new ArrayList<>();
+        StringBuilder buffer = new StringBuilder();
+        int depth = 0;
+        for (int i = 0; i < input.length(); i++) {
+            char c = input.charAt(i);
+            buffer.append(c);
+            if (c == ';' && depth == 0) {
+                segments.add(buffer.toString());
+                buffer = new StringBuilder();
+            } else {
+                if (c == '{') depth++;
+                if (c == '}') depth--;
+            }
+        }
+        segments.add(buffer.toString());
+
+        StringBuilder output = new StringBuilder();
+        for (String segment : segments) {
+            output.append(compileRootSegment(segment));
+        }
+
+        return output.toString();
+    }
+
+    private static String compileRootSegment(String segment) throws CompileException {
+        if (segment.startsWith("package ")) return "";
+        if (segment.strip().startsWith("import ")) return "#include <temp.h>\n";
+        if (segment.contains("class ")) return "struct Temp {\n};\n";
+        throw new CompileException("Invalid root segment", segment);
     }
 }
