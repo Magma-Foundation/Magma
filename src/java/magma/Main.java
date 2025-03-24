a92e6a5a2569cf13660b899e3ff9bbb78ade5e23
SirMathhman <sirmathhman@gmail.com>
2025-03-24 12:17:58 -0400

    Build?

--- src/java/magma/Main.java
+++ src/java/magma/Main.java
@@ -7,11 +7,16 @@ import java.nio.file.Paths;
 public class Main {
     public static void main(String[] args) {
         try {
-            String output = "int main(){\n\treturn 0;\n}";
+            String input = Files.readString(Paths.get(".", "src", "java", "magma", "Main.java"));
+            String output = compile(input);
             Files.writeString(Paths.get(".", "out", "main.c"), output);
         } catch (IOException e) {
             //noinspection CallToPrintStackTrace
             e.printStackTrace();
         }
+    }
+
+    private static String compile(String input) {
+        return input + "int main(){\n\treturn 0;\n}";
     }
 }
