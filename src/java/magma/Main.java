e8577915708d25bbca6478fe9b54f16a6cb45b54
SirMathhman <sirmathhman@gmail.com>
2025-03-26 17:50:14 -0400

    Main.c

--- src/java/magma/Main.java
+++ src/java/magma/Main.java
@@ -2,16 +2,51 @@ package magma;
 
 import java.io.IOException;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
         try {
-            String input = Files.readString(Paths.get(".", "src", "java", "magma", "Main.java"));
-            throw new CompileException("Invalid root", input);
+            Path source = Paths.get(".", "src", "java", "magma", "Main.java");
+            String input = Files.readString(source);
+            String output = compile(input);
+            Files.writeString(source.resolveSibling("Main.c"), output);
         } catch (IOException | CompileException e) {
             //noinspection CallToPrintStackTrace
             e.printStackTrace();
         }
+    }
+
+    private static String compile(String input) throws CompileException {
+        ArrayList<String> segments = new ArrayList<>();
+        StringBuilder buffer = new StringBuilder();
+        int depth = 0;
+        for (int i = 0; i < input.length(); i++) {
+            char c = input.charAt(i);
+            buffer.append(c);
+            if (c == ';' && depth == 0) {
+                segments.add(buffer.toString());
+                buffer = new StringBuilder();
+            } else {
+                if (c == '{') depth++;
+                if (c == '}') depth--;
+            }
+        }
+        segments.add(buffer.toString());
+
+        StringBuilder output = new StringBuilder();
+        for (String segment : segments) {
+            output.append(compileRootSegment(segment));
+        }
+        return output.toString();
+    }
+
+    private static String compileRootSegment(String input) throws CompileException {
+        if (input.startsWith("package ")) return "";
+        if (input.strip().startsWith("import ")) return "#include <temp.h>\n";
+        if(input.contains("class ")) return "struct Temp {\n};\n";
+        throw new CompileException("Invalid root segment", input);
     }
 }
