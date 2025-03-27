09a7187a8c5355afeb2f82fb12cc8334b060fe0a
SirMathhman <sirmathhman@gmail.com>
2025-03-27 19:46:54 -0400

    Windows directory

--- src/java/magma/Main.java
+++ src/java/magma/Main.java
@@ -11,7 +11,8 @@ import java.util.stream.Stream;
 
 public class Main {
     public static void main(String[] args) {
-        try (Stream<Path> stream = Files.walk(Paths.get(".", "src", "java"))) {
+        Path sourceDirectory = Paths.get(".", "src", "java");
+        try (Stream<Path> stream = Files.walk(sourceDirectory)) {
             Set<Path> sources = stream.filter(Files::isRegularFile)
                     .filter(path -> path.toString().endsWith(".java"))
                     .collect(Collectors.toSet());
@@ -21,23 +22,47 @@ public class Main {
 
                 ArrayList<String> segments = new ArrayList<>();
                 StringBuilder buffer = new StringBuilder();
+                int depth = 0;
                 for (int i = 0; i < input.length(); i++) {
                     char c = input.charAt(i);
                     buffer.append(c);
-                    if (c == ';') {
+                    if (c == ';' && depth == 0) {
                         segments.add(buffer.toString());
                         buffer = new StringBuilder();
+                    } else {
+                        if (c == '{') depth++;
+                        if (c == '}') depth--;
                     }
                 }
                 segments.add(buffer.toString());
 
+                StringBuilder output = new StringBuilder();
                 for (String segment : segments) {
-                    throw new CompileException("Invalid root segment", segment);
+                    output.append(compileRootSegment(segment));
                 }
+
+                Path relative = sourceDirectory.relativize(source);
+                Path parent = relative.getParent();
+                Path targetDirectory = Paths.get(".", "src", "windows");
+                Path targetParent = targetDirectory.resolve(parent);
+                if (!Files.exists(targetParent)) Files.createDirectories(targetParent);
+
+                String nameWithExt = relative.getFileName().toString();
+                String name = nameWithExt.substring(0, nameWithExt.lastIndexOf("."));
+                Path target = targetParent.resolve(name + ".c");
+
+                Files.writeString(target, output.toString());
             }
         } catch (IOException | CompileException e) {
             //noinspection CallToPrintStackTrace
             e.printStackTrace();
         }
+    }
+
+    private static String compileRootSegment(String segment) throws CompileException {
+        if (segment.startsWith("package ")) return "";
+        if (segment.strip().startsWith("import ")) return "#include <temp.h>\n";
+        if (segment.contains("class ")) return "struct Temp {\n};\n";
+        throw new CompileException("Invalid root segment", segment);
     }
 }
