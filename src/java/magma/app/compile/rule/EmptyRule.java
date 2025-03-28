ac7dcfc85c7c3f68bb1698f443aef4d76b66e18e
SirMathhman <sirmathhman@gmail.com>
2025-03-28 19:49:21 -0400

    Repair PrefixRule

--- src/java/magma/app/compile/rule/EmptyRule.java
+++ src/java/magma/app/compile/rule/EmptyRule.java
@@ -3,29 +3,21 @@ package magma.app.compile.rule;
 import magma.api.result.Err;
 import magma.api.result.Ok;
 import magma.api.result.Result;
-import magma.api.result.Tuple;
 import magma.app.compile.CompileError;
-import magma.app.compile.Compiler;
 import magma.app.compile.MapNode;
 import magma.app.compile.Node;
 import magma.app.compile.ParseState;
 import magma.app.compile.StringContext;
 
 public class EmptyRule implements Rule {
-    private Result<Tuple<String, String>, CompileError> apply(ParseState state, String stripped) {
-        if (stripped.isEmpty()) return Compiler.generateEmpty();
-        return new Err<>(new CompileError("Input not empty", new StringContext(stripped)));
-    }
-
     @Override
     public Result<Node, CompileError> parse(ParseState state, String input) {
-        return apply(state, input).mapValue(tuple -> {
-            return new MapNode().withString("header", tuple.left()).withString("target", tuple.right());
-        });
+        if (input.isEmpty()) return new Ok<>(new MapNode());
+        return new Err<>(new CompileError("Must be empty", new StringContext(input)));
     }
 
     @Override
     public Result<String, CompileError> generate(Node node) {
-        return new Ok<>(new MapOutput().with("header", node.findString("header").orElse("")));
+        return new Ok<>("");
     }
 }
--- src/java/magma/app/compile/rule/PrefixRule.java
+++ src/java/magma/app/compile/rule/PrefixRule.java
@@ -1,40 +1,29 @@
 package magma.app.compile.rule;
 
 import magma.api.result.Err;
-import magma.api.result.Ok;
 import magma.api.result.Result;
-import magma.api.result.Tuple;
 import magma.app.compile.CompileError;
-import magma.app.compile.MapNode;
 import magma.app.compile.Node;
 import magma.app.compile.ParseState;
 import magma.app.compile.StringContext;
 
 public record PrefixRule(String prefix, SuffixRule childRule) implements Rule {
-    static Err<Tuple<String, String>, CompileError> createPrefixErr(String input, String prefix) {
-        return new Err<>(new CompileError("Prefix '" + prefix + "' not present", new StringContext(input)));
-    }
-
-    private Result<Tuple<String, String>, CompileError> apply(ParseState state, String input) {
-        if (input.startsWith(prefix)) {
-            String slice = input.substring(prefix.length());
-            return childRule.parse(state, slice)
-                    .flatMapValue(childRule::generate)
-                    .mapValue(Output::toTuple);
-        }
-
-        return createPrefixErr(input, prefix);
-    }
-
     @Override
     public Result<Node, CompileError> parse(ParseState state, String input) {
-        return apply(state, input).mapValue(tuple -> {
-            return new MapNode().withString("header", tuple.left()).withString("target", tuple.right());
-        });
+        if (input.startsWith(prefix)) {
+            String slice = input.substring(prefix.length());
+            return childRule.parse(state, slice);
+        }
+
+        String format = "Prefix '%s' not present";
+        String message = format.formatted(prefix);
+        StringContext context = new StringContext(input);
+        CompileError error = new CompileError(message, context);
+        return new Err<>(error);
     }
 
     @Override
     public Result<String, CompileError> generate(Node node) {
-        return new Ok<>(new MapOutput().with("header", node.findString("header").orElse("")));
+        return childRule.generate(node).mapValue(output -> prefix + output);
     }
 }
