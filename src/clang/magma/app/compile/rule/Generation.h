29c917569857a80eec5718902c6bef232cf82ba2
SirMathhman <sirmathhman@gmail.com>
2025-03-28 19:18:55 -0400

    Separate

---
+++ src/clang/magma/app/compile/rule/Generation.h
@@ -0,0 +1,6 @@
+#ifndef magma_app_compile_rule_Generation
+#define magma_app_compile_rule_Generation
+#include "../../../../magma/api/collect/Map_.h"
+struct Generation {
+};
+#endif
---
+++ src/clang/magma/app/compile/rule/Output.h
@@ -0,0 +1,6 @@
+#ifndef magma_app_compile_rule_Output
+#define magma_app_compile_rule_Output
+#include "../../../../magma/api/result/Tuple.h"
+struct Output {
+};
+#endif
