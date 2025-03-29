79b4f16f934d321065eafdd4d2e0dfc5e3f04f2e
SirMathhman <sirmathhman@gmail.com>
2025-03-29 16:00:39 -0400

    Repair OrRule

--- src/java/magma/compile/CompileError.java
+++ src/java/magma/compile/CompileError.java
@@ -1,19 +1,33 @@
 package magma.compile;
 
+import jvm.collect.list.Lists;
+import magma.collect.Joiner;
+import magma.collect.list.List_;
 import magma.compile.context.Context;
 import magma.error.Error;
 
 public class CompileError implements Error {
     private final String message;
     private final Context context;
+    private final List_<CompileError> errors;
 
     public CompileError(String message, Context context) {
+        this(message, context, Lists.empty());
+    }
+
+    public CompileError(String message, Context context, List_<CompileError> errors) {
         this.message = message;
         this.context = context;
+        this.errors = errors;
     }
 
     @Override
     public String display() {
-        return message + ": " + context.display();
+        String joined = errors.stream()
+                .map(CompileError::display)
+                .collect(new Joiner(""))
+                .orElse("");
+
+        return message + ": " + context.display() + joined;
     }
 }
--- src/java/magma/compile/rule/OrRule.java
+++ src/java/magma/compile/rule/OrRule.java
@@ -2,34 +2,32 @@ package magma.compile.rule;
 
 import magma.collect.list.List_;
 import magma.compile.CompileError;
-import magma.compile.MapNode;
 import magma.compile.Node;
+import magma.compile.context.Context;
+import magma.compile.context.NodeContext;
 import magma.compile.context.StringContext;
 import magma.result.Err;
 import magma.result.Ok;
 import magma.result.Result;
 
-public record OrRule(List_<Rule> rules) implements Rule {
-    static OrState fold(String input, OrState orState, Rule rule) {
-        return rule.parse(input).flatMapValue(rule::generate)
-                .findValue()
-                .map(orState::withValue)
-                .orElse(orState);
-    }
+import java.util.function.Function;
+import java.util.function.Supplier;
 
-    private Result<String, CompileError> compile(String input) {
+public record OrRule(List_<Rule> rules) implements Rule {
+    private <T> Result<T, CompileError> apply(Function<Rule, Result<T, CompileError>> applicator, Supplier<Context> context) {
         return rules.stream()
-                .foldWithInitial(new OrState(), (orState, rule) -> fold(input, orState, rule))
-                .toOption().<Result<String, CompileError>>match(Ok::new, () -> new Err<>(new CompileError("Invalid root segment", new StringContext(input))));
+                .foldWithInitial(new OrState<T>(), (orState, rule) -> applicator.apply(rule).match(orState::withValue, orState::withError))
+                .toResult()
+                .<Result<T, CompileError>>match(Ok::new, errors -> new Err<>(new CompileError("Invalid root segment", context.get(), errors)));
     }
 
     @Override
     public Result<Node, CompileError> parse(String input) {
-        return compile(input).mapValue(value -> new MapNode().withString("value", value));
+        return apply(rule -> rule.parse(input), () -> new StringContext(input));
     }
 
     @Override
     public Result<String, CompileError> generate(Node input) {
-        return new Ok<>(input.findString("value").orElse(""));
+        return apply(rule -> rule.generate(input), () -> new NodeContext(input));
     }
 }
