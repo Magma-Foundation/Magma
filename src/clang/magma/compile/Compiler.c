78c4d12d524d9a721e062f80e3c660bd6c8afbf7
SirMathhman <sirmathhman@gmail.com>
2025-03-30 19:35:46 -0400

    Rebuild

--- src/clang/magma/compile/Compiler.c
+++ src/clang/magma/compile/Compiler.c
@@ -1,10 +1,12 @@
 #include "Compiler.h"
-struct Result_Map__String_String_CompileError compile(struct String input, struct List__String namespace, struct String name){State state = new State(namespace, name);return JavaLang.createJavaRootRule().parse(input).flatMapValue(__lambda0__).flatMapValue(__lambda1__).flatMapValue(__lambda2__).flatMapValue(__lambda3__).flatMapValue(__lambda4__).flatMapValue(__lambda5__).flatMapValue(__lambda6__).flatMapValue(Compiler.generateRoots);
+struct Result_Map__String_String_CompileError postLoad(struct State state, struct Node tree){return TreeTransformingStage(Formatter()).transform(tree, state).flatMapValue(Compiler.generateRoots);
+}
+struct Result_Node_CompileError preLoad(struct String input, struct State state){return JavaLang.createJavaRootRule().parse(input).flatMapValue(__lambda0__).flatMapValue(__lambda1__).flatMapValue(__lambda2__).flatMapValue(__lambda3__).flatMapValue(__lambda4__).flatMapValue(__lambda5__);
 }
 struct Result_Map__String_String_CompileError generateRoots(struct Node roots){return roots.streamNodes().foldToResult(Maps.empty(), Compiler.generateTarget);
 }
 struct Result_Map__String_String_CompileError generateTarget(struct Map__String_String current, struct Tuple_String_Node tuple){String extension = tuple.left();
-        Node root = tuple.right();return CLang.createCRootRule().generate(root).mapValue(__lambda7__);
+        Node root = tuple.right();return CLang.createCRootRule().generate(root).mapValue(__lambda6__);
 }
 auto __lambda0__();
 auto __lambda1__();
@@ -13,5 +15,4 @@ auto __lambda3__();
 auto __lambda4__();
 auto __lambda5__();
 auto __lambda6__();
-auto __lambda7__();
 
--- src/clang/magma/compile/Compiler.h
+++ src/clang/magma/compile/Compiler.h
@@ -1,7 +1,6 @@
 #ifndef magma_compile_Compiler
 #define magma_compile_Compiler
 #include "../../windows/collect/map/Maps.h"
-#include "../../magma/collect/list/List_.h"
 #include "../../magma/collect/map/Map_.h"
 #include "../../magma/compile/lang/CLang.h"
 #include "../../magma/compile/lang/FlattenRoot.h"
@@ -18,14 +17,15 @@ struct Compiler{
 };
 // expand Result_Map__String_String_CompileError = Result<struct Map__String_String, struct CompileError>
 // expand Map__String_String = Map_<struct String, struct String>
-// expand List__String = List_<struct String>
+// expand Result_Node_CompileError = Result<struct Node, struct CompileError>
 // expand Result_Map__String_String_CompileError = Result<struct Map__String_String, struct CompileError>
 // expand Map__String_String = Map_<struct String, struct String>
 // expand Result_Map__String_String_CompileError = Result<struct Map__String_String, struct CompileError>
 // expand Map__String_String = Map_<struct String, struct String>
 // expand Map__String_String = Map_<struct String, struct String>
 // expand Tuple_String_Node = Tuple<struct String, struct Node>
-struct Result_Map__String_String_CompileError compile(struct String input, struct List__String namespace, struct String name);
+struct Result_Map__String_String_CompileError postLoad(struct State state, struct Node tree);
+struct Result_Node_CompileError preLoad(struct String input, struct State state);
 struct Result_Map__String_String_CompileError generateRoots(struct Node roots);
 struct Result_Map__String_String_CompileError generateTarget(struct Map__String_String current, struct Tuple_String_Node tuple);
 auto __lambda0__();
@@ -35,6 +35,5 @@ auto __lambda3__();
 auto __lambda4__();
 auto __lambda5__();
 auto __lambda6__();
-auto __lambda7__();
 #endif
 
