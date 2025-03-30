d49b2b76f9eabb9c3f194d0ce744b9a714e1d839
SirMathhman <sirmathhman@gmail.com>
2025-03-30 14:40:54 -0400

    Fix symbols?

--- src/java/magma/compile/lang/r/SymbolRule.java
+++ src/java/magma/compile/lang/r/SymbolRule.java
@@ -20,7 +20,7 @@ public record SymbolRule(Rule rule) implements Rule {
     private boolean isSymbol(String input) {
         for (int i = 0; i < input.length(); i++) {
             char c = input.charAt(i);
-            if (c == '_' || Character.isLetter(c)) continue;
+            if (c == '_' || Character.isLetter(c) || (i != 0 && Character.isDigit(c))) continue;
             return false;
         }
 
