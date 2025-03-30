e15c1822d753113185ffcaec3e90898291301319
SirMathhman <sirmathhman@gmail.com>
2025-03-30 13:33:02 -0400

    Unwrap structs

--- src/java/magma/compile/lang/JavaCTransformer.java
+++ src/java/magma/compile/lang/JavaCTransformer.java
@@ -4,6 +4,7 @@ import jvm.collect.list.Lists;
 import jvm.collect.stream.Streams;
 import magma.collect.list.ListCollector;
 import magma.collect.list.List_;
+import magma.compile.MapNode;
 import magma.compile.Node;
 import magma.option.Tuple;
 
@@ -25,7 +26,34 @@ public class JavaCTransformer implements Transformer {
             return node.withNodeList("children", newChildren);
         }
 
+        if (node.is("interface")) {
+            Tuple<List_<Node>, List_<Node>> children = node.findNodeList("children")
+                    .orElse(Lists.empty())
+                    .stream()
+                    .foldWithInitial(new Tuple<>(Lists.empty(), Lists.empty()), JavaCTransformer::bucketClassMember);
+
+            Node withChildren = node.retype("struct").withNodeList("children", children.left());
+            return new MapNode("group")
+                    .withNode("child", withChildren)
+                    .withNodeList("functions", children.right());
+        }
+
+        if (node.is("method")) {
+            return node.retype("function");
+        }
+
         return node;
+    }
+
+    private static Tuple<List_<Node>, List_<Node>> bucketClassMember(Tuple<List_<Node>, List_<Node>> tuple, Node element) {
+        List_<Node> definitions = tuple.left();
+        List_<Node> others = tuple.right();
+
+        if (element.is("definition")) {
+            return new Tuple<>(definitions.add(element), others);
+        } else {
+            return new Tuple<>(definitions, others.add(element));
+        }
     }
 
     @Override
