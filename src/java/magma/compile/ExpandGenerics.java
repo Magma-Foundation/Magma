07727c5061a1a2895f7b645275df7c8a0aff3932
SirMathhman <sirmathhman@gmail.com>
2025-03-30 22:26:55 -0400

    Pull out generic types

--- src/java/magma/compile/ExpandGenerics.java
+++ src/java/magma/compile/ExpandGenerics.java
@@ -1,16 +1,11 @@
 package magma.compile;
 
-import jvm.collect.list.JavaList;
 import jvm.collect.list.Lists;
-import jvm.collect.stream.Streams;
 import magma.collect.list.List_;
-import magma.collect.stream.Joiner;
 import magma.compile.transform.State;
 import magma.compile.transform.Transformer;
 import magma.result.Ok;
 import magma.result.Result;
-
-import java.util.Map;
 
 public class ExpandGenerics implements Transformer {
     @Override
@@ -37,7 +32,7 @@ public class ExpandGenerics implements Transformer {
                     .withNodeList("params", Lists.of(param)));
         }
 
-        if(value.equals("Supplier")) {
+        if (value.equals("Supplier")) {
             List_<Node> arguments = type.findNodeList("arguments").orElseGet(Lists::empty);
             Node returns = arguments.get(0);
 
@@ -51,48 +46,12 @@ public class ExpandGenerics implements Transformer {
 
     @Override
     public Result<Node, CompileError> afterPass(State state, Node node) {
-        if(node.is("generic")) {
-            String value = stringify(node);
-
-            Node symbol = new MapNode("symbol-type").withString("value", value);
-            Node expansion = new MapNode("expansion")
-                    .withString("name", value)
-                    .withNode("type", node);
-
+        if (node.is("generic")) {
             return new Ok<>(new MapNode("group")
-                    .withNode("child", symbol)
-                    .withNodeList("expansions", Lists.of(expansion)));
-        }
-
-        if (node.is("construction")) {
-            Node type = node.findNode("type").orElse(new MapNode());
-            return new Ok<>(node.retype("invocation")
-                    .withNode("caller", new MapNode("symbol-value").withString("value", stringify(type))));
+                    .withNode("child", node)
+                    .withNodeList("expansions", Lists.of(node)));
         }
 
         return new Ok<>(node);
-    }
-
-    private String stringify(Node node) {
-        if (node.is("generic")) {
-            String caller = node.findNodeList("base")
-                    .orElse(Lists.empty())
-                    .stream()
-                    .map(element -> element.findString("value"))
-                    .flatMap(Streams::fromOption)
-                    .collect(new Joiner("_"))
-                    .orElse("");
-
-            return node.findNodeList("arguments").orElse(new JavaList<>())
-                    .stream()
-                    .map(this::stringify)
-                    .collect(new Joiner("_"))
-                    .map(arguments -> caller + "_" + arguments)
-                    .orElse(caller);
-        }
-
-        return node.findString("value")
-                .or(() -> node.findNode("child").orElse(new MapNode()).findString("value"))
-                .orElse("?");
     }
 }
