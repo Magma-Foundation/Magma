7eceb4ac832115fddf7fe53458d5ce63ee8b77bf
SirMathhman <sirmathhman@gmail.com>
2025-04-03 18:57:15 -0400

    Nuke

--- src/java/magma/CompileException.java
+++
@@ -1,7 +0,0 @@
-package magma;
-
-public class CompileException extends Exception {
-    public CompileException(String message, String context) {
-        super(message + ": " + context);
-    }
-}
--- src/java/magma/Rule.java
+++
@@ -1,9 +0,0 @@
-package magma;
-
-import magma.result.Result;
-
-public interface Rule {
-    Result<Node, CompileException> parse(String input);
-
-    Result<String, CompileException> generate(Node input);
-}
