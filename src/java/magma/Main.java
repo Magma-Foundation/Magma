0dda6cbc866609effe201c813a50285a36054ebc
SirMathhman <sirmathhman@gmail.com>
2025-04-05 15:31:11 -0400

    Split segments?

--- src/java/magma/Main.java
+++ src/java/magma/Main.java
@@ -5,40 +5,50 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.function.Function;
 
 public class Main {
     public static void main(String[] args) {
         try {
             Path source = Paths.get(".", "src", "java", "magma", "Main.java");
             String input = Files.readString(source);
-            ArrayList<String> segments = new ArrayList<>();
-            StringBuilder buffer = new StringBuilder();
-            int depth = 0;
-            for (int i = 0; i < input.length(); i++) {
-                char c = input.charAt(i);
-                buffer.append(c);
-
-                if (c == ';' && depth == 0) {
-                    segments.add(buffer.toString());
-                    buffer = new StringBuilder();
-                } else {
-                    if (c == '{') depth++;
-                    if (c == '}') depth--;
-                }
-            }
-            segments.add(buffer.toString());
-
-            StringBuilder output = new StringBuilder();
-            for (String segment : segments) {
-                output.append(compileRootSegment(segment));
-            }
-
+            String string = compile(input, Main::compileRootSegment);
             Path target = source.resolveSibling("main.c");
-            Files.writeString(target, output + "int main(){\n\treturn 0;\n}\n");
+            Files.writeString(target, string + "int main(){\n\treturn 0;\n}\n");
         } catch (IOException e) {
             //noinspection CallToPrintStackTrace
             e.printStackTrace();
         }
+    }
+
+    private static String compile(String input, Function<String, String> compiler) {
+        ArrayList<String> segments = new ArrayList<>();
+        StringBuilder buffer = new StringBuilder();
+        int depth = 0;
+        for (int i = 0; i < input.length(); i++) {
+            char c = input.charAt(i);
+            buffer.append(c);
+
+            if (c == ';' && depth == 0) {
+                segments.add(buffer.toString());
+                buffer = new StringBuilder();
+            } else if (c == '}' && depth == 1) {
+                segments.add(buffer.toString());
+                buffer = new StringBuilder();
+                depth--;
+            } else {
+                if (c == '{') depth++;
+                if (c == '}') depth--;
+            }
+        }
+        segments.add(buffer.toString());
+
+        StringBuilder output = new StringBuilder();
+        for (String segment : segments) {
+            output.append(compiler.apply(segment));
+        }
+
+        return output.toString();
     }
 
     private static String compileRootSegment(String input) {
@@ -54,13 +64,22 @@ public class Main {
                 String withEnd = afterKeyword.substring(contentStart + "{".length()).strip();
                 if (withEnd.endsWith("}")) {
                     String content = withEnd.substring(0, withEnd.length() - "}".length());
+                    compile(content, Main::compileClassSegment);
                     return "struct " + name + " {\n};\n" + content;
                 }
             }
         }
 
-        System.err.println("Invalid segment: " + input);
+        return invalidate("root segment", input);
+    }
+
+    private static String invalidate(String type, String input) {
+        System.err.println("Invalid " + type + ": " + input);
         return input;
+    }
+
+    private static String compileClassSegment(String input) {
+        return invalidate("class segment", input);
     }
 }
 
--- src/java/magma/main.c
+++ src/java/magma/main.c
@@ -3,63 +3,79 @@
 #include <temp.h>
 #include <temp.h>
 #include <temp.h>
+#include <temp.h>
 struct Main {
 };
 public static void main(String[] args) {
         try {
             Path source = Paths.get(".", "src", "java", "magma", "Main.java");
             String input = Files.readString(source);
-            ArrayList<String> segments = new ArrayList<>();
-            StringBuilder buffer = new StringBuilder();
-            int depth = 0;
-            for (int i = 0; i < input.length(); i++) {
-                char c = input.charAt(i);
-                buffer.append(c);
-
-                if (c == ';' && depth == 0) {
-                    segments.add(buffer.toString());
-                    buffer = new StringBuilder();
-                } else {
-                    if (c == '{') depth++;
-                    if (c == '}') depth--;
-                }
-            }
-            segments.add(buffer.toString());
-
-            StringBuilder output = new StringBuilder();
-            for (String segment : segments) {
-                output.append(compileRootSegment(segment));
-            }
-
+            String string = compile(input, Main::compileRootSegment);
             Path target = source.resolveSibling("main.c");
-            Files.writeString(target, output + "int main(){\n\treturn 0;\n}\n");
+            Files.writeString(target, string + "int main(){\n\treturn 0;\n}\n");
         } catch (IOException e) {
             //noinspection CallToPrintStackTrace
             e.printStackTrace();
         }
     }
 
-    private static String compileRootSegment(String input) {
-        if (input.startsWith("package ")) return "";
-        if (input.strip().startsWith("import ")) return "#include <temp.h>\n";
+    private static String compile(String input, Function<String, String> compiler) {
+        ArrayList<String> segments = new ArrayList<>();
+        StringBuilder buffer = new StringBuilder();
+        int depth = 0;
+        for (int i = 0; i < input.length(); i++) {
+            char c = input.charAt(i);
+            buffer.append(c);
 
-        int classIndex = input.indexOf("class ");
+            if (c == ';' && depth == 0) {
+                segments.add(buffer.toString());
+                buffer = new StringBuilder();
+            } else if (c == '}' && depth == 1) {
+                segments.add(buffer.toString());
+                buffer = new StringBuilder();
+                depth--;
+            } else {
+                if (c == '{') depth++;
+                if (c == '}') depth--;
+            }
+        }
+        segments.add(buffer.toString());
+
+        StringBuilder output = new StringBuilder();
+        for (String segment : segments) {
+            output.append(compiler.apply(segment));
+        }
+
+        return output.toString();
+    struct ");
         if (classIndex >= 0) {
-            String afterKeyword = input.substring(classIndex + "class ".length());
+};
+String afterKeyword = input.substring(classIndex + "class ".length());
             int contentStart = afterKeyword.indexOf("{");
             if (contentStart >= 0) {
                 String name = afterKeyword.substring(0, contentStart).strip();
                 String withEnd = afterKeyword.substring(contentStart + "{".length()).strip();
                 if (withEnd.endsWith("}")) {
                     String content = withEnd.substring(0, withEnd.length() - "}".length());
+                    compile(content, Main::compileClassSegment);
                     return "struct " + name + " {\n};\n" + content;
                 }
             }
         }
 
-        System.err.println("Invalid segment: " + input);
+        return invalidate("root segment", input);
+    
+
+    private static String invalidate(String type, String input) {
+        System.err.println("Invalid " + type + ": " + input);
         return input;
     }
+
+    private static String compileClassSegment(String input) {
+        return invalidate("class segment", input);
+    }
+}
+
 int main(){
 	return 0;
 }
