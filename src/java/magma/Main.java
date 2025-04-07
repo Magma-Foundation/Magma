b51c49216c1a92bece07d53d99bd924597920382
SirMathhman <sirmathhman@gmail.com>
2025-04-06 20:04:18 -0400

    Output?

--- src/java/magma/Main.java
+++ src/java/magma/Main.java
@@ -8,8 +8,12 @@ import java.nio.file.Paths;
 public class Main {
     public static void main(String[] args) {
         try {
-            Path target = Paths.get(".", "src", "java", "magma", "main.c");
-            Files.writeString(target, "int main(){\n\treturn 0;\n}\n");
+            Path source = Paths.get(".", "src", "java", "magma", "Main.java");
+            String input = Files.readString(source);
+            String output = "/* " + input + " */\nint main(){\n\treturn 0;\n}\n";
+
+            Path target = source.resolveSibling("main.c");
+            Files.writeString(target, output);
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
--- src/java/magma/main.c
+++ src/java/magma/main.c
@@ -1,3 +1,24 @@
+/* package magma;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class Main {
+    public static void main(String[] args) {
+        try {
+            Path source = Paths.get(".", "src", "java", "magma", "Main.java");
+            String input = Files.readString(source);
+            String output = "/* " + input + " */\nint main(){\n\treturn 0;\n}\n";
+
+            Path target = source.resolveSibling("main.c");
+            Files.writeString(target, output);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+} */
 int main(){
 	return 0;
 }
