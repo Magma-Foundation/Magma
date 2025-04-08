3497dc85d3402473a001e3ac9c2cec5efe43d5fa
SirMathhman <sirmathhman@gmail.com>
2025-04-08 08:40:51 -0400

    Invalidate

--- src/java/magma/Main.java
+++ src/java/magma/Main.java
@@ -2,14 +2,32 @@ package magma;
 
 import java.io.IOException;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
 
 public class Main {
+    private static class CompileException extends Exception {
+        public CompileException(String message, String context) {
+            super(message + ": " + context);
+        }
+    }
+
     public static void main(String[] args) {
         try {
-            Files.writeString(Paths.get(".", "src", "java", "magma", "main.c"), "int main(){\n\treturn 0;\n}");
-        } catch (IOException e) {
-            throw new RuntimeException(e);
+            Path source = Paths.get(".", "src", "java", "magma", "Main.java");
+            String input = Files.readString(source);
+
+            String output = compile(input);
+
+            Path target = source.resolveSibling("main.c");
+            Files.writeString(target, output + "int main(){\n\treturn 0;\n}");
+        } catch (IOException | CompileException e) {
+            //noinspection CallToPrintStackTrace
+            e.printStackTrace();
         }
+    }
+
+    private static String compile(String input) throws CompileException {
+        throw new CompileException("Invalid root", input);
     }
 }
