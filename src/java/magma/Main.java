a2446f5bb3956138e68f4bba9dcb7b62d0b5f614
SirMathhman <sirmathhman@gmail.com>
2025-04-09 20:15:08 -0400

    Modifiers

--- src/java/magma/Main.java
+++ src/java/magma/Main.java
@@ -1,11 +1,15 @@
 package magma;
 
+import org.jetbrains.annotations.NotNull;
+
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) {
@@ -22,12 +26,16 @@ public class Main {
     private static String compile(String input) {
         ArrayList<String> segments = new ArrayList<>();
         StringBuilder buffer = new StringBuilder();
+        int depth = 0;
         for (int i = 0; i < input.length(); i++) {
             char c = input.charAt(i);
             buffer.append(c);
-            if (c == ';') {
+            if (c == ';' && depth == 0) {
                 segments.add(buffer.toString());
                 buffer = new StringBuilder();
+            } else {
+                if (c == '{') depth++;
+                if (c == '}') depth--;
             }
         }
         segments.add(buffer.toString());
@@ -42,14 +50,32 @@ public class Main {
 
     private static String compileRootSegment(String input) {
         if (input.startsWith("package ")) return "";
-        if (input.strip().startsWith("import ")) {
-            String right = input.strip().substring("import ".length());
+
+        String stripped = input.strip();
+        if (stripped.startsWith("import ")) {
+            String right = stripped.substring("import ".length());
             if (right.endsWith(";")) {
                 String content = right.substring(0, right.length() - ";".length());
                 String joined = String.join("/", content.split(Pattern.quote(".")));
                 return "#include \"./" + joined + "\"\n";
             }
         }
+
+        int classIndex = input.indexOf("class ".toString());
+        if (classIndex >= 0) {
+            String[] oldModifiers = input.substring(0, classIndex).strip().split(" ");
+            String newModifiers = Arrays.stream(oldModifiers)
+                    .map(String::strip)
+                    .map(Main::generatePlaceholder)
+                    .collect(Collectors.joining(" "));
+
+            return newModifiers + " struct Temp {\n};\n";
+        }
+
+        return generatePlaceholder(input);
+    }
+
+    private static String generatePlaceholder(String input) {
         return "/* " + input + " */";
     }
 }
--- src/java/magma/main.c
+++ src/java/magma/main.c
@@ -1,55 +1,11 @@
+#include "./org/jetbrains/annotations/NotNull"
 #include "./java/io/IOException"
 #include "./java/nio/file/Files"
 #include "./java/nio/file/Path"
 #include "./java/nio/file/Paths"
 #include "./java/util/ArrayList"
+#include "./java/util/Arrays"
 #include "./java/util/regex/Pattern"
-/* 
-
-public class Main {
-    public static void main(String[] args) {
-        try {
-            Path source = Paths.get(".", "src", "java", "magma", "Main.java"); *//* 
-            String input = Files.readString(source); *//* 
-            Path target = source.resolveSibling("main.c"); *//* 
-            Files.writeString(target, compile(input)); *//* 
-        } catch (IOException e) {
-            throw new RuntimeException(e); *//* 
-        }
-    }
-
-    private static String compile(String input) {
-        ArrayList<String> segments = new ArrayList<>(); *//* 
-        StringBuilder buffer = new StringBuilder(); *//* 
-        for (int i = 0; *//*  i < input.length(); *//*  i++) {
-            char c = input.charAt(i); *//* 
-            buffer.append(c); *//* 
-            if (c == '; *//* ') {
-                segments.add(buffer.toString()); *//* 
-                buffer = new StringBuilder(); *//* 
-            }
-        }
-        segments.add(buffer.toString()); *//* 
-
-        StringBuilder output = new StringBuilder(); *//* 
-        for (String segment : segments) {
-            output.append(compileRootSegment(segment)); *//* 
-        }
-
-        return output.toString(); *//* 
-    }
-
-    private static String compileRootSegment(String input) {
-        if (input.startsWith("package ")) return ""; *//* 
-        if (input.strip().startsWith("import ")) {
-            String right = input.strip().substring("import ".length()); *//* 
-            if (right.endsWith("; *//* ")) {
-                String content = right.substring(0, right.length() - "; *//* ".length()); *//* 
-                String joined = String.join("/", content.split(Pattern.quote("."))); *//* 
-                return "#include \"./" + joined + "\"\n"; *//* 
-            }
-        }
-        return "/* " + input + " */"; *//* 
-    }
-}
- */
+#include "./java/util/stream/Collectors"
+/* public */ struct Temp {
+};
