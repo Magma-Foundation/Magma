e8fe627a03c5dabfd798a616906323c6c6c1b818
SirMathhman <sirmathhman@gmail.com>
2025-04-13 12:48:04 -0400

    Divide root segments

--- src/java/magma/main.c
+++ src/java/magma/main.c
@@ -1,19 +1,84 @@
-/* package magma;
+/* package magma;*//* 
 
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
+import java.io.IOException;*//* 
+import java.nio.file.Files;*//* 
+import java.nio.file.Path;*//* 
+import java.nio.file.Paths;*//* 
+import java.util.ArrayList;*//* 
+import java.util.List;*//* 
 
 public class Main {
+    private static class State {
+        private final List<String> segments;*//* 
+        private StringBuilder buffer;*//* 
+
+        private State(List<String> segments, StringBuilder buffer) {
+            this.segments = segments;*//* 
+            this.buffer = buffer;*//* 
+        }
+
+        public State() {
+            this(new ArrayList<>(), new StringBuilder());*//* 
+        }
+
+        private State advance() {
+            this.segments().add(this.buffer.toString());*//* 
+            this.buffer = new StringBuilder();*//* 
+            return this;*//* 
+        }
+
+        private State append(char c) {
+            this.buffer.append(c);*//* 
+            return this;*//* 
+        }
+
+        public List<String> segments() {
+            return this.segments;*//* 
+        }
+    }
+
     public static void main(String[] args) {
         try {
-            Path source = Paths.get(".", "src", "java", "magma", "Main.java");
-            String input = Files.readString(source);
-            Path target = source.resolveSibling("main.c");
-            Files.writeString(target, "/* " + input + "*/");
+            Path source = Paths.get(".", "src", "java", "magma", "Main.java");*//* 
+            String input = Files.readString(source);*//* 
+            Path target = source.resolveSibling("main.c");*//* 
+            Files.writeString(target, compile(input));*//* 
         } catch (IOException e) {
-            throw new RuntimeException(e);
+            throw new RuntimeException(e);*//* 
         }
+    }
+
+    private static String compile(String input) {
+        List<String> segments = divide(input);*//* 
+        StringBuilder output = new StringBuilder();*//* 
+        for (String segment : segments) {
+            output.append(generatePlaceholder(segment));*//* 
+        }
+        return output.toString();*//* 
+    }
+
+    private static List<String> divide(String input) {
+        State current = new State();*//* 
+
+        for (int i = 0;*//*  i < input.length();*//*  i++) {
+            char c = input.charAt(i);*//* 
+            current = extracted(current, c);*//* 
+        }
+
+        return current.advance().segments();*//* 
+    }
+
+    private static State extracted(State current, char c) {
+        State appended = current.append(c);*//* 
+        if (c == ';*//* ') {
+            return appended.advance();*//* 
+        }
+        else {
+            return appended;*//* 
+        }
+    }
+
+    private static String generatePlaceholder(String input) {
+        return "/* " + input + "*/";*//* 
     }
 }*/
