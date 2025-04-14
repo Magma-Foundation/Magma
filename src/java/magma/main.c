84cc75268c2f9dee180e7f17a049f9beff392510
SirMathhman <sirmathhman@gmail.com>
2025-04-14 16:38:31 -0400

    Remove duplicates

--- src/java/magma/main.c
+++ src/java/magma/main.c
@@ -5,8 +5,38 @@ import java.nio.file.Files; *//*
 import java.nio.file.Path; *//* 
 import java.nio.file.Paths; *//* 
 import java.util.ArrayList; *//* 
+import java.util.List; *//* 
 
 public class Main {
+    private static class State {
+        private final List<String> segments; *//* 
+        private StringBuilder buffer; *//* 
+
+        private State(List<String> segments, StringBuilder buffer) {
+            this.segments = segments; *//* 
+            this.buffer = buffer; *//* 
+        }
+
+        public State() {
+            this(new ArrayList<>(), new StringBuilder()); *//* 
+        }
+
+        private State advance() {
+            this.segments.add(this.buffer.toString()); *//* 
+            this.buffer = new StringBuilder(); *//* 
+            return this; *//* 
+        }
+
+        private State append(char c) {
+            this.buffer.append(c); *//* 
+            return this; *//* 
+        }
+
+        public List<String> segments() {
+            return this.segments; *//* 
+        }
+    }
+
     public static void main(String[] args) {
         try {
             Path source = Paths.get(".", "src", "java", "magma", "Main.java"); *//* 
@@ -19,24 +49,34 @@ public class Main {
     }
 
     private static String compile(String input) {
-        ArrayList<String> segments = new ArrayList<>(); *//* 
-        StringBuilder buffer = new StringBuilder(); *//* 
-        for (int i = 0; *//*  i < input.length(); *//*  i++) {
-            char c = input.charAt(i); *//* 
-            buffer.append(c); *//* 
-            if(c == '; *//* ') {
-                segments.add(buffer.toString()); *//* 
-                buffer= new StringBuilder(); *//* 
-            }
-        }
-        segments.add(buffer.toString()); *//* 
+        List<String> segments = extracted(input, new State()); *//* 
 
         StringBuilder output = new StringBuilder(); *//* 
         for (String segment : segments) {
             output.append(compileRootSegment(segment)); *//* 
         }
-        
+
         return output.toString(); *//* 
+    }
+
+    private static List<String> extracted(String input, State state) {
+        State current = state; *//* 
+        for (int i = 0; *//*  i < input.length(); *//*  i++) {
+            char c = input.charAt(i); *//* 
+            current = divideStatementChar(current, c); *//* 
+        }
+
+        return current.advance().segments; *//* 
+    }
+
+    private static State divideStatementChar(State current, char c) {
+        State appended = current.append(c); *//* 
+        if (c == '; *//* ') {
+            return appended.advance(); *//* 
+        }
+        else {
+            return appended; *//* 
+        }
     }
 
     private static String compileRootSegment(String input) {
