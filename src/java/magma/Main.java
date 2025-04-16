962bbeebd1442265fb23e0a0f946247e510d7d8d
SirMathhman <sirmathhman@gmail.com>
2025-04-16 17:20:12 -0400

    Main copy

--- src/java/magma/Main.java
+++ src/java/magma/Main.java
@@ -1,7 +1,20 @@
 package magma;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 public class Main {
     public static void main(String[] args) {
-
+        try {
+            Path source = Paths.get(".", "src", "java", "magma", "Main.java");
+            String input = Files.readString(source);
+            Path target = source.resolveSibling("main.c");
+            Files.writeString(target, "/* " + input + " */");
+        } catch (IOException e) {
+            //noinspection CallToPrintStackTrace
+            e.printStackTrace();
+        }
     }
 }
