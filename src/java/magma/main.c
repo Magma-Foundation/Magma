fd49ea071267679b0f7a693db4415983e4e025b4
SirMathhman <sirmathhman@gmail.com>
2025-04-16 17:21:27 -0400

    Root segments

---
+++ src/java/magma/main.c
@@ -0,0 +1,46 @@
+/* package magma; *//* 
+
+import java.io.IOException; *//* 
+import java.nio.file.Files; *//* 
+import java.nio.file.Path; *//* 
+import java.nio.file.Paths; *//* 
+import java.util.ArrayList; *//* 
+
+public class Main {
+    public static void main(String[] args) {
+        try {
+            Path source = Paths.get(".", "src", "java", "magma", "Main.java"); *//* 
+            String input = Files.readString(source); *//* 
+            Path target = source.resolveSibling("main.c"); *//* 
+            Files.writeString(target, compile(input)); *//* 
+        } catch (IOException e) {
+            //noinspection CallToPrintStackTrace
+            e.printStackTrace(); *//* 
+        }
+    }
+
+    private static String compile(String input) {
+        ArrayList<String> segments = new ArrayList<>(); *//* 
+        StringBuilder buffer = new StringBuilder(); *//* 
+        for (int i = 0; *//*  i < input.length(); *//*  i++) {
+            char c = input.charAt(i); *//* 
+            buffer.append(c); *//* 
+            if (c == '; *//* ') {
+                segments.add(buffer.toString()); *//* 
+                buffer = new StringBuilder(); *//* 
+            }
+        }
+        segments.add(buffer.toString()); *//* 
+
+        StringBuilder output = new StringBuilder(); *//* 
+        for (String segment : segments) {
+            output.append(compileRootSegment(segment)); *//* 
+        }
+        return output.toString(); *//* 
+    }
+
+    private static String compileRootSegment(String input) {
+        return "/* " + input + " */"; *//* 
+    }
+}
+ */
