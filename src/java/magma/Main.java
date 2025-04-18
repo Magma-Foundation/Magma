4d071ee171a0594261b0a2f3b6d2cfd9cf8277f0
SirMathhman <sirmathhman@gmail.com>
2025-04-18 08:40:33 -0400

    Hmmmm

--- src/java/magma/Main.java
+++ src/java/magma/Main.java
@@ -15,19 +15,28 @@ public class Main {
 
             ArrayList<String> segments = new ArrayList<>();
             StringBuilder buffer = new StringBuilder();
+            int depth = 0;
             for (int i = 0; i < input.length(); i++) {
                 char c = input.charAt(i);
                 buffer.append(c);
-                if(c == ';') {
+                if (c == ';' && depth == 0) {
                     segments.add(buffer.toString());
                     buffer = new StringBuilder();
+                }
+                else {
+                    if (c == '{') {
+                        depth++;
+                    }
+                    if (c == '}') {
+                        depth--;
+                    }
                 }
             }
             segments.add(buffer.toString());
 
 
             String collect = segments.stream()
-                    .map(value -> "{\n\t\t\"value:\" : \"" + value + "\"\n\t},  ")
+                    .map(value -> "{\n\t\t\"value:\" : \"" + value.strip() + "\"\n\t},  ")
                     .collect(Collectors.joining());
 
             Path target = source.resolveSibling("Main.java.ast.json");
