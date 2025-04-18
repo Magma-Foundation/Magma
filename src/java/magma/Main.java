c7657c5738afdecdf40864d09f9cd99ee6481edb
SirMathhman <sirmathhman@gmail.com>
2025-04-18 09:03:06 -0400

    Cleanup structure

--- src/java/magma/Main.java
+++ src/java/magma/Main.java
@@ -6,26 +6,59 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Main {
-    private record Node(Map<String, String> strings) {
+    private record Node(Map<String, String> strings, Map<String, java.util.List<Node>> nodeLists) {
         public Node() {
-            this(new HashMap<>());
+            this(new HashMap<>(), new HashMap<>());
+        }
+
+        private static String createJSONProperty(int depth, String name, String value) {
+            String indent = "\t".repeat(depth);
+            return "\n" + indent + "\"" + name + "\" : " + value;
         }
 
         private String format(int depth) {
-            String joined = this.strings().entrySet()
+            Stream<String> stringString = this.formatStrings(depth);
+            Stream<String> stringStream = this.formatNodeLists(depth);
+
+            String content = Stream.concat(stringString, stringStream).collect(Collectors.joining(", "));
+            String indent = "\t".repeat(depth);
+            return "{" + content + "\n" + indent + "}";
+        }
+
+        private Stream<String> formatNodeLists(int depth) {
+            return this.nodeLists.entrySet()
                     .stream()
-                    .map(entry -> createStringProperty(depth + 1, entry.getKey(), entry.getValue()))
+                    .map(entry -> this.formatNodeList(depth, entry));
+        }
+
+        private String formatNodeList(int depth, Map.Entry<String, List<Node>> entry) {
+            String joined = entry.getValue()
+                    .stream()
+                    .map(node -> node.format(depth + 1))
                     .collect(Collectors.joining(", "));
 
-            return createJSONObject(depth, joined);
+            return createJSONProperty(depth + 1, entry.getKey(), "[" + joined + "]");
+        }
+
+        private Stream<String> formatStrings(int depth) {
+            return this.strings.entrySet()
+                    .stream()
+                    .map(entry -> createJSONProperty(depth + 1, entry.getKey(), "\"" + entry.getValue() + "\""));
         }
 
         public Node withString(String propertyKey, String propertyValue) {
             this.strings.put(propertyKey, propertyValue);
+            return this;
+        }
+
+        public Node withNodeList(String propertyKey, List<Node> propertyValues) {
+            this.nodeLists.put(propertyKey, propertyValues);
             return this;
         }
     }
@@ -56,51 +89,32 @@ public class Main {
             }
             segments.add(buffer.toString());
 
-
-            String collect = segments.stream()
+            List<Node> children = segments.stream()
                     .map(Main::compileRootSegment)
-                    .collect(Collectors.joining(", "));
-            String children = createJSONProperty(1, "children", "[" + collect + "]");
+                    .toList();
 
+            String formatted = new Node().withNodeList("children", children).format(0);
             Path target = source.resolveSibling("Main.java.ast.json");
-            Files.writeString(target, createJSONObject(0, children));
+            Files.writeString(target, formatted);
         } catch (IOException e) {
             //noinspection CallToPrintStackTrace
             e.printStackTrace();
         }
     }
 
-    private static String compileRootSegment(String value) {
-        String stripped = value.strip();
-        if (stripped.endsWith(";")) {
-            String withoutEnd = stripped.substring(0, stripped.length() - ";".length());
+    private static Node compileRootSegment(String value) {
+        if (value.strip().endsWith(";")) {
+            String withoutEnd = value.strip().substring(0, value.strip().length() - ";".length());
             int prefixSegment = withoutEnd.indexOf(" ");
             if (prefixSegment >= 0) {
                 String left = withoutEnd.substring(0, prefixSegment);
                 String right = withoutEnd.substring(prefixSegment + " ".length());
 
-                return new Node().withString("type", left).withString("segments", right).format(1);
+                return new Node().withString("type", left).withString("segments", right);
             }
         }
 
-        return toValue(stripped);
+        return new Node().withString("value", value);
     }
 
-    private static String toValue(String stripped) {
-        return new Node(Map.of("value", stripped)).format(1);
-    }
-
-    private static String createStringProperty(int depth, String name, String content) {
-        return createJSONProperty(depth, name, "\"" + content + "\"");
-    }
-
-    private static String createJSONObject(int depth, String content) {
-        String indent = "\t".repeat(depth);
-        return "{" + content + "\n" + indent + "}";
-    }
-
-    private static String createJSONProperty(int depth, String name, String value) {
-        String indent = "\t".repeat(depth);
-        return "\n" + indent + "\"" + name + "\" : " + value;
-    }
 }
