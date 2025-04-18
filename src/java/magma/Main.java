b710ab8bd33d83a8d6c498ef5cd045c669bfdcf7
SirMathhman <sirmathhman@gmail.com>
2025-04-18 09:47:59 -0400

    Rules

--- src/java/magma/Main.java
+++ src/java/magma/Main.java
@@ -5,13 +5,24 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
+import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class Main {
+    private interface Rule {
+        Optional<Node> parse(String input);
+    }
+
+    interface Divider {
+        List<String> divide(String input);
+    }
+
     private record Node(Map<String, String> strings, Map<String, java.util.List<Node>> nodeLists) {
         public Node() {
             this(new HashMap<>(), new HashMap<>());
@@ -61,6 +72,77 @@ public class Main {
             this.nodeLists.put(propertyKey, propertyValues);
             return this;
         }
+
+        public Node merge(Node other) {
+            this.strings.putAll(other.strings);
+            this.nodeLists.putAll(other.nodeLists);
+            return this;
+        }
+    }
+
+    private record StringRule(String propertyKey) implements Rule {
+        @Override
+        public Optional<Node> parse(String input) {
+            return Optional.of(new Node().withString(this.propertyKey, input));
+        }
+    }
+
+    private record DelimitedDivider(String delimiter) implements Divider {
+        @Override
+        public List<String> divide(String input) {
+            return Arrays.asList(input.split(Pattern.quote(this.delimiter)));
+        }
+    }
+
+    private record DivideRule(String propertyKey, Divider divider, Rule childRule) implements Rule {
+        @Override
+        public Optional<Node> parse(String input) {
+            return this.divider.divide(input)
+                    .stream()
+                    .reduce(Optional.of(new ArrayList<Node>()), this::foldChild, (_, next) -> next)
+                    .map(children -> new Node().withNodeList(this.propertyKey(), children));
+        }
+
+        private Optional<List<Node>> foldChild(Optional<List<Node>> maybeChildren, String child) {
+            return maybeChildren.flatMap(children -> this.childRule.parse(child).map(compiled -> {
+                children.add(compiled);
+                return children;
+            }));
+        }
+
+    }
+
+    private record InfixRule(Rule leftRule, String infix, Rule rightRule) implements Rule {
+        @Override
+        public Optional<Node> parse(String input) {
+            int index = input.indexOf(this.infix);
+            if (index < 0) {
+                return Optional.empty();
+            }
+
+            String left = input.substring(0, index);
+            String right = input.substring(index + this.infix.length());
+            return this.leftRule.parse(left).flatMap(withLeft -> this.rightRule.parse(right).map(withLeft::merge));
+        }
+    }
+
+    private record SuffixRule(Rule childRule, String suffix) implements Rule {
+        @Override
+        public Optional<Node> parse(String input) {
+            if (!input.endsWith(this.suffix())) {
+                return Optional.empty();
+            }
+
+            String slice = input.substring(0, input.length() - this.suffix().length());
+            return this.childRule().parse(slice);
+        }
+    }
+
+    private record StripRule(Rule childRule) implements Rule {
+        @Override
+        public Optional<Node> parse(String value) {
+            return this.childRule.parse(value.strip());
+        }
     }
 
     public static void main(String[] args) {
@@ -103,18 +185,13 @@ public class Main {
     }
 
     private static Node compileRootSegment(String value) {
-        if (value.strip().endsWith(";")) {
-            String withoutEnd = value.strip().substring(0, value.strip().length() - ";".length());
-            int prefixSegment = withoutEnd.indexOf(" ");
-            if (prefixSegment >= 0) {
-                String left = withoutEnd.substring(0, prefixSegment);
-                String right = withoutEnd.substring(prefixSegment + " ".length());
-
-                return new Node().withString("type", left).withString("segments", right);
-            }
-        }
-
-        return new Node().withString("value", value);
+        return createNamespacedRule().parse(value).orElseGet(() -> new Node().withString("value", value));
     }
 
+    private static Rule createNamespacedRule() {
+        Rule type = new StringRule("type");
+        Rule segment = new StringRule("value");
+        DivideRule segments = new DivideRule("segments", new DelimitedDivider("."), segment);
+        return new StripRule(new SuffixRule(new InfixRule(type, " ", segments), ";"));
+    }
 }
