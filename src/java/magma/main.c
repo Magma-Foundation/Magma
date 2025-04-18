3911cc92adfc0b3e96d0620c6d46340bf29d8a38
SirMathhman <sirmathhman@gmail.com>
2025-04-18 18:16:44 -0400

    Root segments

--- src/java/magma/main.c
+++ src/java/magma/main.c
@@ -1,26 +1,52 @@
-/* package magma;
+/* package magma; *//* 
 
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
+import java.io.IOException; *//* 
+import java.nio.file.Files; *//* 
+import java.nio.file.Path; *//* 
+import java.nio.file.Paths; *//* 
+import java.util.ArrayList; *//* 
 
 public class Main {
     public static void main(String[] args) {
         try {
-            Path path = Paths.get(".", "src", "java", "magma", "Main.java");
-            String input = Files.readString(path);
+            Path path = Paths.get(".", "src", "java", "magma", "Main.java"); *//* 
+            String input = Files.readString(path); *//* 
 
-            Path target = path.resolveSibling("main.c");
-            Files.writeString(target, compile(input));
+            Path target = path.resolveSibling("main.c"); *//* 
+            Files.writeString(target, compile(input)); *//* 
         } catch (IOException e) {
             //noinspection CallToPrintStackTrace
-            e.printStackTrace();
+            e.printStackTrace(); *//* 
         }
     }
 
     private static String compile(String input) {
-        return "/* " + input + " */";
+        ArrayList<String> segments = new ArrayList<>(); *//* 
+        StringBuilder buffer = new StringBuilder(); *//* 
+        for (int i = 0; *//*  i < input.length(); *//*  i++) {
+            char c = input.charAt(i); *//* 
+            buffer.append(c); *//* 
+            if (c == '; *//* ') {
+                segments.add(buffer.toString()); *//* 
+                buffer = new StringBuilder(); *//* 
+            }
+        }
+        segments.add(buffer.toString()); *//* 
+
+        StringBuilder output = new StringBuilder(); *//* 
+        for (String segment : segments) {
+            output.append(compileRootSegment(segment)); *//* 
+        }
+
+        return output.toString(); *//* 
+    }
+
+    private static String compileRootSegment(String input) {
+        return generatePlaceholder(input); *//* 
+    }
+
+    private static String generatePlaceholder(String input) {
+        return "/* " + input + " */"; *//* 
     }
 }
  */
