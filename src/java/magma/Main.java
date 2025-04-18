3e87e9567bc7ba0d99dd2acad6bf779661ae2b41
SirMathhman <sirmathhman@gmail.com>
2025-04-18 08:39:49 -0400

    Split?

--- src/java/magma/Main.java
+++ src/java/magma/Main.java
@@ -4,6 +4,8 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) {
@@ -11,8 +13,25 @@ public class Main {
             Path source = Paths.get(".", "src", "java", "magma", "Main.java");
             String input = Files.readString(source);
 
+            ArrayList<String> segments = new ArrayList<>();
+            StringBuilder buffer = new StringBuilder();
+            for (int i = 0; i < input.length(); i++) {
+                char c = input.charAt(i);
+                buffer.append(c);
+                if(c == ';') {
+                    segments.add(buffer.toString());
+                    buffer = new StringBuilder();
+                }
+            }
+            segments.add(buffer.toString());
+
+
+            String collect = segments.stream()
+                    .map(value -> "{\n\t\t\"value:\" : \"" + value + "\"\n\t},  ")
+                    .collect(Collectors.joining());
+
             Path target = source.resolveSibling("Main.java.ast.json");
-            Files.writeString(target, "{\n\t\"value\" : \"" + input + "\"\n}");
+            Files.writeString(target, "{\n\t\"children\" : [" + collect + "]\n}");
         } catch (IOException e) {
             //noinspection CallToPrintStackTrace
             e.printStackTrace();
