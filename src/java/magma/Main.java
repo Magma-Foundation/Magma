7a67ed7819980d79144065fcf83dba6597e599c9
SirMathhman <sirmathhman@gmail.com>
2025-04-18 08:33:59 -0400

    AST?

--- src/java/magma/Main.java
+++ src/java/magma/Main.java
@@ -1,7 +1,18 @@
 package magma;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 public class Main {
     public static void main(String[] args) {
-
+        try {
+            Path target = Paths.get(".", "src", "java", "magma","Main.java.ast");
+            Files.writeString(target, "{}");
+        } catch (IOException e) {
+            //noinspection CallToPrintStackTrace
+            e.printStackTrace();
+        }
     }
 }
