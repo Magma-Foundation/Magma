9e04389805ee377c08aa952b93c58ffeb965f610
SirMathhman <sirmathhman@gmail.com>
2025-04-19 14:11:27 -0400

    Migrate Option

--- src/java/magma/main.c
+++ src/java/magma/main.c
@@ -4,9 +4,10 @@
 /* import java.nio.file.Paths; */
 /* import java.util.ArrayList; */
 /* import java.util.List; */
-/* import java.util.Optional; */
 /* import java.util.function.BiFunction; */
+/* import java.util.function.Consumer; */
 /* import java.util.function.Function; */
+/* import java.util.function.Supplier; */
 /* private static Tuple<CompileState, String> compileRootSegment(CompileState methods, String input) {
         String stripped = input.strip();
         if (stripped.startsWith("package ")) {
@@ -16,29 +17,29 @@
         return compileClass(methods, stripped)
                 .orElseGet(() -> new Tuple<>(methods, generatePlaceholder(stripped) + "\n"));
     } */
-/* private static Optional<Tuple<CompileState, String>> compileClass(CompileState state, String input) {
+/* private static Option<Tuple<CompileState, String>> compileClass(CompileState state, String input) {
         int classIndex = input.indexOf("class ");
         if (classIndex < 0) {
-            return Optional.empty();
+            return Option.empty();
         }
         String modifiers = input.substring(0, classIndex);
         String afterKeyword = input.substring(classIndex + "class ".length());
         int contentStart = afterKeyword.indexOf("{");
         if (contentStart < 0) {
-            return Optional.empty();
+            return Option.empty();
         }
         String name = afterKeyword.substring(0, contentStart).strip();
         String withEnd = afterKeyword.substring(contentStart + "{".length()).strip();
         if (!withEnd.endsWith("}")) {
-            return Optional.empty();
+            return Option.empty();
         }
         String inputContent = withEnd.substring(0, withEnd.length() - "}".length());
         if (!isSymbol(name)) {
-            return Optional.empty();
+            return Option.empty();
         }
         Tuple<CompileState, String> outputContent = compileStatements(state.pushStructName(name), inputContent, Main::compileClassSegment);
         String generated = generatePlaceholder(modifiers) + "struct " + name + " {" + outputContent.right + "};\n";
-        return Optional.of(new Tuple<>(outputContent.left
+        return Option.of(new Tuple<>(outputContent.left
                 .popStructName().addStruct(generated), ""));
     } */
 /* private static boolean isSymbol(String input) {
@@ -58,24 +59,24 @@
                 .or(() -> compileMethod(state, input))
                 .orElseGet(() -> new Tuple<>(state, generatePlaceholder(input)));
     } */
-/* private static Optional<Tuple<CompileState, String>> compileMethod(CompileState state, String input) {
+/* private static Option<Tuple<CompileState, String>> compileMethod(CompileState state, String input) {
         String stripped = input.strip();
         int paramStart = stripped.indexOf("(");
         if (paramStart < 0) {
-            return Optional.empty();
+            return Option.empty();
         }
         String definition = stripped.substring(0, paramStart).strip();
         return compileDefinition(state, definition).flatMap(outputDefinition -> {
             String withParams = stripped.substring(paramStart + "(".length());
             int paramEnd = withParams.indexOf(")");
             if (paramEnd < 0) {
-                return Optional.empty();
+                return Option.empty();
             }
             String oldParameters = withParams.substring(0, paramEnd);
             Tuple<CompileState, String> paramTuple = compileAll(outputDefinition.left, oldParameters, Main::foldValueChar, Main::compileParameter, Main::mergeValues);
             String withBraces = withParams.substring(paramEnd + ")".length()).strip();
             if (!withBraces.startsWith("{") || !withBraces.endsWith("}")) {
-                return Optional.empty();
+                return Option.empty();
             }
             String inputContent = withBraces.substring(1, withBraces.length() - 1);
             CompileState paramState = paramTuple.left;
@@ -84,7 +85,7 @@
             Tuple<CompileState, String> outputContent = compileStatements(paramState, inputContent, Main::compileStatementOrBlock);
             String generated = outputDefinition.right + "(" + paramOutput + "){" + outputContent.right + "\n}\n";
             CompileState compileState = outputContent.left.addMethod(generated);
-            return Optional.of(new Tuple<>(compileState, ""));
+            return Option.of(new Tuple<>(compileState, ""));
         });
     } */
 /* private static Tuple<CompileState, String> compileStatementOrBlock(CompileState state, String input) {
@@ -110,11 +111,11 @@
 /* private static Tuple<CompileState, String> compileParameter(CompileState state, String s) {
         return compileDefinition(state, s).orElseGet(() -> new Tuple<>(state, generatePlaceholder(s)));
     } */
-/* private static Optional<Tuple<CompileState, String>> compileDefinition(CompileState state, String input) {
+/* private static Option<Tuple<CompileState, String>> compileDefinition(CompileState state, String input) {
         String stripped = input.strip();
         int nameSeparator = stripped.lastIndexOf(" ");
         if (nameSeparator < 0) {
-            return Optional.empty();
+            return Option.empty();
         }
         String beforeName = stripped.substring(0, nameSeparator).strip();
         String name = stripped.substring(nameSeparator + " ".length()).strip();
@@ -142,7 +143,7 @@
             });
         }
     } */
-/* private static Optional<String> compileType(String type) {
+/* private static Option<String> compileType(String type) {
         String stripped = type.strip();
         if (stripped.endsWith("[]")) {
             return compileType(stripped.substring(0, stripped.length() - "[]".length()))
@@ -150,25 +151,25 @@
         }
 
         if (type.equals("boolean")) {
-            return Optional.of("int");
+            return Option.of("int");
         }
 
         if (type.equals("char")) {
-            return Optional.of("char");
+            return Option.of("char");
         }
 
         if (type.equals("void")) {
-            return Optional.of("void");
+            return Option.of("void");
         }
 
         if (type.equals("String")) {
-            return Optional.of("char*");
+            return Option.of("char*");
         }
 
         if (isSymbol(type)) {
-            return Optional.of("struct " + stripped);
+            return Option.of("struct " + stripped);
         }
-        return Optional.empty();
+        return Option.empty();
     } */
 /* private static String generatePlaceholder(String input) {
         String replaced = input
@@ -196,6 +197,54 @@
      */};
 /* public  */struct Main {/* interface Result<T, X> {
         <R> R match(Function<T, R> whenOk, Function<X, R> whenErr);
+    } *//* 
+
+
+    interface Option<T> {
+        static <T> Option<T> of(T value) {
+            return new Some<>(value);
+        }
+
+        static <T> Option<T> empty() {
+            return new None<>();
+        }
+
+        void ifPresent(Consumer<T> ifPresent);
+
+        Option<T> or(Supplier<Option<T>> other);
+
+        T orElseGet(Supplier<T> other);
+
+        <R> Option<R> flatMap(Function<T, Option<R>> mapper);
+
+        <R> Option<R> map(Function<T, R> mapper);
+    } *//* 
+
+    record Some<T>(T value) implements Option<T> {
+        @Override
+        public void ifPresent(Consumer<T> ifPresent) {
+            ifPresent.accept(this.value);
+        }
+
+        @Override
+        public Option<T> or(Supplier<Option<T>> other) {
+            return this;
+        }
+
+        @Override
+        public T orElseGet(Supplier<T> other) {
+            return this.value;
+        }
+
+        @Override
+        public <R> Option<R> flatMap(Function<T, Option<R>> mapper) {
+            return mapper.apply(this.value);
+        }
+
+        @Override
+        public <R> Option<R> map(Function<T, R> mapper) {
+            return new Some<>(mapper.apply(this.value));
+        }
     } *//* 
 
     private record JavaList<T>(List<T> list) {
@@ -257,26 +306,26 @@
         }
     } *//* 
 
-    private static Optional<IOException> run() {
+    private static Option<IOException> run() {
         Path source = Paths.get(".", "src", "java", "magma", "Main.java");
-        return readString(source).match(input -> runWithInput(source, input), Optional::of);
+        return readString(source).match(input -> runWithInput(source, input), Option::of);
     } *//* 
 
-    private static Optional<IOException> runWithInput(Path source, String input) {
+    private static Option<IOException> runWithInput(Path source, String input) {
         Path target = source.resolveSibling("main.c");
         String compile = compile(input);
         return writeString(target, compile).or(Main::build);
     } *//* 
 
-    private static Optional<IOException> build() {
+    private static Option<IOException> build() {
         return start().match(process -> {
             try {
                 process.waitFor();
-                return Optional.empty();
+                return Option.empty();
             } catch (InterruptedException e) {
-                return Optional.of(new IOException(e));
+                return Option.of(new IOException(e));
             }
-        }, Optional::of);
+        }, Option::of);
     } *//* 
 
     private static Result<Process, IOException> start() {
@@ -290,12 +339,12 @@
         }
     } *//* 
 
-    private static Optional<IOException> writeString(Path target, String compile) {
+    private static Option<IOException> writeString(Path target, String compile) {
         try {
             Files.writeString(target, compile);
-            return Optional.empty();
+            return Option.empty();
         } catch (IOException e) {
-            return Optional.of(e);
+            return Option.of(e);
         }
     } *//* 
 
@@ -364,6 +413,30 @@
 }
 /* private */ struct DivideState exit_DivideState(/*  */){
 	/* return new DivideState(this.segments, this.buffer, this.depth - 1) */;/*  */
+}
+/* static class None<T> implements Option<T> {
+        @Override
+        public */ void ifPresent_Main(/* Consumer<T> ifPresent */){
+	/* }
+
+        @Override
+        public Option<T> or(Supplier<Option<T>> other) {
+            return other.get() */;
+	/* }
+
+        @Override
+        public T orElseGet(Supplier<T> other) {
+            return other.get() */;
+	/* }
+
+        @Override
+        public <R> Option<R> flatMap(Function<T, Option<R>> mapper) {
+            return new None<>() */;
+	/* }
+
+        @Override
+        public <R> Option<R> map(Function<T, R> mapper) {
+            return new None<>() */;/* } */
 }
 /* public static */ void __main__(char** args_Main){
 	/* run().ifPresent(Throwable::printStackTrace) */;/*  */
