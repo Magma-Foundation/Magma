3545fb0da60acec3f6e83a78023a34bf303793d0
SirMathhman <sirmathhman@gmail.com>
2025-04-21 12:34:22 -0400

    Extract DivideState

--- src/java/magma/Main.java
+++ src/java/magma/Main.java
@@ -10,31 +10,42 @@ import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class Main {
-    private static class State {
+    private interface DivideState {
+        DivideState advance();
+
+        DivideState append(char c);
+
+        Stream<String> stream();
+    }
+
+    private static class MutableDivideState implements DivideState {
         private final List<String> segments;
         private StringBuilder buffer;
 
-        private State(List<String> segments, StringBuilder buffer) {
+        private MutableDivideState(List<String> segments, StringBuilder buffer) {
             this.segments = segments;
             this.buffer = buffer;
         }
 
-        public State() {
+        public MutableDivideState() {
             this(new ArrayList<>(), new StringBuilder());
         }
 
-        private State advance() {
+        @Override
+        public DivideState advance() {
             this.segments.add(this.buffer.toString());
             this.buffer = new StringBuilder();
             return this;
         }
 
-        private State append(char c) {
+        @Override
+        public DivideState append(char c) {
             this.buffer.append(c);
             return this;
         }
 
-        private Stream<String> stream() {
+        @Override
+        public Stream<String> stream() {
             return this.segments.stream();
         }
     }
@@ -52,13 +63,13 @@ public class Main {
     }
 
     private static String compile(String input) {
-        return divide(input, new State())
+        return divide(input, new MutableDivideState())
                 .map(Main::compileRootSegment)
                 .collect(Collectors.joining());
     }
 
-    private static Stream<String> divide(String input, State state) {
-        State current = state;
+    private static Stream<String> divide(String input, DivideState state) {
+        DivideState current = state;
         for (int i = 0; i < input.length(); i++) {
             char c = input.charAt(i);
             current = foldStatementChar(current, c);
@@ -67,8 +78,8 @@ public class Main {
         return current.advance().stream();
     }
 
-    private static State foldStatementChar(State state, char c) {
-        State appended = state.append(c);
+    private static DivideState foldStatementChar(DivideState state, char c) {
+        DivideState appended = state.append(c);
         if (c == ';') {
             return appended.advance();
         }
