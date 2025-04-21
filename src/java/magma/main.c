80902af41a7c37cebbad8801b61a9f94d662fe93
SirMathhman <sirmathhman@gmail.com>
2025-04-21 12:29:41 -0400

    Remove duplicates

--- src/java/magma/main.c
+++ src/java/magma/main.c
@@ -5,8 +5,40 @@ import java.nio.file.Files;*//*
 import java.nio.file.Path;*//* 
 import java.nio.file.Paths;*//* 
 import java.util.ArrayList;*//* 
+import java.util.List;*//* 
+import java.util.stream.Collectors;*//* 
+import java.util.stream.Stream;*//* 
 
 public class Main {
+    private static class State {
+        private final List<String> segments;*//* 
+        private StringBuilder buffer;*//* 
+
+        private State(List<String> segments, StringBuilder buffer) {
+            this.segments = segments;*//* 
+            this.buffer = buffer;*//* 
+        }
+
+        public State() {
+            this(new ArrayList<>(), new StringBuilder());*//* 
+        }
+
+        private State advance() {
+            this.segments.add(this.buffer.toString());*//* 
+            this.buffer = new StringBuilder();*//* 
+            return this;*//* 
+        }
+
+        private State append(char c) {
+            this.buffer.append(c);*//* 
+            return this;*//* 
+        }
+
+        private Stream<String> stream() {
+            return this.segments.stream();*//* 
+        }
+    }
+
     public static void main(String[] args) {
         try {
             Path source = Paths.get(".", "src", "java", "magma", "Main.java");*//* 
@@ -20,24 +52,27 @@ public class Main {
     }
 
     private static String compile(String input) {
-        ArrayList<String> segments = new ArrayList<>();*//* 
-        StringBuilder buffer = new StringBuilder();*//* 
+        return divide(input, new State())
+                .map(Main::compileRootSegment)
+                .collect(Collectors.joining());*//* 
+    }
+
+    private static Stream<String> divide(String input, State state) {
+        State current = state;*//* 
         for (int i = 0;*//*  i < input.length();*//*  i++) {
             char c = input.charAt(i);*//* 
-            buffer.append(c);*//* 
-            if (c == ';*//* ') {
-                segments.add(buffer.toString());*//* 
-                buffer = new StringBuilder();*//* 
-            }
-        }
-        segments.add(buffer.toString());*//* 
-
-        StringBuilder output = new StringBuilder();*//* 
-        for (String segment : segments) {
-            output.append(compileRootSegment(segment));*//* 
+            current = foldStatementChar(current, c);*//* 
         }
 
-        return output.toString();*//* 
+        return current.advance().stream();*//* 
+    }
+
+    private static State foldStatementChar(State state, char c) {
+        State appended = state.append(c);*//* 
+        if (c == ';*//* ') {
+            return appended.advance();*//* 
+        }
+        return appended;*//* 
     }
 
     private static String compileRootSegment(String input) {
