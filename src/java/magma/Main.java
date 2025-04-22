ef8a2eb82043c930f8ac541d3c2df0ae92cacd72
SirMathhman <sirmathhman@gmail.com>
2025-04-21 23:07:12 -0400

    Classes?

--- src/java/magma/Main.java
+++ src/java/magma/Main.java
@@ -4,15 +4,40 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
+    private static class State {
+        private final List<String> segments;
+        private StringBuilder buffer;
+
+        private State(List<String> segments, StringBuilder buffer) {
+            this.segments = segments;
+            this.buffer = buffer;
+        }
+
+        public State() {
+            this(new ArrayList<>(), new StringBuilder());
+        }
+
+        private void append(char c) {
+            this.buffer.append(c);
+        }
+
+        private void advance() {
+            this.segments.add(this.buffer.toString());
+            this.buffer = new StringBuilder();
+        }
+    }
+
     public static void main(String[] args) {
         try {
             Path source = Paths.get(".", "src", "java", "magma", "Main.java");
             String input = Files.readString(source);
 
             Path target = source.resolveSibling("main.c");
-            Files.writeString(target, input);
+            Files.writeString(target, compile(input));
 
             new ProcessBuilder("clang.exe", "./src/java/magma/main.c", "-o", "main.exe")
                     .inheritIO()
@@ -22,5 +47,30 @@ public class Main {
             //noinspection CallToPrintStackTrace
             e.printStackTrace();
         }
+    }
+
+    private static String compile(String input) {
+        State state = new State();
+        for (int i = 0; i < input.length(); i++) {
+            char c = input.charAt(i);
+            state.append(c);
+            if (c == ';') {
+                state.advance();
+            }
+        }
+        state.advance();
+
+        StringBuilder output = new StringBuilder();
+        for (String segment : state.segments) {
+            output.append(compileRootSegment(segment));
+        }
+        return output.toString();
+    }
+
+    private static String compileRootSegment(String input) {
+        if (input.contains("class")) {
+            return "struct Temp {\n};\n";
+        }
+        return input;
     }
 }
