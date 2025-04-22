b8d4b7f211e6ce0dd696156c0cdce61f2a7ae970
SirMathhman <sirmathhman@gmail.com>
2025-04-22 11:34:10 -0400

    Braces

--- src/java/magma/Main.java
+++ src/java/magma/Main.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
@@ -12,10 +13,43 @@ public class Main {
             String input = Files.readString(source);
 
             Path target = source.resolveSibling("main.c");
-            Files.writeString(target, "/*" + input + "*/");
+            Files.writeString(target, compile(input));
         } catch (IOException e) {
             //noinspection CallToPrintStackTrace
             e.printStackTrace();
         }
+    }
+
+    private static String compile(String input) {
+        ArrayList<String> segments = new ArrayList<>();
+        StringBuilder buffer = new StringBuilder();
+        int depth = 0;
+        for (int i = 0; i < input.length(); i++) {
+            char c = input.charAt(i);
+            buffer.append(c);
+            if (c == ';' && depth == 0) {
+                segments.add(buffer.toString());
+                buffer = new StringBuilder();
+            }
+            else {
+                if (c == '{') {
+                    depth++;
+                }
+                if (c == '}') {
+                    depth--;
+                }
+            }
+        }
+        segments.add(buffer.toString());
+
+        StringBuilder output = new StringBuilder();
+        for (String segment : segments) {
+            output.append(compileRootSegment(segment));
+        }
+        return output.toString();
+    }
+
+    private static String compileRootSegment(String input) {
+        return "/*" + input + "*/";
     }
 }
