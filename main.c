57c2fc0540cdab41fb85f97bae523db1aa78f201
SirMathhman <sirmathhman@gmail.com>
2025-04-22 18:07:46 -0400

    Structs?

--- main.c
+++ main.c
@@ -5,9 +5,9 @@ import java.nio.file.Files; *//*
 import java.nio.file.Path; *//* 
 import java.nio.file.Paths; *//* 
 import java.util.ArrayList; *//* 
+import java.util.function.Function; *//* 
 
-public class Main {
-    public static void main(String[] args) {
+public  */struct Main {/* public static void main(String[] args) {
         try {
             Path source = Paths.get(".", "src", "java", "magma", "Main.java");
             String input = Files.readString(source);
@@ -25,6 +25,10 @@ public class Main {
     }
 
     private static String compile(String input) {
+        return compileAll(input, Main::compileRootSegment);
+    }
+
+    private static String compileAll(String input, Function<String, String> compileRootSegment) {
         ArrayList<String> segments = new ArrayList<>();
         StringBuilder buffer = new StringBuilder();
         int depth = 0;
@@ -48,14 +52,37 @@ public class Main {
 
         StringBuilder output = new StringBuilder();
         for (String segment : segments) {
-            output.append(compileRootSegment(segment));
+            output.append(compileRootSegment.apply(segment));
         }
 
         return output.toString();
     }
 
     private static String compileRootSegment(String input) {
+        int classIndex = input.indexOf("class ");
+        if (classIndex >= 0) {
+            String beforeKeyword = input.substring(0, classIndex);
+            String afterKeyword = input.substring(classIndex + "class ".length());
+            int contentStart = afterKeyword.indexOf("{");
+            if (contentStart >= 0) {
+                String name = afterKeyword.substring(0, contentStart).strip();
+                String withEnd = afterKeyword.substring(contentStart + "{".length()).strip();
+                if (withEnd.endsWith("}")) {
+                    String inputContent = withEnd.substring(0, withEnd.length() - "}".length());
+                    String outputContent = compileAll(inputContent, Main::compileClassSegment);
+                    return generatePlaceholder(beforeKeyword) + "struct " + name + " {" + outputContent + "\n}\n";
+                }
+            }
+        }
+        return generatePlaceholder(input);
+    }
+
+    private static String compileClassSegment(String input) {
+        return generatePlaceholder(input);
+    }
+
+    private static String generatePlaceholder(String input) {
         return "/* " + input + " */";
     }
-}
  */
+}
--- src/java/magma/Main.java
+++ src/java/magma/Main.java
@@ -5,6 +5,7 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.function.Function;
 
 public class Main {
     public static void main(String[] args) {
@@ -25,6 +26,10 @@ public class Main {
     }
 
     private static String compile(String input) {
+        return compileAll(input, Main::compileRootSegment);
+    }
+
+    private static String compileAll(String input, Function<String, String> compileRootSegment) {
         ArrayList<String> segments = new ArrayList<>();
         StringBuilder buffer = new StringBuilder();
         int depth = 0;
@@ -48,13 +53,36 @@ public class Main {
 
         StringBuilder output = new StringBuilder();
         for (String segment : segments) {
-            output.append(compileRootSegment(segment));
+            output.append(compileRootSegment.apply(segment));
         }
 
         return output.toString();
     }
 
     private static String compileRootSegment(String input) {
+        int classIndex = input.indexOf("class ");
+        if (classIndex >= 0) {
+            String beforeKeyword = input.substring(0, classIndex);
+            String afterKeyword = input.substring(classIndex + "class ".length());
+            int contentStart = afterKeyword.indexOf("{");
+            if (contentStart >= 0) {
+                String name = afterKeyword.substring(0, contentStart).strip();
+                String withEnd = afterKeyword.substring(contentStart + "{".length()).strip();
+                if (withEnd.endsWith("}")) {
+                    String inputContent = withEnd.substring(0, withEnd.length() - "}".length());
+                    String outputContent = compileAll(inputContent, Main::compileClassSegment);
+                    return generatePlaceholder(beforeKeyword) + "struct " + name + " {" + outputContent + "\n}\n";
+                }
+            }
+        }
+        return generatePlaceholder(input);
+    }
+
+    private static String compileClassSegment(String input) {
+        return generatePlaceholder(input);
+    }
+
+    private static String generatePlaceholder(String input) {
         return "/* " + input + " */";
     }
 }
