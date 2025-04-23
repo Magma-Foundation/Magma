deef69a51a716e633353ccf022dd4239981d68f3
SirMathhman <sirmathhman@gmail.com>
2025-04-22 22:45:03 -0400

    Copy

---
+++ src/java/magma/Main.java
@@ -0,0 +1,7 @@
+void main() throws IOException {
+    var source = Paths.get(".", "src", "java", "magma", "Main.java");
+    var input = Files.readString(source);
+
+    var target = source.resolveSibling("main.c");
+    Files.writeString(target, "/* " + input + "*/");
+}
