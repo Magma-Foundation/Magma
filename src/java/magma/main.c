237bee4a295c39010f74e7f209cf595782d054c5
SirMathhman <sirmathhman@gmail.com>
2025-04-22 22:36:56 -0400

    ???

--- src/java/magma/main.c
+++
@@ -1,7 +0,0 @@
-/* void main() throws IOException {
-    var source = Paths.get(".", "src", "java", "magma", "Main.java");
-    var input = Files.readString(source);
-
-    var target = source.resolveSibling("main.c");
-    Files.writeString(target, "/* " + input + " */");
-} */
