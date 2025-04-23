ac186064846d70ef6474fa11b41bfb201948af9b
SirMathhman <sirmathhman@gmail.com>
2025-04-23 08:46:49 -0400

    Slicing

--- src/java/magma/Main.java
+++ src/java/magma/Main.java
@@ -4,9 +4,35 @@ void main() {
         var input = Files.readString(source);
 
         var target = source.resolveSibling("main.c");
-        Files.writeString(target, "/* " + input + " */");
+        Files.writeString(target, this.compileRoot(input));
     } catch (IOException e) {
         //noinspection CallToPrintStackTrace
         e.printStackTrace();
     }
+}
+
+private String compileRoot(String input) {
+    var segments = new ArrayList<String>();
+    var buffer = new StringBuilder();
+    for (var i = 0; i < input.length(); i++) {
+        var c = input.charAt(i);
+        buffer.append(c);
+
+        if (c == ';') {
+            segments.add(buffer.toString());
+            buffer = new StringBuilder();
+        }
+    }
+    segments.add(buffer.toString());
+
+    var output = new StringBuilder();
+    for (var segment : segments) {
+        output.append(this.generatePlaceholder(segment));
+    }
+
+    return output.toString();
+}
+
+private String generatePlaceholder(String input) {
+    return "/* " + input + " */";
 }
--- src/java/magma/main.c
+++ src/java/magma/main.c
@@ -1,11 +1,38 @@
 /* void main() {
     try {
-        var source = Paths.get(".", "src", "java", "magma", "Main.java");
-        var input = Files.readString(source);
+        var source = Paths.get(".", "src", "java", "magma", "Main.java"); *//* 
+        var input = Files.readString(source); *//* 
 
-        var target = source.resolveSibling("main.c");
-        Files.writeString(target, "/* " +  input + " */");
+        var target = source.resolveSibling("main.c"); *//* 
+        Files.writeString(target, this.compileRoot(input)); *//* 
     } catch (IOException e) {
-        throw new RuntimeException(e);
+        //noinspection CallToPrintStackTrace
+        e.printStackTrace(); *//* 
     }
+}
+
+private String compileRoot(String input) {
+    var segments = new ArrayList<String>(); *//* 
+    var buffer = new StringBuilder(); *//* 
+    for (var i = 0; *//*  i < input.length(); *//*  i++) {
+        var c = input.charAt(i); *//* 
+        buffer.append(c); *//* 
+
+        if (c == '; *//* ') {
+            segments.add(buffer.toString()); *//* 
+            buffer = new StringBuilder(); *//* 
+        }
+    }
+    segments.add(buffer.toString()); *//* 
+
+    var output = new StringBuilder(); *//* 
+    for (var segment : segments) {
+        output.append(this.generatePlaceholder(segment)); *//* 
+    }
+
+    return output.toString(); *//* 
+}
+
+private String generatePlaceholder(String input) {
+    return "/* " + input + " */"; *//* 
 } */
