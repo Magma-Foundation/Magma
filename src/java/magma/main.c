f68afd3c9ad2551e71b020da4cb852cf77ce2c89
SirMathhman <sirmathhman@gmail.com>
2025-04-23 18:00:03 -0400

    Root segments

--- src/java/magma/main.c
+++ src/java/magma/main.c
@@ -1,20 +1,45 @@
-/* package magma;
+/* package magma; *//* 
 
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
+import java.io.IOException; *//* 
+import java.nio.file.Files; *//* 
+import java.nio.file.Paths; *//* 
+import java.util.ArrayList; *//* 
 
 public class Main {
     public static void main() {
         try {
-            var source = Paths.get(".", "src", "java", "magma", "Main.java");
-            var input = Files.readString(source);
+            var source = Paths.get(".", "src", "java", "magma", "Main.java"); *//* 
+            var input = Files.readString(source); *//* 
 
-            var target = source.resolveSibling("main.c");
-            Files.writeString(target, "/* " + input + " */");
+            var target = source.resolveSibling("main.c"); *//* 
+            Files.writeString(target, compile(input)); *//* 
         } catch (IOException e) {
-            e.printStackTrace();
+            e.printStackTrace(); *//* 
         }
+    }
+
+    private static String compile(String input) {
+        var segments = new ArrayList<String>(); *//* 
+        var buffer = new StringBuilder(); *//* 
+        for (var i = 0; *//*  i < input.length(); *//*  i++) {
+            var c = input.charAt(i); *//* 
+            buffer.append(c); *//* 
+            if(c == '; *//* ') {
+                segments.add(buffer.toString()); *//* 
+                buffer = new StringBuilder(); *//* 
+            }
+        }
+        segments.add(buffer.toString()); *//* 
+
+        var output = new StringBuilder(); *//* 
+        for (var segment : segments) {
+            output.append(compileRootSegment(segment)); *//* 
+        }
+        return output.toString(); *//* 
+    }
+
+    private static String compileRootSegment(String input) {
+        return "/* " + input + " */"; *//* 
     }
 }
  */
