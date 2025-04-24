d38428c7a7ed8dcd39dd1f0cddc7466ff4159c47
SirMathhman <sirmathhman@gmail.com>
2025-04-23 22:34:54 -0400

    Class segments

--- src/java/magma/Main.java
+++ src/java/magma/Main.java
@@ -4,8 +4,48 @@ void main() {
         var input = Files.readString(source);
 
         var target = source.resolveSibling("main.c");
-        Files.writeString(target, "/* " + input + " */");
+        Files.writeString(target, this.compileRoot(input));
     } catch (IOException e) {
         e.printStackTrace();
     }
+}
+
+private String compileRoot(String input) {
+    var segments = new ArrayList<String>();
+    var buffer = new StringBuilder();
+    var depth = 0;
+    for (var i = 0; i < input.length(); i++) {
+        var c = input.charAt(i);
+        buffer.append(c);
+
+        if (c == ';' && depth == 0) {
+            segments.add(buffer.toString());
+            buffer = new StringBuilder();
+        }
+        else if (c == '}' && depth == 1) {
+            segments.add(buffer.toString());
+            buffer = new StringBuilder();
+            depth--;
+        }
+        else {
+            if (c == '{') {
+                depth++;
+            }
+            if (c == '}') {
+                depth--;
+            }
+        }
+    }
+    segments.add(buffer.toString());
+
+    var output = new StringBuilder();
+    for (var segment : segments) {
+        output.append(this.compileRootSegment(segment));
+    }
+
+    return output.toString();
+}
+
+private String compileRootSegment(String input) {
+    return "/* " + input + " */";
 }
