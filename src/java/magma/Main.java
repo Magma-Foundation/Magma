15414bfd65d1c6d785d11be9e0df12c247d5ffd2
SirMathhman <sirmathhman@gmail.com>
2025-04-23 22:32:19 -0400

    Copy

--- src/java/magma/Main.java
+++ src/java/magma/Main.java
@@ -1,3 +1,11 @@
 void main() {
+    try {
+        var source = Paths.get(".", "src", "java", "magma", "Main.java");
+        var input = Files.readString(source);
 
+        var target = source.resolveSibling("main.c");
+        Files.writeString(target, "/* " + input + " */");
+    } catch (IOException e) {
+        e.printStackTrace();
+    }
 }
