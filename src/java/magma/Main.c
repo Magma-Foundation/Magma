0090c1c9d84ca9846a6eddf5f6497b1732380265
SirMathhman <sirmathhman@gmail.com>
2025-04-24 21:27:20 -0400

    Root segments

--- src/java/magma/Main.c
+++ src/java/magma/Main.c
@@ -1,20 +1,81 @@
-/* package magma;
+/* package magma; *//* 
 
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
+import java.io.IOException; *//* 
+import java.nio.file.Files; *//* 
+import java.nio.file.Paths; *//* 
+import java.util.ArrayList; *//* 
+import java.util.List; *//* 
 
 public class Main {
+    private static class State {
+        private final List<String> segments; *//* 
+        private StringBuilder buffer; *//* 
+
+        private State() {
+            this(new ArrayList<>(), new StringBuilder()); *//* 
+        }
+
+        private State(List<String> segments, StringBuilder buffer) {
+            this.buffer = buffer; *//* 
+            this.segments = segments; *//* 
+        }
+
+        private State advance() {
+            this.segments.add(this.buffer.toString()); *//* 
+            this.buffer = new StringBuilder(); *//* 
+            return this; *//* 
+        }
+
+        private State append(char c) {
+            this.buffer.append(c); *//* 
+            return this; *//* 
+        }
+    }
+
     public static void main(String[] args) {
         try {
-            var source = Paths.get(".", "src", "java", "magma", "Main.java");
-            var input = Files.readString(source);
+            var source = Paths.get(".", "src", "java", "magma", "Main.java"); *//* 
+            var input = Files.readString(source); *//* 
 
-            var target = source.resolveSibling("Main.c");
-            Files.writeString(target, "/* " + input + " */");
+            var target = source.resolveSibling("Main.c"); *//* 
+            Files.writeString(target, compile(input)); *//* 
         } catch (IOException e) {
-            e.printStackTrace();
+            e.printStackTrace(); *//* 
         }
+    }
+
+    private static String compile(String input) {
+        var segments = divide(input); *//* 
+
+        var output = new StringBuilder(); *//* 
+        for (var segment : segments) {
+            output.append(compileRootSegment(segment)); *//* 
+        }
+
+        return output.toString(); *//* 
+    }
+
+    private static List<String> divide(String input) {
+        var current = new State(); *//* 
+        for (var i = 0; *//*  i < input.length(); *//*  i++) {
+            var c = input.charAt(i); *//* 
+            current = foldStatementChar(current, c); *//* 
+        }
+        return current.advance().segments; *//* 
+    }
+
+    private static State foldStatementChar(State current, char c) {
+        var appended = current.append(c); *//* 
+        if (c == '; *//* ') {
+            return appended.advance(); *//* 
+        }
+        else {
+            return appended; *//* 
+        }
+    }
+
+    private static String compileRootSegment(String input) {
+        return "/* " + input + " */"; *//* 
     }
 }
  */
