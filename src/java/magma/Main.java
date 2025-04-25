2d308cee954eccab91e024c00c702946f5e62708
SirMathhman <sirmathhman@gmail.com>
2025-04-24 21:23:20 -0400

    Main

---
+++ src/java/magma/Main.java
@@ -0,0 +1,19 @@
+package magma;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+public class Main {
+    public static void main(String[] args) {
+        try {
+            var source = Paths.get(".", "src", "java", "magma", "Main.java");
+            var input = Files.readString(source);
+
+            var target = source.resolveSibling("Main.c");
+            Files.writeString(target, "/* " + input + " */");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
