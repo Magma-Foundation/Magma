f4c6d5e11841290c7d492d5beb355b130963a7e5
SirMathhman <sirmathhman@gmail.com>
2025-04-25 22:05:52 -0400

    Simple Main

--- src/java/magma/Main.java
+++ src/java/magma/Main.java
@@ -1,7 +1,29 @@
 package magma;
 
-public class Main {
-    public static void main(String[] args) {
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
+public class Main {
+    private static class CompileException extends Exception {
+        public CompileException(String message, String context) {
+            super(message + ": " + context);
+        }
+    }
+
+    public static void main() {
+        try {
+            var source = Paths.get(".", "src", "java", "magma", "Main.java");
+            var input = Files.readString(source);
+
+            var target = source.resolveSibling("Main.c");
+            Files.writeString(target, compile(input));
+        } catch (IOException | CompileException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static String compile(String input) throws CompileException {
+        throw new CompileException("Invalid input", input);
     }
 }
