e47bf55e701724c5ef3cdf2c8c677814ca7b3906
SirMathhman <sirmathhman@gmail.com>
2025-04-25 22:07:24 -0400

    Root segments

--- src/java/magma/Main.java
+++ src/java/magma/Main.java
@@ -3,6 +3,7 @@ package magma;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 
 public class Main {
     private static class CompileException extends Exception {
@@ -24,6 +25,29 @@ public class Main {
     }
 
     private static String compile(String input) throws CompileException {
+        var segments = new ArrayList<String>();
+        var buffer = new StringBuilder();
+        for (var i = 0; i < input.length(); i++) {
+            var c = input.charAt(i);
+            buffer.append(c);
+            if (c == ';') {
+                segments.add(buffer.toString());
+                buffer = new StringBuilder();
+            }
+        }
+        segments.add(buffer.toString());
+
+        var output = new StringBuilder();
+        for (var segment : segments) {
+            output.append(compileRootSegment(segment));
+        }
+
+        return output.toString();
+    }
+
+    private static String compileRootSegment(String input) throws CompileException {
+
+
         throw new CompileException("Invalid input", input);
     }
 }
