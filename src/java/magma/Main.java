399b125f016540694ede5d1c844edef1f070fa52
SirMathhman <sirmathhman@gmail.com>
2025-04-28 10:19:28 -0400

    foldStatementChar

--- src/java/magma/Main.java
+++ src/java/magma/Main.java
@@ -8,6 +8,89 @@ import java.util.List;
 import java.util.function.Function;
 
 public class Main {
+    private static class State {
+        private final String input;
+        private final List<String> segments;
+        private int index;
+        private StringBuilder buffer;
+        private int depth;
+
+        private State(String input, int index, StringBuilder buffer, int depth, List<String> segments) {
+            this.input = input;
+            this.index = index;
+            this.buffer = buffer;
+            this.depth = depth;
+            this.segments = segments;
+        }
+
+        private State enter() {
+            this.setDepth(this.getDepth() + 1);
+            return this;
+        }
+
+        private State exit() {
+            this.setDepth(this.getDepth() - 1);
+            return this;
+        }
+
+        private State advance() {
+            this.segments().add(this.getBuffer().toString());
+            this.setBuffer(new StringBuilder());
+            return this;
+        }
+
+        private boolean isShallow() {
+            return this.getDepth() == 1;
+        }
+
+        private char currentChar() {
+            return this.input().charAt(this.getIndex());
+        }
+
+        private char pop() {
+            var escaped = this.currentChar();
+            next(this);
+            return escaped;
+        }
+
+        private State append(char c) {
+            this.getBuffer().append(c);
+            return this;
+        }
+
+        public int getIndex() {
+            return this.index;
+        }
+
+        public void setIndex(int index) {
+            this.index = index;
+        }
+
+        public StringBuilder getBuffer() {
+            return this.buffer;
+        }
+
+        public void setBuffer(StringBuilder buffer) {
+            this.buffer = buffer;
+        }
+
+        public int getDepth() {
+            return this.depth;
+        }
+
+        public void setDepth(int depth) {
+            this.depth = depth;
+        }
+
+        public String input() {
+            return this.input;
+        }
+
+        public List<String> segments() {
+            return this.segments;
+        }
+    }
+
     public static final List<String> methods = new ArrayList<>();
 
     public static void main() {
@@ -27,54 +110,15 @@ public class Main {
     }
 
     private static String compileAll(String input, Function<String, String> compiler) {
+        return divideAll(input, compiler);
+    }
+
+    private static String divideAll(String input, Function<String, String> compiler) {
         var segments = new ArrayList<String>();
         var buffer = new StringBuilder();
         var depth = 0;
-        var i = 0;
-        while (i < input.length()) {
-            var c = input.charAt(i);
-
-            if (c == '\'') {
-                buffer.append(c);
-                i++;
-
-                var maybeSlash = input.charAt(i);
-                buffer.append(maybeSlash);
-                i++;
-
-                if (maybeSlash == '\\') {
-                    var escaped = input.charAt(i);
-                    buffer.append(escaped);
-                    i++;
-                }
-
-                var slash = input.charAt(i);
-                buffer.append(slash);
-                i++;
-                continue;
-            }
-
-            buffer.append(c);
-            i++;
-            if (c == ';' && depth == 0) {
-                segments.add(buffer.toString());
-                buffer = new StringBuilder();
-            }
-            else if (c == '}' && depth == 1) {
-                segments.add(buffer.toString());
-                buffer = new StringBuilder();
-                depth--;
-            }
-            else {
-                if (c == '{') {
-                    depth++;
-                }
-                if (c == '}') {
-                    depth--;
-                }
-            }
-        }
-        segments.add(buffer.toString());
+        var index = 0;
+        extracted(new State(input, index, buffer, depth, segments));
 
         var output = new StringBuilder();
         for (var segment : segments) {
@@ -82,6 +126,58 @@ public class Main {
         }
 
         return output.toString();
+    }
+
+    private static void extracted(State state) {
+        while (state.getIndex() < state.input().length()) {
+            var c = state.pop();
+            if (c == '\'') {
+                state.append(c);
+                var maybeSlash = popAndAppend(state);
+
+                if (maybeSlash == '\\') {
+                    popAndAppend(state);
+                }
+
+                popAndAppend(state);
+                continue;
+            }
+
+            state = foldStatementChar(state, c);
+        }
+
+        state.advance();
+    }
+
+    private static State foldStatementChar(State state, char c) {
+        var appended = state.append(c);
+        if (c == ';' && isLevel(state)) {
+            return appended.advance();
+        }
+        if (c == '}' && appended.isShallow()) {
+            return appended.advance().exit();
+        }
+        if (c == '{') {
+            return appended.enter();
+        }
+        if (c == '}') {
+            return appended.exit();
+        }
+        return appended;
+    }
+
+    private static char popAndAppend(State state) {
+        var escaped = state.pop();
+        state.append(escaped);
+        return escaped;
+    }
+
+    private static boolean isLevel(State state) {
+        return state.getDepth() == 0;
+    }
+
+    private static void next(State state) {
+        state.setIndex(state.getIndex() + 1);
     }
 
     private static String compileRootSegment(String input) {
