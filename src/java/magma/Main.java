7b1fb2e168123de70d4bb8e84a7c1d8f9f800d66
SirMathhman <sirmathhman@gmail.com>
2025-04-30 23:16:02 -0400

    Remove duplicates

--- src/java/magma/Main.java
+++ src/java/magma/Main.java
@@ -4,8 +4,25 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
+    private record State(List<String> segments, StringBuilder buffer) {
+        public State() {
+            this(new ArrayList<>(), new StringBuilder());
+        }
+
+        private State advance() {
+            var copy = new ArrayList<String>(this.segments);
+            copy.add(this.buffer.toString());
+            return new State(copy, new StringBuilder());
+        }
+
+        private State append(char c) {
+            return new State(this.segments, this.buffer.append(c));
+        }
+    }
+
     public static void main() {
         try {
             var source = Paths.get(".", "src", "java", "magma", "Main.java");
@@ -19,28 +36,31 @@ public class Main {
     }
 
     private static String compileRoot(String input) {
-        var segments = new ArrayList<String>();
-        var buffer = new StringBuilder();
-        for (var i = 0; i < input.length(); i++) {
-            var c = input.charAt(i);
-            buffer = buffer.append(c);
-
-            if (c == ';') {
-                var copy = new ArrayList<String>(segments);
-                copy.add(buffer.toString());
-                segments = copy;
-                buffer = new StringBuilder();
-            }
-        }
-        var copy = new ArrayList<String>(segments);
-        copy.add(buffer.toString());
+        var segments = divide(input);
 
         var output = new StringBuilder();
-        for (var segment : copy) {
+        for (var segment : segments) {
             output.append(generatePlaceholder(segment));
         }
 
         return output.toString();
+    }
+
+    private static List<String> divide(String input) {
+        State current = new State();
+        for (var i = 0; i < input.length(); i++) {
+            var c = input.charAt(i);
+            current = foldStatementChar(current, c);
+        }
+        return current.advance().segments;
+    }
+
+    private static State foldStatementChar(State state, char c) {
+        var appended = state.append(c);
+        if (c == ';') {
+            return appended.advance();
+        }
+        return appended;
     }
 
     private static String generatePlaceholder(String input) {
