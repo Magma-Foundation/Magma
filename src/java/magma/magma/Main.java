f2299036db5cf6802383272180400b01c996a17b
SirMathhman <sirmathhman@gmail.com>
2025-05-03 20:28:12 -0400

    Main.c

--- src/java/magma/magma/Main.java
+++
@@ -1,7 +0,0 @@
-package magma.magma;
-
-public class Main {
-    public static void main() {
-
-    }
-}
---
+++ src/java/magma/main.c
@@ -0,0 +1,20 @@
+/* package magma;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+public class Main {
+    public static void main() {
+        try {
+            var source = Paths.get(".", "src", "java", "magma", "Main.java");
+            var target = source.resolveSibling("main.c");
+
+            var input = Files.readString(source);
+            Files.writeString(target, "/* " + input + " */");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
+ */
