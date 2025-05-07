631ca70b9ba8b7ae8db15201c179484eb6c6baef
SirMathhman <sirmathhman@gmail.com>
2025-05-07 09:10:03 -0400

    Retain packages, imports

--- src/java/magma/Main.java
+++ src/java/magma/Main.java
@@ -3,6 +3,7 @@ package magma;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 
 public class Main {
     public static void main() {
@@ -12,9 +13,43 @@ public class Main {
 
         try {
             var input = Files.readString(source);
-            Files.writeString(target, "/* " + input + " */");
+            Files.writeString(target, compile(input));
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    private static String compile(String input) {
+        var segments = new ArrayList<String>();
+        var buffer = new StringBuffer();
+        for (var i = 0; i < input.length(); i++) {
+            var c = input.charAt(i);
+            buffer.append(c);
+            if (c == ';') {
+                segments.add(buffer.toString());
+                buffer = new StringBuffer();
+            }
+        }
+        segments.add(buffer.toString());
+
+        var output = new StringBuilder();
+        for (var segment : segments) {
+            output.append(compileRootSegment(segment));
+        }
+
+        return output.toString();
+    }
+
+    private static String compileRootSegment(String input) {
+        var stripped = input.strip();
+        if (stripped.startsWith("package ") || stripped.startsWith("import ")) {
+            return stripped + "\n";
+        }
+
+        return generatePlaceholder(input);
+    }
+
+    private static String generatePlaceholder(String input) {
+        return "/* " + input + " */";
     }
 }
--- src/java/magma/main.c
+++ src/java/magma/main.c
@@ -1,21 +1,56 @@
-/* package magma;
-
+package magma;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.ArrayList;
+/* 
 
 public class Main {
     public static void main() {
-        var root = Paths.get(".", "src", "java", "magma");
-        var source = root.resolve("Main.java");
-        var target = root.resolve("main.c");
+        var root = Paths.get(".", "src", "java", "magma"); *//* 
+        var source = root.resolve("Main.java"); *//* 
+        var target = root.resolve("main.c"); *//* 
 
         try {
-            var input = Files.readString(source);
-            Files.writeString(target, "/* " + input + " */");
+            var input = Files.readString(source); *//* 
+            Files.writeString(target, compile(input)); *//* 
         } catch (IOException e) {
-            e.printStackTrace();
+            e.printStackTrace(); *//* 
         }
+    }
+
+    private static String compile(String input) {
+        var segments = new ArrayList<String>(); *//* 
+        var buffer = new StringBuffer(); *//* 
+        for (var i = 0; *//*  i < input.length(); *//*  i++) {
+            var c = input.charAt(i); *//* 
+            buffer.append(c); *//* 
+            if (c == '; *//* ') {
+                segments.add(buffer.toString()); *//* 
+                buffer = new StringBuffer(); *//* 
+            }
+        }
+        segments.add(buffer.toString()); *//* 
+
+        var output = new StringBuilder(); *//* 
+        for (var segment : segments) {
+            output.append(compileRootSegment(segment)); *//* 
+        }
+
+        return output.toString(); *//* 
+    }
+
+    private static String compileRootSegment(String input) {
+        var stripped = input.strip(); *//* 
+        if (stripped.startsWith("package ") || stripped.startsWith("import ")) {
+            return stripped + "\n"; *//* 
+        }
+
+        return generatePlaceholder(input); *//* 
+    }
+
+    private static String generatePlaceholder(String input) {
+        return "/* " + input + " */"; *//* 
     }
 }
  */
