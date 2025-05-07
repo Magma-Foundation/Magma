4377303bcf124a4985bfe849a3ecf0871ed6cc4d
SirMathhman <sirmathhman@gmail.com>
2025-05-07 09:19:20 -0400

    Remove duplicates

--- src/java/magma/main.c
+++ src/java/magma/main.c
@@ -3,9 +3,35 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.List;
 /* 
 
 public class Main {
+    private static class State {
+        private final List<String> segments; *//* 
+        private StringBuilder buffer; *//* 
+
+        private State(List<String> segments, StringBuilder buffer) {
+            this.segments = segments; *//* 
+            this.buffer = buffer; *//* 
+        }
+
+        public State() {
+            this(new ArrayList<>(), new StringBuilder()); *//* 
+        }
+
+        private State append(char c) {
+            this.buffer.append(c); *//* 
+            return this; *//* 
+        }
+
+        private State advance() {
+            this.segments.add(this.buffer.toString()); *//* 
+            this.buffer = new StringBuilder(); *//* 
+            return this; *//* 
+        }
+    }
+
     public static void main() {
         var root = Paths.get(".", "src", "java", "magma"); *//* 
         var source = root.resolve("Main.java"); *//* 
@@ -20,24 +46,33 @@ public class Main {
     }
 
     private static String compile(String input) {
-        var segments = new ArrayList<String>(); *//* 
-        var buffer = new StringBuffer(); *//* 
-        for (var i = 0; *//*  i < input.length(); *//*  i++) {
-            var c = input.charAt(i); *//* 
-            buffer.append(c); *//* 
-            if (c == '; *//* ') {
-                segments.add(buffer.toString()); *//* 
-                buffer = new StringBuffer(); *//* 
-            }
-        }
-        segments.add(buffer.toString()); *//* 
-
+        var segments = divide(input); *//* 
         var output = new StringBuilder(); *//* 
         for (var segment : segments) {
             output.append(compileRootSegment(segment)); *//* 
         }
 
         return output.toString(); *//* 
+    }
+
+    private static List<String> divide(String input) {
+        State state = new State(); *//* 
+        for (var i = 0; *//*  i < input.length(); *//*  i++) {
+            var c = input.charAt(i); *//* 
+            state = foldChar(state, c); *//* 
+        }
+
+        return state.advance().segments; *//* 
+    }
+
+    private static State foldChar(State state, char c) {
+        var appended = state.append(c); *//* 
+        if (c == '; *//* ') {
+            return appended.advance(); *//* 
+        }
+        else {
+            return appended; *//* 
+        }
     }
 
     private static String compileRootSegment(String input) {
