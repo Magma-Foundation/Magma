852ab25e3940abb64b01faa01baf0f7b1a1d3a13
SirMathhman <sirmathhman@gmail.com>
2025-05-09 09:41:00 -0400

    Replace with impls

--- src/java/magma/Main.java
+++ src/java/magma/Main.java
@@ -2,18 +2,49 @@ package magma;
 
 import java.io.IOException;
 import java.nio.file.Files;
-import java.nio.file.Paths;
 
 class Main {
+    private interface Path {
+        void writeString(String input) throws IOException;
+
+        String readString() throws IOException;
+
+        Path resolve(String child);
+    }
+
+    private record JVMPath(java.nio.file.Path path) implements Path {
+        @Override
+        public void writeString(String input) throws IOException {
+            Files.writeString(this.path(), input);
+        }
+
+        @Override
+        public String readString() throws IOException {
+            return Files.readString(this.path());
+        }
+
+        @Override
+        public Path resolve(String child) {
+            return new JVMPath(this.path.resolve(child));
+        }
+    }
+
+    private static class Paths {
+        public static Path get(String first, String... more) {
+            return new JVMPath(java.nio.file.Paths.get(first, more));
+        }
+    }
+
     void main() {
         var parent = Paths.get(".", "src", "java", "magma");
-        var source = parent.resolve("main.mgs");
-        var target = parent.resolve("main.c");
+        var jvmPath = parent.resolve("main.mgs");
+        var jvmPath1 = parent.resolve("main.c");
         try {
-            var input = Files.readString(source);
-            Files.writeString(target, input);
+            var input = jvmPath.readString();
+            jvmPath1.writeString(input);
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
+
 }
--- src/java/magma/main.c
+++ src/java/magma/main.c
@@ -1,3 +1,7 @@
-int main() }
+#include <stdio.h>
+
+int main() {
+    char* parent = "./src/java/magma";
+    char* source =
     return 0;
 }
