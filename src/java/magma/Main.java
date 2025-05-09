b16f081bcad5b65489160be0d8f915d317c03a5d
SirMathhman <sirmathhman@gmail.com>
2025-05-09 11:48:05 -0400

    Cleanup

--- src/java/magma/Main.java
+++ src/java/magma/Main.java
@@ -2,16 +2,18 @@ package magma;
 
 import java.io.IOException;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
 
 public class Main {
     public static void main() {
         try {
             var root = Paths.get(".", "src", "java", "magma");
-            var input = Files.readString(root.resolve("main.java"));
-
+            var source = root.resolve("main.java");
             var target = root.resolve("main.c");
-            Files.writeString(target, generatePlaceholder(input) + "\nint main(){\n\treturn 0;\n}\n");
+
+            var input = Files.readString(source);
+            Files.writeString(target, compile(input));
 
             new ProcessBuilder("clang.exe", target.toAbsolutePath().toString(), "-o", "main.exe")
                     .inheritIO()
@@ -20,6 +22,10 @@ public class Main {
         } catch (IOException | InterruptedException e) {
             e.printStackTrace();
         }
+    }
+
+    private static String compile(String input) {
+        return generatePlaceholder(input) + "\nint main(){\n\treturn 0;\n}\n";
     }
 
     private static String generatePlaceholder(String input) {
--- src/java/magma/main.c
+++ src/java/magma/main.c
@@ -2,16 +2,18 @@
 
 import java.io.IOException;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
 
 public class Main {
     public static void main() {
         try {
             var root = Paths.get(".", "src", "java", "magma");
-            var input = Files.readString(root.resolve("main.java"));
-
+            var source = root.resolve("main.java");
             var target = root.resolve("main.c");
-            Files.writeString(target, generatePlaceholder(input) + "\nint main(){\n\treturn 0;\n}\n");
+
+            var input = Files.readString(source);
+            Files.writeString(target, compile(input));
 
             new ProcessBuilder("clang.exe", target.toAbsolutePath().toString(), "-o", "main.exe")
                     .inheritIO()
@@ -22,11 +24,15 @@ public class Main {
         }
     }
 
+    private static String compile(String input) {
+        return generatePlaceholder(input) + "\nint main(){\n\treturn 0;\n}\n";
+    }
+
     private static String generatePlaceholder(String input) {
         var replaced = input
                 .replace("content-start", "content-start")
                 .replace("content-end", "content-end");
-        
+
         return "content-start" + replaced + " content-end";
     }
 }
