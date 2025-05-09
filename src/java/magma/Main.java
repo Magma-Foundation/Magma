31186ab75441b1011bb92b025b4a253bea54456b
SirMathhman <sirmathhman@gmail.com>
2025-05-09 09:35:30 -0400

    OK

--- src/java/magma/Main.java
+++ src/java/magma/Main.java
@@ -1,10 +1,18 @@
-void main() {
-    var source = Paths.get(".", "src", "java", "magma", "main.mgs");
-    var target = source.resolveSibling("main.c");
-    try {
-        var input = Files.readString(source);
-        Files.writeString(target, input);
-    } catch (IOException e) {
-        e.printStackTrace();
+package magma;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+class Main {
+    void main() {
+        var source = Paths.get(".", "src", "java", "magma", "main.mgs");
+        var target = source.resolveSibling("main.c");
+        try {
+            var input = Files.readString(source);
+            Files.writeString(target, input);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
