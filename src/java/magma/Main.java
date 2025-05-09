0a79d96b080fa4a11418a8b2b61082f1bd010c49
SirMathhman <sirmathhman@gmail.com>
2025-05-09 11:49:40 -0400

    Root segments

--- src/java/magma/Main.java
+++ src/java/magma/Main.java
@@ -2,8 +2,8 @@ package magma;
 
 import java.io.IOException;
 import java.nio.file.Files;
-import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 
 public class Main {
     public static void main() {
@@ -25,7 +25,28 @@ public class Main {
     }
 
     private static String compile(String input) {
-        return generatePlaceholder(input) + "\nint main(){\n\treturn 0;\n}\n";
+        var segments = new ArrayList<String>();
+        var buffer = new StringBuilder();
+        for (var i = 0; i < input.length(); i++) {
+            var c = input.charAt(i);
+            buffer.append(c);
+            if (c == ';') {
+                segments.add(buffer.toString());
+                buffer = new StringBuilder();
+            }
+        }
+        segments.add(buffer.toString());
+
+        var output = new StringBuilder();
+        for (var segment : segments) {
+            output.append(compileRootSegment(segment));
+        }
+
+        return output + "\nint main(){\n\treturn 0;\n}\n";
+    }
+
+    private static String compileRootSegment(String input) {
+        return generatePlaceholder(input);
     }
 
     private static String generatePlaceholder(String input) {
--- src/java/magma/main.c
+++ src/java/magma/main.c
@@ -1,39 +1,60 @@
-/* package magma;
+/* package magma; *//* 
 
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
+import java.io.IOException; *//* 
+import java.nio.file.Files; *//* 
+import java.nio.file.Paths; *//* 
+import java.util.ArrayList; *//* 
 
 public class Main {
     public static void main() {
         try {
-            var root = Paths.get(".", "src", "java", "magma");
-            var source = root.resolve("main.java");
-            var target = root.resolve("main.c");
+            var root = Paths.get(".", "src", "java", "magma"); *//* 
+            var source = root.resolve("main.java"); *//* 
+            var target = root.resolve("main.c"); *//* 
 
-            var input = Files.readString(source);
-            Files.writeString(target, compile(input));
+            var input = Files.readString(source); *//* 
+            Files.writeString(target, compile(input)); *//* 
 
             new ProcessBuilder("clang.exe", target.toAbsolutePath().toString(), "-o", "main.exe")
                     .inheritIO()
                     .start()
-                    .waitFor();
+                    .waitFor(); *//* 
         } catch (IOException | InterruptedException e) {
-            e.printStackTrace();
+            e.printStackTrace(); *//* 
         }
     }
 
     private static String compile(String input) {
-        return generatePlaceholder(input) + "\nint main(){\n\treturn 0;\n}\n";
+        var segments = new ArrayList<String>(); *//* 
+        var buffer = new StringBuilder(); *//* 
+        for (var i = 0; *//*  i < input.length(); *//*  i++) {
+            var c = input.charAt(i); *//* 
+            buffer.append(c); *//* 
+            if (c == '; *//* ') {
+                segments.add(buffer.toString()); *//* 
+                buffer = new StringBuilder(); *//* 
+            }
+        }
+        segments.add(buffer.toString()); *//* 
+
+        var output = new StringBuilder(); *//* 
+        for (var segment : segments) {
+            output.append(compileRootSegment(segment)); *//* 
+        }
+
+        return output + "\nint main(){\n\treturn 0; *//* \n}\n"; *//* 
+    }
+
+    private static String compileRootSegment(String input) {
+        return generatePlaceholder(input); *//* 
     }
 
     private static String generatePlaceholder(String input) {
         var replaced = input
                 .replace("content-start", "content-start")
-                .replace("content-end", "content-end");
+                .replace("content-end", "content-end"); *//* 
 
-        return "content-start" + replaced + " content-end";
+        return "content-start" + replaced + " content-end"; *//* 
     }
 }
  */
