2a16f088f8287f5ca308775df09669cb04756aa8
SirMathhman <sirmathhman@gmail.com>
2025-05-10 16:11:21 -0400

    Build?

--- src/java/magma/Main.java
+++ src/java/magma/Main.java
@@ -3,6 +3,7 @@ package magma;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 
 public class Main {
     public static void main() {
@@ -12,15 +13,41 @@ public class Main {
             var target = parent.resolve("main.ts");
 
             var input = Files.readString(source);
-            Files.writeString(target, generatePlaceholder(input));
+            Files.writeString(target, compile(input));
 
-            new ProcessBuilder("cmd", "/d", "npm.exe", "exec", "ts-node", target.toAbsolutePath().toString())
+            new ProcessBuilder("cmd", "/c", "npm", "exec", "ts-node", target.toAbsolutePath().toString())
                     .inheritIO()
                     .start()
                     .waitFor();
         } catch (IOException | InterruptedException e) {
             throw new RuntimeException(e);
         }
+    }
+
+    private static String compile(String input) {
+        var segments = new ArrayList<String>();
+        var buffer = new StringBuilder();
+        for (var i = 0; i < input.length(); i++) {
+            var c = input.charAt(i);
+            buffer.append(c);
+
+            if (c == ';') {
+                segments.add(buffer.toString());
+                buffer = new StringBuilder();
+            }
+        }
+        segments.add(buffer.toString());
+
+        var output = new StringBuilder();
+        for (var segment : segments) {
+            output.append(compileRootSegment(segment));
+        }
+
+        return output.toString();
+    }
+
+    private static String compileRootSegment(String segment) {
+        return generatePlaceholder(segment);
     }
 
     private static String generatePlaceholder(String input) {
--- src/java/magma/main.ts
+++ src/java/magma/main.ts
@@ -1,34 +1,61 @@
-/* package magma;
+/* package magma; *//* 
 
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
+import java.io.IOException; *//* 
+import java.nio.file.Files; *//* 
+import java.nio.file.Paths; *//* 
+import java.util.ArrayList; *//* 
 
 public class Main {
     public static void main() {
         try {
-            var parent = Paths.get(".", "src", "java", "magma");
-            var source = parent.resolve("Main.java");
-            var target = parent.resolve("main.ts");
+            var parent = Paths.get(".", "src", "java", "magma"); *//* 
+            var source = parent.resolve("Main.java"); *//* 
+            var target = parent.resolve("main.ts"); *//* 
 
-            var input = Files.readString(source);
-            Files.writeString(target, generatePlaceholder(input));
+            var input = Files.readString(source); *//* 
+            Files.writeString(target, compile(input)); *//* 
 
-            new ProcessBuilder("cmd", "/d", "npm.exe", "exec", "ts-node", target.toAbsolutePath().toString())
+            new ProcessBuilder("cmd", "/c", "npm", "exec", "ts-node", target.toAbsolutePath().toString())
                     .inheritIO()
                     .start()
-                    .waitFor();
+                    .waitFor(); *//* 
         } catch (IOException | InterruptedException e) {
-            throw new RuntimeException(e);
+            throw new RuntimeException(e); *//* 
         }
+    }
+
+    private static String compile(String input) {
+        var segments = new ArrayList<String>(); *//* 
+        var buffer = new StringBuilder(); *//* 
+        for (var i = 0; *//*  i < input.length(); *//*  i++) {
+            var c = input.charAt(i); *//* 
+            buffer.append(c); *//* 
+
+            if(c == '; *//* ') {
+            segments.add(buffer.toString()); *//* 
+            buffer = new StringBuilder(); *//* 
+            }
+        }
+        segments.add(buffer.toString()); *//* 
+
+        var output = new StringBuilder(); *//* 
+        for (var segment : segments) {
+            output.append(compileRootSegment(segment)); *//* 
+        }
+
+        return output.toString(); *//* 
+    }
+
+    private static String compileRootSegment(String segment) {
+        return generatePlaceholder(segment); *//* 
     }
 
     private static String generatePlaceholder(String input) {
         var replaced = input
                 .replace("content-start", "content-start")
-                .replace("content-end", "content-end");
+                .replace("content-end", "content-end"); *//* 
 
-        return "content-start " + replaced + " content-end";
+        return "content-start " + replaced + " content-end"; *//* 
     }
 }
  */
