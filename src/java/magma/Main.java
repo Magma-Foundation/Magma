22161204ea04da382b230662497e97688d1b26dd
SirMathhman <sirmathhman@gmail.com>
2025-05-14 18:15:57 -0400

    Root segments

--- src/java/magma/Main.java
+++ src/java/magma/Main.java
@@ -3,6 +3,7 @@ package magma;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 
 public class Main {
     public static void main() {
@@ -10,10 +11,35 @@ public class Main {
         var target = source.resolveSibling("main.ts");
         try {
             var input = Files.readString(source);
-            Files.writeString(target, placeholder(input));
+            Files.writeString(target, compile(input));
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
+    }
+
+    private static String compile(String input) {
+        var segments = new ArrayList<String>();
+        var buffer = new StringBuilder();
+        for (var i = 0; i < input.length(); i++) {
+            var c = input.charAt(i);
+            buffer.append(c);
+            if(c == ';') {
+                segments.add(buffer.toString());
+                buffer = new StringBuilder();
+            }
+        }
+
+        segments.add(buffer.toString());
+
+        var output = new StringBuilder();
+        for (var segment : segments) {
+            output.append(compileRootSegment(segment));
+        }
+        return output.toString();
+    }
+
+    private static String compileRootSegment(String segment) {
+        return placeholder(segment);
     }
 
     private static String placeholder(String input) {
--- src/java/magma/main.ts
+++ src/java/magma/main.ts
@@ -1,26 +1,52 @@
-/*package magma;
+/*package magma;*//*
 
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
+import java.io.IOException;*//*
+import java.nio.file.Files;*//*
+import java.nio.file.Paths;*//*
+import java.util.ArrayList;*//*
 
 public class Main {
     public static void main() {
-        var source = Paths.get(".", "src", "java", "magma", "Main.java");
-        var target = source.resolveSibling("main.ts");
+        var source = Paths.get(".", "src", "java", "magma", "Main.java");*//*
+        var target = source.resolveSibling("main.ts");*//*
         try {
-            var input = Files.readString(source);
-            Files.writeString(target, placeholder(input));
+            var input = Files.readString(source);*//*
+            Files.writeString(target, compile(input));*//*
         } catch (IOException e) {
-            throw new RuntimeException(e);
+            throw new RuntimeException(e);*//*
         }
+    }
+
+    private static String compile(String input) {
+        var segments = new ArrayList<String>();*//*
+        var buffer = new StringBuilder();*//*
+        for (var i = 0;*//* i < input.length();*//* i++) {
+            var c = input.charAt(i);*//*
+            buffer.append(c);*//*
+            if(c == ';*//*') {
+                segments.add(buffer.toString());*//*
+                buffer = new StringBuilder();*//*
+            }
+        }
+
+        segments.add(buffer.toString());*//*
+
+        var output = new StringBuilder();*//*
+        for (var segment : segments) {
+            output.append(compileRootSegment(segment));*//*
+        }
+        return output.toString();*//*
+    }
+
+    private static String compileRootSegment(String segment) {
+        return placeholder(segment);*//*
     }
 
     private static String placeholder(String input) {
         var replaced = input
                 .replace("start", "start")
-                .replace("end", "end");
-        
-        return "start" + replaced + "end";
+                .replace("end", "end");*//*
+
+        return "start" + replaced + "end";*//*
     }
 }*/
