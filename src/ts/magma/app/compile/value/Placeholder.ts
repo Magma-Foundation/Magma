e00877edd70f7bdb1a1cfef7509ba714e73ceb5e
SirMathhman <sirmathhman@gmail.com>
2025-05-21 11:50:22 -0400

    Fix strange dependency that Value had on CompileState

---
+++ src/ts/magma/app/compile/value/Placeholder.ts
@@ -0,0 +1,120 @@
+/*[
+	JVMList: jvm.api.collect.list, 
+	Lists: jvm.api.collect.list, 
+	Files: jvm.api.io, 
+	Actual: magma.annotate, 
+	Namespace: magma.annotate, 
+	Collector: magma.api.collect, 
+	EmptyHead: magma.api.collect.head, 
+	FlatMapHead: magma.api.collect.head, 
+	Head: magma.api.collect.head, 
+	HeadedIter: magma.api.collect.head, 
+	MapHead: magma.api.collect.head, 
+	RangeHead: magma.api.collect.head, 
+	SingleHead: magma.api.collect.head, 
+	Iter: magma.api.collect, 
+	Iters: magma.api.collect, 
+	Joiner: magma.api.collect, 
+	Iterable: magma.api.collect.list, 
+	List: magma.api.collect.list, 
+	ListCollector: magma.api.collect.list, 
+	Console: magma.api.io, 
+	IOError: magma.api.io, 
+	Path: magma.api.io, 
+	None: magma.api.option, 
+	Option: magma.api.option, 
+	Some: magma.api.option, 
+	Err: magma.api.result, 
+	Ok: magma.api.result, 
+	Result: magma.api.result, 
+	Characters: magma.api.text, 
+	Strings: magma.api.text, 
+	Tuple2: magma.api, 
+	Tuple2Impl: magma.api, 
+	CompileState: magma.app.compile, 
+	Context: magma.app.compile, 
+	ConstructionCaller: magma.app.compile.define, 
+	ConstructorHeader: magma.app.compile.define, 
+	Definition: magma.app.compile.define, 
+	MethodHeader: magma.app.compile.define, 
+	Parameter: magma.app.compile.define, 
+	Dependency: magma.app.compile, 
+	DivideState: magma.app.compile, 
+	ImmutableCompileState: magma.app.compile, 
+	ImmutableContext: magma.app.compile, 
+	ImmutableDivideState: magma.app.compile, 
+	ImmutableRegistry: magma.app.compile, 
+	ImmutableStack: magma.app.compile, 
+	Import: magma.app.compile, 
+	Registry: magma.app.compile, 
+	Stack: magma.app.compile, 
+	Whitespace: magma.app.compile.text, 
+	FunctionType: magma.app.compile.type, 
+	PrimitiveType: magma.app.compile.type, 
+	TemplateType: magma.app.compile.type, 
+	Type: magma.app.compile.type, 
+	VariadicType: magma.app.compile.type, 
+	AccessValue: magma.app.compile.value, 
+	Argument: magma.app.compile.value, 
+	Caller: magma.app.compile.value, 
+	Invokable: magma.app.compile.value, 
+	Lambda: magma.app.compile.value, 
+	Not: magma.app.compile.value, 
+	Operation: magma.app.compile.value, 
+	Placeholder: magma.app.compile.value, 
+	StringValue: magma.app.compile.value, 
+	Symbol: magma.app.compile.value, 
+	Value: magma.app.compile.value, 
+	Compiler: magma.app, 
+	PathSource: magma.app.io, 
+	Source: magma.app.io, 
+	Location: magma.app, 
+	Main: magma.app, 
+	Platform: magma.app, 
+	Sources: magma.app, 
+	Targets: magma.app
+]*/
+import { Compiler } from "../../../../magma/app/Compiler";
+import { Value } from "../../../../magma/app/compile/value/Value";
+import { Option } from "../../../../magma/api/option/Option";
+import { None } from "../../../../magma/api/option/None";
+import { Definition } from "../../../../magma/app/compile/define/Definition";
+import { Type } from "../../../../magma/app/compile/type/Type";
+import { CompileState } from "../../../../magma/app/compile/CompileState";
+import { PrimitiveType } from "../../../../magma/app/compile/type/PrimitiveType";
+export class Placeholder {
+	input: string;
+	constructor (input: string) {
+		this.input = input;
+	}
+	generate(): string {
+		return Compiler.generatePlaceholder(this.input)/*unknown*/;
+	}
+	isFunctional(): boolean {
+		return false/*unknown*/;
+	}
+	findChild(): Option<Value> {
+		return new None<Value>()/*unknown*/;
+	}
+	asDefinition(): Option<Definition> {
+		return new None<Definition>()/*unknown*/;
+	}
+	toValue(): Option<Value> {
+		return new None<Value>()/*unknown*/;
+	}
+	resolve(state: CompileState): Type {
+		return PrimitiveType.Unknown/*unknown*/;
+	}
+	isVar(): boolean {
+		return false/*unknown*/;
+	}
+	generateBeforeName(): string {
+		return ""/*unknown*/;
+	}
+	generateAsEnumValue(structureName: string): Option<string> {
+		return new None<string>()/*unknown*/;
+	}
+	generateSimple(): string {
+		return this.generate()/*unknown*/;
+	}
+}
---
+++ src/ts/magma/app/compile/value/Symbol.ts
@@ -0,0 +1,117 @@
+/*[
+	JVMList: jvm.api.collect.list, 
+	Lists: jvm.api.collect.list, 
+	Files: jvm.api.io, 
+	Actual: magma.annotate, 
+	Namespace: magma.annotate, 
+	Collector: magma.api.collect, 
+	EmptyHead: magma.api.collect.head, 
+	FlatMapHead: magma.api.collect.head, 
+	Head: magma.api.collect.head, 
+	HeadedIter: magma.api.collect.head, 
+	MapHead: magma.api.collect.head, 
+	RangeHead: magma.api.collect.head, 
+	SingleHead: magma.api.collect.head, 
+	Iter: magma.api.collect, 
+	Iters: magma.api.collect, 
+	Joiner: magma.api.collect, 
+	Iterable: magma.api.collect.list, 
+	List: magma.api.collect.list, 
+	ListCollector: magma.api.collect.list, 
+	Console: magma.api.io, 
+	IOError: magma.api.io, 
+	Path: magma.api.io, 
+	None: magma.api.option, 
+	Option: magma.api.option, 
+	Some: magma.api.option, 
+	Err: magma.api.result, 
+	Ok: magma.api.result, 
+	Result: magma.api.result, 
+	Characters: magma.api.text, 
+	Strings: magma.api.text, 
+	Tuple2: magma.api, 
+	Tuple2Impl: magma.api, 
+	CompileState: magma.app.compile, 
+	Context: magma.app.compile, 
+	ConstructionCaller: magma.app.compile.define, 
+	ConstructorHeader: magma.app.compile.define, 
+	Definition: magma.app.compile.define, 
+	MethodHeader: magma.app.compile.define, 
+	Parameter: magma.app.compile.define, 
+	Dependency: magma.app.compile, 
+	DivideState: magma.app.compile, 
+	ImmutableCompileState: magma.app.compile, 
+	ImmutableContext: magma.app.compile, 
+	ImmutableDivideState: magma.app.compile, 
+	ImmutableRegistry: magma.app.compile, 
+	ImmutableStack: magma.app.compile, 
+	Import: magma.app.compile, 
+	Registry: magma.app.compile, 
+	Stack: magma.app.compile, 
+	Whitespace: magma.app.compile.text, 
+	FunctionType: magma.app.compile.type, 
+	PrimitiveType: magma.app.compile.type, 
+	TemplateType: magma.app.compile.type, 
+	Type: magma.app.compile.type, 
+	VariadicType: magma.app.compile.type, 
+	AccessValue: magma.app.compile.value, 
+	Argument: magma.app.compile.value, 
+	Caller: magma.app.compile.value, 
+	Invokable: magma.app.compile.value, 
+	Lambda: magma.app.compile.value, 
+	Not: magma.app.compile.value, 
+	Operation: magma.app.compile.value, 
+	Placeholder: magma.app.compile.value, 
+	StringValue: magma.app.compile.value, 
+	Symbol: magma.app.compile.value, 
+	Value: magma.app.compile.value, 
+	Compiler: magma.app, 
+	PathSource: magma.app.io, 
+	Source: magma.app.io, 
+	Location: magma.app, 
+	Main: magma.app, 
+	Platform: magma.app, 
+	Sources: magma.app, 
+	Targets: magma.app
+]*/
+import { Type } from "../../../../magma/app/compile/type/Type";
+import { CompileState } from "../../../../magma/app/compile/CompileState";
+import { Definition } from "../../../../magma/app/compile/define/Definition";
+import { PrimitiveType } from "../../../../magma/app/compile/type/PrimitiveType";
+import { Value } from "../../../../magma/app/compile/value/Value";
+import { Option } from "../../../../magma/api/option/Option";
+import { Some } from "../../../../magma/api/option/Some";
+import { None } from "../../../../magma/api/option/None";
+export class Symbol {
+	value: string;
+	constructor (value: string) {
+		this.value = value;
+	}
+	generate(): string {
+		return this.value/*unknown*/;
+	}
+	resolve(state: CompileState): Type {
+		return state.stack().resolveValue(this.value).map((definition: Definition) => definition.findType()/*unknown*/).orElse(PrimitiveType.Unknown)/*unknown*/;
+	}
+	toValue(): Option<Value> {
+		return new Some<Value>(this)/*unknown*/;
+	}
+	findChild(): Option<Value> {
+		return new None<Value>()/*unknown*/;
+	}
+	isFunctional(): boolean {
+		return false/*unknown*/;
+	}
+	isVar(): boolean {
+		return false/*unknown*/;
+	}
+	generateBeforeName(): string {
+		return ""/*unknown*/;
+	}
+	generateAsEnumValue(structureName: string): Option<string> {
+		return new None<string>()/*unknown*/;
+	}
+	generateSimple(): string {
+		return this.generate()/*unknown*/;
+	}
+}
