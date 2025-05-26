c304dd8b1e6fd74981eda6123f73b73947b650cd
SirMathhman <sirmathhman@gmail.com>
2025-05-26 15:11:45 -0400

    Introduce ParseState as a wrapper for now

--- src/java/magmac/app/stage/ParserImpl.java
+++ src/java/magmac/app/stage/ParserImpl.java
@@ -6,6 +6,7 @@ import magmac.api.collect.MapCollector;
 import magmac.app.compile.node.MapNode;
 import magmac.app.compile.node.Node;
 import magmac.app.io.Location;
+import magmac.app.stage.parse.ParseState;
 
 import java.util.ArrayList;
 import java.util.Collection;
@@ -15,31 +16,36 @@ import java.util.Map;
 import java.util.Optional;
 
 public class ParserImpl implements Parser {
-
     private static Tuple2<Location, Node> parse(Location location, Node root) {
         return ParserImpl.getLocationNodeTuple2(location, root);
     }
 
     private static Tuple2<Location, Node> getLocationNodeTuple2(Location location, Node root) {
         List<Node> children = root.findNodeList("children").orElse(new ArrayList<>());
-        Tuple2<Location, List<Node>> initial = new Tuple2<>(location, new ArrayList<Node>());
-        Tuple2<Location, List<Node>> dependencies = Iters.fromList(children).fold(initial, (currentTuple, node) -> {
-            Tuple2<Location, Node> parsed = ParserImpl.beforePass(location, node);
-            currentTuple.right().add(parsed.right());
-            return new Tuple2<>(currentTuple.left(), currentTuple.right());
+        Tuple2<ParseState, List<Node>> initial = new Tuple2<>(new ParseState(location), new ArrayList<Node>());
+        Tuple2<ParseState, List<Node>> dependencies = Iters.fromList(children).fold(initial, (currentTuple, node) -> {
+            ParseState currentState = currentTuple.left();
+            List<Node> currentElements = currentTuple.right();
+
+            Tuple2<ParseState, Node> parsed = ParserImpl.beforePass(currentState, node);
+            ParseState newState = parsed.left();
+            Node newElement = parsed.right();
+
+            currentElements.add(newElement);
+            return new Tuple2<>(newState, currentElements);
         });
 
         List<Node> copy = new ArrayList<Node>();
         copy.add(new MapNode("class").withString("name", location.name()));
         copy.addAll(dependencies.right());
 
-        return new Tuple2<>(dependencies.left(), new MapNode().withNodeList("children", copy));
+        return new Tuple2<>(dependencies.left().location(), new MapNode().withNodeList("children", copy));
     }
 
-    private static Tuple2<Location, Node> beforePass(Location state, Node node) {
+    private static Tuple2<ParseState, Node> beforePass(ParseState state, Node node) {
         if (node.is("import")) {
             Node dependency = new MapNode("dependency")
-                    .withString("parent", state.name())
+                    .withString("parent", state.location().name())
                     .withString("child", node.findString("child").orElse(""));
 
             return new Tuple2<>(state, dependency);
@@ -48,8 +54,8 @@ public class ParserImpl implements Parser {
         return new Tuple2<>(state, node);
     }
 
-    private static Map<Location, Node> afterParse(Map<Location, Node> parsedRoots) {
-        List<Node> allChildren = parsedRoots.values()
+    private static Map<Location, Node> afterAll(Map<Location, Node> roots) {
+        List<Node> allChildren = roots.values()
                 .stream()
                 .map(node -> node.findNodeList("children"))
                 .flatMap(Optional::stream)
@@ -69,6 +75,6 @@ public class ParserImpl implements Parser {
 
     @Override
     public Map<Location, Node> parseAll(Map<Location, Node> roots) {
-        return ParserImpl.afterParse(ParserImpl.parseAllRaw(roots));
+        return ParserImpl.afterAll(ParserImpl.parseAllRaw(roots));
     }
 }
---
+++ src/java/magmac/app/stage/parse/ParseState.java
@@ -0,0 +1,6 @@
+package magmac.app.stage.parse;
+
+import magmac.app.io.Location;
+
+public record ParseState(Location location) {
+}
