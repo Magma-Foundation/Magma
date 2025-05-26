a473abb59036e62e5a7d5ba631ded3443cf4183d
SirMathhman <sirmathhman@gmail.com>
2025-05-26 15:05:06 -0400

    Hmmm

--- src/java/magmac/app/compile/lang/java/JavaRoots.java
+++ src/java/magmac/app/compile/lang/java/JavaRoots.java
@@ -5,14 +5,15 @@ import magmac.app.compile.rule.DivideRule;
 import magmac.app.compile.rule.OrRule;
 import magmac.app.compile.rule.Rule;
 import magmac.app.compile.rule.StringRule;
+import magmac.app.compile.rule.TypeRule;
 
 import java.util.List;
 
-public class JavaRoots {
+public final class JavaRoots {
     public static Rule createRule() {
-        return new DivideRule("children", new OrRule(List.of(
+        return new TypeRule("root", new DivideRule("children", new OrRule(List.of(
                 Imports.createImportRule(),
                 new StringRule("value")
-        )));
+        ))));
     }
 }
