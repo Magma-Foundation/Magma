c3034e7c6980bfab3255402c3a00ee2a8e898669
SirMathhman <sirmathhman@gmail.com>
2025-05-26 15:14:50 -0400

    After pass, before pass

--- src/java/magmac/app/stage/ParserImpl.java
+++ src/java/magmac/app/stage/ParserImpl.java
@@ -17,12 +17,14 @@ import java.util.Optional;
 
 public class ParserImpl implements Parser {
     private static Tuple2<Location, Node> parse(Location location, Node root) {
-        return ParserImpl.getLocationNodeTuple2(location, root);
+        ParseState initial = new ParseState(location);
+        Tuple2<ParseState, Node> parsed = ParserImpl.parseTree(initial, root);
+        return new Tuple2<>(parsed.left().location(), parsed.right());
     }
 
-    private static Tuple2<Location, Node> getLocationNodeTuple2(Location location, Node root) {
+    private static Tuple2<ParseState, Node> parseTree(ParseState state, Node root) {
         List<Node> children = root.findNodeList("children").orElse(new ArrayList<>());
-        Tuple2<ParseState, List<Node>> initial = new Tuple2<>(new ParseState(location), new ArrayList<Node>());
+        Tuple2<ParseState, List<Node>> initial = new Tuple2<>(state, new ArrayList<Node>());
         Tuple2<ParseState, List<Node>> dependencies = Iters.fromList(children).fold(initial, (currentTuple, node) -> {
             ParseState currentState = currentTuple.left();
             List<Node> currentElements = currentTuple.right();
@@ -35,11 +37,20 @@ public class ParserImpl implements Parser {
             return new Tuple2<>(newState, currentElements);
         });
 
-        List<Node> copy = new ArrayList<Node>();
-        copy.add(new MapNode("class").withString("name", location.name()));
-        copy.addAll(dependencies.right());
+        Node children1 = new MapNode().withNodeList("children", dependencies.right());
+        return ParserImpl.afterPass(dependencies.left(), children1);
+    }
 
-        return new Tuple2<>(dependencies.left().location(), new MapNode().withNodeList("children", copy));
+    private static Tuple2<ParseState, Node> afterPass(ParseState state, Node node) {
+        if (node.is("root")) {
+            List<Node> copy = new ArrayList<Node>();
+            copy.add(new MapNode("class").withString("name", state.location().name()));
+            copy.addAll(node.findNodeList("children").orElse(new ArrayList<>()));
+
+            return new Tuple2<>(state, node);
+        }
+
+        return new Tuple2<>(state, node);
     }
 
     private static Tuple2<ParseState, Node> beforePass(ParseState state, Node node) {
