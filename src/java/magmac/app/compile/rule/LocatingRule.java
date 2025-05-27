df50d1a6cb02dfb071d4c3962b520a918c247493
SirMathhman <sirmathhman@gmail.com>
2025-05-27 19:08:43 -0400

    Extract Splitter

--- src/java/magmac/app/compile/rule/LocatingRule.java
+++ src/java/magmac/app/compile/rule/LocatingRule.java
@@ -9,40 +9,40 @@ import magmac.app.compile.rule.locate.Locator;
 
 public final class LocatingRule implements Rule {
     private final Rule leftRule;
-    private final String infix;
+    private final Splitter splitter;
     private final Rule rightRule;
-    private final Locator locator;
 
-    private LocatingRule(Rule leftRule, String infix, Rule rightRule, Locator locator) {
+    private LocatingRule(Rule leftRule, Splitter splitter, Rule rightRule) {
         this.leftRule = leftRule;
-        this.infix = infix;
         this.rightRule = rightRule;
-        this.locator = locator;
+        this.splitter = splitter;
     }
 
     public static Rule First(Rule leftRule, String infix, Rule rightRule) {
-        return new LocatingRule(leftRule, infix, rightRule, new FirstLocator());
+        return LocatingRule.createLocatingRule(leftRule, infix, rightRule, new FirstLocator());
     }
 
     public static Rule Last(Rule leftRule, String infix, Rule rightRule) {
-        return new LocatingRule(leftRule, infix, rightRule, new LastLocator());
+        return LocatingRule.createLocatingRule(leftRule, infix, rightRule, new LastLocator());
+    }
+
+    private static Rule createLocatingRule(Rule leftRule, String infix, Rule rightRule, Locator locator) {
+        return new LocatingRule(leftRule, new LocatingSplitter(infix, locator), rightRule);
     }
 
     @Override
     public CompileResult<Node> lex(String input) {
-        return this.locator.locate(input, this.infix).map(separator -> {
-            String left = input.substring(0, separator);
-            String right = input.substring(separator + this.infix.length());
+        return this.splitter.split(input).map(tuple -> {
+            String left = tuple.left();
+            String right = tuple.right();
             return this.leftRule.lex(left).merge(() -> this.rightRule.lex(right), Node::merge);
-        }).orElseGet(() -> {
-            return CompileErrors.createStringError("Infix '" + this.infix + "' not present", input);
-        });
+        }).orElseGet(() -> CompileErrors.createStringError(this.splitter.createMessage(), input));
     }
 
     @Override
     public CompileResult<String> generate(Node node) {
         return this.leftRule.generate(node).merge(
                 () -> this.rightRule.generate(node),
-                (leftString, rightString) -> leftString + this.infix + rightString);
+                (leftString, rightString) -> this.splitter.merge(leftString, rightString));
     }
 }
