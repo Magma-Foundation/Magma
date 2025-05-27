f1a9ec98cca06b6521b187ff364a59fe8113fcc7
SirMathhman <sirmathhman@gmail.com>
2025-05-27 10:00:27 -0400

    Introduce ListCollector

---
+++ src/java/magmac/api/collect/ListCollector.java
@@ -0,0 +1,17 @@
+package magmac.api.collect;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public record ListCollector<T>() implements Collector<T, List<T>> {
+    @Override
+    public List<T> createInitial() {
+        return new ArrayList<>();
+    }
+
+    @Override
+    public List<T> fold(List<T> current, T element) {
+        current.add(element);
+        return current;
+    }
+}
--- src/java/magmac/app/compile/rule/DivideRule.java
+++ src/java/magmac/app/compile/rule/DivideRule.java
@@ -1,24 +1,32 @@
 package magmac.app.compile.rule;
 
-import magmac.api.iter.Iters;
 import magmac.api.collect.Joiner;
+import magmac.api.collect.ListCollector;
 import magmac.api.collect.ResultCollector;
-import magmac.api.result.Ok;
+import magmac.api.iter.Iter;
+import magmac.api.iter.Iters;
 import magmac.api.result.Result;
+import magmac.app.compile.error.CompileError;
+import magmac.app.compile.error.CompileErrors;
 import magmac.app.compile.node.MapNode;
 import magmac.app.compile.node.Node;
 import magmac.app.compile.rule.divide.DivideState;
 import magmac.app.compile.rule.divide.MutableDivideState;
 import magmac.app.compile.rule.fold.Folder;
-import magmac.app.compile.error.CompileError;
-import magmac.app.compile.error.CompileErrors;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
 public record DivideRule(String key, Folder folder, Rule childRule) implements Rule {
-    private List<String> divide(String input) {
+    @Override
+    public Result<Node, CompileError> lex(String input) {
+        return this.divide(input)
+                .map(segment -> this.childRule.lex(segment))
+                .collect(new ResultCollector<>(new ListCollector<>()))
+                .mapValue(children -> new MapNode().withNodeList(this.key(), children));
+    }
+
+    private Iter<String> divide(String input) {
         DivideState current = new MutableDivideState();
         int length = input.length();
         for (int i = 0; i < length; i++) {
@@ -26,22 +34,7 @@ public record DivideRule(String key, Folder folder, Rule childRule) implements R
             current = this.folder.fold(current, c);
         }
 
-        return current.advance().stream().toList();
-    }
-
-    @Override
-    public Result<Node, CompileError> lex(String input) {
-        List<String> segments = this.divide(input);
-        var result = (Result<List<Node>, CompileError>) new Ok<List<Node>, CompileError>(new ArrayList<Node>());
-        for (String segment : segments) {
-            String stripped = segment.strip();
-            result = result.and(() -> this.childRule.lex(stripped)).mapValue(tuple -> {
-                tuple.left().add(tuple.right());
-                return tuple.left();
-            });
-        }
-
-        return result.mapValue(children -> new MapNode().withNodeList(this.key(), children));
+        return Iters.fromList(current.advance().stream().toList());
     }
 
     @Override
