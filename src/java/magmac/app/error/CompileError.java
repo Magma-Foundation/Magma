99e56d3220574dd06bfdf92910529c84263e061c
SirMathhman <sirmathhman@gmail.com>
2025-05-26 23:28:29 -0400

    Sorted

--- src/java/magmac/app/error/CompileError.java
+++ src/java/magmac/app/error/CompileError.java
@@ -4,6 +4,7 @@ import magmac.app.Error;
 import magmac.app.compile.Context;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -18,11 +19,21 @@ public record CompileError(String message, Context context, List<CompileError> e
     }
 
     private String format(int depth) {
-        String joined = this.errors.stream()
+        List<CompileError> copy = new ArrayList<>(this.errors);
+        copy.sort(Comparator.comparingInt(CompileError::computeMaxDepth));
+
+        String joined = copy.stream()
                 .map(compileError -> compileError.format(depth + 1))
                 .map(display -> "\n" + "\t".repeat(depth) + display)
                 .collect(Collectors.joining());
 
         return this.message + ": " + this.context.display() + joined;
+    }
+
+    private int computeMaxDepth() {
+        return 1 + this.errors.stream()
+                .mapToInt(CompileError::computeMaxDepth)
+                .max()
+                .orElse(0);
     }
 }
