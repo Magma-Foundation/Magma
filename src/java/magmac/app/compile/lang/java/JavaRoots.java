310eeded21c2e13ebd3dec3629877ecad3267a50
SirMathhman <sirmathhman@gmail.com>
2025-05-26 23:07:01 -0400

    Structures

--- src/java/magmac/app/compile/lang/java/JavaRoots.java
+++ src/java/magmac/app/compile/lang/java/JavaRoots.java
@@ -2,6 +2,7 @@ package magmac.app.compile.lang.java;
 
 import magmac.app.compile.ast.Namespaced;
 import magmac.app.compile.rule.DivideRule;
+import magmac.app.compile.rule.InfixRule;
 import magmac.app.compile.rule.OrRule;
 import magmac.app.compile.rule.Rule;
 import magmac.app.compile.rule.StringRule;
@@ -13,7 +14,14 @@ public final class JavaRoots {
     public static Rule createRule() {
         return new TypeRule("root", new DivideRule("children", new OrRule(List.of(
                 Namespaced.createRule("package", "package "),
-                Namespaced.createRule("import", "import ")
+                Namespaced.createRule("import", "import "),
+                JavaRoots.createStructureRule("record "),
+                JavaRoots.createStructureRule("interface "),
+                JavaRoots.createStructureRule("class ")
         ))));
+    }
+
+    private static InfixRule createStructureRule(String infix) {
+        return new InfixRule(new StringRule("left"), infix, new StringRule("right"));
     }
 }
