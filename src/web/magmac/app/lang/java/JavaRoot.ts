4fdaa113fc59e0f06b4cd808250214bf0ab9d1e1
SirMathhman <sirmathhman@gmail.com>
2025-05-28 16:17:28 -0400

    Hmmm

---
+++ src/web/magmac/app/lang/java/JavaRoot.ts
@@ -0,0 +1,5 @@
+import { List } from "../../../../magmac/api/collect/list/List";
+import { Lists } from "../../../../magmac/api/collect/list/Lists";
+import { JavaRootSegment } from "../../../../magmac/app/lang/java/root/JavaRootSegment";
+export class JavaRoot {private final rootSegments : List<JavaRootSegment>=Lists.empty( );
+}
