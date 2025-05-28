c0c308e6ddb41556cf4a196ff48e3c3227729273
SirMathhman <sirmathhman@gmail.com>
2025-05-28 19:16:04 -0400

    Integrate

--- src/java/magmac/app/stage/lexer/RuleLexer.java
+++ src/java/magmac/app/stage/lexer/RuleLexer.java
@@ -1,8 +1,7 @@
 package magmac.app.stage.lexer;
 
-import magmac.api.iter.collect.ResultCollector;
 import magmac.app.compile.error.CompileResult;
-import magmac.app.compile.error.CompileResults;
+import magmac.app.compile.error.CompileResultCollector;
 import magmac.app.compile.node.Node;
 import magmac.app.compile.rule.Rule;
 import magmac.app.io.sources.UnitSetCollector;
@@ -22,9 +21,8 @@ public class RuleLexer implements Lexer {
 
     @Override
     public CompileResult<UnitSet<Node>> apply(UnitSet<String> initial) {
-        return CompileResults.fromResult(initial.iter()
+        return initial.iter()
                 .map((Unit<String> entry) -> this.foldEntry(entry))
-                .map((CompileResult<Unit<Node>> tuple2CompileResult) -> tuple2CompileResult.result())
-                .collect(new ResultCollector<>(new UnitSetCollector<>())));
+                .collect(new CompileResultCollector<>(new UnitSetCollector<>()));
     }
 }
