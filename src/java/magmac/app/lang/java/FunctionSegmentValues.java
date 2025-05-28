0110320b65114c57ada6872ad3fdbcdad5e57c2c
SirMathhman <sirmathhman@gmail.com>
2025-05-28 17:51:46 -0400

    Function segments

--- src/java/magmac/app/lang/java/FunctionSegmentValues.java
+++ src/java/magmac/app/lang/java/FunctionSegmentValues.java
@@ -4,10 +4,10 @@ import magmac.api.collect.list.Lists;
 import magmac.app.compile.error.CompileResult;
 import magmac.app.compile.node.Node;
 
-public interface FunctionSegment {
-    static CompileResult<FunctionSegment> deserialize(Node node) {
+public class FunctionSegmentValues {
+    public static CompileResult<FunctionSegmentValue> deserialize(Node node) {
         return Deserializers.or(node, Lists.of(
-                StatementNode::deserialize
+
         ));
     }
 }
---
+++ src/java/magmac/app/lang/java/StatementNode.java
@@ -0,0 +1,15 @@
+package magmac.app.lang.java;
+
+import magmac.api.Option;
+import magmac.app.compile.error.CompileResult;
+import magmac.app.compile.node.Node;
+
+public record StatementNode(FunctionSegmentValue value) implements FunctionSegment {
+    public static Option<CompileResult<FunctionSegment>> deserialize(Node node) {
+        return node.deserializeWithType("statement").map(deserializer -> {
+            return deserializer.node("child", FunctionSegmentValues::deserialize)
+                    .complete(StatementNode::new)
+                    .mapValue(value -> value);
+        });
+    }
+}
