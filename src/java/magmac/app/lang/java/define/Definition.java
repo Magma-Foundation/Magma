06321ead23e809705157c3caae1daf986604d942
SirMathhman <sirmathhman@gmail.com>
2025-05-28 21:12:07 -0400

    Type params definitions

--- src/java/magmac/app/lang/java/define/Definition.java
+++ src/java/magmac/app/lang/java/define/Definition.java
@@ -1,19 +1,23 @@
 package magmac.app.lang.java.define;
 
 import magmac.api.Option;
-import magmac.api.Tuple2;
 import magmac.api.collect.list.List;
-import magmac.api.collect.list.Lists;
 import magmac.app.compile.error.CompileResult;
 import magmac.app.compile.node.InitialDeserializer;
 import magmac.app.compile.node.Node;
 import magmac.app.lang.java.Type;
-import magmac.app.lang.java.type.Types;
 import magmac.app.lang.java.assign.Assignable;
 import magmac.app.lang.java.function.Parameter;
+import magmac.app.lang.java.structure.TypeParam;
+import magmac.app.lang.java.type.Types;
 
-public record Definition(String name, Type type, List<Modifier> modifiers,
-                         List<Annotation> annotations) implements Parameter, Assignable {
+public record Definition(
+        String name,
+        Type type,
+        List<Modifier> modifiers,
+        Option<List<Annotation>> annotations,
+        Option<List<TypeParam>> typeParams
+) implements Parameter, Assignable {
     public static CompileResult<Definition> deserializeError(Node node) {
         return Definition.complete(node.deserialize());
     }
@@ -23,11 +27,13 @@ public record Definition(String name, Type type, List<Modifier> modifiers,
                 .withNode("type", Types::deserialize)
                 .withNodeList("modifiers", Modifier::deserialize)
                 .withNodeListOptionally("annotations", Annotation::deserialize)
-                .complete(Definition::from);
-    }
-
-    private static Definition from(Tuple2<Tuple2<Tuple2<String, Type>, List<Modifier>>, Option<List<Annotation>>> result) {
-        return new Definition(result.left().left().left(), result.left().left().right(), result.left().right(), result.right().orElse(Lists.empty()));
+                .withNodeListOptionally("type-parameters", TypeParam::deserialize)
+                .complete((result) -> new Definition(
+                        result.left().left().left().left(),
+                        result.left().left().left().right(),
+                        result.left().left().right(),
+                        result.left().right(),
+                        result.right()));
     }
 
     public static Option<CompileResult<Definition>> deserialize(Node node) {
