81ebdb6bc4db5f76a7435dfc5b5ba384d994e3ef
SirMathhman <sirmathhman@gmail.com>
2025-05-28 20:13:05 -0400

    Ok

--- src/java/magmac/app/lang/java/Whitespace.java
+++ src/java/magmac/app/lang/java/Whitespace.java
@@ -5,10 +5,11 @@ import magmac.app.compile.error.CompileResult;
 import magmac.app.compile.node.Node;
 import magmac.app.lang.java.function.FunctionSegment;
 import magmac.app.lang.java.function.Parameter;
+import magmac.app.lang.java.root.JavaRootSegment;
 import magmac.app.lang.java.structure.ClassMember;
 import magmac.app.lang.java.value.Argument;
 
-public class Whitespace implements Argument, FunctionSegment, Parameter, ClassMember {
+public class Whitespace implements Argument, FunctionSegment, Parameter, ClassMember, JavaRootSegment {
     public static Option<CompileResult<Whitespace>> deserialize(Node node) {
         return node.deserializeWithType("whitespace").map(deserializer -> {
             return deserializer.complete(Whitespace::new);
--- src/java/magmac/app/lang/java/root/JavaRootSegments.java
+++ src/java/magmac/app/lang/java/root/JavaRootSegments.java
@@ -5,11 +5,13 @@ import magmac.app.compile.error.CompileResult;
 import magmac.app.compile.node.MapNode;
 import magmac.app.compile.node.Node;
 import magmac.app.lang.java.Deserializers;
+import magmac.app.lang.java.Whitespace;
 import magmac.app.lang.java.structure.ClassNode;
 
 final class JavaRootSegments {
     public static CompileResult<JavaRootSegment> deserialize(Node node) {
         return Deserializers.orError("root-segment", node, Lists.of(
+                Deserializers.wrap(Whitespace::deserialize),
                 Namespaced::deserialize,
                 ClassNode::deserialize
         ));
