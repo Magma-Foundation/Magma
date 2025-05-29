ac1aec340a247e21e4694adc602f9fcd85ad4a11
SirMathhman <sirmathhman@gmail.com>
2025-05-28 20:52:23 -0400

    Permit methods with no content

--- src/java/magmac/app/lang/java/function/MethodNode.java
+++ src/java/magmac/app/lang/java/function/MethodNode.java
@@ -8,11 +8,11 @@ import magmac.app.compile.node.Node;
 import magmac.app.lang.java.define.Definition;
 import magmac.app.lang.java.structure.StructureMember;
 
-public record MethodNode(Definition definition, List<FunctionSegment> children, List<Parameter> right) implements StructureMember {
+public record MethodNode(Definition definition, Option<List<FunctionSegment>> maybeChildren, List<Parameter> right) implements StructureMember {
     public static Option<CompileResult<StructureMember>> deserialize(Node node) {
         return node.deserializeWithType("method").map((InitialDeserializer deserializer) -> deserializer
                 .withNode("header", Definition::deserializeError)
-                .withNodeList("children", FunctionSegment::deserialize)
+                .withNodeListOptionally("children", FunctionSegment::deserialize)
                 .withNodeList("parameters", Parameters::deserialize)
                 .complete((tuple) -> new MethodNode(tuple.left().left(), tuple.left().right(), tuple.right()))
                 .mapValue((MethodNode type) -> type));
