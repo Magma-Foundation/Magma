b8562f61438ab49ac33031b47ed2f2fc2009125a
SirMathhman <sirmathhman@gmail.com>
2025-05-29 10:04:05 -0400

    OK

--- src/java/magmac/app/lang/node/StructureNode.java
+++ src/java/magmac/app/lang/node/StructureNode.java
@@ -4,6 +4,7 @@ import magmac.api.Option;
 import magmac.api.Tuple2;
 import magmac.api.collect.list.List;
 import magmac.app.compile.error.CompileResult;
+import magmac.app.compile.node.CompoundDeserializer;
 import magmac.app.compile.node.InitialDeserializer;
 import magmac.app.compile.node.Node;
 import magmac.app.lang.Deserializers;
@@ -25,15 +26,21 @@ public record StructureNode(
     }
 
     private static CompileResult<JavaRootSegment> deserializeHelper(StructureType type, InitialDeserializer deserializer) {
+        return StructureNode.attachOptionals(StructureNode.attachRequired(deserializer)).complete(tuple -> StructureNode.from(type, tuple));
+    }
+
+    private static CompoundDeserializer<Tuple2<Tuple2<String, List<Modifier>>, List<StructureMember>>> attachRequired(InitialDeserializer deserializer) {
         return deserializer.withString("name")
                 .withNodeList("modifiers", Modifier::deserialize)
-                .withNodeList("children", StructureMembers::deserialize)
-                .withNodeListOptionally("implemented", Types::deserialize)
+                .withNodeList("children", StructureMembers::deserialize);
+    }
+
+    private static CompoundDeserializer<Tuple2<Tuple2<Tuple2<Tuple2<Tuple2<Tuple2<Tuple2<String, List<Modifier>>, List<StructureMember>>, Option<List<Type>>>, Option<List<TypeParam>>>, Option<List<Parameter>>>, Option<List<Type>>>, Option<List<Type>>>> attachOptionals(CompoundDeserializer<Tuple2<Tuple2<String, List<Modifier>>, List<StructureMember>>> attachRequired) {
+        return attachRequired.withNodeListOptionally("implemented", Types::deserialize)
                 .withNodeListOptionally("type-parameters", TypeParam::deserialize)
                 .withNodeListOptionally("parameters", Parameters::deserialize)
                 .withNodeListOptionally("extended", Types::deserialize)
-                .withNodeListOptionally("variants", Types::deserialize)
-                .complete(tuple -> StructureNode.from(type, tuple));
+                .withNodeListOptionally("variants", Types::deserialize);
     }
 
     private static StructureNode from(
