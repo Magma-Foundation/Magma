0164af00a47483e603e30118a68d89d5c1f93d74
SirMathhman <sirmathhman@gmail.com>
2025-05-28 21:39:56 -0400

    Extended

--- src/java/magmac/app/lang/java/structure/StructureNode.java
+++ src/java/magmac/app/lang/java/structure/StructureNode.java
@@ -20,7 +20,8 @@ public record StructureNode(
         List<StructureMember> members,
         Option<List<Type>> implemented,
         Option<List<TypeParam>> typeParams,
-        Option<List<Parameter>> parameters
+        Option<List<Parameter>> parameters,
+        Option<List<Type>> extended
 ) implements JavaRootSegment {
     public static Option<CompileResult<JavaRootSegment>> deserialize(StructureType type, Node node) {
         return node.deserializeWithType(type.name().toLowerCase()).map((InitialDeserializer deserializer) -> deserializer
@@ -30,14 +31,16 @@ public record StructureNode(
                 .withNodeListOptionally("implemented", Types::deserialize)
                 .withNodeListOptionally("type-parameters", TypeParam::deserialize)
                 .withNodeListOptionally("parameters", Parameters::deserialize)
+                .withNodeListOptionally("extended", Types::deserialize)
                 .complete(tuple -> StructureNode.from(type, tuple)));
     }
 
     private static StructureNode from(
             StructureType type,
-            Tuple2<Tuple2<Tuple2<Tuple2<Tuple2<String, List<Modifier>>, List<StructureMember>>, Option<List<Type>>>, Option<List<TypeParam>>>, Option<List<Parameter>>> tuple) {
+            Tuple2<Tuple2<Tuple2<Tuple2<Tuple2<Tuple2<String, List<Modifier>>, List<StructureMember>>, Option<List<Type>>>, Option<List<TypeParam>>>, Option<List<Parameter>>>, Option<List<Type>>> tuple) {
         return new StructureNode(type,
-                tuple.left().left().left().left().left(),
+                tuple.left().left().left().left().left().left(),
+                tuple.left().left().left().left().left().right(),
                 tuple.left().left().left().left().right(),
                 tuple.left().left().left().right(),
                 tuple.left().left().right(),
