a3fd156ff5c154e091a356fe4e62535bb58d9ef3
SirMathhman <sirmathhman@gmail.com>
2025-05-28 23:31:55 -0400

    OK

--- src/java/magmac/app/lang/node/FunctionSegment.java
+++ src/java/magmac/app/lang/node/FunctionSegment.java
@@ -3,9 +3,13 @@ package magmac.app.lang.node;
 import magmac.api.collect.list.Lists;
 import magmac.app.compile.error.CompileResult;
 import magmac.app.compile.node.Node;
+import magmac.app.compile.rule.ExactRule;
 import magmac.app.compile.rule.OrRule;
+import magmac.app.compile.rule.PrefixRule;
 import magmac.app.compile.rule.Rule;
+import magmac.app.compile.rule.StringRule;
 import magmac.app.compile.rule.StripRule;
+import magmac.app.compile.rule.TypeRule;
 import magmac.app.lang.Deserializers;
 import magmac.app.lang.LazyRule;
 
@@ -22,12 +26,18 @@ public interface FunctionSegment {
         Rule functionSegmentValueRule = FunctionSegmentValues.createFunctionSegmentValueRule(value, definition);
 
         Rule rule = new OrRule(Lists.of(
+                new StripRule(new ExactRule(";")),
                 Whitespace.createWhitespaceRule(),
                 FunctionStatement.createStatementRule(functionSegmentValueRule),
                 Block.createBlockRule(functionSegmentRule, value, definition),
-                Return.createReturnRule(value)
+                Return.createReturnRule(value),
+                FunctionSegment.createCaseRule()
         ));
 
         return functionSegmentRule.set(new StripRule("before", rule, ""));
+    }
+
+    private static TypeRule createCaseRule() {
+        return new TypeRule("case", new StripRule(new PrefixRule("case", new StringRule("after-keyword"))));
     }
 }
