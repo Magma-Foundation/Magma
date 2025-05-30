b9c5688fecd89b287bc0007b72441509885c1c05
SirMathhman <sirmathhman@gmail.com>
2025-05-29 23:20:45 -0400

    Debug info OK

--- src/java/magmac/app/stage/generate/RuleGenerator.java
+++ src/java/magmac/app/stage/generate/RuleGenerator.java
@@ -1,9 +1,13 @@
 package magmac.app.stage.generate;
 
+import magmac.api.collect.list.Lists;
 import magmac.app.compile.error.CompileResult;
 import magmac.app.compile.error.CompileResultCollector;
+import magmac.app.compile.error.context.NodeContext;
+import magmac.app.compile.error.error.CompileError;
 import magmac.app.compile.node.Node;
 import magmac.app.compile.rule.Rule;
+import magmac.app.error.ImmutableCompileError;
 import magmac.app.io.sources.UnitSetCollector;
 import magmac.app.stage.unit.Unit;
 import magmac.app.stage.unit.UnitSet;
@@ -23,6 +27,14 @@ public class RuleGenerator implements Generator {
     }
 
     private CompileResult<Unit<String>> generateEntry(Unit<Node> entry) {
-        return entry.mapValue(this.rootRule::generate);
+        return entry.mapValue(this.rootRule::generate)
+                .mapErr(err -> this.getDestruct(entry, err));
+    }
+
+    private CompileError getDestruct(Unit<Node> entry, CompileError err) {
+        return entry.destruct((location, node) -> {
+            String message = "Failed to generate entry '" + location + "'";
+            return new ImmutableCompileError(message, new NodeContext(node), Lists.of(err));
+        });
     }
 }
