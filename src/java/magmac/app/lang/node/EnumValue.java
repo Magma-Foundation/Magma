d4c0cba6a3f1326ed2b45f13addc398c5c6f4ea7
SirMathhman <sirmathhman@gmail.com>
2025-06-01 09:06:23 -0400

    OK

--- src/java/magmac/app/lang/node/EnumValue.java
+++ src/java/magmac/app/lang/node/EnumValue.java
@@ -1,13 +1,33 @@
 package magmac.app.lang.node;
 
+import magmac.api.Option;
+import magmac.api.collect.list.List;
+import magmac.api.collect.list.Lists;
 import magmac.app.compile.error.CompileResult;
 import magmac.app.compile.node.Node;
+import magmac.app.compile.rule.FilterRule;
+import magmac.app.compile.rule.LocatingRule;
+import magmac.app.compile.rule.OrRule;
+import magmac.app.compile.rule.Rule;
+import magmac.app.compile.rule.StringRule;
+import magmac.app.compile.rule.StripRule;
+import magmac.app.compile.rule.SuffixRule;
 import magmac.app.lang.Destructors;
 
-final record EnumValue(String value) {
+record EnumValue(String name, Option<List<Value>> argumentsList) {
     public static CompileResult<EnumValue> deserialize(Node node) {
         return Destructors.destruct(node)
-                .withString("value")
-                .complete(EnumValue::new);
+                .withString("name")
+                .withNodeListOptionally("arguments", Values::deserializeOrError)
+                .complete(tuple -> new EnumValue(tuple.left(), tuple.right()));
+    }
+
+    static Rule createEnumValueRule(Rule value) {
+        Rule name = new StripRule(FilterRule.Symbol(new StringRule("name")));
+        Rule rule = new SuffixRule(LocatingRule.First(name, "(", Arguments.createArgumentsRule(value)), ")");
+        return new StripRule(new OrRule(Lists.of(
+                Symbols.createSymbolRule("name"),
+                rule
+        )));
     }
 }
--- src/java/magmac/app/lang/node/EnumValues.java
+++ src/java/magmac/app/lang/node/EnumValues.java
@@ -5,12 +5,9 @@ import magmac.api.collect.list.List;
 import magmac.api.collect.list.Lists;
 import magmac.app.compile.error.CompileResult;
 import magmac.app.compile.node.Node;
-import magmac.app.compile.rule.FilterRule;
-import magmac.app.compile.rule.LocatingRule;
 import magmac.app.compile.rule.NodeListRule;
 import magmac.app.compile.rule.OrRule;
 import magmac.app.compile.rule.Rule;
-import magmac.app.compile.rule.StringRule;
 import magmac.app.compile.rule.StripRule;
 import magmac.app.compile.rule.SuffixRule;
 import magmac.app.compile.rule.TypeRule;
@@ -19,13 +16,7 @@ import magmac.app.lang.ValueFolder;
 
 public record EnumValues(List<EnumValue> values) implements JavaStructureMember {
     public static TypeRule createEnumValuesRule(Rule value) {
-        Rule name = new StripRule(FilterRule.Symbol(new StringRule("name")));
-        Rule rule = new SuffixRule(LocatingRule.First(name, "(", Arguments.createArgumentsRule(value)), ")");
-        Rule enumValue = new StripRule(new OrRule(Lists.of(
-                Symbols.createSymbolRule("value"),
-                rule
-        )));
-
+        Rule enumValue = EnumValue.createEnumValueRule(value);
         Rule enumValues = NodeListRule.createNodeListRule("children", new ValueFolder(), enumValue);
         Rule withEnd = new StripRule(new SuffixRule(enumValues, ";"));
         return new TypeRule("enum-values", new OrRule(Lists.of(
@@ -37,6 +28,6 @@ public record EnumValues(List<EnumValue> values) implements JavaStructureMember 
     public static Option<CompileResult<JavaStructureMember>> deserialize(Node node) {
         return Destructors.destructWithType("enum-values", node)
                 .map(deserializer -> deserializer.withNodeList("children", EnumValue::deserialize)
-                .complete(EnumValues::new));
+                        .complete(EnumValues::new));
     }
 }
