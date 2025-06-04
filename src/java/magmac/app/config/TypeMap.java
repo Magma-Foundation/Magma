beee8e95ba9bfebc43cd1e878046ed210d04a2e3
Gavin Bromberg <10675613+SirMathhman@users.noreply.github.com>
2025-06-03 22:32:19 -0400

    Propagate type resolution errors

--- src/java/magmac/app/config/TypeMap.java
+++ src/java/magmac/app/config/TypeMap.java
@@ -1,7 +1,17 @@
 package magmac.app.config;
 
+import magmac.api.Option;
+import magmac.api.Some;
 import magmac.api.Tuple2;
 import magmac.api.collect.list.List;
+import magmac.app.io.Location;
 
 public record TypeMap(List<Tuple2<List<String>, String>> types) {
+    public Option<Location> find(String name) {
+        return this.types
+                .iter()
+                .filter(tuple -> tuple.right().equals(name))
+                .next()
+                .map(tuple -> new Location(tuple.left(), tuple.right()));
+    }
 }
